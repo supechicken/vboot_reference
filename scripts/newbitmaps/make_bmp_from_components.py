ca2a2b4655449d5014526b10d653ca1a00a670cd
Bill Richardson <wfrichar@chromium.org>
2011-05-01 19:02:16 -0700

    Change GBB bmpblock to version 1.1, supporting direct HWID rendering.
    With version 1.0, the BIOS displays its screens using composited images, but
    we still have to create a new bmp image for every HWID. Version 1.1 lets us
    render the ASCII HWID string directly, so the BIOS screens don't need
    modification just because the HWID changes.
    
    In the yaml file, we just replace the hwid image with a magic string, like
    so:
    
      bmpblock: 1.1
    
      [...]
    
      screens:
        en_remove:
          - [  0,   0, remove_bg]
          - [256, 534, en_model_text]
          - [314, 534, $HWID]
          - [192, 479, url]
          - [195, 453, en_remove_text]
    
    This change modifies the bmpblk_utility to accept and generate both 1.0 and
    1.1 versions. It also updates the supporting scripts (most of which aren't
    needed anymore) and adds a new DEFAULT.yaml file which can be used as the
    basis for all locales.
    
    Change-Id: I015cd0a96fb8562557b5e43f24b374281df361a9

--- scripts/newbitmaps/make_bmp_from_components.py
+++ scripts/newbitmaps/make_bmp_from_components.py
@@ -13,6 +13,16 @@ import re
 import tempfile
 
 
+if len(sys.argv) < 2 or sys.argv[1] != "--force":
+  print "This script is deprecated."
+  print "The latest BIOSes can render HWIDs directly from ASCII."
+  print "Use --force to proceed anyway."
+  sys.exit(1)
+
+# do it anyway with --force
+if len(sys.argv) > 1 and sys.argv[1] == "--force":
+  sys.argv = sys.argv[:1] + sys.argv[2:]
+
 if len(sys.argv) < 2:
   print "usage: %s autotest/../hardware_Components/" % sys.argv[0]
   sys.exit(1)
