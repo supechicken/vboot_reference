5fd5f28837e4d036a7f8e9b4699785f0919e0ed3
Bill Richardson <wfrichar@chromium.org>
2012-03-19 13:25:54 -0700

    Update preamble headers to v3, but keep v2 support.
    BUG=chromium-os:20124
    TEST=manual
    
    make
    make runtests

--- utility/vbutil_firmware.c
+++ utility/vbutil_firmware.c
@@ -10,6 +10,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "cryptolib.h"
@@ -17,6 +18,8 @@
 #include "kernel_blob.h"
 #include "vboot_common.h"
 
+/* global options */
+static int opt_preamble_fmt = 2;        /* 0 = create v3, verify either */
 
 /* Command line options */
 enum {
@@ -29,6 +32,8 @@ enum {
   OPT_FV,
   OPT_KERNELKEY,
   OPT_FLAGS,
+  OPT_NAME,
+  OPT_FORMAT,
 };
 
 static struct option long_opts[] = {
@@ -41,6 +46,8 @@ static struct option long_opts[] = {
   {"fv", 1, 0,                        OPT_FV                      },
   {"kernelkey", 1, 0,                 OPT_KERNELKEY               },
   {"flags", 1, 0,                     OPT_FLAGS                   },
+  {"name", 1, 0,                      OPT_NAME                    },
+  {"format", 1, 0,                    OPT_FORMAT                  },
   {NULL, 0, 0, 0}
 };
 
@@ -60,6 +67,8 @@ static int PrintHelp(void) {
        "  --kernelkey <file>          Kernel subkey in .vbpubk format\n"
        "optional OPTIONS are:\n"
        "  --flags <number>            Preamble flags (defaults to 0)\n"
+       "  --name <string>             Human-readable description\n"
+       "  --format <number>           Use 3 for new platforms, 2 for existing\n"
        "\n"
        "For '--verify <file>', required OPTIONS are:\n"
        "  --signpubkey <file>         Signing public key in .vbpubk format\n"
@@ -72,16 +81,34 @@ static int PrintHelp(void) {
 }
 
 
+
+/* Returns the flags from a firmware preamble, or a default value for
+ * older preamble versions which didn't contain flags.  Use this
+ * function to ensure compatibility with older preamble versions
+ * (2.0).  Assumes the preamble has already been verified via
+ * VerifyFirmwarePreamble(). */
+uint32_t VbGetFirmwarePreambleFlags(const VbFirmwarePreambleUnion* preamble) {
+  if (IS_V3(preamble))
+      return preamble->v3.flags;
+
+  /* Old 2.0 structure; return default flags. */
+  if (preamble->m.header_version_minor < 1)
+    return 0;
+
+  return preamble->v2.flags;
+}
+
+
 /* Create a firmware .vblock */
 static int Vblock(const char* outfile, const char* keyblock_file,
                   const char* signprivate, uint64_t version,
                   const char* fv_file, const char* kernelkey_file,
-                  uint32_t preamble_flags) {
+                  uint32_t preamble_flags, const char *name) {
 
   VbPrivateKey* signing_key;
   VbPublicKey* kernel_subkey;
-  VbSignature* body_sig;
-  VbFirmwarePreambleHeader* preamble;
+  VbSignature* body_digest;
+  VbFirmwarePreambleUnion* preamble;
   VbKeyBlockHeader* key_block;
   uint64_t key_block_size;
   uint8_t* fv_data;
@@ -90,35 +117,33 @@ static int Vblock(const char* outfile, const char* keyblock_file,
   uint64_t i;
 
   if (!outfile) {
-    VbExError("Must specify output filename\n");
-    return 1;
+    Fatal("Must specify output filename\n");
   }
   if (!keyblock_file || !signprivate || !kernelkey_file) {
-    VbExError("Must specify all keys\n");
-    return 1;
+    Fatal("Must specify all keys\n");
   }
   if (!fv_file) {
-    VbExError("Must specify firmware volume\n");
-    return 1;
+    Fatal("Must specify firmware volume\n");
+  }
+
+  if (name && strlen(name)+1 > sizeof(preamble->v3.name)) {
+    Fatal("Name string is too long\n");
   }
 
   /* Read the key block and keys */
   key_block = (VbKeyBlockHeader*)ReadFile(keyblock_file, &key_block_size);
   if (!key_block) {
-    VbExError("Error reading key block.\n");
-    return 1;
+    Fatal("Error reading key block.\n");
   }
 
   signing_key = PrivateKeyRead(signprivate);
   if (!signing_key) {
-    VbExError("Error reading signing key.\n");
-    return 1;
+    Fatal("Error reading signing key.\n");
   }
 
   kernel_subkey = PublicKeyRead(kernelkey_file);
   if (!kernel_subkey) {
-    VbExError("Error reading kernel subkey.\n");
-    return 1;
+    Fatal("Error reading kernel subkey.\n");
   }
 
   /* Read and sign the firmware volume */
@@ -126,40 +151,41 @@ static int Vblock(const char* outfile, const char* keyblock_file,
   if (!fv_data)
     return 1;
   if (!fv_size) {
-    VbExError("Empty firmware volume file\n");
-    return 1;
+    Fatal("Empty firmware volume file\n");
   }
-  body_sig = CalculateSignature(fv_data, fv_size, signing_key);
-  if (!body_sig) {
-    VbExError("Error calculating body signature\n");
-    return 1;
-  }
-  free(fv_data);
 
   /* Create preamble */
-  preamble = CreateFirmwarePreamble(version,
-                                    kernel_subkey,
-                                    body_sig,
-                                    signing_key,
-                                    preamble_flags);
-  if (!preamble) {
-    VbExError("Error creating preamble.\n");
-    return 1;
+  if (2 == opt_preamble_fmt) {
+    body_digest = CalculateSignature(fv_data, fv_size, signing_key);
+    if (!body_digest) {
+      Fatal("Error calculating body signature\n");
+    }
+    preamble = (VbFirmwarePreambleUnion*)CreateFirmwarePreamble2_1(
+      version, kernel_subkey, body_digest, signing_key, preamble_flags);
+  } else {
+    body_digest = CalculateHash(fv_data, fv_size, signing_key);
+    if (!body_digest) {
+      Fatal("Error calculating body digest\n");
+    }
+    preamble = (VbFirmwarePreambleUnion*)CreateFirmwarePreamble(
+      version, kernel_subkey, body_digest, signing_key, preamble_flags, name);
   }
+  if (!preamble) {
+    Fatal("Error creating preamble.\n");
+  }
+  free(fv_data);
 
   /* Write the output file */
   f = fopen(outfile, "wb");
   if (!f) {
-    VbExError("Can't open output file %s\n", outfile);
-    return 1;
+    Fatal("Can't open output file %s\n", outfile);
   }
   i = ((1 != fwrite(key_block, key_block_size, 1, f)) ||
-       (1 != fwrite(preamble, preamble->preamble_size, 1, f)));
+       (1 != fwrite(preamble, preamble->m.preamble_size, 1, f)));
   fclose(f);
   if (i) {
-    VbExError("Can't write output file %s\n", outfile);
+    Fatal("Can't write output file %s\n", outfile);
     unlink(outfile);
-    return 1;
   }
 
   /* Success */
@@ -170,7 +196,7 @@ static int Verify(const char* infile, const char* signpubkey,
                   const char* fv_file, const char* kernelkey_file) {
 
   VbKeyBlockHeader* key_block;
-  VbFirmwarePreambleHeader* preamble;
+  VbFirmwarePreambleUnion* preamble;
   VbPublicKey* data_key;
   VbPublicKey* sign_key;
   VbPublicKey* kernel_subkey;
@@ -183,36 +209,31 @@ static int Verify(const char* infile, const char* signpubkey,
   uint32_t flags;
 
   if (!infile || !signpubkey || !fv_file) {
-    VbExError("Must specify filename, signpubkey, and fv\n");
-    return 1;
+    Fatal("Must specify filename, signpubkey, and fv\n");
   }
 
   /* Read public signing key */
   sign_key = PublicKeyRead(signpubkey);
   if (!sign_key) {
-    VbExError("Error reading signpubkey.\n");
-    return 1;
+    Fatal("Error reading signpubkey.\n");
   }
 
   /* Read blob */
   blob = ReadFile(infile, &blob_size);
   if (!blob) {
-    VbExError("Error reading input file\n");
-    return 1;
+    Fatal("Error reading input file\n");
   }
 
   /* Read firmware volume */
   fv_data = ReadFile(fv_file, &fv_size);
   if (!fv_data) {
-    VbExError("Error reading firmware volume\n");
-    return 1;
+    Fatal("Error reading firmware volume\n");
   }
 
   /* Verify key block */
   key_block = (VbKeyBlockHeader*)blob;
   if (0 != KeyBlockVerify(key_block, blob_size, sign_key, 0)) {
-    VbExError("Error verifying key block.\n");
-    return 1;
+    Fatal("Error verifying key block.\n");
   }
   free(sign_key);
   now += key_block->key_block_size;
@@ -232,25 +253,39 @@ static int Verify(const char* infile, const char* signpubkey,
 
   rsa = PublicKeyToRSA(&key_block->data_key);
   if (!rsa) {
-    VbExError("Error parsing data key.\n");
-    return 1;
+    Fatal("Error parsing data key.\n");
   }
 
   /* Verify preamble */
-  preamble = (VbFirmwarePreambleHeader*)(blob + now);
-  if (0 != VerifyFirmwarePreamble(preamble, blob_size - now, rsa)) {
-    VbExError("Error verifying preamble.\n");
-    return 1;
+  preamble = (VbFirmwarePreambleUnion*)(blob + now);
+  if (IS_V3(preamble)) {
+    if (2 == opt_preamble_fmt) {
+      Fatal("Preamble is v3, accepting v2 only.\n");
+    }
+    if (0 != VerifyFirmwarePreamble(&preamble->v3, blob_size - now, rsa)) {
+      Fatal("Error verifying v3 preamble.\n");
+    }
+  } else {
+    if (3 == opt_preamble_fmt) {
+      Fatal("Preamble is v2, accepting v3 only.\n");
+    }
+    if (0 != VerifyFirmwarePreamble2_x(&preamble->v2, blob_size - now, rsa)) {
+      Fatal("Error verifying v2 preamble.\n");
+    }
   }
-  now += preamble->preamble_size;
 
-  flags = VbGetFirmwarePreambleFlags(preamble);
+  now += preamble->m.preamble_size;
+
   printf("Preamble:\n");
-  printf("  Size:                  %" PRIu64 "\n", preamble->preamble_size);
+  printf("  Size:                  %" PRIu64 "\n", preamble->m.preamble_size);
   printf("  Header version:        %" PRIu32 ".%" PRIu32"\n",
-         preamble->header_version_major, preamble->header_version_minor);
-  printf("  Firmware version:      %" PRIu64 "\n", preamble->firmware_version);
-  kernel_subkey = &preamble->kernel_subkey;
+         preamble->m.header_version_major, preamble->m.header_version_minor);
+  printf("  Firmware version:      %" PRIu64 "\n",
+         FP_MEMBER(preamble, firmware_version));
+  if (IS_V3(preamble))
+    kernel_subkey = &preamble->v3.kernel_subkey;
+  else
+    kernel_subkey = &preamble->v2.kernel_subkey;
   printf("  Kernel key algorithm:  %" PRIu64 " %s\n",
          kernel_subkey->algorithm,
          (kernel_subkey->algorithm < kNumAlgorithms ?
@@ -261,8 +296,11 @@ static int Verify(const char* infile, const char* signpubkey,
   PrintPubKeySha1Sum(kernel_subkey);
   printf("\n");
   printf("  Firmware body size:    %" PRIu64 "\n",
-         preamble->body_signature.data_size);
+         FP_MEMBER_V3_V2(preamble, body_digest, body_signature).data_size);
+  flags = VbGetFirmwarePreambleFlags(preamble);
   printf("  Preamble flags:        %" PRIu32 "\n", flags);
+  if (IS_V3(preamble))
+    printf("  Name:                  %s\n", preamble->v3.name);
 
   /* TODO: verify body size same as signature size */
 
@@ -270,9 +308,15 @@ static int Verify(const char* infile, const char* signpubkey,
   if (flags & VB_FIRMWARE_PREAMBLE_USE_RO_NORMAL) {
     printf("Preamble requests USE_RO_NORMAL; skipping body verification.\n");
   } else {
-    if (0 != VerifyData(fv_data, fv_size, &preamble->body_signature, rsa)) {
-      VbExError("Error verifying firmware body.\n");
-      return 1;
+    if (IS_V3(preamble)) {
+      if (0 != EqualData(fv_data, fv_size, &preamble->v3.body_digest, rsa)) {
+        Fatal("Error verifying (v3) firmware body.\n");
+      }
+    } else {
+      if (0 != VerifyData(fv_data, fv_size, &preamble->v2.body_signature,
+                          rsa)) {
+        Fatal("Error verifying (v2) firmware body.\n");
+      }
     }
     printf("Body verification succeeded.\n");
   }
@@ -281,7 +325,6 @@ static int Verify(const char* infile, const char* signpubkey,
     if (0 != PublicKeyWrite(kernelkey_file, kernel_subkey)) {
       fprintf(stderr,
               "vbutil_firmware: unable to write kernel subkey\n");
-      return 1;
     }
   }
 
@@ -298,6 +341,7 @@ int main(int argc, char* argv[]) {
   uint64_t version = 0;
   char* fv_file = NULL;
   char* kernelkey_file = NULL;
+  char* name = NULL;
   uint32_t preamble_flags = 0;
   int mode = 0;
   int parse_error = 0;
@@ -353,6 +397,19 @@ int main(int argc, char* argv[]) {
           parse_error = 1;
         }
         break;
+
+      case OPT_NAME:
+        name = optarg;
+        break;
+
+      case OPT_FORMAT:
+        opt_preamble_fmt = strtoul(optarg, &e, 0);
+        if (!*optarg || (e && *e) ||
+            (opt_preamble_fmt != 2 && opt_preamble_fmt != 3)) {
+          printf("Invalid --format\n");
+          parse_error = 1;
+        }
+        break;
     }
   }
 
@@ -362,7 +419,7 @@ int main(int argc, char* argv[]) {
   switch(mode) {
     case OPT_MODE_VBLOCK:
       return Vblock(filename, key_block_file, signprivate, version, fv_file,
-                    kernelkey_file, preamble_flags);
+                    kernelkey_file, preamble_flags, name);
     case OPT_MODE_VERIFY:
       return Verify(filename, signpubkey, fv_file, kernelkey_file);
     default:
