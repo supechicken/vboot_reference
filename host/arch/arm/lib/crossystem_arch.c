6aac5d3a555c9ebcc2e1e5309da4465dd27e6fd6
Randall Spangler <rspangler@chromium.org>
2012-08-03 12:48:24 -0700

    Crossystem should return at-boot switch positions from VbSharedData
    This is more reliable than reading them through FDT/ACPI, since it reflects
    the positions as shown to verified boot code.
    
    Notes:
    1. This affects ALL platforms with virtual dev switches (x86 AND arm)
    2. The fix should have no effect on older platforms, but I haven't tested those.
    
    BUG=chrome-os-partner:11805
    TEST=manual
    
    1. boot in normal mode.
    
    devsw_boot             = 0                              # Developer switch position at boot
    recovery_reason        = 0                              # Recovery mode reason for current boot
    recoverysw_boot        = 0                              # Recovery switch position at boot
    wpsw_boot              = 1                              # Firmware write protect hardware switch position at boot
    
    2. boot in developer mode.
    
    localhost ~ # crossystem
    devsw_boot             = 1                              # Developer switch position at boot
    recovery_reason        = 0                              # Recovery mode reason for current boot
    recoverysw_boot        = 0                              # Recovery switch position at boot
    wpsw_boot              = 1                              # Firmware write protect hardware switch position at boot
    
    3. boot in developer-recovery mode using keyboard combo.
    
    devsw_boot             = 1                              # Developer switch position at boot
    recovery_reason        = 2                              # Recovery mode reason for current boot
    recoverysw_boot        = 1                              # Recovery switch position at boot
    wpsw_boot              = 1                              # Firmware write protect hardware switch position at boot
    
    4. disable WP and reboot.  wpsw_boot should be 0.
    
    Change-Id: If4156b5e14c6923c5b331c7e5feaabbffe1dad37

--- host/arch/arm/lib/crossystem_arch.c
+++ host/arch/arm/lib/crossystem_arch.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+/* Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -368,19 +368,14 @@ VbSharedDataHeader *VbSharedDataRead(void) {
 int VbGetArchPropertyInt(const char* name) {
   if (!strcasecmp(name, "fmap_base"))
     return ReadFdtInt("fmap-offset");
-  else if (!strcasecmp(name, "devsw_boot"))
-    return ReadFdtBool("boot-developer-switch");
-  else if (!strcasecmp(name, "recoverysw_boot"))
-    return ReadFdtBool("boot-recovery-switch");
-  else if (!strcasecmp(name, "wpsw_boot"))
-    return ReadFdtBool("boot-write-protect-switch");
   else if (!strcasecmp(name, "devsw_cur"))
     return VbGetVarGpio("developer-switch");
   else if (!strcasecmp(name, "recoverysw_cur"))
     return VbGetVarGpio("recovery-switch");
   else if (!strcasecmp(name, "wpsw_cur"))
-  return VbGetVarGpio("write-protect-switch");
+    return VbGetVarGpio("write-protect-switch");
   else if (!strcasecmp(name, "recoverysw_ec_boot"))
+    /* TODO: read correct value using ectool */
     return 0;
   else
     return -1;
