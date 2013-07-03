7812a217b8894c869a46c288d169c2300aececb5
Don Garrett <dgarrett@google.com>
2013-07-02 17:17:51 -0700

    Add a script to remove /boot and it's contents from an image.
    This is a helper script intended for the signing servers to use to strip
    out the /boot directory from images just before signing.
    
    BUG=chromium:210352
    TEST=Manually used to strip and image and validated the results.
    BRANCH=None
    
    Change-Id: I814522284143d8f90651e13000d428718aeca1e4

---
+++ scripts/image_signing/strip_image.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Script to manipulate the tag files in the output of build_image
+
+# Load common constants.  This should be the first executable line.
+# The path to common.sh should be relative to your script's location.
+. "$(dirname "$0")/common.sh"
+
+load_shflags
+
+DEFINE_string image "chromiumos_image.bin" \
+  "Input file name of Chrome OS image to tag/stamp."
+
+# Parse command line.
+FLAGS "$@" || exit 1
+eval set -- "${FLAGS_ARGV}"
+
+# Abort on error.
+set -e
+
+if [ -z "${FLAGS_image}" ] || [ ! -s "${FLAGS_image}" ] ; then
+  echo "Error: need a valid file by --image"
+  exit 1
+fi
+
+# Swiped/modifed from $SRC/src/scripts/base_library/base_image_util.sh.
+zero_free_space() {
+  local rootfs="$1"
+
+  echo "Zeroing freespace in ${rootfs}"
+  # dd is a silly thing and will produce a "No space left on device" message
+  # that cannot be turned off and is confusing to unsuspecting victims.
+  info "${rootfs}/filler"
+  ( sudo dd if=/dev/zero of="${rootfs}/filler" bs=4096 conv=fdatasync \
+      status=noxfer || true ) 2>&1 | grep -v "No space left on device"
+  sudo rm "${rootfs}/filler"
+}
+
+
+strip_boot() {
+  local image=$1
+
+  # Grab firmware image from the autoupdate bundle (shellball).
+  local rootfs_dir=$(make_temp_dir)
+  mount_image_partition ${image} 3 ${rootfs_dir}
+
+  # Force unmount of the rootfs on function exit as it is needed later.
+  trap "sudo umount ${rootfs_dir}" RETURN
+
+  sudo rm -rf "${rootfs_dir}/boot" &&
+    echo "/boot directory was removed."
+
+  # To prevent the files we just removed from the FS from remaining as non-
+  # zero trash blocks that bloat payload sizes, need to zero them. This was
+  # done when the image was built, but needs to be repeated now that we've
+  # modified it in a non-trivial way.
+  zero_free_space "${rootfs_dir}"
+}
+
+
+IMAGE=$(readlink -f "${FLAGS_image}")
+if [[ -z "${IMAGE}" || ! -f "${IMAGE}" ]]; then
+  echo "Missing required argument: --from (image to update)"
+  usage
+  exit 1
+fi
+
+strip_boot "${IMAGE}"
