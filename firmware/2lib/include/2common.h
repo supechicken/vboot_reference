fc85e959c2872874aa6bdc134274bc74492b3a16
Randall Spangler <rspangler@chromium.org>
2014-05-13 09:24:52 -0700

    vboot2: Add crypto functions
    This is the first of several CLs adding a more memory- and
    code-efficient firmware verification library.  This CL adds the crypto
    library (modified from firmware/lib/cryptolib) and unit tests for it.
    
    BUG=chromium:370082
    BRANCH=none
    TEST=make clean && COV=1 make
    
    Change-Id: I4240eab227bb197cacc6c8e7a6397127d74414a2
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

---
+++ firmware/2lib/include/2common.h
@@ -0,0 +1,42 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Common functions between firmware and kernel verified boot.
+ */
+
+#ifndef VBOOT_REFERENCE_VBOOT_2COMMON_H_
+#define VBOOT_REFERENCE_VBOOT_2COMMON_H_
+
+#include "2return_codes.h"
+
+struct vb2_public_key;
+
+/* Return the greater of A and B */
+#ifndef VB2_MAX
+#define VB2_MAX(A, B) ((A) > (B) ? (A) : (B))
+#endif
+
+/* Debug output */
+#if defined(VBOOT_DEBUG) && !defined(VB2_DEBUG)
+#include <stdio.h>
+#define VB2_DEBUG(format, args...) printf(format, ## args)
+#else
+#define VB2_DEBUG(format, args...)
+#endif
+
+/**
+ * Align a buffer and check its size.
+ *
+ * @param **ptr		Pointer to pointer to align
+ * @param *size		Points to size of buffer pointed to by *ptr
+ * @param align		Required alignment (must be power of 2)
+ * @param want_size	Required size
+ * @return VB2_SUCCESS, or non-zero if error.
+ */
+int vb2_align(uint8_t **ptr,
+	      uint32_t *size,
+	      uint32_t align,
+	      uint32_t want_size);
+
+#endif  /* VBOOT_REFERENCE_VBOOT_2COMMON_H_ */
