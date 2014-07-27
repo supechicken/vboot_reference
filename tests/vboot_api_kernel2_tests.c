6b7f3803a2c2d5cae66b74c0b30a84aeafc80bf6
Sheng-Liang Song <ssl@chromium.org>
2014-07-26 21:34:28 -0700

    vboot: Notify EC of current boot mode
    Notify EC of boot mode, i.e. normal, developer or recovery.
    This is necessary for battery firmware updates.
    
    BUG=chrome-os-partner:24741
    BRANCH=ToT
    TEST=Verified on samus & glimmer.
    
    Passed runalltests.
    ~/trunk/src/platform/vboot_reference $ make runalltests -j
    
    Change-Id: I1613ede34b4a929d1e8114fb519861f349377e94
    Signed-off-by: Sheng-Liang Song <ssl@chromium.org>

--- tests/vboot_api_kernel2_tests.c
+++ tests/vboot_api_kernel2_tests.c
@@ -262,7 +262,9 @@ static void VbUserConfirmsTest(void)
 static void VbBootTest(void)
 {
 	ResetMocks();
+	VbExEcEnteringMode(0, VB_EC_NORMAL);
 	TEST_EQ(VbBootNormal(&cparams, &lkp), 1002, "VbBootNormal()");
+	TEST_EQ(VbGetMode(), VB_EC_NORMAL, "vboot_mode normal");
 }
 
 static void VbBootDevTest(void)
@@ -273,7 +275,9 @@ static void VbBootDevTest(void)
 
 	/* Proceed after timeout */
 	ResetMocks();
+	VbExEcEnteringMode(0, VB_EC_DEVELOPER);
 	TEST_EQ(VbBootDeveloper(&cparams, &lkp), 1002, "Timeout");
+	TEST_EQ(VbGetMode(), VB_EC_DEVELOPER, "vboot_mode developer");
 	TEST_EQ(screens_displayed[0], VB_SCREEN_DEVELOPER_WARNING,
 		"  warning screen");
 	VbNvGet(VbApiKernelGetVnc(), VBNV_RECOVERY_REQUEST, &u);
@@ -448,8 +452,11 @@ static void VbBootRecTest(void)
 	/* Shutdown requested in loop */
 	ResetMocks();
 	shutdown_request_calls_left = 10;
+	VbExEcEnteringMode(0, VB_EC_RECOVERY);
 	TEST_EQ(VbBootRecovery(&cparams, &lkp), VBERROR_SHUTDOWN_REQUESTED,
 		"Shutdown requested");
+	TEST_EQ(VbGetMode(), VB_EC_RECOVERY, "vboot_mode recovery");
+
 	VbNvGet(VbApiKernelGetVnc(), VBNV_RECOVERY_REQUEST, &u);
 	TEST_EQ(u, 0, "  recovery reason");
 	TEST_EQ(screens_displayed[0], VB_SCREEN_BLANK,
