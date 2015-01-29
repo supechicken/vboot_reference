00c43e647f7e488a08a680023dc67992ea024ed7
Daisuke Nojiri <dnojiri@chromium.org>
2015-01-29 14:37:25 -0800

    add vb2api_get_pcr_digest
    this api allows firmware to get the digest of boot mode state.
    
    BUG=chromium:451609
    TEST=make fwlib2
    BRANCH=tot
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: Idca7bc5f6aed947689ad7cf219805aad35047c7d

---
+++ firmware/2lib/include/2tpm_bootmode.h
@@ -0,0 +1,16 @@
+/* Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Functions for updating the TPM state with the status of boot path.
+ */
+
+#ifndef VBOOT_REFERENCE_2TPM_BOOTMODE_H_
+#define VBOOT_REFERENCE_2TPM_BOOTMODE_H_
+
+#include "2api.h"
+
+const uint8_t *vb2_get_boot_state_digest(struct vb2_context *ctx,
+					 uint32_t *digest_size);
+
+#endif  /* VBOOT_REFERENCE_2TPM_BOOTMODE_H_ */
