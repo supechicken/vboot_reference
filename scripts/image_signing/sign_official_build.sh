71bf21e6dc3cd224a2bbe7d6b26801d6cb4f6326
Vincent Palatin <vpalatin@chromium.org>
2015-11-17 11:18:19 -0800

    sign_official_build: add a fallback key for standalone accessories
    The standalone accessories are using a key name like this:
    key_<product>.pem or key_<product>.vbprik2
    when it doesn't exist, fallback using key.pem or key.vbprik2.
    
    BRANCH=none
    BUG=chrome-os-partner:47557
    TEST=manual:
    ./scripts/image_signing/sign_official_build.sh accessory_usbpd ../ec/build/zinger/ec.bin tests/devkeys-acc /tmp/ec-zinger.TEST.SIGNED.bin
    ./scripts/image_signing/sign_official_build.sh accessory_rwsig ../ec/private/build/hadoken/keyboard_app.bin tests/devkeys-acc /tmp/ec-hadoken.TEST.SIGNED.bin
    ./scripts/image_signing/sign_official_build.sh accessory_rwsig ../ec/private/build/hadoken/keyboard_app.bin /tmp /tmp/ec-hadoken.TEST.SIGNED.bin
    
    Change-Id: I68863664bdb9da1695e91b1986f3a0148af7da26

--- scripts/image_signing/sign_official_build.sh
+++ scripts/image_signing/sign_official_build.sh
@@ -883,10 +883,12 @@ elif [[ "${TYPE}" == "update_payload" ]]; then
   sign_update_payload ${INPUT_IMAGE} ${KEY_DIR} ${OUTPUT_IMAGE}
 elif [[ "${TYPE}" == "accessory_usbpd" ]]; then
   KEY_NAME="${KEY_DIR}/key_$(basename $(dirname ${INPUT_IMAGE}))"
+  [ -e "${KEY_NAME}.pem" ] || KEY_NAME="key"
   cp "${INPUT_IMAGE}" "${OUTPUT_IMAGE}"
   futility sign --type usbpd1 --pem "${KEY_NAME}.pem" "${OUTPUT_IMAGE}"
 elif [[ "${TYPE}" == "accessory_rwsig" ]]; then
   KEY_NAME="${KEY_DIR}/key_$(basename $(dirname ${INPUT_IMAGE}))"
+  [ -e "${KEY_NAME}.vbprik2" ] || KEY_NAME="key"
   cp "${INPUT_IMAGE}" "${OUTPUT_IMAGE}"
   futility sign --type rwsig --prikey "${KEY_NAME}.vbprik2" "${OUTPUT_IMAGE}"
 else
