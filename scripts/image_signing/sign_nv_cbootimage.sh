ec9303e9614b594d56deac3f6d151ed58f786a06
Furquan Shaikh <furquan@google.com>
2016-01-13 14:49:03 -0800

    sign_nv_cbootimage: Update signing scripts for tegra bootloader and lp0
    CQ-DEPEND=CL:*244234
    BUG=chrome-os-partner:43018
    BRANCH=none
    TEST=Verified that signed bootloader and lp0 work fine on dev-PKC
    fused devices.
    
    Reference:
    https://code.google.com/p/chrome-os-partner/issues/attachmentText?id=43018&aid=430180067000&name=sign-lp0.sh&token=ABZ6GAcjz4b2mEk55WqREzwTnWWpEyE_7A%3A1452631308268
    https://code.google.com/p/chrome-os-partner/issues/attachmentText?id=43018&aid=430180060000&name=sign.sh&token=ABZ6GAcRky67XrzMOgKZfkJxr70qm-tTAg%3A1452631308240
    
    Change-Id: Icb024a1d9a61714df6e52d94c96cf43481cac869
    Signed-off-by: Furquan Shaikh <furquan@google.com>

--- scripts/image_signing/sign_nv_cbootimage.sh
+++ scripts/image_signing/sign_nv_cbootimage.sh
@@ -41,34 +41,99 @@ main() {
 
   if [[ "${type}" == "bootloader" ]]; then
 
+    local config_file=$(make_temp_file)
+
     pushd "${work_dir}" >/dev/null
 
-    cat >update.cfg <<EOF
-PkcKey = ${pkc_key}, --save;
-ReSignBl;
-EOF
+    # Get bootloader length
+    BL_LENGTH=`bct_dump "${firmware_image}" | grep "Bootloader\[0\].Length"\
+               | awk -F ' ' '{print $4}' | awk -F ';' '{print $1}'`
 
-    # This also generates a file pubkey.sha which contains the hash of public
-    # key required by factory to burn into PKC fuses. Move pubkey.sha into
-    # ${firmware_image}.pubkey.sha.
-    cbootimage -s "${soc}" -u update.cfg "${firmware_image}" \
-      "${signed_fw}"
+    # Extract bootloader to sign
+    dd bs=1 skip=32768 if="${firmware_image}" of="${signed_fw}.bl.tosig" \
+      count="${BL_LENGTH}" >/dev/null 2>&1
+
+    # Calculate rsa signature for bootloader
+    openssl dgst -sha256 -sigopt rsa_padding_mode:pss -sigopt \
+      rsa_pss_saltlen:-1 -sign "${pkc_key}" -out "${signed_fw}.bl.sig" \
+      "${signed_fw}.bl.tosig"
+
+    # Update bootloader's rsa signature, aes hash and bct's aes hash
+    echo "RsaPssSigBlFile = ${signed_fw}.bl.sig;" > "${config_file}"
+    echo "RehashBl;" >> "${config_file}"
+    cbootimage -s "${soc}" -u "${config_file}" "${firmware_image}" \
+      "${signed_fw}.tmp" >/dev/null
+
+    # Extract the part of bct which needs to be rsa signed
+    dd bs=1 if="${signed_fw}.tmp" of="${signed_fw}.bct.tosig" count=8944 \
+      skip=1296 >/dev/null 2>&1
+
+    # Calculate rsa signature for bct
+    openssl dgst -sha256 -sigopt rsa_padding_mode:pss -sigopt \
+      rsa_pss_saltlen:-1 -sign "${pkc_key}" -out "${signed_fw}.bct.sig" \
+      "${signed_fw}.bct.tosig"
+
+    # Create public key modulus from key file
+    openssl rsa -in "${pkc_key}" -noout -modulus -out "${signed_fw}.key.mod"
+    # Remove prefix
+    cut -d= -f2 < "${signed_fw}.key.mod" > "${signed_fw}.key.mod.tmp"
+    # Convert from hexdecimal to binary
+    xxd -r -p -l 256 "${signed_fw}.key.mod.tmp" "${signed_fw}.key.mod.bin"
+
+    # Update bct's rsa signature and modulus
+    echo "RsaPssSigBctFile = ${signed_fw}.bct.sig;" > "${config_file}"
+    echo "RsaKeyModulusFile = ${signed_fw}.key.mod.bin;" >> "${config_file}"
+    cbootimage -s "${soc}" -u "${config_file}" "${signed_fw}.tmp" \
+      "${signed_fw}" >/dev/null
+
+    # Calculate hash of public key modulus
+    objcopy -I binary --reverse-bytes=256 "${signed_fw}.key.mod.bin" \
+      "${signed_fw}.key.mod.bin.rev"
+    openssl dgst -sha256 -binary -out "${signed_fw}.key.sha" \
+      "${signed_fw}.key.mod.bin.rev"
 
     popd >/dev/null
+
     # Copy signed firmware image and public key hash to current directory.
-    mv "${work_dir}/pubkey.sha" "${firmware_image}.pubkey.sha"
     mv "${signed_fw}" "${firmware_image}"
+    mv "${signed_fw}.key.sha" "${firmware_image}.pubkey.sha"
 
   elif [[ "${type}" == "lp0_firmware" ]]; then
 
     pushd "${work_dir}" >/dev/null
 
-    cat >update.cfg <<EOF
-PkcKey = ${pkc_key};
-RsaSign = 0x220,, 288, 16, Complete;
-EOF
+    cp "${firmware_image}" "${signed_fw}"
 
-    cbootimage --sign update.cfg "${firmware_image}" "${signed_fw}"
+    # Extract the part of the binary which needs to be signed
+    dd bs=1 skip=544 if="${firmware_image}" of="${signed_fw}.tosig" >/dev/null \
+      2>&1
+
+    # Calculate rsa-pss signature
+    openssl dgst -sha256 -sigopt rsa_padding_mode:pss \
+      -sigopt rsa_pss_saltlen:-1 -sign "${pkc_key}" -out "${signed_fw}.rsa.sig" \
+      "${signed_fw}.tosig"
+
+    # Reverse rsa signature to meet tegra soc ordering requirement
+    objcopy -I binary --reverse-bytes=256 "${signed_fw}.rsa.sig" \
+      "${signed_fw}.rsa.sig.rev"
+
+    # Inject rsa-pss signature into the binary image's header
+    dd conv=notrunc bs=1 seek=288 count=256 if="${signed_fw}.rsa.sig.rev" \
+      of="${signed_fw}" >/dev/null 2>&1
+
+    # Generate public key modulus from key file
+    openssl rsa -in "${pkc_key}" -noout -modulus -out "${signed_fw}.key.mod"
+    # Remove prefix
+    cut -d= -f2 < "${signed_fw}.key.mod" > "${signed_fw}.key.mod.tmp"
+    # Convert from hexdecimal to binary
+    xxd -r -p -l 256 "${signed_fw}.key.mod.tmp" "${signed_fw}.key.mod.bin"
+    # Reverse byte order
+    objcopy -I binary --reverse-bytes=256 "${signed_fw}.key.mod.bin" \
+      "${signed_fw}.key.mod.bin.rev"
+
+    # Inject public key modulus into the binary image's header
+    dd conv=notrunc bs=1 seek=16 count=256 if="${signed_fw}.key.mod.bin.rev" \
+      of="${signed_fw}" >/dev/null 2>&1
 
     popd >/dev/null
     mv "${signed_fw}" "${firmware_image}"
