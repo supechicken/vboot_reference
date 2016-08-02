6baf31228f231d1d934f3c5f7978fb4da9476e99
Victor Hsieh <victorhsieh@chromium.org>
2016-08-02 16:47:01 -0700

    Add script to sign Android image
    sign_android_image.sh is the main script that signs the image. It makes
    similar changes to an image like the Android official signing tool
    (sign_target_files_apks.py) does, but more Chrome OS specific.
    
    TEST=./sign_official_build.sh recovery recovery_image.bin \
             ../../tests/devkeys/ out_img
    TEST=same above but with a recovery image without Android image
    BUG=b:29915721
    
    Change-Id: I0ae5f0ad8d2b05e485d60262558517ea563bf527

---
+++ scripts/keygeneration/create_new_android_keys.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Copyright (c) 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+
+usage() {
+  cat <<EOF
+Usage: $0 DIR
+
+Generate Android's 4 framework key pairs at DIR.
+
+EOF
+
+  if [[ $# -ne 0 ]]; then
+    echo "ERROR: $*" >&2
+    exit 1
+  else
+    exit 0
+  fi
+}
+
+SUBJECT='/C=US/ST=California/L=Mountain View/O=Google Inc./OU=Android/CN=Android'
+
+# Generate .pk8 and .x509.pem at the given directory.
+make_pair() {
+  local dir=$1
+  local name=$2
+
+  openssl genrsa -3 -out "${dir}/temp.pem" 2048
+  openssl req -new -x509 -key "${dir}/temp.pem" -out "${dir}/${name}.x509.pem" \
+    -days 10000 -subj "${SUBJECT}"
+  openssl pkcs8 -in "${dir}/temp.pem" -topk8 -outform DER \
+    -out "${dir}/${name}.pk8" -nocrypt
+
+  shred --remove "${dir}/temp.pem"
+}
+
+main() {
+  if [[ $# -ne 1 ]]; then
+    usage "Invalid argument."
+  fi
+
+  local dir=$1
+
+  make_pair "${dir}" platform
+  make_pair "${dir}" shared
+  make_pair "${dir}" media
+  make_pair "${dir}" releasekey
+}
+
+main "$@"
