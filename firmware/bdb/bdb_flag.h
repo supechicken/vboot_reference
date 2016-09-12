813451ea9922a4883c250aca5690ec43af4cd7f5
Daisuke Nojiri <dnojiri@chromium.org>
2016-09-12 12:47:14 -0700

    bdb: Add secret deriving code for SP-RO
    This patch adds code which dervies secrets from BDS. It's supposed to be
    done by SP-RO, hence the code is mostly useful for testing (or emulation).
    
    vba_extend_secrets_ro takes a function pointer to a hash extend
    function. It'll be used to try different sha256 extend algorithms.
    
    BUG=chromium:649555
    BRANCH=none
    TEST=make runtests
    
    Change-Id: I8fef6b851fb84686d8bcdd948b36160016687c51
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- firmware/bdb/bdb_flag.h
+++ firmware/bdb/bdb_flag.h
@@ -7,6 +7,7 @@
 #define VBOOT_REFERENCE_FIRMWARE_BDB_BDB_FLAG_H
 
 /* Indicate whether kernel data key is verified */
-#define VBA_CONTEXT_FLAG_KERNEL_DATA_KEY_VERIFIED	(1 << 0)
+#define VBA_CONTEXT_FLAG_BDB_KEY_EFUSED			(1 << 0)
+#define VBA_CONTEXT_FLAG_KERNEL_DATA_KEY_VERIFIED	(1 << 1)
 
 #endif
