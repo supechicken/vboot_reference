e47e7c580f268c3733f9b02164517e82b6f9a2a2
Randall Spangler <rspangler@chromium.org>
2016-10-18 12:00:07 -0700

    vboot: Remove vboot1 cryptolib padding source
    The old vboot1 cryptolib hard-coded many of its padding arrays in a
    padding.c file.  Use the equivalent vboot2 apis instead.
    
    This change is almost exclusively on the host and test side; the only
    firmware impact is on a single line of debug output.
    
    BUG=chromium:611535
    BRANCH=none
    TEST=make runtests; emerge-kevin coreboot depthcharge
    
    Change-Id: If689ffd92f0255847bea2424950da4547b2c0df3
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

--- utility/pad_digest_utility.c
+++ utility/pad_digest_utility.c
@@ -16,43 +16,51 @@
 #include "2sha.h"
 #include "file_keys.h"
 #include "host_common.h"
-#include "padding.h"
+#include "host_signature2.h"
 #include "signature_digest.h"
 
 
-int main(int argc, char* argv[]) {
-  int algorithm = -1;
-  int error_code = 0;
-  uint8_t* digest = NULL;
-  uint8_t* padded_digest = NULL;
-  uint32_t len;
-  uint32_t padded_digest_len;
+int main(int argc, char* argv[])
+{
+	int algorithm = -1;
+	int error_code = 0;
+	uint8_t* digest = NULL;
+	uint8_t* padded_digest = NULL;
+	uint32_t len;
 
-  if (argc != 3) {
-    fprintf(stderr, "Usage: %s <alg_id> <digest_file>", argv[0]);
-    return -1;
-  }
-  algorithm = atoi(argv[1]);
-  if (algorithm < 0 || algorithm >= kNumAlgorithms) {
-    fprintf(stderr, "Invalid Algorithm!\n");
-    return -1;
-  }
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <alg_id> <digest_file>", argv[0]);
+		return -1;
+	}
+	algorithm = atoi(argv[1]);
+	if (algorithm < 0 || algorithm >= VB2_ALG_COUNT) {
+		fprintf(stderr, "Invalid Algorithm!\n");
+		return -1;
+	}
 
-  if (VB2_SUCCESS != vb2_read_file(argv[2], &digest, &len)) {
-    fprintf(stderr, "Could not read file: %s\n", argv[2]);
-    return -1;
-  }
+	enum vb2_hash_algorithm hash_alg = vb2_crypto_to_hash(algorithm);
+	uint32_t digest_size = vb2_digest_size(hash_alg);
+	uint32_t digestinfo_size = 0;
+	const uint8_t* digestinfo = NULL;
+	if (VB2_SUCCESS != vb2_digest_info(hash_alg, &digestinfo,
+					   &digestinfo_size)) {
+		VBDEBUG(("SignatureBuf(): Couldn't get digest info\n"));
+		return -1;
+	}
+	uint32_t padded_digest_len = digest_size + digestinfo_size;
 
-  padded_digest = PrependDigestInfo(algorithm, digest);
-  const int digest_size = vb2_digest_size(vb2_crypto_to_hash(algorithm));
-  padded_digest_len = (digest_size + digestinfo_size_map[algorithm]);
+	if (VB2_SUCCESS != vb2_read_file(argv[2], &digest, &len)) {
+		fprintf(stderr, "Could not read file: %s\n", argv[2]);
+		return -1;
+	}
 
-  if (!padded_digest)
-    error_code = -1;
-  if(padded_digest &&
-     1 != fwrite(padded_digest, padded_digest_len, 1, stdout))
-    error_code = -1;
-  free(padded_digest);
-  free(digest);
-  return error_code;
+	padded_digest = PrependDigestInfo(hash_alg, digest);
+	if (!padded_digest)
+		error_code = -1;
+	if(padded_digest &&
+	   1 != fwrite(padded_digest, padded_digest_len, 1, stdout))
+		error_code = -1;
+	free(padded_digest);
+	free(digest);
+	return error_code;
 }
