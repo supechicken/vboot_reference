2f743d8444b07f7d9064c67e87add0b79fd9149e
Mike Frysinger <vapier@chromium.org>
2017-01-11 21:12:16 -0500

    image_signing: support signing of OCI containers
    BUG=chromium:660209
    TEST=`./sign_official_build.sh oci-container fastboot/ ../tests/devkeys` works
    TEST=signing an image inserted the container pubkey
    BRANCH=None
    
    Change-Id: I75793b03e93f2c18b1495a3ec729ad04d2e17401

---
+++ scripts/image_signing/insert_container_publickey.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Load common constants and variables.
+. "$(dirname "$0")/common.sh"
+
+main() {
+  set -e
+
+  local image="$1"
+  local pub_key="$2"
+  if [ $# -ne 2 ]; then
+    cat <<EOF
+Usage: $PROG <image.bin> <public_key.pem>
+Installs the container verification public key <public_key.pem> to <image.bin>.
+EOF
+    exit 1
+  fi
+  local rootfs=$(make_temp_dir)
+  local key_location="/usr/share/misc/"
+  mount_image_partition "${image}" 3 "${rootfs}"
+  sudo install \
+    -D -o root -g root -m 644 \
+    "${pub_key}" "${rootfs}/${key_location}/oci-container-key-pub.pem"
+  info "Container verification key was installed." \
+       "Do not forget to resign the image!"
+}
+
+main "$@"
