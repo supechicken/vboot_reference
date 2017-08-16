414663a71817bbd771d8da9f9ab31d000d49e010
Daisuke Nojiri <dnojiri@chromium.org>
2017-08-15 18:05:22 -0700

    Support EC early firmware selection
    Chromebox ECs perform EFS: verifying firmware before the AP boots.
    This patch adds support for EC EFS.
    
    EC EFS uses two slots: one is active slot and one is used for update.
    AP is agnostic about which slot is active and which slot is for
    update. Instead, AP recognizes one slot as 'active' and the other
    as 'update' (or non active) slot.
    
    After update is successful, AP issues a switch slot command to
    activate the updated slot. Then, it code-reboots the system to run
    the new RW image.
    
    BUG=b:37316498
    BRANCH=none
    CQ-DEPEND=CL:616248,CL:616347
    TEST=buildall. On Fizz, verify:
    1. RW_B is old and updated by soft sync. RW_B is activated and
    executed after reboot. System continues to boot to OS.
    2. RW_A is old and updated by soft sync. RW_A is activated and
    executed after reboot. System continues to boot to OS.
    
    Change-Id: I6ca7686eb28b0b548785cf2c02eca1b67018f469
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- firmware/include/vboot_api.h
+++ firmware/include/vboot_api.h
@@ -308,7 +308,12 @@ enum VbSelectFirmware_t {
 	VB_SELECT_FIRMWARE_B = 2,
 	/* Read only firmware for normal or developer path. */
 	VB_SELECT_FIRMWARE_READONLY = 3,
-        VB_SELECT_FIRMWARE_COUNT,
+	/* Rewritable EC firmware currently set active */
+	VB_SELECT_FIRMWARE_EC_ACTIVE = 4,
+	/* Rewritable EC firmware currently not set active thus updatable */
+	VB_SELECT_FIRMWARE_EC_UPDATE = 5,
+	/* Keep this at the end */
+	VB_SELECT_FIRMWARE_COUNT,
 };
 
 /* Data only used by VbSelectFirmware() */
@@ -961,6 +966,12 @@ VbError_t VbExEcDisableJump(int devidx);
 
 /**
  * Read the SHA-256 hash of the selected EC image.
+ *
+ * @param devidx    Device index. 0: EC, 1: PD.
+ * @param select    Image to get hash of. RO or RW.
+ * @param hash      Pointer to the hash.
+ * @param hash_size Pointer to the hash size.
+ * @return          VBERROR_... error, VBERROR_SUCCESS on success.
  */
 VbError_t VbExEcHashImage(int devidx, enum VbSelectFirmware_t select,
 			  const uint8_t **hash, int *hash_size);
@@ -1016,6 +1027,14 @@ VbError_t VbExEcVbootDone(int in_recovery);
  * Request EC to stop discharging and cut-off battery.
  */
 VbError_t VbExEcBatteryCutOff(void);
+
+/**
+ * Switch active slot
+ *
+ * @param devidx Device index. 0: EC, 1: PD.
+ * @return       VBERROR_... error, VBERROR_SUCCESS on success.
+ */
+VbError_t VbExEcSwitchSlot(int devidx);
 
 /*
  * severity levels for an auxiliary firmware update request
