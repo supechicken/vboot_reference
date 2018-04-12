ee5d0b60eba34ac4efe50537a5921f0cecd626d1
Ting Shen <phoenixshen@google.com>
2018-04-12 15:37:53 +0800

    WIP: vboot_api: Add alt-os mode moot path
    BUG=b:70804764
    TEST=none
    BRANCH=firmware-eve-campfire-9584.131.B
    CQ-DEPEND=CL:972762
    
    Change-Id: I33c257f0975cde9adff157f2a64b184fefa818fc
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- firmware/lib/include/rollback_index.h
+++ firmware/lib/include/rollback_index.h
@@ -29,6 +29,10 @@
 #define ROLLBACK_SPACE_KERNEL_VERSION 2
 #define ROLLBACK_SPACE_KERNEL_UID 0x4752574C  /* 'GRWL' */
 
+#define ALT_OS_ENABLE  (1 << 0)
+#define ALT_OS_LAST_BOOT (1 << 1)
+#define ALT_OS_BOOT_SEEN (1 << 2)
+
 typedef struct RollbackSpaceKernel {
 	/* Struct version, for backwards compatibility */
 	uint8_t struct_version;
@@ -36,8 +40,10 @@ typedef struct RollbackSpaceKernel {
 	uint32_t uid;
 	/* Kernel versions */
 	uint32_t kernel_versions;
+	/* Flags for alt-os mode */
+	uint8_t alt_os_flags;
 	/* Reserved for future expansion */
-	uint8_t reserved[3];
+	uint8_t reserved[2];
 	/* Checksum (v2 and later only) */
 	uint8_t crc8;
 } __attribute__((packed)) RollbackSpaceKernel;
@@ -159,5 +165,12 @@ uint32_t SafeWrite(uint32_t index, const void *data, uint32_t length);
  * Utility function to turn the virtual dev-mode flag on or off. 0=off, 1=on.
  */
 uint32_t SetVirtualDevMode(int val);
+
+/**
+ * Utility function to turn the alt-os mode flag on or off. 0=off, 1=on.
+ */
+uint32_t SetAltOSMode(int val);
+
+uint32_t GetAltOSModeFlags(uint8_t* flags);
 
 #endif  /* VBOOT_REFERENCE_ROLLBACK_INDEX_H_ */
