3dee2a33f4bb2904b66b2bfa1f00e930db04a33a
Joel Kitching <kitching@google.com>
2018-05-02 14:03:46 +0800

    firmware: add context and flags for Alt OS
    BUG=None
    TEST=None
    
    Change-Id: If139e6ccc74864c607d7d6b5156418b23336a66c

--- firmware/include/vboot_struct.h
+++ firmware/include/vboot_struct.h
@@ -241,6 +241,8 @@ typedef struct VbKernelPreambleHeader {
 #define VBSD_OPROM_LOADED                0x00020000
 /* Don't try for boot failures */
 #define VBSD_NOFAIL_BOOT                 0x00040000
+/* Trigger AltOS for this boot */
+#define VBSD_TRIGGER_ALT_OS              0x00080000
 
 /*
  * Supported flags by header version.  It's ok to add new flags while keeping
