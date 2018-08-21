629aa867cebc837d027f388840f94710ea1e44c2
Hung-Te Lin <hungte@chromium.org>
2018-08-21 22:03:44 +0800

    futility: cmd_update: Preserve image sections before update
    When updating RO (or going to compare with contents), we need to
    preserve (copy) section data from system active firmware image.
    
    The `preserve_images` will try to preserve a list of known sections in
    full update (`--wp=0`) mode, so we VPD data and HWID won't be lost.
    
    BUG=chromium:875551
    TEST=make futil; tests/futility/run_test_scripts.sh
    BRANCH=None
    
    Change-Id: I85c4ba972853dbc0fc101bee269c0effe70988b1
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>

--- tests/futility/test_update.sh
+++ tests/futility/test_update.sh
@@ -14,31 +14,24 @@ PEPPY_BIOS="${SCRIPTDIR}/data/bios_peppy_mp.bin"
 LINK_BIOS="${SCRIPTDIR}/data/bios_link_mp.bin"
 LINK_VERSION="Google_Link.2695.1.133"
 PEPPY_VERSION="Google_Peppy.4389.89.0"
+LINK_HWID="X86 LINK TEST 6638"
+PEPPY_HWID="X86 PEPPY TEST 4211"
 
 # Work in scratch directory
 cd "$OUTDIR"
 set -o pipefail
 
-# Prepare temporary files.
-cp -f "${LINK_BIOS}" "${TMP}.emu"
-
-# Test full update and image loading.
-# Test command execution.
-output="$(${FUTILITY} update -i "${PEPPY_BIOS}" --wp 0 --emulate "${TMP}.emu")"
-target="$(echo "${output}" | sed -n 's/^>> Target.*(//p' | sed 's/).*//')"
-current="$(echo "${output}" | sed -n 's/^>> Current.*(//p' | sed 's/).*//')"
-expected1="RO:${PEPPY_VERSION}, RW/A:${PEPPY_VERSION}, RW/B:${PEPPY_VERSION}"
-expected2="RO:${LINK_VERSION}, RW/A:${LINK_VERSION}, RW/B:${LINK_VERSION}"
-test "${target}" = "${expected1}"
-test "${current}" = "${expected2}"
-cmp "${TMP}.emu" "${PEPPY_BIOS}"
-
 # $TMP.emu is PEPPY now. Prepare new testing targets.
 mkdir -p "${TMP}.unpack"
 (cd "${TMP}.unpack"; "${FUTILITY}" dump_fmap -x "${LINK_BIOS}")
-cp -f "${TMP}.emu" "${TMP}.expected.rw"
-cp -f "${TMP}.emu" "${TMP}.expected.a"
-cp -f "${TMP}.emu" "${TMP}.expected.b"
+cp -f "${PEPPY_BIOS}" "${TMP}.expected.full"
+cp -f "${PEPPY_BIOS}" "${TMP}.expected.rw"
+cp -f "${PEPPY_BIOS}" "${TMP}.expected.a"
+cp -f "${PEPPY_BIOS}" "${TMP}.expected.b"
+"${FUTILITY}" gbb -s --hwid="${LINK_HWID}" "${TMP}.expected.full"
+"${FUTILITY}" load_fmap "${TMP}.expected.full" \
+	RW_VPD:${TMP}.unpack/RW_VPD \
+	RO_VPD:${TMP}.unpack/RO_VPD
 "${FUTILITY}" load_fmap "${TMP}.expected.rw" \
 	RW_SECTION_A:${TMP}.unpack/RW_SECTION_A \
 	RW_SECTION_B:${TMP}.unpack/RW_SECTION_B \
@@ -49,7 +42,19 @@ cp -f "${TMP}.emu" "${TMP}.expected.b"
 "${FUTILITY}" load_fmap "${TMP}.expected.b" \
 	RW_SECTION_B:${TMP}.unpack/RW_SECTION_B
 
+# Test full update and image loading.
+cp -f "${LINK_BIOS}" "${TMP}.emu"
+output="$(${FUTILITY} update -i "${PEPPY_BIOS}" --wp 0 --emulate "${TMP}.emu")"
+target="$(echo "${output}" | sed -n 's/^>> Target.*(//p' | sed 's/).*//')"
+current="$(echo "${output}" | sed -n 's/^>> Current.*(//p' | sed 's/).*//')"
+expected1="RO:${PEPPY_VERSION}, RW/A:${PEPPY_VERSION}, RW/B:${PEPPY_VERSION}"
+expected2="RO:${LINK_VERSION}, RW/A:${LINK_VERSION}, RW/B:${LINK_VERSION}"
+test "${target}" = "${expected1}"
+test "${current}" = "${expected2}"
+cmp "${TMP}.emu" "${TMP}.expected.full"
+
 # Test RW-only update.
+cp -f "${PEPPY_BIOS}" "${TMP}.emu"
 "${FUTILITY}" update -i "${LINK_BIOS}" --emulate "${TMP}.emu" --wp 1
 cmp "${TMP}.emu" "${TMP}.expected.rw"
 
