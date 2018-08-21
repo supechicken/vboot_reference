3b711fb4398861f4e71399be729d6e1d2d9a746d
Hung-Te Lin <hungte@chromium.org>
2018-08-21 17:49:06 +0800

    futility: cmd_update: Support loading image files by -i, -e, --pd_image
    To specify images, we want to read them from files specified from
    command line:
    
     -i: AP (host) firmware image.
     -e: EC firmware image.
     --pd_image: PD firmware image (deprecated).
    
    BUG=chromium:875551
    TEST=make futil; ./build/futility/futility -i /build/eve/firmware/image.bin
    BRANCH=None
    
    Change-Id: I3c2dbe3d3ce4619aa7e044a154be3aba7ab9181c
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>

--- futility/cmd_update.c
+++ futility/cmd_update.c
@@ -6,17 +6,61 @@
  * A reference implementation for AP (and supporting images) firmware updater.
  */
 
+#include <assert.h>
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "futility.h"
+#include "host_misc.h"
 #include "utility.h"
 
 typedef const char * const CONST_STRING;
 
-struct updater_config {
+/* flashrom programmers. */
+static CONST_STRING PROG_HOST = "host",
+		    PROG_EC = "ec",
+		    PROG_PD = "ec:dev=1";
+
+struct firmware_image {
+	const char *programmer;
+	uint32_t size;
+	uint8_t *data;
+	char *file_name;
+	char *ro_version, *rw_version_a, *rw_version_b;
 };
+
+struct updater_config {
+	struct firmware_image image, old_image;
+	struct firmware_image ec_image, pd_image;
+};
+
+static int load_image(const char *file_name, struct firmware_image *image)
+{
+	Debug("%s: Load image file from %s...\n", __FUNCTION__, file_name);
+
+	if (vb2_read_file(file_name, &image->data, &image->size) != VB2_SUCCESS)
+	{
+		Error("%s: Failed to load %s\n", __FUNCTION__, file_name);
+		return -1;
+	}
+
+	Debug("%s: Image size: %d\n", __FUNCTION__, image->size);
+	assert(image->data);
+	image->file_name = strdup(file_name);
+
+	return 0;
+}
+
+static void free_image(struct firmware_image *image)
+{
+	free(image->data);
+	free(image->file_name);
+	free(image->ro_version);
+	free(image->rw_version_a);
+	free(image->rw_version_b);
+	memset(image, 0, sizeof(*image));
+}
 
 enum updater_error_codes {
 	UPDATE_ERR_NONE,
@@ -33,20 +77,34 @@ static int update_firmware(struct updater_config *cfg)
 	return UPDATE_ERR_NONE;
 }
 
+static void unload_updater_config(struct updater_config *cfg)
+{
+	free_image(&cfg->image);
+	free_image(&cfg->old_image);
+	free_image(&cfg->ec_image);
+	free_image(&cfg->pd_image);
+}
+
 /* Command line options */
 static struct option const long_opts[] = {
 	/* name  has_arg *flag val */
+	{"image", 1, NULL, 'i'},
+	{"ec_image", 1, NULL, 'e'},
+	{"pd_image", 1, NULL, 'P'},
 	{"help", 0, NULL, 'h'},
 	{NULL, 0, NULL, 0},
 };
 
-static CONST_STRING short_opts = "h";
+static CONST_STRING short_opts = "hi:e:";
 
 static void print_help(int argc, char *argv[])
 {
 	printf("\n"
 		"Usage:  " MYNAME " %s [OPTIONS]\n"
 		"\n"
+		"-i, --image=FILE    \tAP (host) firmware image (image.bin)\n"
+		"-e, --ec_image=FILE \tEC firmware image (i.e, ec.bin)\n"
+		"    --pd_image=FILE \tPD firmware image (i.e, pd.bin)\n"
 		"",
 		argv[0]);
 }
@@ -54,13 +112,28 @@ static void print_help(int argc, char *argv[])
 static int do_update(int argc, char *argv[])
 {
 	int i, errorcnt = 0;
-	struct updater_config cfg = {};
+	struct updater_config cfg = {
+		.image = { .programmer = PROG_HOST, },
+		.old_image = { .programmer = PROG_HOST, },
+		.ec_image = { .programmer = PROG_EC, },
+		.pd_image = { .programmer = PROG_PD, },
+	};
 
 	printf(">> Firmware updater started.\n");
 
 	opterr = 0;
 	while ((i = getopt_long(argc, argv, short_opts, long_opts, 0)) != -1) {
 		switch (i) {
+		case 'i':
+			errorcnt += load_image(optarg, &cfg.image);
+			break;
+		case 'e':
+			errorcnt += load_image(optarg, &cfg.ec_image);
+			break;
+		case 'P':
+			errorcnt += load_image(optarg, &cfg.pd_image);
+			break;
+
 		case 'h':
 			print_help(argc, argv);
 			return !!errorcnt;
@@ -95,6 +168,7 @@ static int do_update(int argc, char *argv[])
 	       errorcnt ? "FAILED": "DONE",
 	       errorcnt ? "stopped due to error" : "exited successfully");
 
+	unload_updater_config(&cfg);
 	return !!errorcnt;
 }
 
