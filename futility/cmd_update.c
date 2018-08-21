ac7bbb25be0fe2d157af87534c5b579056cad443
Hung-Te Lin <hungte@chromium.org>
2018-08-21 18:57:19 +0800

    futility: cmd_update: Add real update commands
    Add '-t' for updater '--mode=autoupdate', and '--wp' to override write
    protection status.
    
    The `update_firmware` now has basic logic by reading -t/--wp params.
    
    BUG=chromium:875551
    TEST=make futil; ./build/futility/futility -i /build/eve/firmware/image.bin
         futility -i IMAGE -t; futility -i IMAGE --wp=0
    BRANCH=None
    
    Change-Id: Ifbfc4fb76f954483e779c8b508377d07561b67da
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>

--- futility/cmd_update.c
+++ futility/cmd_update.c
@@ -22,9 +22,13 @@ typedef const char * const CONST_STRING;
 
 /* FMAP section names. */
 static CONST_STRING FMAP_RO_FRID = "RO_FRID",
+		    FMAP_RW_SECTION_A = "RW_SECTION_A",
+		    FMAP_RW_SECTION_B = "RW_SECTION_B",
 		    FMAP_RW_FWID = "RW_FWID",
 		    FMAP_RW_FWID_A = "RW_FWID_A",
-		    FMAP_RW_FWID_B = "RW_FWID_B";
+		    FMAP_RW_FWID_B = "RW_FWID_B",
+		    FMAP_RW_SHARED = "RW_SHARED",
+		    FMAP_RW_LEGACY = "RW_LEGACY";
 
 /* flashrom programmers. */
 static CONST_STRING PROG_HOST = "host",
@@ -60,6 +64,8 @@ struct updater_config {
 	struct firmware_image image, old_image;
 	struct firmware_image ec_image, pd_image;
 	struct system_env env;
+	int try_update;
+	int write_protection;
 };
 
 static int host_flashrom(enum flashrom_ops op, const char *image_path,
@@ -217,6 +223,30 @@ static void free_image(struct firmware_image *image)
 	memset(image, 0, sizeof(*image));
 }
 
+static int write_firmware(struct updater_config *cfg,
+			  struct firmware_image *image,
+			  const char *section)
+{
+	/* TODO(hungte) replace by mkstemp */
+	const char *tmp_file = "/tmp/.fwupdate.write";
+	FILE *fp = fopen(tmp_file, "wb");
+	if (!fp)
+		return -1;
+	fwrite(image->data, image->size, 1, fp);
+	fclose(fp);
+	return cfg->env.flashrom(FLASHROM_WRITE, tmp_file, image->programmer, 1,
+				 section);
+}
+
+static int write_optional_firmware(struct updater_config *cfg,
+				   struct firmware_image *image,
+				   const char *section)
+{
+	if (!image->data)
+		return 0;
+	return write_firmware(cfg, image, section);
+}
+
 enum updater_error_codes {
 	UPDATE_ERR_NONE,
 	UPDATE_ERR_NO_IMAGE,
@@ -233,6 +263,7 @@ static const char * const updater_error_messages[] = {
 
 static int update_firmware(struct updater_config *cfg)
 {
+	int wp_enabled;
 	struct firmware_image *image_from = &cfg->old_image,
 			      *image_to = &cfg->image;
 	if (!image_to->data)
@@ -254,6 +285,36 @@ static int update_firmware(struct updater_config *cfg)
 	printf(">> Current system: %s (RO:%s, RW/A:%s, RW/B:%s).\n",
 	       image_from->file_name, image_from->ro_version,
 	       image_from->rw_version_a, image_from->rw_version_b);
+
+	/* TODO(hungte) Auto detect WP if needed. */
+	wp_enabled = cfg->write_protection;
+
+	if (cfg->try_update) {
+		Error("Not supported yet.\n");
+		return UPDATE_ERR_UNKNOWN;
+	}
+
+	if (wp_enabled) {
+		printf(">> Updating %s, %s, and %s.\n", FMAP_RW_SECTION_A,
+		       FMAP_RW_SECTION_B, FMAP_RW_SHARED);
+		/*
+		 * TODO(hungte) Speed up by flashing multiple sections in one
+		 * command, or provide diff file.
+		 */
+		write_firmware(cfg, image_to, FMAP_RW_SECTION_A);
+		write_firmware(cfg, image_to, FMAP_RW_SECTION_B);
+		write_firmware(cfg, image_to, FMAP_RW_SHARED);
+
+		if (firmware_section_exists(image_to, FMAP_RW_LEGACY))
+			write_firmware(cfg, image_to, FMAP_RW_LEGACY);
+	} else {
+		printf(">> Updating entire firmware images.\n");
+
+		/* FMAP may be different so we should just update all. */
+		write_firmware(cfg, image_to, NULL);
+		write_optional_firmware(cfg, &cfg->ec_image, NULL);
+		write_optional_firmware(cfg, &cfg->pd_image, NULL);
+	}
 	return UPDATE_ERR_NONE;
 }
 
@@ -271,11 +332,13 @@ static struct option const long_opts[] = {
 	{"image", 1, NULL, 'i'},
 	{"ec_image", 1, NULL, 'e'},
 	{"pd_image", 1, NULL, 'P'},
+	{"try", 0, NULL, 't'},
+	{"wp", 1, NULL, 'W'},
 	{"help", 0, NULL, 'h'},
 	{NULL, 0, NULL, 0},
 };
 
-static const char * const short_opts = "hi:e:";
+static const char * const short_opts = "hi:e:t";
 static int errorcnt = 0;
 
 static void print_help(int argc, char *argv[])
@@ -286,6 +349,8 @@ static void print_help(int argc, char *argv[])
 		"-i, --image=FILE   \tAP (host) firmware image (image.bin)\n"
 		"-e, --ec_image=FILE\tEC firmware image (i.e, ec.bin)\n"
 		"    --pd_image=FILE\tPD firmware image (i.e, pd.bin)\n"
+		"-t, --try          \tUse A/B trial update if possible\n"
+		"    --wp=1|0       \tSpecify write protection status\n"
 		"",
 		argv[0]);
 }
@@ -301,6 +366,8 @@ static int do_update(int argc, char *argv[])
 		.env = {
 			.flashrom = host_flashrom,
 		},
+		.try_update = 0,
+		.write_protection = 1,
 	};
 
 	opterr = 0;		/* quiet, you */
@@ -314,6 +381,12 @@ static int do_update(int argc, char *argv[])
 			break;
 		case 'P':
 			errorcnt += load_image(optarg, &cfg.pd_image);
+			break;
+		case 't':
+			cfg.try_update = 1;
+			break;
+		case 'W':
+			cfg.write_protection = atoi(optarg);
 			break;
 
 		case 'h':
