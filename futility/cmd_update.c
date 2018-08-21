53cbd82ea0fa7edf801b2a4b51fd266e0412faca
Hung-Te Lin <hungte@chromium.org>
2018-08-21 23:09:57 +0800

    futility: cmd_update: Check contents before starting to update
    In try-boot updating flow, we have to first check if RO content needs to be
    changed or not, and do full (ro+ro) update if WP is disabled. Also, before
    starting to update RW-A or RW-B, we should also check if the active system
    already has same firmware contents.
    
    BUG=chromium:875551
    TEST=make futil; ./build/futility/futility -i /build/eve/firmware/image.bin -t
    BRANCH=None
    
    Change-Id: Ie2f75f9aab4696c75aedafbf45e418ee98a2a4b4
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>

--- futility/cmd_update.c
+++ futility/cmd_update.c
@@ -23,7 +23,8 @@ typedef const char * const CONST_STRING;
 #define RETURN_ON_FAILURE(x) do {int r = (x); if (r) return r;} while (0);
 
 /* FMAP section names. */
-static CONST_STRING FMAP_RO_FRID = "RO_FRID",
+static CONST_STRING FMAP_RO_SECTION = "RO_SECTION",
+		    FMAP_RO_FRID = "RO_FRID",
 		    FMAP_RO_GBB = "GBB",
 		    FMAP_RO_VPD = "RO_VPD",
 		    FMAP_RW_VPD = "RW_VPD",
@@ -420,10 +421,29 @@ static int preserve_images(struct updater_config *cfg)
 	return errcnt;
 }
 
+static int compare_section(const struct firmware_section *a,
+			   const struct firmware_section *b)
+{
+	if (a->size != b->size)
+		return a->size - b->size;
+	return memcmp(a->data, b->data, a->size);
+}
+
+static int images_have_same_section(struct firmware_image *image_from,
+				    struct firmware_image *image_to,
+				    const char *section_name)
+{
+	struct firmware_section from, to;
+
+	find_firmware_section(&from, image_from, section_name);
+	find_firmware_section(&to, image_to, section_name);
+	return compare_section(&from, &to) == 0;
+}
 enum updater_error_codes {
 	UPDATE_ERR_NONE,
 	UPDATE_ERR_NO_IMAGE,
 	UPDATE_ERR_SYSTEM_IMAGE,
+	UPDATE_ERR_TARGET,
 	UPDATE_ERR_UNKNOWN,
 };
 
@@ -431,6 +451,7 @@ static const char * const updater_error_messages[] = {
 	[UPDATE_ERR_NONE] = "None",
 	[UPDATE_ERR_NO_IMAGE] = "No image to update; try specify with -i.",
 	[UPDATE_ERR_SYSTEM_IMAGE] = "Cannot load system active firmware.",
+	[UPDATE_ERR_TARGET] = "No valid RW target to update. Abort.",
 	[UPDATE_ERR_UNKNOWN] = "Unknown error.",
 };
 
@@ -465,7 +486,23 @@ static int update_firmware(struct updater_config *cfg)
 	while (cfg->try_update) {
 		const char *target;
 
+		preserve_gbb(image_from, image_to);
+		if (!images_have_same_section(image_from, image_to,
+					      FMAP_RO_SECTION) && !wp_enabled) {
+			printf("WP disabled and RO changed. Do full update.\n");
+			break;
+		}
 		/* TODO(hungte): Support vboot1. */
+		target = decide_rw_target(&cfg->env, TARGET_SELF);
+		if (target == NULL) {
+			return UPDATE_ERR_TARGET;
+		}
+		printf("Checking %s contents...\n", target);
+		if (images_have_same_section(image_from, image_to, target)) {
+			printf(">> No need to update.\n");
+			return UPDATE_ERR_NONE;
+		}
+
 		target = decide_rw_target(&cfg->env, TARGET_UPDATE);
 		printf(">> Updating %s with trial boots.\n", target);
 		write_firmware(cfg, image_to, target);
