92f4d6521315a561cb1f8e3d0cac9409b30f6b29
David Riley <davidriley@chromium.org>
2018-11-06 15:10:34 -0800

    image_signing: Verify that cr50 signing includes a manifest with restrictions.
    Signing without device or board restrictions is not supported until
    there is a way to explicitly indicate that the build is okay to be
    signed without restrictions.
    
    BRANCH=cr50
    TEST=sign_official_build.sh cr50_firmware input tests/devkeys output
    BUG=b:74100307
    
    Change-Id: Ic123df4396d7d497347de40a5ff448940c0b1982

---
+++ scripts/image_signing/verify_cr50_manifest.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+. "$(dirname "$0")/common.sh"
+
+load_shflags || exit 1
+
+FLAGS_HELP="Usage: ${PROG} [options] <manifest>
+
+Validates that cr50 signing manifest includes restrictions.
+"
+
+# Parse command line.
+FLAGS "$@" || exit 1
+eval set -- "${FLAGS_ARGV}"
+
+# Abort on error and uninitialized variables.
+set -e
+set -u
+
+# Verify that the manifest file includes either a board or device restriction.
+verify_restrictions() {
+  [[ $# -eq 1 ]] || die "Usage: verify_restrictions <manifest>"
+  local manifest_file="$1"
+
+  local restrictions=($(awk '
+    BEGIN {
+      board = 0;
+      device = 0;
+    };
+    /"board_id(|_mask|_flags)": (0x[0-9a-f]+|[0-9]+),/ && strtonum($2) != 0 {
+      board += 1
+    };
+    /"DEV_ID[01]": [-0-9a-f]+,/ && strtonum($2) != 0 { device += 1 };
+    END { print board, device };
+  ' "${manifest_file}"))
+
+  if [[ ${#restrictions[@]} != 2 ]]; then
+    die "Invalid number of restrictions counted."
+  fi
+
+  local board="${restrictions[0]}"
+  local device="${restrictions[1]}"
+
+  if [[ "${board}" == "0" && "${device}" == "0" ]]; then
+    die "Manifest lacks board or device restrictions."
+  fi
+}
+
+main() {
+  if [[ $# -ne 1 ]]; then
+    flags_help
+    exit 1
+  fi
+
+  local manifest_file="$1"
+
+  if [[ ! -e "${manifest_file}" ]]; then
+    die "Missing manifest file: ${manifest_file}"
+  fi
+
+  verify_restrictions "${manifest_file}"
+}
+main "$@"
