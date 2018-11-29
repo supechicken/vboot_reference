04c8b7dae26cc4469861339e2e06d14f0ee96c3e
Joel Kitching <kitching@google.com>
2018-11-29 19:39:05 +0800

    2api: add call for reset Cr50
    BUG=None
    TEST=None
    
    Change-Id: I7eec4d97f4c59c63ab5cd0910e6a37f7e2877fc6

--- firmware/2lib/include/2api.h
+++ firmware/2lib/include/2api.h
@@ -727,4 +727,12 @@ int vb2ex_tpm_get_mode(uint8_t *mode_val);
  */
 int vb2ex_tpm_set_mode(uint8_t mode_val);
 
+/*
+ * Ask the Cr50 to reset.  If everything goes well, this function should
+ * never return, since a Cr50 reset implies an AP reset.
+ *
+ * @returns VB2_ERROR_UNKNOWN.
+ */
+int vb2ex_tpm_reset(void);
+
 #endif  /* VBOOT_2_API_H_ */
