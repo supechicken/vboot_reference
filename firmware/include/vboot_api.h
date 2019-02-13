8f6e7ea9c1362ffad492e1b6e2a896129bc537e1
Matt Delco <delco@google.com>
2019-02-13 12:58:58 -0800

    vboot: add diagnostic mode
    This change adds diagnostic mode.  When enabled for a board (based on
    defconfig in depthcharge) the user can press Ctrl-C or F12 at a recovery
    mode screen, at which point an nv bit is set and the system reboots.
    Upon reboot, if the nv bit is set then the user is prompted to confirm
    launch of the diagnostic rom via the power button.  If user confirms
    then the diagnostic payload is verified and run (if verify fails or
    payload doesn't run then a recovery reason is recorded and system
    reboots to recovery mode).  If the user does not confirm then the system
    reboots.
    
    BUG=b:124358784
    BRANCH=None
    TEST=Locally built and flashed using change that enabled feature for atlas
    and set to use payload 2 (tianocore) rather than 5 (diagnostic).  Confirmed
    that Ctrl-C is functional or not based on defconfig and that Ctrl-C
    sets NV bit and reboots.  Confirmed that NV bit can be set and
    queried via crossystem. Confirmed that during boot confirmation screen
    appears or not based on NV bit.  Confirmed that pressing power button
    caused payload to be verified and run.  Confirmed that non-matching hash
    (build configured to use sha1 rather than sha256) caused payload to not
    be run and system reboot to recovery.  Confirmed that Esc or timeout
    caused system to reboot.
    CQ-DEPEND=CL:1471056
    
    Change-Id: I8979d4eeb443bf64b727ee86a814c46d1d27ff37
    Signed-off-by: Matt Delco <delco@google.com>

--- firmware/include/vboot_api.h
+++ firmware/include/vboot_api.h
@@ -376,6 +376,10 @@ void VbExDebug(const char *format, ...)
 /*****************************************************************************/
 /* Timer and delay (first two from utility.h) */
 
+#define VB_USEC_PER_MSEC	1000ULL
+#define VB_MSEC_PER_SEC		VB_USEC_PER_MSEC
+#define VB_USEC_PER_SEC		(VB_USEC_PER_MSEC * VB_MSEC_PER_SEC)
+
 /**
  * Read a microsecond timer.
  *
@@ -683,6 +687,8 @@ enum VbScreenType_t {
 	VB_SCREEN_SET_VENDOR_DATA = 0x214,
 	/* Confirm vendor data menu screen */
 	VB_SCREEN_CONFIRM_VENDOR_DATA = 0x215,
+	/* Confirm reboot for running diagnostics rom */
+	VB_SCREEN_CONFIRM_DIAG = 0x216,
 };
 
 /**
@@ -1013,6 +1019,7 @@ enum {
 };
 
 enum VbAltFwIndex_t {
+	VB_ALTFW_DIAGNOSTIC = -1,
 	VB_ALTFW_DEFAULT = 0,
 	VB_ALTFW_FIRST = 1,
 	VB_ALTFW_SECOND,
@@ -1033,7 +1040,8 @@ enum VbAltFwIndex_t {
  *     >0 (i.e., positive #) run a payload by # based in altfw/list file
  *     <0 (i.e., negative #) run a specific payload by name without using
  *        the altfw/list file.  Typically payloads in this category will be
- *        verified before they are run. Currently no #s are defined.
+ *        verified before they are run. Currently these #s are defined:
+ *          -1 diagnostic payload
  */
 int VbExLegacy(enum VbAltFwIndex_t altfw_num);
 
