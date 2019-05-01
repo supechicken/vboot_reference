22b55ab6ac000fe296f6f82ce267bb4453eff04f
Karthikeyan Ramasubramanian <kramasub@chromium.org>
2019-05-01 10:46:46 -0600

    firmware/lib/ec_sync_all: Refactor the EC SW Sync flow
    Perform all the operations regarding EC AUX FW update once the EC update
    is applied and EC has jumped to the updated image. This way the
    information regarding the devices that require EC AUX FW update can be
    probed and obtained from EC consistently.
    
    BUG=b:128820536,b:119046668
    BRANCH=None
    TEST=Ensure that the device boots to ChromeOS. Ensure that the Aux FW
    update is handled after EC update is applied and jumped to the updated
    EC image.
    
    Bobba:
    sync_one_ec: jumping to EC-RW
    send_packet: CrosEC result code 12
    EC returned from reboot after 22467us
    ps8751.1: vendor 0x1da0 product 0x8751 device 0x0001 fw_rev 0x39
    Protect aux fw 0
    
    Casta:
    sync_one_ec: jumping to EC-RW
    send_packet: CrosEC result code 12
    EC returned from reboot after 43014us
    ps8751.0: vendor 0x1da0 product 0x8751 device 0x0001 fw_rev 0x39
    ps8751.1: vendor 0x1da0 product 0x8751 device 0x0001 fw_rev 0x39
    Protect aux fw 0
    Protect aux fw 1
    
    Ampton:
    sync_one_ec: jumping to EC-RW
    EC returned from reboot after 41743us
    send_packet: CrosEC result code 1
    Ec doesn't support aux FW probe, skipping...
    
    Change-Id: I28956543dfe1e059e15212dceada8bc517c0e7fc
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>

--- firmware/lib/ec_sync_all.c
+++ firmware/lib/ec_sync_all.c
@@ -37,20 +37,39 @@ static VbError_t ec_sync_disable_display(struct vb2_context *ctx,
 	return VBERROR_SUCCESS;
 }
 
+static int check_reboot_for_display(struct vb2_context *ctx,
+					struct vb2_shared_data *sd)
+{
+	if (!(sd->flags & VB2_SD_FLAG_DISPLAY_AVAILABLE)) {
+		VB2_DEBUG("Reboot to initialize display\n");
+		vb2_nv_set(ctx, VB2_NV_DISPLAY_REQUEST, 1);
+		return 1;
+	}
+	return 0;
+}
+
+static void display_wait_screen(struct vb2_context *ctx, const char *fw_name)
+{
+	VB2_DEBUG("%s update is slow. Show WAIT screen.\n", fw_name);
+	VbDisplayScreen(ctx, VB_SCREEN_WAIT, 0, NULL);
+}
+
 VbError_t ec_sync_all(struct vb2_context *ctx)
 {
 	struct vb2_shared_data *sd = vb2_get_sd(ctx);
 	VbAuxFwUpdateSeverity_t fw_update;
 	VbError_t rv;
 
-	rv = ec_sync_check_aux_fw(ctx, &fw_update);
-	if (rv)
-		return rv;
-
 	/* Phase 1; this determines if we need an update */
 	VbError_t phase1_rv = ec_sync_phase1(ctx);
+	/*
+	 * Besides checking the severity of the EC update, check the severity
+	 * of the Aux FW update here speculatively, so that we can avoid the
+	 * double reboot if the old EC-RW supports the chipinfo probing command.
+	 */
 	int need_wait_screen = ec_will_update_slowly(ctx) ||
-		(fw_update == VB_AUX_FW_SLOW_UPDATE);
+		(!ec_sync_check_aux_fw(ctx, &fw_update) &&
+		 fw_update == VB_AUX_FW_SLOW_UPDATE);
 
 	/*
 	 * Check if we need to reboot to initialize the display before we can
@@ -59,12 +78,8 @@ VbError_t ec_sync_all(struct vb2_context *ctx)
 	 * Do this before we check if ec_sync_phase1() requires a reboot for
 	 * some other reason, since there's no reason to reboot twice.
 	 */
-	int reboot_for_display = (need_wait_screen &&
-				  !(sd->flags & VB2_SD_FLAG_DISPLAY_AVAILABLE));
-	if (reboot_for_display) {
-		VB2_DEBUG("Reboot to initialize display\n");
-		vb2_nv_set(ctx, VB2_NV_DISPLAY_REQUEST, 1);
-	}
+	int reboot_for_display = need_wait_screen &&
+				check_reboot_for_display(ctx, sd);
 
 	/* Reboot if phase 1 needed it, or if we need to initialize display */
 	if (phase1_rv)
@@ -73,15 +88,30 @@ VbError_t ec_sync_all(struct vb2_context *ctx)
 		return VBERROR_REBOOT_REQUIRED;
 
 	/* Display the wait screen if we need it */
-	if (need_wait_screen) {
-		VB2_DEBUG("EC is slow. Show WAIT screen.\n");
-		VbDisplayScreen(ctx, VB_SCREEN_WAIT, 0, NULL);
-	}
+	if (need_wait_screen)
+		display_wait_screen(ctx, "EC FW");
 
 	/* Phase 2; Applies update and/or jumps to the correct EC image */
 	rv = ec_sync_phase2(ctx);
 	if (rv)
 		return rv;
+
+	/* Check the severity of the AUX FW update */
+	rv = ec_sync_check_aux_fw(ctx, &fw_update);
+	if (rv)
+		return rv;
+
+	/*
+	 * If AUX FW update is slow and if the WAIT screen is not already
+	 * displayed, then display it.
+	 */
+	if (!need_wait_screen && fw_update == VB_AUX_FW_SLOW_UPDATE) {
+		need_wait_screen = 1;
+		reboot_for_display = check_reboot_for_display(ctx, sd);
+		if (reboot_for_display)
+			return VBERROR_REBOOT_REQUIRED;
+		display_wait_screen(ctx, "AUX FW");
+	}
 
 	/*
 	 * Do Aux FW software sync and protect devices tunneled through the EC.
