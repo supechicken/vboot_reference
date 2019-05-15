bafa9f6a247068af3e63dc35158905d201b712bb
Igor <igorcov@chromium.org>
2019-05-15 17:27:58 +0200

    vboot: Make sure the .developer_mode file is present is switch from dev mode is displayed
    The file ".developer_mode" is created by clobber only when switch to
    developer mode is done and the device is reset.
    
    There are cases when the switch to dev mode is not finished though, when
    it is disabled or when the user decides to cancel the switch.
    
    In that case the screen that the device will switch from dev mode and
    the data will be cleared is displayed. To be consistent with what the
    screen is displaying, we create the .developer_mode file here so that at
    startup the device will indeed clear all the data.
    
    BUG=chromium:959160
    BRANCH=None
    TEST=Manual
    
    Change-Id: Ia669196addd3b793128f633f12bb16e3f6d01854

--- firmware/lib/vboot_ui.c
+++ firmware/lib/vboot_ui.c
@@ -13,6 +13,7 @@
 #include "2nvstorage.h"
 #include "2rsa.h"
 #include "ec_sync.h"
+#include "fcntl.h"
 #include "gbb_access.h"
 #include "gbb_header.h"
 #include "load_kernel_fw.h"
@@ -515,6 +516,13 @@ static const char dev_disable_msg[] =
 	"For more information, see http://dev.chromium.org/chromium-os/fwmp\n"
 	"\n";
 
+void vb2_display_developer_to_norm(struct vb2_context *ctx)
+{
+	creat("/mnt/stateful_partition/.developer_mode", 0666);
+	VbDisplayScreen(ctx,
+			VB_SCREEN_DEVELOPER_TO_NORM, 0, NULL);
+}
+
 VbError_t vb2_developer_ui(struct vb2_context *ctx)
 {
 	struct vb2_shared_data *sd = vb2_get_sd(ctx);
@@ -567,8 +575,7 @@ VbError_t vb2_developer_ui(struct vb2_context *ctx)
 	/* If dev mode is disabled, only allow TONORM */
 	while (disable_dev_boot) {
 		VB2_DEBUG("dev_disable_boot is set\n");
-		VbDisplayScreen(ctx,
-				VB_SCREEN_DEVELOPER_TO_NORM, 0, NULL);
+		vb2_display_developer_to_norm(ctx);
 		VbExDisplayDebugInfo(dev_disable_msg, 0);
 
 		/* Ignore space in VbUserConfirms()... */
@@ -576,8 +583,7 @@ VbError_t vb2_developer_ui(struct vb2_context *ctx)
 		case 1:
 			VB2_DEBUG("leaving dev-mode\n");
 			vb2_nv_set(ctx, VB2_NV_DISABLE_DEV_REQUEST, 1);
-			VbDisplayScreen(ctx,
-				VB_SCREEN_TO_NORM_CONFIRMED, 0, NULL);
+			vb2_display_developer_to_norm(ctx);
 			VbExSleepMs(5000);
 			return VBERROR_REBOOT_REQUIRED;
 		case -1:
@@ -630,18 +636,14 @@ VbError_t vb2_developer_ui(struct vb2_context *ctx)
 						VB_BEEP_NOT_ALLOWED);
 					break;
 				}
-				VbDisplayScreen(ctx,
-					VB_SCREEN_DEVELOPER_TO_NORM,
-					0, NULL);
+				vb2_display_developer_to_norm(ctx);
 				/* Ignore space in VbUserConfirms()... */
 				switch (VbUserConfirms(ctx, 0)) {
 				case 1:
 					VB2_DEBUG("leaving dev-mode\n");
 					vb2_nv_set(ctx, VB2_NV_DISABLE_DEV_REQUEST,
 						1);
-					VbDisplayScreen(ctx,
-						VB_SCREEN_TO_NORM_CONFIRMED,
-						0, NULL);
+					vb2_display_developer_to_norm(ctx);
 					VbExSleepMs(5000);
 					return VBERROR_REBOOT_REQUIRED;
 				case -1:
