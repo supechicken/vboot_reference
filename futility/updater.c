0827c9f1716a7077fa3f37fc253698fc21636c13
Hung-Te Lin <hungte@chromium.org>
2019-05-23 02:54:09 +0800

    futility: updater: Report EC and PD images on emulation
    When testing updater behavior with emulation (--emulate), there was no
    way to know if EC and PD images were correctly found from archive and
    expected to be flashed (for example if we want to test the difference
    between modes).
    
    Since we do flash EC/PD in recovery and factory modes, it is better
    to still allow loading EC/PD images, and simply not writing them
    (and print some messages as indication) in emulation.
    
    BUG=chromium:965092
    TEST=make tests futil; BUILD_RUN=$(pwd)/build \
         tests/futility/run_test_scripts.sh $(pwd)/build/futility
    BRANCH=None
    
    Change-Id: I3bbbd75cb8adf2e238a593d3dee0b2491abe7719
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>

--- futility/updater.c
+++ futility/updater.c
@@ -886,7 +886,8 @@ static int write_firmware(struct updater_config *cfg,
 static int write_optional_firmware(struct updater_config *cfg,
 				   const struct firmware_image *image,
 				   const char *section_name,
-				   int check_programmer_wp)
+				   int check_programmer_wp,
+				   int skip_on_emulation)
 {
 	if (!image->data) {
 		VB2_DEBUG("No data in <%s> image.\n", image->programmer);
@@ -895,6 +896,12 @@ static int write_optional_firmware(struct updater_config *cfg,
 	if (section_name && !firmware_section_exists(image, section_name)) {
 		VB2_DEBUG("Image %s<%s> does not have section %s.\n",
 			  image->file_name, image->programmer, section_name);
+		return 0;
+	}
+	if (skip_on_emulation) {
+		INFO("(dummy-emulation) Writing %s from %s to %s (%d bytes).\n",
+		     section_name ? section_name : "whole image",
+		     image->file_name, image->programmer, image->size);
 		return 0;
 	}
 
@@ -1576,7 +1583,7 @@ static enum updater_error_codes update_rw_firmware(
 	if (write_firmware(cfg, image_to, FMAP_RW_SECTION_A) ||
 	    write_firmware(cfg, image_to, FMAP_RW_SECTION_B) ||
 	    write_firmware(cfg, image_to, FMAP_RW_SHARED) ||
-	    write_optional_firmware(cfg, image_to, FMAP_RW_LEGACY, 0))
+	    write_optional_firmware(cfg, image_to, FMAP_RW_LEGACY, 0, 0))
 		return UPDATE_ERR_WRITE_FIRMWARE;
 
 	return UPDATE_ERR_DONE;
@@ -1647,8 +1654,8 @@ static enum updater_error_codes update_whole_firmware(
 
 	/* FMAP may be different so we should just update all. */
 	if (write_firmware(cfg, image_to, NULL) ||
-	    write_optional_firmware(cfg, &cfg->ec_image, NULL, 1) ||
-	    write_optional_firmware(cfg, &cfg->pd_image, NULL, 1))
+	    write_optional_firmware(cfg, &cfg->ec_image, NULL, 1, 1) ||
+	    write_optional_firmware(cfg, &cfg->pd_image, NULL, 1, 1))
 		return UPDATE_ERR_WRITE_FIRMWARE;
 
 	return UPDATE_ERR_DONE;
@@ -1832,7 +1839,7 @@ static int updater_load_images(struct updater_config *cfg,
 		if (!errorcnt)
 			errorcnt += updater_setup_quirks(cfg, arg);
 	}
-	if (cfg->emulation || arg->host_only)
+	if (arg->host_only)
 		return errorcnt;
 
 	if (!cfg->ec_image.data && ec_image)
@@ -2048,7 +2055,6 @@ int updater_setup_config(struct updater_config *cfg,
 	/* Set up archive and load images. */
 	if (arg->emulation) {
 		/* Process emulation file first. */
-		check_single_image = 1;
 		cfg->emulation = arg->emulation;
 		VB2_DEBUG("Using file %s for emulation.\n", arg->emulation);
 		errorcnt += !!load_firmware_image(
