719c7458df32570b14e8025fdb99d86f08a242ca
Joel Kitching <kitching@google.com>
2019-07-25 18:26:18 +0800

    WIP: vboot: move vb2_context to workbuf
    Move vb2_context to live at the start of workbuf, instead of
    vb2_shared_data.  The latter now follows second.
    
    BUG=b:124141368, chromium:994060
    TEST=make clean && make runtests
    BRANCH=none
    
    Change-Id: If2421756572a43ba58b9da9f00e56a8f26ad3ad5
    Signed-off-by: Joel Kitching <kitching@google.com>

--- firmware/lib/vboot_api_kernel.c
+++ firmware/lib/vboot_api_kernel.c
@@ -204,13 +204,6 @@ static vb2_error_t vb2_kernel_setup(struct vb2_context *ctx,
 	uint32_t r;
 	vb2_error_t rv;
 
-	rv = vb2_init_context(ctx);
-	if (VB2_SUCCESS != rv) {
-		VB2_DEBUG("Can't init vb2_context\n");
-		vb2api_fail(ctx, VB2_RECOVERY_RW_SHARED_DATA, rv);
-		return VBERROR_INIT_SHARED_DATA;
-	}
-
 	/* Start timer */
 	shared->timer_vb_select_and_load_kernel_enter = VbExGetTimer();
 
