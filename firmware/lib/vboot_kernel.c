30332221364195b572a5a7bd8f579af8bf95871c
Joel Kitching <kitching@google.com>
2019-07-30 10:07:45 +0800

    vboot: migrate GBB-related error handling to vboot2
    Pass on values returned from vb2_gbb_* functions instead
    of using VBERROR_INVALID_GBB on error.
    
    BUG=b:124141368, chromium:988410
    TEST=make clean && make runtests
    BRANCH=none
    
    Change-Id: I66b99393f0fcc9eabe629d08b35247764cfbcefb
    Signed-off-by: Joel Kitching <kitching@google.com>

--- firmware/lib/vboot_kernel.c
+++ firmware/lib/vboot_kernel.c
@@ -444,9 +444,8 @@ vb2_error_t LoadKernel(struct vb2_context *ctx, LoadKernelParams *params)
 	VbSharedDataKernelCall *shcall = NULL;
 	int found_partitions = 0;
 	uint32_t lowest_version = LOWEST_TPM_VERSION;
-	vb2_error_t retval = VB2_ERROR_UNKNOWN;
+	vb2_error_t rv = VB2_ERROR_UNKNOWN;
 	int recovery = VB2_RECOVERY_LK_UNSPECIFIED;
-	vb2_error_t rv;
 
 	vb2_workbuf_from_ctx(ctx, &wb);
 
@@ -476,7 +475,6 @@ vb2_error_t LoadKernel(struct vb2_context *ctx, LoadKernelParams *params)
 		rv = vb2_gbb_read_recovery_key(ctx, &kernel_subkey, NULL, &wb);
 		if (VB2_SUCCESS != rv) {
 			VB2_DEBUG("GBB read recovery key failed.\n");
-			retval = VBERROR_INVALID_GBB;
 			goto load_kernel_exit;
 		}
 	} else {
@@ -653,23 +651,23 @@ gpt_done:
 			shared->kernel_version_tpm = lowest_version;
 
 		/* Success! */
-		retval = VB2_SUCCESS;
+		rv = VB2_SUCCESS;
 	} else if (found_partitions > 0) {
 		shcall->check_result = VBSD_LKC_CHECK_INVALID_PARTITIONS;
 		recovery = VB2_RECOVERY_RW_INVALID_OS;
-		retval = VBERROR_INVALID_KERNEL_FOUND;
+		rv = VBERROR_INVALID_KERNEL_FOUND;
 	} else {
 		shcall->check_result = VBSD_LKC_CHECK_NO_PARTITIONS;
 		recovery = VB2_RECOVERY_RW_NO_OS;
-		retval = VBERROR_NO_KERNEL_FOUND;
+		rv = VBERROR_NO_KERNEL_FOUND;
 	}
 
 load_kernel_exit:
 	/* Store recovery request, if any */
 	vb2_nv_set(ctx, VB2_NV_RECOVERY_REQUEST,
-		   VB2_SUCCESS != retval ?
+		   VB2_SUCCESS != rv ?
 		   recovery : VB2_RECOVERY_NOT_REQUESTED);
 
-	shcall->return_code = (uint8_t)retval;
-	return retval;
+	shcall->return_code = (uint8_t)rv;
+	return rv;
 }
