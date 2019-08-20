c3d34b98f72afba28873900682260345fd300f0f
Joel Kitching <kitching@google.com>
2019-08-20 17:40:00 +0800

    vboot/secdata: implement initial vboot2 FWMP support
    BUG=b:124141368, chromium:972956
    TEST=make clean && make runtests
    BRANCH=none
    
    Change-Id: I716eba28e0e37eed3c443fced4d2d1196a692bef
    Signed-off-by: Joel Kitching <kitching@google.com>

---
+++ tests/vb2_secdata_fwmp_tests.c
@@ -0,0 +1,77 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Tests for firmware management parameters (FWMP) library.
+ */
+
+#include "2common.h"
+#include "2misc.h"
+#include "2secdata.h"
+#include "test_common.h"
+
+static void test_changed(struct vb2_context *c, int changed, const char *why)
+{
+	if (changed)
+		TEST_NEQ(c->flags & VB2_CONTEXT_SECDATA_FWMP_CHANGED, 0, why);
+	else
+		TEST_EQ(c->flags & VB2_CONTEXT_SECDATA_FWMP_CHANGED, 0, why);
+
+	c->flags &= ~VB2_CONTEXT_SECDATA_FWMP_CHANGED;
+};
+
+static void secdata_fwmp_test(void)
+{
+	uint8_t workbuf[VB2_FIRMWARE_WORKBUF_RECOMMENDED_SIZE]
+		__attribute__ ((aligned (VB2_WORKBUF_ALIGN)));
+	struct vb2_context c = {
+		.flags = 0,
+		.workbuf = workbuf,
+		.workbuf_size = sizeof(workbuf),
+	};
+	struct vb2_secdata_fwmp *sec =
+		(struct vb2_secdata_fwmp *)c.secdata_fwmp;
+	struct vb2_shared_data *sd = vb2_get_sd(&c);
+
+	/* Check size constant */
+	TEST_TRUE(sizeof(struct vb2_secdata_fwmp) <= VB2_SECDATA_FWMP_MAX_SIZE,
+		  "Struct size constant");
+
+	/* Size too large */
+	memset(c.secdata_fwmp, 0xff, sizeof(c.secdata_fwmp));
+	TEST_EQ(vb2api_secdata_fwmp_check(&c), VB2_ERROR_SECDATA_FWMP_SIZE,
+		"Check large size");
+	TEST_EQ(vb2_secdata_fwmp_init(&c), VB2_ERROR_SECDATA_FWMP_SIZE,
+		"Init large size");
+
+	/* Size too small */
+	memset(c.secdata_fwmp, 0, sizeof(c.secdata_fwmp));
+	TEST_EQ(vb2api_secdata_fwmp_check(&c), VB2_ERROR_SECDATA_FWMP_SIZE,
+		"Check small size");
+	TEST_EQ(vb2_secdata_fwmp_init(&c), VB2_ERROR_SECDATA_FWMP_SIZE,
+		"Init small size");
+
+	/* Blank data is invalid */
+	memset(c.secdata_fwmp, 0xa6, sizeof(c.secdata_fwmp));
+	sec->struct_size = sizeof(c.secdata_fwmp);
+	TEST_EQ(vb2api_secdata_fwmp_check(&c), VB2_ERROR_SECDATA_FWMP_CRC,
+		"Check blank CRC");
+	TEST_EQ(vb2_secdata_fwmp_init(&c), VB2_ERROR_SECDATA_FWMP_CRC,
+		"Init blank CRC");
+
+	/* Create good data */
+	TEST_SUCC(vb2api_secdata_fwmp_create(&c), "Create");
+	TEST_SUCC(vb2api_secdata_fwmp_check(&c), "Check created CRC");
+	TEST_SUCC(vb2_secdata_fwmp_init(&c), "Init created CRC");
+	TEST_NEQ(sd->status & VB2_SD_STATUS_SECDATA_FWMP_INIT, 0,
+		 "Init set SD status");
+	sd->status &= ~VB2_SD_STATUS_SECDATA_FWMP_INIT;
+	test_changed(&c, 1, "Create changes data");
+}
+
+int main(int argc, char* argv[])
+{
+	secdata_fwmp_test();
+
+	return gTestSuccess ? 0 : 255;
+}
