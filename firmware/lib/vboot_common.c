5796e85fc0568d17cd70f4162a72df092bba91ce
Joel Kitching <kitching@google.com>
2019-09-04 15:52:25 +0800

    vboot: keep const and non-const signature and key accessors
    Keep const (default) and non-const (_mutable suffix) versions
    of `data` field accessors for vb2_signature and vb2_packed_key.
    
    No need for separate 2packed_key.c file -- just static inline
    the functions.
    
    vb2_verify_packed_key_inside should return type vb2_error_t.
    
    BUG=b:124141368, chromium:968464
    TEST=make clean && make runtests
    BRANCH=none
    
    Change-Id: I96722a746f26abbb6e19a365ce74f0bfda0da381
    Signed-off-by: Joel Kitching <kitching@google.com>

--- firmware/lib/vboot_common.c
+++ firmware/lib/vboot_common.c
@@ -86,7 +86,9 @@ int PublicKeyCopy(struct vb2_packed_key *dest, const struct vb2_packed_key *src)
 	dest->key_size = src->key_size;
 	dest->algorithm = src->algorithm;
 	dest->key_version = src->key_version;
-	memcpy(GetPublicKeyData(dest), GetPublicKeyDataC(src), src->key_size);
+	memcpy((struct vb2_packed_key *)vb2_packed_key_data(dest),
+	       vb2_packed_key_data(src),
+	       src->key_size);
 	return 0;
 }
 
