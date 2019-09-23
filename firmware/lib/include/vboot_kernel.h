3da72e0ef9a503f0ff7a5ddb8a6020fc23a05466
Joel Kitching <kitching@google.com>
2019-09-23 22:53:49 +0800

    vboot: add vb2ex_kernel_commit_data to write nvdata
    Rather than calling vb2_nv_commit, which checks
    VB2_CONTEXT_NVDATA_CHANGED and then calls VbExNvStorageWrite
    if necessary, create a new external function
    vb2ex_kernel_commit_data, which commits any nvdata/secdata and
    locks secdata_kernel.
    
    Currently the depthcharge implementation of this function only
    writes nvdata, but once secdata TPM drivers have been migrated
    from vboot_reference to depthcharge, it will also commit these
    data spaces.
    
    Note that in normal cases, this function should not be necessary,
    and depthcharge should take care of writing nvdata/secdata once
    flow has returned to it.  However, in certain cases (as described
    in the function documentation), this is difficult or impossible
    to achieve.  For these cases, vboot may directly call
    vb2ex_kernel_commit_data.
    
    Finally, this CL also removes the VbExNvStorageRead call from
    vb2_kernel_setup, and the data is instead read in depthcharge
    CL:1819379, right before calling VbSelectAndLoadKernel.
    
    As such, both the VbExNvStorageRead and VbExNvStorageWrite
    functions may be removed.
    
    BUG=b:124141368, chromium:972956, chromium:1006689
    TEST=make clean && make runtests
    BRANCH=none
    
    Change-Id: I6575fd9f5b208ef5e040a604d0db38719d9c6289
    Signed-off-by: Joel Kitching <kitching@google.com>
    Cq-Depend: chromium:1819379

--- firmware/lib/include/vboot_kernel.h
+++ firmware/lib/include/vboot_kernel.h
@@ -80,15 +80,6 @@ vb2_error_t VbBootDeveloperMenu(struct vb2_context *ctx);
 vb2_error_t VbBootRecoveryMenu(struct vb2_context *ctx);
 
 /**
- * Commit NvStorage.
- *
- * This may be called by UI functions which need to save settings before they
- * sit in an infinite loop waiting for shutdown (this is, by a UI state which
- * will never return).
- */
-void vb2_nv_commit(struct vb2_context *ctx);
-
-/**
  * Reinitialize global state. This should only need to be called by init tests.
  */
 void vb2_init_ui(void);
