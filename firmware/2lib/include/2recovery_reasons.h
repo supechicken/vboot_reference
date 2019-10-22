5b4096f5b9a9d3cc22e203adf65abda12a85deee
Daisuke Nojiri <dnojiri@chromium.org>
2019-10-22 15:37:14 -0700

    EFS: Implement EFS2 and NO_BOOT mode
    EFS v1 allowed Chromeboxes to verify RW without AP. EFS v2 will bring
    the benefts to Chromebooks, which are:
    
    - Reduce RO dependency and presence. Allow more code to be updated
      in the fields.
    - Remove jumptag and workarounds needed for late sysjump.
    
    Major imporvements over v1 are:
    
    - No A/B slot required.
    - No signature in RW or public key in RO.
    - Rollback-attack protection.
    
    For battery-equipped devices, additional benefts are:
    
    - Immediate boot on drained battery.
    - Support recovery mode regardless of battery condition.
    - Faster charge in S5/G3.
    
    EC-Cr50 communication is based on the shared UART (go/ec-cr50-comm).
    EFS2 is documented in go/ec-efs2.
    
    BUG=chromium:1020578,chromium:1045217
    TEST=Boot Helios in NORMAL/RECOVERY/NO_BOOT mode.
    TEST=Verify EC is updated by software sync in Depthcharge.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: Ie07e6f6ce46c0955a6a0adf595633e65c4ffe724

--- firmware/2lib/include/2recovery_reasons.h
+++ firmware/2lib/include/2recovery_reasons.h
@@ -234,6 +234,9 @@ enum vb2_nv_recovery {
 	/* FWMP secure data initialization error */
 	VB2_RECOVERY_SECDATA_FWMP_INIT = 0x62,
 
+	/* Failed to get boot mode from TPM/Cr50 */
+	VB2_RECOVERY_CR50_BOOT_MODE = 0x63,
+
 	/* Unspecified/unknown error in rewritable firmware */
 	VB2_RECOVERY_RW_UNSPECIFIED = 0x7f,
 
