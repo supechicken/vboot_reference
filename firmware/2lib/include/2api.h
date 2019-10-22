d4ff7017ce0a32ca762410b84ff554b7f21c90bb
Daisuke Nojiri <dnojiri@chromium.org>
2019-10-22 15:37:14 -0700

    EFS: Implement EFS2 and NO_BOOT mode
    EFS v1 allowed Chromeboxes to verify RW without AP. EFS v2 will bring
    the benefts to Chromebooks, which are:
    
    - Reduce RO dependency and presence. Allow more code to be updated
      in the fields.
    - Remove jumptag and workarounds needed for late sysjump.
    
    Major imporvements over v1 are:
    
    - No A/B slot required.
    - No signature in RW or public key in RO.
    - Rollback-attack protection.
    
    For battery-equipped devices, additional benefts are:
    
    - Immediate boot on drained battery.
    - Support recovery mode regardless of battery condition.
    - Faster charge in S5/G3.
    
    EC-Cr50 communication is based on the shared UART (go/ec-cr50-comm).
    EFS2 is documented in go/ec-efs2.
    
    BUG=chromium:1020578,chromium:1045217
    TEST=Boot Helios in NORMAL/RECOVERY/NO_BOOT mode.
    TEST=Verify EC is updated by software sync in Depthcharge.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: Ie07e6f6ce46c0955a6a0adf595633e65c4ffe724

--- firmware/2lib/include/2api.h
+++ firmware/2lib/include/2api.h
@@ -192,6 +192,18 @@ enum vb2_context_flags {
 	 * Deprecated with CL:1975390.
 	 */
 	VB2_CONTEXT_DEPRECATED_DETACHABLE_UI = (1 << 22),
+
+	/*
+	 * EC firmware supports early firmware selection ver2; EC verifies its
+	 * RW hash by itself but a reference hash is stored in Cr50. In EFS2,
+	 * EC carries only one RW copy and NO_BOOT flag to handle RW corruption.
+	 */
+	VB2_CONTEXT_EC_EFS2 = (1 << 23),
+
+	/*
+	 * NO_BOOT means the OS is not allowed to boot. Only relevant for EFS2.
+	 */
+	VB2_CONTEXT_NO_BOOT = (1 << 24),
 };
 
 /* Helper for aligning fields in vb2_context. */
@@ -948,6 +960,8 @@ enum vb2_firmware_selection {
 	/* Keep this at the end */
 	VB_SELECT_FIRMWARE_COUNT,
 };
+
+#define EC_EFS_BOOT_FLAG_NO_BOOT	1
 
 /**
  * Sync the Embedded Controller device to the expected version.
