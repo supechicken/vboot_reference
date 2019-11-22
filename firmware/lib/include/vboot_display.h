112a9efa6ad1c17278cb84e7b41556511e16e0e6
Yu-Ping Wu <yupingso@chromium.org>
2019-11-22 14:38:50 +0800

    WIP: groot vboot_reference changes
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: I8db60be21759d2797c972bc5b98710500a1ccba1
    Signed-off-by: Shelley Chen <shchen@google.com>

--- firmware/lib/include/vboot_display.h
+++ firmware/lib/include/vboot_display.h
@@ -13,6 +13,9 @@ vb2_error_t VbDisplayScreen(struct vb2_context *ctx, uint32_t screen, int force,
 vb2_error_t VbDisplayMenu(struct vb2_context *ctx,
 			uint32_t screen, int force, uint32_t selected_index,
 			uint32_t disabled_idx_mask);
+vb2_error_t VbDisplayGroot(struct vb2_context *ctx,
+			uint32_t screen, int force, uint32_t selected_index,
+			uint32_t disabled_idx_mask);
 vb2_error_t VbDisplayDebugInfo(struct vb2_context *ctx);
 vb2_error_t VbCheckDisplayKey(struct vb2_context *ctx, uint32_t key,
 			    const VbScreenData *data);
