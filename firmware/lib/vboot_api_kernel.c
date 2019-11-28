6a16a143de4bb4f9e02dc9618ee98611a6ff2d7f
Joel Kitching <kitching@google.com>
2019-11-28 15:40:17 +0800

    vboot: only clear recovery requests at kernel verification
    Instead of clearing recovery requests early on in firmware
    verification, defer this task until kernel verification has
    begun.
    
    If the system is rebooted for any non-vboot-related reason when
    entering recovery mode (e.g. FSP initialization), the recovery
    request will still be available in nvdata.
    
    Additionally, relocate the reboot triggered by memory training
    into VbSelectAndLoadKernel.
    
    BUG=b:124141368, b:35576380
    TEST=make clean && make runtests
    BRANCH=none
    
    Change-Id: I787e45c7ed4f2bebf570bb9c1a8e9e371f2a040b
    Signed-off-by: Joel Kitching <kitching@google.com>

--- firmware/lib/vboot_api_kernel.c
+++ firmware/lib/vboot_api_kernel.c
@@ -266,15 +266,6 @@ static vb2_error_t vb2_kernel_setup(struct vb2_context *ctx,
 	 */
 	sd->vbsd = shared;
 
-	/*
-	 * If we're in recovery mode just to do memory retraining, all we
-	 * need to do is reboot.
-	 */
-	if (sd->recovery_reason == VB2_RECOVERY_TRAIN_AND_REBOOT) {
-		VB2_DEBUG("Reboot after retraining in recovery.\n");
-		return VBERROR_REBOOT_REQUIRED;
-	}
-
 	/* Fill in params for calls to LoadKernel() */
 	memset(&lkp, 0, sizeof(lkp));
 	lkp.kernel_buffer = kparams->kernel_buffer;
@@ -453,6 +444,29 @@ vb2_error_t VbSelectAndLoadKernel(struct vb2_context *ctx,
 
 	/* Select boot path */
 	if (ctx->flags & VB2_CONTEXT_RECOVERY_MODE) {
+		/*
+		 * Clear recovery request and subcode from nvdata, so that we
+		 * don't get stuck in recovery mode after reboot.  Should be
+		 * called at some point after we are certain the system does
+		 * not require any reboots for non-vboot-related reasons (e.g.
+		 * FSP initialization), and before triggering a reboot to exit
+		 * transient recovery mode (e.g. memory retraining request).
+		 */
+		vb2_nv_set(ctx, VB2_NV_RECOVERY_REQUEST,
+			   VB2_RECOVERY_NOT_REQUESTED);
+		vb2_nv_set(ctx, VB2_NV_RECOVERY_SUBCODE,
+			   VB2_RECOVERY_NOT_REQUESTED);
+
+		/*
+		 * If we're in recovery mode just to do memory retraining, all
+		 * we need to do is reboot.
+		 */
+		if (sd->recovery_reason == VB2_RECOVERY_TRAIN_AND_REBOOT) {
+			VB2_DEBUG("Reboot after retraining in recovery.\n");
+			rv = VBERROR_REBOOT_REQUIRED;
+			goto VbSelectAndLoadKernel_exit;
+		}
+
 		/* Recovery boot.  This has UI. */
 		if (ctx->flags & VB2_CONTEXT_DETACHABLE_UI)
 			rv = VbBootRecoveryMenu(ctx);
