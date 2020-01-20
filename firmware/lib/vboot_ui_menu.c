2f1a1580488105143e3ff4331752a570b3b296c2
Hsuan Ting Chen <roccochen@chromium.org>
2020-01-20 17:21:27 +0800

    vboot: Introduce menu ui
    According to crbug.com/1033815, add files for MENU_UI:
    - vboot_ui_menu.c for entry point functions
    
    In this patch the two entry points for MENU_UI only show VB_SCREEN_BLANK
    and boot from interl disk. Other implementations will be added in
    seperate CLs.
    
    BRANCH=none
    BUG=b:146399181
    TEST=USE="legacy_menu_ui" emerge-nami vboot_reference
    TEST=USE="menu_ui" emerge-nami vboot_reference
    
    Change-Id: I18adee5986ac8a0266e9245ede122c96d72fb72a
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>

---
+++ firmware/lib/vboot_ui_menu.c
@@ -0,0 +1,27 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * High-level firmware wrapper API - user interface for RW firmware
+ */
+
+#include "2common.h"
+#include "vboot_api.h"
+#include "vboot_display.h"
+#include "vboot_kernel.h"
+#include "vboot_ui_common.h"
+
+/* Developer mode entry point. */
+vb2_error_t VbBootDeveloperMenu(struct vb2_context *ctx)
+{
+	VbDisplayScreen(ctx, VB_SCREEN_BLANK, 0, NULL);
+	return VbTryLoadKernel(ctx, VB_DISK_FLAG_FIXED);
+}
+
+/* Recovery mode entry point. */
+vb2_error_t VbBootRecoveryMenu(struct vb2_context *ctx)
+{
+	VbDisplayScreen(ctx, VB_SCREEN_BLANK, 0, NULL);
+	return VbTryLoadKernel(ctx, VB_DISK_FLAG_FIXED);
+}
+
