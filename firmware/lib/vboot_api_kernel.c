c2b935765c8e82ebcfe3830e6cf7cd77b494d5cb
Hsuan Ting Chen <roccochen@chromium.org>
2020-01-20 17:21:27 +0800

    vboot: Introduce menu UI
    According to crbug.com/1033815, add files for MENU_UI:
    - vboot_ui_menu.c for entry point functions
    
    In this patch the two entry points for MENU_UI only show VB_SCREEN_BLANK
    and boot from interl disk. Other implementations will be added in
    seperate CLs.
    
    BRANCH=none
    BUG=b:146399181
    TEST=USE="legacy_clamshell_ui" emerge-nami vboot_reference
    TEST=USE="legacy_menu_ui" emerge-nami vboot_reference
    TEST=USE="menu_ui" emerge-nami vboot_reference
    
    Change-Id: I18adee5986ac8a0266e9245ede122c96d72fb72a
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>

--- firmware/lib/vboot_api_kernel.c
+++ firmware/lib/vboot_api_kernel.c
@@ -384,7 +384,9 @@ vb2_error_t VbSelectAndLoadKernel(struct vb2_context *ctx,
 		}
 
 		/* Recovery boot.  This has UI. */
-		if (LEGACY_MENU_UI)
+		if (MENU_UI)
+			rv = VbBootRecoveryMenu(ctx);
+		else if (LEGACY_MENU_UI)
 			rv = VbBootRecoveryLegacyMenu(ctx);
 		else
 			rv = VbBootRecoveryLegacyClamshell(ctx);
@@ -407,7 +409,9 @@ vb2_error_t VbSelectAndLoadKernel(struct vb2_context *ctx,
 			rv = VBERROR_REBOOT_REQUIRED;
 	} else if (ctx->flags & VB2_CONTEXT_DEVELOPER_MODE) {
 		/* Developer boot.  This has UI. */
-		if (LEGACY_MENU_UI)
+		if (MENU_UI)
+			rv = VbBootDeveloperMenu(ctx);
+		else if (LEGACY_MENU_UI)
 			rv = VbBootDeveloperLegacyMenu(ctx);
 		else
 			rv = VbBootDeveloperLegacyClamshell(ctx);
