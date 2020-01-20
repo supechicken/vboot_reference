3c60be19fefd4efe162fb58c426844b3b9cb96e0
Hsuan Ting Chen <roccochen@chromium.org>
2020-01-20 17:21:27 +0800

    vboot: Introduce menu UI
    According to crbug.com/1033815, add files for MENU_UI:
    - vboot_ui_menu.c for entry point functions
    
    There are three entry points: vb2_developer_menu(),
    vb2_broken_recovery_menu(), and vb2_manual_recovery_menu(). The first
    entry point is for developer mode, while the others are for broken and
    manual recovery mode.
    
    Only booting from internal disk and waiting for a recovery image are
    implemented in this CL. Other functionalities will be added in seperate CLs.
    
    BRANCH=none
    BUG=b:146399181
    TEST=USE="legacy_clamshell_ui" emerge-nami vboot_reference
    TEST=USE="legacy_menu_ui" emerge-nami vboot_reference
    TEST=USE="menu_ui" emerge-nami vboot_reference
    
    Cq-Depend: chromium:2043102
    Change-Id: I18adee5986ac8a0266e9245ede122c96d72fb72a
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>

---
+++ firmware/2lib/include/2ui.h
@@ -0,0 +1,56 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * User interfaces for developer and recovery mode menus.
+ */
+
+/* Delay type (in ms) of developer and recovery mode menu looping */
+#define KEY_DELAY		20	/* Check keyboard inputs */
+
+/*****************************************************************************/
+/* Entry points */
+
+/**
+ * Handle a developer-mode boot using menu UI.
+ *
+ * Enter the developer menu, which provides options to use legacy bootloader,
+ * switch out of developer mode, or by default, continue booting Chrome OS.
+ *
+ * This menu support users to insert external media directly, to press certain
+ * combo key sets, and to choose between menu options via arrow keys and
+ * the enter key (or volume key/power key in detachable devices).
+ *
+ * If a timeout occurs, leave the waiting loop and boot from the default option.
+ *
+ * TODO(roccochen): all menu functionalities
+ *
+ * @param ctx		Vboot context
+ * @returns VB2_SUCCESS, or non-zero error code.
+ */
+vb2_error_t vb2_developer_menu(struct vb2_context *ctx);
+
+/**
+ * Handle a non-manual recovery (BROKEN) using menu UI.
+ *
+ * Enter the recovery menu, which shows that an unrecoverable error was
+ * encountered last boot. This menu loop and wait for the user to reset or
+ * shut down.
+ *
+ * @param ctx		Vboot context
+ * @returns VB2_SUCCESS, or non-zero error code.
+ */
+vb2_error_t vb2_broken_recovery_menu(struct vb2_context *ctx);
+
+/**
+ * Handle a recovery-mode boot using menu UI.
+ *
+ * Enter the recovery menu, which prompts the user to insert recovery media or
+ * navigate the step-by-step recovery flow via keyboards.
+ *
+ * @param ctx		Vboot context
+ * @returns VB2_SUCCESS, or non-zero error code.
+ */
+vb2_error_t vb2_manual_recovery_menu(struct vb2_context *ctx);
+
+
