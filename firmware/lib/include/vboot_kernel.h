ed91922db5886b07b748c964564ade18368735f5
Hsuan Ting Chen <roccochen@chromium.org>
2020-01-20 17:21:27 +0800

    vboot: Introduce menu UI
    According to crbug.com/1033815, add files for MENU_UI:
    - vboot_ui_menu.c for entry point functions
    
    In this patch the two entry points for MENU_UI only show VB_SCREEN_BLANK
    and boot from interl disk. Other implementations will be added in
    seperate CLs.
    
    BRANCH=none
    BUG=b:146399181
    TEST=USE="legacy_clamshell_ui" emerge-nami vboot_reference
    TEST=USE="legacy_menu_ui" emerge-nami vboot_reference
    TEST=USE="menu_ui" emerge-nami vboot_reference
    
    Cq-Depend: chromium:2043102
    Change-Id: I18adee5986ac8a0266e9245ede122c96d72fb72a
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>

--- firmware/lib/include/vboot_kernel.h
+++ firmware/lib/include/vboot_kernel.h
@@ -156,4 +156,46 @@ vb2_error_t VbBootDeveloperLegacyMenu(struct vb2_context *ctx);
  */
 vb2_error_t VbBootRecoveryLegacyMenu(struct vb2_context *ctx);
 
+/**
+ * Handle a developer-mode boot using menu UI.
+ *
+ * Enter the developer menu, which provides options to use legacy bootloader,
+ * switch out of developer mode, or by default, continue booting Chrome OS.
+ *
+ * This menu support users to insert external media directly, to press certain
+ * combo key sets, and to choose between menu options via arrow keys and
+ * the enter key (or volume key/power key in detachable devices).
+ *
+ * If a timeout occurs, leave the waiting loop and boot from the default option.
+ *
+ * TODO(roccochen): all menu functionalities
+ *
+ * @param ctx		Vboot context
+ * @returns VB2_SUCCESS, or non-zero error code.
+ */
+vb2_error_t vb2_developer_menu(struct vb2_context *ctx);
+
+/**
+ * Handle a non-manual recovery (BROKEN) using menu UI.
+ *
+ * Enter the recovery menu, which shows that an unrecoverable error was
+ * encountered last boot. This menu loop and wait for the user to reset or
+ * shut down.
+ *
+ * @param ctx		Vboot context
+ * @returns VB2_SUCCESS, or non-zero error code.
+ */
+vb2_error_t vb2_broken_recovery_menu(struct vb2_context *ctx);
+
+/**
+ * Handle a recovery-mode boot using menu UI.
+ *
+ * Enter the recovery menu, which prompts the user to insert recovery media or
+ * navigate the step-by-step recovery flow via keyboards.
+ *
+ * @param ctx		Vboot context
+ * @returns VB2_SUCCESS, or non-zero error code.
+ */
+vb2_error_t vb2_manual_recovery_menu(struct vb2_context *ctx);
+
 #endif  /* VBOOT_REFERENCE_VBOOT_KERNEL_H_ */
