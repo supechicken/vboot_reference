b89213dab8b0ff6bd5a6a077ee1dba2d485c39f0
dnojiri <dnojiri@chromium.org>
2020-02-06 11:50:46 -0800

    Add vb2_secdata_kernel_v10
    This patch adds vb2_secdata_kernel_v10. It has ec_hash field to support
    EFS2.
    
    BUG=chromium:1045217
    TEST=emerge-hatch depthcharge
    TEST=make runtests
    
    Change-Id: I18d5097ed799e790a2742d54c25c89a7559cbcb2
    Signed-off-by: dnojiri <dnojiri@chromium.org>

--- firmware/2lib/include/2secdata_struct.h
+++ firmware/2lib/include/2secdata_struct.h
@@ -10,6 +10,8 @@
 
 #include "2crc8.h"
 #include "2sysincludes.h"
+#include "2constants.h"
+#include "2sha.h"
 
 /*****************************************************************************/
 /* Firmware secure storage space */
@@ -36,13 +38,29 @@ struct vb2_secdata_firmware {
 /*****************************************************************************/
 /* Kernel secure storage space */
 
+#define MAJOR_VER(x)	(((x) & 0xf0) >> 4)
+#define MINOR_VER(x)	((x) & 0x0f)
+
 /* Kernel space - KERNEL_NV_INDEX, locked with physical presence. */
-#define VB2_SECDATA_KERNEL_VERSION 2
+#define VB2_SECDATA_KERNEL_VERSION_V02 (0 << 4 | 2 << 0)  /* 0.2 */
+#define VB2_SECDATA_KERNEL_VERSION_V10 (1 << 4 | 0 << 0)  /* 1.0 */
 #define VB2_SECDATA_KERNEL_UID 0x4752574c  /* 'LWRG' */
 
-struct vb2_secdata_kernel {
+/*
+ * We'll never convert v0.2 to v1.0 or the other way. v0.2 or v1.0 data will be
+ * passed around between AP and TPM without modification.
+ *
+ * 1. Old BIOS on old device will read/write v0.2 data from/to TPM.
+ * 2. New BIOS on old device will read/write v0.2 data from/to TPM.
+ * 1. Old BIOS on new device will read/write v0.2 data from/to TPM.
+ * 4. New BIOS on new device will read/write v1.0 data from/to TPM.
+ *
+ * Except reading EC hash for EFS2, Cr50 will be transparently handling secdata
+ * (as a blob).
+ */
+struct vb2_secdata_kernel_v02 {
 	/* Struct version, for backwards compatibility */
-	uint8_t struct_version;
+	uint8_t struct_version;  /* 0.2 (or 0x02 in v0 format) */
 
 	/* Unique ID to detect space redefinition */
 	uint32_t uid;
@@ -56,6 +74,38 @@ struct vb2_secdata_kernel {
 	/* CRC; must be last field in struct */
 	uint8_t crc8;
 } __attribute__((packed));
+_Static_assert(sizeof(struct vb2_secdata_kernel_v02)
+	       == VB2_SECDATA_KERNEL_SIZE_V02,
+	       "VB2_SECDATA_KERNEL_SIZE_V02 incorrect");
+_Static_assert(sizeof(struct vb2_secdata_kernel_v02)
+	       < VB2_SECDATA_KERNEL_MAX_SIZE,
+	       "VB2_SECDATA_KERNEL_SIZE_V02 exceeds max size");
+
+struct vb2_secdata_kernel_v10 {
+	/* Struct version, for backwards compatibility */
+	uint8_t struct_version;  /* 1.0 (or 0x10 in v0 format) */
+
+	/* size of the struct */
+	uint8_t struct_size;
+
+	/* 8-bit CRC for everything below */
+	uint8_t crc8;
+
+	/* Reserved for future expansion */
+	uint8_t reserved0;
+
+	/* Kernel versions */
+	uint32_t kernel_versions;
+
+	/* EC hash used for EFS2 */
+	uint8_t ec_hash[VB2_SHA256_DIGEST_SIZE];
+} __attribute__((packed));
+_Static_assert(sizeof(struct vb2_secdata_kernel_v10)
+	       == VB2_SECDATA_KERNEL_SIZE_V10,
+	       "VB2_SECDATA_KERNEL_SIZE_V10 incorrect");
+_Static_assert(sizeof(struct vb2_secdata_kernel_v10)
+	       < VB2_SECDATA_KERNEL_MAX_SIZE,
+	       "VB2_SECDATA_KERNEL_SIZE_V10 exceeds max size");
 
 /*****************************************************************************/
 /* Firmware management parameters (FWMP) space */
--- tests/vb2_secdata_kernel_tests.c
+++ tests/vb2_secdata_kernel_tests.c
@@ -18,7 +18,8 @@ static uint8_t workbuf[VB2_FIRMWARE_WORKBUF_RECOMMENDED_SIZE]
 	__attribute__((aligned(VB2_WORKBUF_ALIGN)));
 static struct vb2_context *ctx;
 static struct vb2_shared_data *sd;
-static struct vb2_secdata_kernel *sec;
+static struct vb2_secdata_kernel_v02 *sec02;
+static struct vb2_secdata_kernel_v10 *sec10;
 
 static void reset_common_data(void)
 {
@@ -28,7 +29,8 @@ static void reset_common_data(void)
 
 	sd = vb2_get_sd(ctx);
 
-	sec = (struct vb2_secdata_kernel *)ctx->secdata_kernel;
+	sec02 = (struct vb2_secdata_kernel_v02 *)ctx->secdata_kernel;
+	sec10 = (struct vb2_secdata_kernel_v10 *)ctx->secdata_kernel;
 }
 
 static void test_changed(struct vb2_context *c, int changed, const char *why)
@@ -41,21 +43,30 @@ static void test_changed(struct vb2_context *c, int changed, const char *why)
 	c->flags &= ~VB2_CONTEXT_SECDATA_KERNEL_CHANGED;
 };
 
+static void secdata_kernel_create_v02(void)
+{
+	memset(sec02, 0, sizeof(*sec02));
+	sec02->struct_version = VB2_SECDATA_KERNEL_VERSION_V02;
+	sec02->crc8 = vb2_secdata_kernel_calc_crc8(ctx);
+	sd->status |= VB2_SD_STATUS_SECDATA_KERNEL_INIT;
+}
+
 static void secdata_kernel_test(void)
 {
-	uint32_t v = 1;
 	reset_common_data();
-
-	/* Check size constant */
-	TEST_EQ(VB2_SECDATA_KERNEL_SIZE, sizeof(struct vb2_secdata_kernel),
-		"Struct size constant");
 
 	/* Blank data is invalid */
 	memset(&ctx->secdata_kernel, 0xa6, sizeof(ctx->secdata_kernel));
+	sec02->struct_version = VB2_SECDATA_KERNEL_VERSION_V02;
 	TEST_EQ(vb2api_secdata_kernel_check(ctx),
-		VB2_ERROR_SECDATA_KERNEL_CRC, "Check blank CRC");
+		VB2_ERROR_SECDATA_KERNEL_CRC, "Check blank CRC (v0.2)");
 	TEST_EQ(vb2_secdata_kernel_init(ctx),
 		VB2_ERROR_SECDATA_KERNEL_CRC, "Init blank CRC");
+	sec10->struct_version = VB2_SECDATA_KERNEL_VERSION_V10;
+	TEST_EQ(vb2api_secdata_kernel_check(ctx),
+		VB2_ERROR_SECDATA_KERNEL_STRUCT_SIZE, "Check blank size (v1)");
+	TEST_EQ(vb2_secdata_kernel_init(ctx),
+		VB2_ERROR_SECDATA_KERNEL_STRUCT_SIZE, "Init blank size");
 
 	/* Ensure zeroed buffers are invalid */
 	memset(&ctx->secdata_kernel, 0, sizeof(ctx->secdata_kernel));
@@ -63,16 +74,15 @@ static void secdata_kernel_test(void)
 		"Zeroed buffer (invalid version)");
 
 	/* Try with bad version */
-	TEST_EQ(vb2api_secdata_kernel_create(ctx), VB2_SECDATA_KERNEL_SIZE,
+	TEST_EQ(vb2api_secdata_kernel_create(ctx), VB2_SECDATA_KERNEL_SIZE_V10,
 		"Create");
-	sec->struct_version -= 1;
-	sec->crc8 = vb2_crc8(sec, offsetof(struct vb2_secdata_kernel, crc8));
+	sec10->struct_version -= 1;
 	TEST_EQ(vb2api_secdata_kernel_check(ctx),
 		VB2_ERROR_SECDATA_KERNEL_VERSION, "Check invalid version");
 	TEST_EQ(vb2_secdata_kernel_init(ctx),
 		VB2_ERROR_SECDATA_KERNEL_VERSION, "Init invalid version");
 
-	/* Create good data */
+	/* Create good data (v1) and corrupt it. */
 	vb2api_secdata_kernel_create(ctx);
 	TEST_SUCC(vb2api_secdata_kernel_check(ctx), "Check created CRC");
 	TEST_SUCC(vb2_secdata_kernel_init(ctx), "Init created CRC");
@@ -80,24 +90,108 @@ static void secdata_kernel_test(void)
 		 "Init set SD status");
 	sd->status &= ~VB2_SD_STATUS_SECDATA_KERNEL_INIT;
 	test_changed(ctx, 1, "Create changes data");
-
-	/* Now corrupt it */
 	ctx->secdata_kernel[2]++;
 	TEST_EQ(vb2api_secdata_kernel_check(ctx),
 		VB2_ERROR_SECDATA_KERNEL_CRC, "Check invalid CRC");
 	TEST_EQ(vb2_secdata_kernel_init(ctx),
 		VB2_ERROR_SECDATA_KERNEL_CRC, "Init invalid CRC");
 
-	/* Make sure UID is checked */
+	/* Create good data (v0.2) and corrupt it. */
+	secdata_kernel_create_v02();
+	ctx->secdata_kernel[2]++;
+	TEST_EQ(vb2api_secdata_kernel_check(ctx),
+		VB2_ERROR_SECDATA_KERNEL_CRC, "Check invalid CRC");
+	TEST_EQ(vb2_secdata_kernel_init(ctx),
+		VB2_ERROR_SECDATA_KERNEL_CRC, "Init invalid CRC");
+}
 
-	vb2api_secdata_kernel_create(ctx);
-	sec->uid++;
-	sec->crc8 = vb2_crc8(sec, offsetof(struct vb2_secdata_kernel, crc8));
-	TEST_EQ(vb2_secdata_kernel_init(ctx), VB2_ERROR_SECDATA_KERNEL_UID,
-		"Init invalid struct UID");
+static void secdata_kernel_access_test_v10(void)
+{
+	uint32_t v = 1;
+	const uint8_t *p;
+	uint8_t ec_hash[VB2_SHA256_DIGEST_SIZE];
+
+	reset_common_data();
 
 	/* Read/write versions */
 	vb2api_secdata_kernel_create(ctx);
+	vb2_secdata_kernel_init(ctx);
+	ctx->flags = 0;
+	v = vb2_secdata_kernel_get(ctx, VB2_SECDATA_KERNEL_VERSIONS);
+	TEST_EQ(v, 0, "Versions created 0");
+	test_changed(ctx, 0, "Get doesn't change data");
+	vb2_secdata_kernel_set(ctx, VB2_SECDATA_KERNEL_VERSIONS, 0x123456ff);
+	test_changed(ctx, 1, "Set changes data");
+	vb2_secdata_kernel_set(ctx, VB2_SECDATA_KERNEL_VERSIONS, 0x123456ff);
+	test_changed(ctx, 0, "Set again doesn't change data");
+	v = vb2_secdata_kernel_get(ctx, VB2_SECDATA_KERNEL_VERSIONS);
+	TEST_EQ(v, 0x123456ff, "Versions changed");
+
+	/* Invalid field fails */
+	TEST_ABORT(vb2_secdata_kernel_get(ctx, -1), "Get invalid");
+	TEST_ABORT(vb2_secdata_kernel_set(ctx, -1, 456), "Set invalid");
+	test_changed(ctx, 0, "Set invalid field doesn't change data");
+
+	/* Read/write uninitialized data fails */
+	sd->status &= ~VB2_SD_STATUS_SECDATA_KERNEL_INIT;
+	TEST_ABORT(vb2_secdata_kernel_get(ctx, VB2_SECDATA_KERNEL_VERSIONS),
+		   "Get uninitialized");
+	test_changed(ctx, 0, "Get uninitialized doesn't change data");
+	TEST_ABORT(vb2_secdata_kernel_set(ctx, VB2_SECDATA_KERNEL_VERSIONS,
+					  0x123456ff),
+		   "Set uninitialized");
+	test_changed(ctx, 0, "Set uninitialized doesn't change data");
+
+	/* Test EC hash set */
+	vb2api_secdata_kernel_create(ctx);
+	vb2_secdata_kernel_init(ctx);
+	memset(ec_hash, 0xaa, sizeof(ec_hash));
+	TEST_SUCC(vb2_secdata_kernel_set_ec_hash(
+			ctx, ec_hash, sizeof(ec_hash)), "Set EC hash");
+	TEST_EQ(memcmp(ec_hash, sec10->ec_hash, sizeof(ec_hash)), 0,
+		       "Check EC hash");
+	test_changed(ctx, 1, "Set EC hash changes data");
+
+	sec10->struct_version = VB2_SECDATA_KERNEL_VERSION_V02;
+	TEST_EQ(vb2_secdata_kernel_set_ec_hash(ctx, ec_hash, sizeof(ec_hash)),
+		VB2_ERROR_SECDATA_KERNEL_STRUCT_VERSION,
+		"Can't set EC hash for v0.2");
+	test_changed(ctx, 0, "Failing to set EC hash doesn't change data");
+	sec10->struct_version = VB2_SECDATA_KERNEL_VERSION_V10;
+
+	sd->status &= ~VB2_SD_STATUS_SECDATA_KERNEL_INIT;
+	TEST_EQ(vb2_secdata_kernel_set_ec_hash(ctx, ec_hash, sizeof(ec_hash)),
+		VB2_ERROR_SECDATA_KERNEL_UNINITIALIZED,
+		"Can't set EC hash before init");
+	sd->status |= VB2_SD_STATUS_SECDATA_KERNEL_INIT;
+
+	TEST_EQ(vb2_secdata_kernel_set_ec_hash(ctx, ec_hash, sizeof(ec_hash)-1),
+		VB2_ERROR_SECDATA_KERNEL_BUFFER_SIZE,
+		"Can't set EC hash of wrong size");
+
+	/* Test EC hash get */
+	p = vb2_secdata_kernel_get_ec_hash(ctx);
+	TEST_PTR_EQ(p, sec10->ec_hash, "Get EC hash returns pointer");
+	test_changed(ctx, 0, "Get EC hash doesn't change data");
+
+	sec10->struct_version = VB2_SECDATA_KERNEL_VERSION_V02;
+	TEST_PTR_EQ(vb2_secdata_kernel_get_ec_hash(ctx), NULL,
+		    "Can't get EC hash for v0.2");
+	sec10->struct_version = VB2_SECDATA_KERNEL_VERSION_V10;
+
+	sd->status &= ~VB2_SD_STATUS_SECDATA_KERNEL_INIT;
+	TEST_PTR_EQ(vb2_secdata_kernel_get_ec_hash(ctx), NULL,
+		    "Can't get EC hash before init");
+	sd->status |= VB2_SD_STATUS_SECDATA_KERNEL_INIT;
+}
+
+static void secdata_kernel_access_test_v02(void)
+{
+	uint32_t v = 1;
+	reset_common_data();
+
+	/* Read/write versions */
+	secdata_kernel_create_v02();
 	vb2_secdata_kernel_init(ctx);
 	ctx->flags = 0;
 	v = vb2_secdata_kernel_get(ctx, VB2_SECDATA_KERNEL_VERSIONS);
@@ -129,6 +223,8 @@ static void secdata_kernel_test(void)
 int main(int argc, char* argv[])
 {
 	secdata_kernel_test();
+	secdata_kernel_access_test_v10();
+	secdata_kernel_access_test_v02();
 
 	return gTestSuccess ? 0 : 255;
 }
