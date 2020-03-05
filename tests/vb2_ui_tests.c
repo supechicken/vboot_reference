974a952115e5bfeeb1a06aa0e870613ed709c11a
Hsuan Ting Chen <roccochen@chromium.org>
2020-03-05 14:57:51 +0800

    vboot: Introduce menu UI
    According to crbug.com/1033815, add files for MENU_UI:
    - 2ui.{c,h} for entry point functions called by kernel selection.
    - vb2_ui_tests.c for tests (normal boot included).
    
    There are three entry points: vb2_developer_menu(),
    vb2_broken_recovery_menu(), and vb2_manual_recovery_menu().
    
    Only infinite loop for this CL, others will be added in separate CLs.
    
    BRANCH=none
    BUG=b:146399181
    TEST=USE="legacy_clamshell_ui" emerge-nami vboot_reference depthcharge
    TEST=USE="legacy_menu_ui" emerge-nami vboot_reference depthcharge
    TEST=USE="menu_ui" emerge-nami vboot_reference depthcharge
    TEST=make runtests
    
    Cq-Depend: chromium:2043102
    Change-Id: I5a23eb006754b5ff08eb42bcd5021374995eab40
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>

---
+++ tests/vb2_ui_tests.c
@@ -0,0 +1,41 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Tests menu UI
+ */
+
+#include "test_common.h"
+
+/* Tests */
+
+/* VbootNormal() tests from vboot_kernel.h */
+static void normal_tests(void)
+{
+	/* TODO(roccochen) */
+}
+
+static void developer_tests(void)
+{
+	/* TODO(roccochen) */
+}
+
+static void broken_recovery_tests(void)
+{
+	/* TODO(roccochen) */
+}
+
+static void manual_recovery_tests(void)
+{
+	/* TODO(roccochen) */
+}
+
+int main(void)
+{
+	normal_tests();
+	developer_tests();
+	broken_recovery_tests();
+	manual_recovery_tests();
+
+	return gTestSuccess ? 0 : 255;
+}
