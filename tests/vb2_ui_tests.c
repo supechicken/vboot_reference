62ca44b4f5ee6639edd430e94f5cf4a93beb41f2
Hsuan Ting Chen <roccochen@chromium.org>
2020-03-10 16:22:53 +0800

    vboot: Implement delay loops for developer menu
    Add config DETACHABLE to control the navigation in menu UI.
    
    Implement delay loops for menu UI developer entry point:
    
    vb2_developer_menu:
    - Wait for keyboard input via vb2_handle_menu_input.
    - Set a timer, exit the loop when timeout.
      - timeout at 30 sec, beep at 20 and 20.5 sec.
      - timeout at 2 sec if short delay is set.
    - Handle combo key sets:
      - Enter = shutdown request.
      - Ctrl+D = boot from internal.
      - Ctrl+L = boot legacy BIOS with the default payload.
      - 0-9 = boot legacy BIOS with a specific payload name.
    - For DETACHABLE:
      - VB_BUTTON_VOL_DOWN_LONG_PRESS = boot form internal.
    
    Other entries and inputs will be handled in separated CLs.
    
    BRANCH=none
    BUG=b:146399181, chromium:1033815
    TEST=USE="menu_ui" emerge-nami vboot_reference depthcharge
    TEST=USE="menu_ui detachable" emerge-nami vboot_reference depthcharge
    TEST=make runtests
    
    Change-Id: I55d0211f7e5d34e6d24a9129324c42bcf1a81101
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>

--- tests/vb2_ui_tests.c
+++ tests/vb2_ui_tests.c
@@ -18,10 +18,21 @@
 static uint8_t workbuf[VB2_KERNEL_WORKBUF_RECOMMENDED_SIZE]
 	__attribute__((aligned(VB2_WORKBUF_ALIGN)));
 static struct vb2_context *ctx;
+static struct vb2_gbb_header gbb;
+
+static uint32_t mock_keypress[64];
+static uint32_t mock_keyflags[64];
+static uint32_t mock_keypress_count;
+static uint32_t mock_keypress_total;
 
 static enum vb2_screen mock_screens_displayed[64];
 static uint32_t mock_locales_displayed[64];
-static uint32_t mock_screens_count = 0;
+static uint32_t mock_screens_count;
+
+static uint64_t mock_get_timer_last_retval[2];  /* check if finished late */
+static uint64_t mock_time;
+static uint64_t mock_time_fixed = (31ULL * VB_USEC_PER_SEC);
+static int mock_vbexbeep_called;
 
 static enum vb2_dev_default_boot mock_default_boot;
 static int mock_dev_boot_allowed;
@@ -29,12 +40,31 @@ static int mock_dev_boot_legacy_allowed;
 static int mock_dev_boot_usb_allowed;
 
 static int mock_vbexlegacy_called;
+static vb2_error_t mock_vbexlegacy_retval;
 static enum VbAltFwIndex_t mock_altfw_num;
 
 static vb2_error_t mock_vbtlk_retval[5];
 static uint32_t mock_vbtlk_expected_flag[5];
 static int mock_vbtlk_count;
 static int mock_vbtlk_total;
+
+static void add_mock_key(uint32_t press, uint32_t flags)
+{
+	if (mock_keypress_total >= ARRAY_SIZE(mock_keypress) ||
+	    mock_keypress_total >= ARRAY_SIZE(mock_keyflags)) {
+		TEST_TRUE(0, "Test failed as mock_key ran out of entries!");
+		return;
+	}
+
+	mock_keypress[mock_keypress_total] = press;
+	mock_keyflags[mock_keypress_total] = flags;
+	mock_keypress_total++;
+}
+
+static void add_mock_keypress(uint32_t press)
+{
+	add_mock_key(press, 0);
+}
 
 static void add_mock_vbtlk(vb2_error_t retval, uint32_t get_info_flags)
 {
@@ -54,10 +84,23 @@ static void reset_common_data()
 {
 	TEST_SUCC(vb2api_init(workbuf, sizeof(workbuf), &ctx),
 		  "vb2api_init failed");
+
+	memset(&gbb, 0, sizeof(gbb));
+
 	vb2_nv_init(ctx);
+
+	memset(mock_keypress, 0, sizeof(mock_keypress));
+	memset(mock_keyflags, 0, sizeof(mock_keyflags));
+	mock_keypress_count = 0;
+	mock_keypress_total = 0;
 
 	memset(mock_screens_displayed, 0, sizeof(mock_screens_displayed));
 	mock_screens_count = 0;
+
+	memset(mock_get_timer_last_retval, 0,
+	       sizeof(mock_get_timer_last_retval));
+	mock_time = mock_time_fixed;
+	mock_vbexbeep_called = 0;
 
 	mock_default_boot = VB2_DEV_DEFAULT_BOOT_DISK;
 	mock_dev_boot_allowed = 1;
@@ -65,6 +108,7 @@ static void reset_common_data()
 	mock_dev_boot_usb_allowed = 0;
 
 	mock_vbexlegacy_called = 0;
+	mock_vbexlegacy_retval = VB2_SUCCESS;
 	mock_altfw_num = -100;
 
 	memset(mock_vbtlk_retval, 0, sizeof(mock_vbtlk_retval));
@@ -74,6 +118,46 @@ static void reset_common_data()
 }
 
 /* Mock functions */
+
+struct vb2_gbb_header *vb2_get_gbb(struct vb2_context *c)
+{
+	return &gbb;
+}
+
+uint32_t VbExKeyboardRead(void)
+{
+	return VbExKeyboardReadWithFlags(NULL);
+}
+
+uint32_t VbExKeyboardReadWithFlags(uint32_t *key_flags)
+{
+	if (mock_keypress_count < mock_keypress_total) {
+		if (key_flags != NULL)
+			*key_flags = mock_keyflags[mock_keypress_count];
+		return mock_keypress[mock_keypress_count++];
+	}
+
+	return 0;
+}
+
+uint64_t VbExGetTimer(void)
+{
+	mock_get_timer_last_retval[1] = mock_get_timer_last_retval[0];
+	mock_get_timer_last_retval[0] = mock_time;
+
+	return mock_time;
+}
+
+void VbExSleepMs(uint32_t msec)
+{
+	mock_time += msec * VB_USEC_PER_MSEC;
+}
+
+vb2_error_t VbExBeep(uint32_t msec, uint32_t frequency)
+{
+	mock_vbexbeep_called++;
+	return VB2_SUCCESS;
+}
 
 enum vb2_dev_default_boot vb2_get_dev_boot_target(struct vb2_context *c)
 {
@@ -100,7 +184,7 @@ vb2_error_t VbExLegacy(enum VbAltFwIndex_t altfw_num)
 	mock_vbexlegacy_called++;
 	mock_altfw_num = altfw_num;
 
-	return VB2_SUCCESS;
+	return mock_vbexlegacy_retval;
 }
 
 vb2_error_t VbTryLoadKernel(struct vb2_context *c, uint32_t get_info_flags)
@@ -139,7 +223,10 @@ vb2_error_t vb2ex_display_ui(enum vb2_screen screen, uint32_t locale)
 
 static void developer_tests(void)
 {
-	/* Proceed */
+	int i;
+	char test_name[256];
+
+	/* Proceed after timeout */
 	reset_common_data();
 	add_mock_vbtlk(VB2_SUCCESS, VB_DISK_FLAG_FIXED);
 	TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS, "proceed");
@@ -148,9 +235,56 @@ static void developer_tests(void)
 	TEST_EQ(mock_screens_count, 1, "  no extra screens");
 	TEST_EQ(vb2_nv_get(ctx, VB2_NV_RECOVERY_REQUEST), 0,
 		"  recovery reason");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed >=
+		  30 * VB_USEC_PER_SEC, "  finished delay");
+	TEST_TRUE(mock_get_timer_last_retval[1] - mock_time_fixed <
+		  30 * VB_USEC_PER_SEC, "  not finished too late");
+	TEST_EQ(mock_vbexbeep_called, 2, "  beep twice");
 	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
 
-	/* Proceed to legacy */
+	/* Proceed after short delay */
+	reset_common_data();
+	add_mock_vbtlk(VB2_SUCCESS, VB_DISK_FLAG_FIXED);
+	gbb.flags |= VB2_GBB_FLAG_DEV_SCREEN_SHORT_DELAY;
+	TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS, "proceed");
+	TEST_EQ(mock_screens_displayed[0], VB2_SCREEN_BLANK,
+		"  final blank screen");
+	TEST_EQ(mock_screens_count, 1, "  no extra screens");
+	TEST_EQ(vb2_nv_get(ctx, VB2_NV_RECOVERY_REQUEST), 0,
+		"  recovery reason");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed >=
+		  2 * VB_USEC_PER_SEC, "  finished short delay");
+	TEST_TRUE(mock_get_timer_last_retval[1] - mock_time_fixed <
+		  2 * VB_USEC_PER_SEC, "  not finished too late");
+	TEST_EQ(mock_vbexbeep_called, 0, "  no beep for short delay twice");
+	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
+
+	/* Reset timer whenever seeing a new key */
+	reset_common_data();
+	add_mock_keypress('A');  /* Not a shortcut key */
+	add_mock_keypress('A');  /* Need two keys since it read before slept */
+	add_mock_vbtlk(VB2_SUCCESS, VB_DISK_FLAG_FIXED);
+	TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS,
+		"Timeout after seeing a key");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed >=
+		  30 * VB_USEC_PER_SEC, "  finished delay");
+	TEST_TRUE(mock_get_timer_last_retval[1] - mock_time_fixed >=
+		  30 * VB_USEC_PER_SEC, "  finished delay a little later");
+	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
+
+	/* Use normal delay after seeing a new key even if GBB is set */
+	reset_common_data();
+	add_mock_keypress('A');  /* Not a shortcut key */
+	add_mock_keypress('A');  /* Need two keys since it read before slept */
+	add_mock_vbtlk(VB2_SUCCESS, VB_DISK_FLAG_FIXED);
+	gbb.flags |= VB2_GBB_FLAG_DEV_SCREEN_SHORT_DELAY;
+	TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS,
+		"Use normal delay even if GBB is set");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed >=
+		  30 * VB_USEC_PER_SEC, "  finished normal delay");
+	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
+
+	/* Proceed to legacy after timeout */
 	reset_common_data();
 	mock_default_boot = VB2_DEV_DEFAULT_BOOT_LEGACY;
 	mock_dev_boot_legacy_allowed = 1;
@@ -160,6 +294,11 @@ static void developer_tests(void)
 	TEST_EQ(mock_screens_displayed[0], VB2_SCREEN_BLANK,
 		"  final blank screen");
 	TEST_EQ(mock_screens_count, 1, "  no extra screens");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed >=
+		  30 * VB_USEC_PER_SEC, "  finished delay");
+	TEST_TRUE(mock_get_timer_last_retval[1] - mock_time_fixed <
+		  30 * VB_USEC_PER_SEC, "  not finished too late");
+	TEST_EQ(mock_vbexbeep_called, 2, "  beep twice");
 	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
 
 	/* Proceed to legacy only if enabled */
@@ -174,9 +313,35 @@ static void developer_tests(void)
 	TEST_EQ(mock_screens_count, 1, "  no extra screens");
 	TEST_EQ(vb2_nv_get(ctx, VB2_NV_RECOVERY_REQUEST), 0,
 		"  no recovery");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed >=
+		  30 * VB_USEC_PER_SEC, "  finished delay");
+	TEST_TRUE(mock_get_timer_last_retval[1] - mock_time_fixed <
+		  30 * VB_USEC_PER_SEC, "  not finished too late");
+	TEST_EQ(mock_vbexbeep_called, 2, "  beep twice");
 	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
 
-	/* Proceed to usb */
+	/* If legacy failed, tries fixed disk */
+	reset_common_data();
+	add_mock_vbtlk(VB2_SUCCESS, VB_DISK_FLAG_FIXED);
+	mock_default_boot = VB2_DEV_DEFAULT_BOOT_LEGACY;
+	mock_dev_boot_legacy_allowed = 1;
+	mock_vbexlegacy_retval = VB2_ERROR_MOCK;
+	TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS,
+		"legacy failed");
+	TEST_EQ(mock_vbexlegacy_called, 1, "  try legacy");
+	TEST_EQ(mock_screens_displayed[0], VB2_SCREEN_BLANK,
+		"  final blank screen");
+	TEST_EQ(mock_screens_count, 1, "  no extra screens");
+	TEST_EQ(vb2_nv_get(ctx, VB2_NV_RECOVERY_REQUEST), 0,
+		"  no recovery");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed >=
+		  30 * VB_USEC_PER_SEC, "  finished delay");
+	TEST_TRUE(mock_get_timer_last_retval[1] - mock_time_fixed <
+		  30 * VB_USEC_PER_SEC, "  not finished too late");
+	TEST_EQ(mock_vbexbeep_called, 2, "  beep twice");
+	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
+
+	/* Proceed to usb after timeout */
 	reset_common_data();
 	add_mock_vbtlk(VB2_SUCCESS, VB_DISK_FLAG_REMOVABLE);
 	mock_default_boot = VB2_DEV_DEFAULT_BOOT_USB;
@@ -185,6 +350,11 @@ static void developer_tests(void)
 	TEST_EQ(mock_screens_displayed[0], VB2_SCREEN_BLANK,
 		"  final blank screen");
 	TEST_EQ(mock_screens_count, 1, "  no extra screens");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed >=
+		  30 * VB_USEC_PER_SEC, "  finished delay");
+	TEST_TRUE(mock_get_timer_last_retval[1] - mock_time_fixed <
+		  30 * VB_USEC_PER_SEC, "  not finished too late");
+	TEST_EQ(mock_vbexbeep_called, 2, "  beep twice");
 	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
 
 	/* Proceed to usb only if enabled */
@@ -198,7 +368,157 @@ static void developer_tests(void)
 	TEST_EQ(mock_screens_count, 1, "  no extra screens");
 	TEST_EQ(vb2_nv_get(ctx, VB2_NV_RECOVERY_REQUEST), 0,
 		"  no recovery");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed >=
+		  30 * VB_USEC_PER_SEC, "  finished delay");
+	TEST_TRUE(mock_get_timer_last_retval[1] - mock_time_fixed <
+		  30 * VB_USEC_PER_SEC, "  not finished too late");
+	TEST_EQ(mock_vbexbeep_called, 2, "  beep twice");
 	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
+
+	/* If no usb, tries fixed disk */
+	reset_common_data();
+	add_mock_vbtlk(VB2_ERROR_LK, VB_DISK_FLAG_REMOVABLE);
+	add_mock_vbtlk(VB2_SUCCESS, VB_DISK_FLAG_FIXED);
+	mock_default_boot = VB2_DEV_DEFAULT_BOOT_USB;
+	mock_dev_boot_usb_allowed = 1;
+	TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS,
+		"  default usb with no disk");
+	TEST_EQ(mock_vbexlegacy_called, 0, "  not legacy");
+	TEST_EQ(mock_screens_displayed[0], VB2_SCREEN_BLANK,
+		"  final blank screen");
+	TEST_EQ(mock_screens_count, 1, "  no extra screens");
+	TEST_EQ(vb2_nv_get(ctx, VB2_NV_RECOVERY_REQUEST), 0,
+		"  no recovery");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed >=
+		  30 * VB_USEC_PER_SEC, "  finished delay");
+	TEST_TRUE(mock_get_timer_last_retval[1] - mock_time_fixed <
+		  30 * VB_USEC_PER_SEC, "  not finished too late");
+	TEST_EQ(mock_vbexbeep_called, 2, "  beep twice");
+	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
+
+	/* Enter = shutdown requested in loop */
+	reset_common_data();
+	add_mock_keypress(VB_KEY_ENTER);
+	TEST_EQ(vb2_developer_menu(ctx), VBERROR_SHUTDOWN_REQUESTED,
+		"shutdown requested");
+	TEST_EQ(mock_vbexlegacy_called, 0, "  not legacy");
+	TEST_EQ(mock_screens_displayed[0], VB2_SCREEN_BLANK,
+		"  final blank screen");
+	TEST_EQ(mock_screens_count, 1, "  no extra screens");
+	TEST_EQ(vb2_nv_get(ctx, VB2_NV_RECOVERY_REQUEST), 0,
+		"  no recovery");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed <
+		  30 * VB_USEC_PER_SEC, "  delay loop aborted");
+	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
+
+
+	/* Ctrl+D = boot from internal in loop */
+	reset_common_data();
+	add_mock_keypress(VB_KEY_CTRL('D'));
+	add_mock_vbtlk(VB2_SUCCESS, VB_DISK_FLAG_FIXED);
+	TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS, "Ctrl+D");
+	TEST_EQ(mock_vbexlegacy_called, 0, "  not legacy");
+	TEST_EQ(mock_screens_displayed[0], VB2_SCREEN_BLANK,
+		"  final blank screen");
+	TEST_EQ(mock_screens_count, 1, "  no extra screens");
+	TEST_EQ(vb2_nv_get(ctx, VB2_NV_RECOVERY_REQUEST), 0,
+		"  no recovery");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed <
+		  30 * VB_USEC_PER_SEC, "  delay loop aborted");
+	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
+
+	/* Ctrl+D doesn't boot legacy even if default boot specified */
+	reset_common_data();
+	add_mock_keypress(VB_KEY_CTRL('D'));
+	add_mock_vbtlk(VB2_SUCCESS, VB_DISK_FLAG_FIXED);
+	mock_default_boot = VB2_DEV_DEFAULT_BOOT_LEGACY;
+	mock_dev_boot_legacy_allowed = 1;
+	TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS, "Ctrl+D no legacy");
+	TEST_EQ(mock_vbexlegacy_called, 0, "  not legacy");
+	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
+
+	/* DETACHABLE volume-down long press shortcut acts like Ctrl+D */
+	if (DETACHABLE) {
+		reset_common_data();
+		add_mock_keypress(VB_BUTTON_VOL_DOWN_LONG_PRESS);
+		add_mock_vbtlk(VB2_SUCCESS, VB_DISK_FLAG_FIXED);
+		TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS,
+			"DETACHABLE volume-down long press");
+		TEST_EQ(mock_vbexlegacy_called, 0, "  not legacy");
+		TEST_EQ(mock_screens_displayed[0], VB2_SCREEN_BLANK,
+			"  final blank screen");
+		TEST_EQ(mock_screens_count, 1, "  no extra screens");
+		TEST_EQ(vb2_nv_get(ctx, VB2_NV_RECOVERY_REQUEST), 0,
+			"  no recovery");
+		TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed <
+			  30 * VB_USEC_PER_SEC, "  delay loop aborted");
+		TEST_EQ(mock_vbtlk_count, mock_vbtlk_total,
+			"  used up mock_vbtlk");
+	}
+
+	/* Ctrl+L tries legacy boot mode only if enabled */
+	reset_common_data();
+	add_mock_keypress(VB_KEY_CTRL('L'));
+	add_mock_vbtlk(VB2_SUCCESS, VB_DISK_FLAG_FIXED);
+	TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS, "Ctrl+L disabled");
+	TEST_EQ(mock_vbexlegacy_called, 0, "  not legacy");
+	TEST_EQ(mock_screens_displayed[0], VB2_SCREEN_BLANK,
+		"  final blank screen");
+	TEST_EQ(mock_screens_count, 1, "  no extra screens");
+	TEST_EQ(vb2_nv_get(ctx, VB2_NV_RECOVERY_REQUEST), 0,
+		"  no recovery");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed >=
+		  30 * VB_USEC_PER_SEC, "  finished delay");
+	TEST_TRUE(mock_get_timer_last_retval[1] - mock_time_fixed <
+		  30 * VB_USEC_PER_SEC, "  not finished too late");
+	TEST_EQ(mock_vbexbeep_called, 2, "  beep twice");
+	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
+
+	/* Ctrl+L = boot legacy if enabled */
+	reset_common_data();
+	add_mock_keypress(VB_KEY_CTRL('L'));
+	mock_dev_boot_legacy_allowed = 1;
+	TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS, "Ctrl+L");
+	TEST_EQ(mock_vbexlegacy_called, 1, "  try legacy");
+	TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed <
+		  30 * VB_USEC_PER_SEC, "  delay loop aborted");
+	TEST_EQ(mock_vbtlk_count, mock_vbtlk_total, "  used up mock_vbtlk");
+
+	/* 0...9 = boot alternative firmware */
+	for (i = 0; i <= 9; i++) {
+		/* disabled */
+		sprintf(test_name, "key %d disabled", i);
+		reset_common_data();
+		add_mock_keypress('0' + i);
+		add_mock_vbtlk(VB2_SUCCESS, VB_DISK_FLAG_FIXED);
+		TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS, test_name);
+		TEST_EQ(mock_vbexlegacy_called, 0, "  not legacy");
+		TEST_EQ(mock_screens_displayed[0], VB2_SCREEN_BLANK,
+			"  final blank screen");
+		TEST_EQ(mock_screens_count, 1, "  no extra screens");
+		TEST_EQ(vb2_nv_get(ctx, VB2_NV_RECOVERY_REQUEST), 0,
+			"  no recovery");
+		TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed >=
+			  30 * VB_USEC_PER_SEC, "  finished delay");
+		TEST_TRUE(mock_get_timer_last_retval[1] - mock_time_fixed <
+			  30 * VB_USEC_PER_SEC, "  not finished too late");
+		TEST_EQ(mock_vbexbeep_called, 2, "  beep twice");
+		TEST_EQ(mock_vbtlk_count, mock_vbtlk_total,
+			"  used up mock_vbtlk");
+
+		/* enabled */
+		sprintf(test_name, "key %d", i);
+		reset_common_data();
+		add_mock_keypress('0' + i);
+		mock_dev_boot_legacy_allowed = 1;
+		TEST_EQ(vb2_developer_menu(ctx), VB2_SUCCESS, test_name);
+		TEST_EQ(mock_vbexlegacy_called, 1, "  try legacy");
+		TEST_EQ(mock_altfw_num, i, "  check altfw_num");
+		TEST_TRUE(mock_get_timer_last_retval[0] - mock_time_fixed <
+			  30 * VB_USEC_PER_SEC, "  delay loop aborted");
+		TEST_EQ(mock_vbtlk_count, mock_vbtlk_total,
+			"  used up mock_vbtlk");
+	}
 }
 
 static void broken_recovery_tests(void)
