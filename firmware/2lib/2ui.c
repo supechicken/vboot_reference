67eaf5520fcbb88d6b9b226ba9b67286061959c4
Hsuan Ting Chen <roccochen@chromium.org>
2020-03-10 16:22:53 +0800

    vboot: Implement delay loops for menu UI entries
    Implement delay loops for three menu UI entry points:
    
    Common:
    - Wait for keyboard input via vb2_handle_menu_input
    
    vb2_developer_menu:
    - Set a timer via vboot_audio, exit the loop while a timeout occurred.
    - Handle combo key sets:
      - Ctrl+D = boot from internal.
    
    Other inputs will be handled in separated CLs.
    
    BRANCH=none
    BUG=b:146399181, chromium:1033815
    TEST=USE="menu_ui" emerge-nami vboot_reference depthcharge
    TEST=make runtests
    
    Change-Id: I55d0211f7e5d34e6d24a9129324c42bcf1a81101
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>

--- firmware/2lib/2ui.c
+++ firmware/2lib/2ui.c
@@ -12,54 +12,160 @@
 #include "2return_codes.h"
 #include "2secdata.h"
 #include "2ui.h"
+#include "vboot_audio.h"
 #include "vboot_kernel.h"
+
+/* Delay type (in ms) of developer and recovery mode menu looping */
+#define KEY_DELAY		20	/* Check keyboard inputs */
+
+/*****************************************************************************/
+/* Menu actions */
+
+static vb2_error_t vb2_handle_menu_input(struct vb2_context *ctx,
+					 uint32_t key, uint32_t key_flags)
+{
+	/* TODO(roccochen): handle keyboard input */
+
+	switch (key) {
+	case 0:
+		/* nothing pressed */
+		break;
+	case VB_KEY_ENTER:
+		return VBERROR_SHUTDOWN_REQUESTED;
+	default:
+		VB2_DEBUG("pressed key 0x%x\n", key);
+	}
+
+	return VBERROR_KEEP_LOOPING;
+}
 
 /*****************************************************************************/
 /* Entry points */
 
 vb2_error_t vb2_developer_menu(struct vb2_context *ctx)
 {
-	enum vb2_dev_default_boot default_boot;
+	vb2_error_t rv;
 
-	/* TODO(roccochen): Init, wait for user, and boot. */
+	/* TODO(roccochen): Init menus. */
 	vb2ex_display_ui(VB2_SCREEN_BLANK, 0);
 
-	/* If dev mode was disabled, loop forever. */
-	if (!vb2_dev_boot_allowed(ctx))
-		while (1);
+	/* Get audio/delay context */
+	vb2_audio_start(ctx);
 
-	/* Boot from the default option. */
-	default_boot = vb2_get_dev_boot_target(ctx);
+	/* We'll loop until we finish the delay or are interrupted. */
+	do {
+		/* TODO(roccochen): Make sure user knows dev mode disabled */
+		/* TODO(roccochen): Check if booting from usb */
 
-	/* Boot legacy does not return on success */
-	if (default_boot == VB2_DEV_DEFAULT_BOOT_LEGACY &&
-	    vb2_dev_boot_legacy_allowed(ctx))
-		VbExLegacy(VB_ALTFW_DEFAULT);
+		/* Scan keyboard inputs. */
+		uint32_t key = VbExKeyboardRead();
 
-	if (default_boot == VB2_DEV_DEFAULT_BOOT_USB &&
-	    vb2_dev_boot_usb_allowed(ctx) &&
-	    VbTryLoadKernel(ctx, VB_DISK_FLAG_REMOVABLE) == VB2_SUCCESS)
-		return VB2_SUCCESS;
+		/*
+		 * TODO(roccochen): handle the following combo key sets
+		 *
+		 * Valid combo key sets:
+		 * Ctrl+L = boot alternative bootloader
+		 * 0...9 = allow selection of the default '0' bootloader
+		 *
+		 * Valid combo press (for DETACHABLE):
+		 * VOL_DOWN_LONG_PRESS = boot from internal disk
+		 */
+		if (key == VB_KEY_CTRL('D')) {
+			if (vb2_dev_boot_allowed(ctx))
+				rv = VbTryLoadKernel(ctx, VB_DISK_FLAG_FIXED);
+			else
+				rv = VBERROR_KEEP_LOOPING;
+		} else {
+			rv = vb2_handle_menu_input(ctx, key, 0);
+		}
 
-	return VbTryLoadKernel(ctx, VB_DISK_FLAG_FIXED);
+		/* Have loaded a kernel or decided to shut down now. */
+		if (rv != VBERROR_KEEP_LOOPING)
+			break;
+
+		/* Reset 30 second timer whenever we see a new key. */
+		if (key != 0)
+			vb2_audio_start(ctx);
+
+		VbExSleepMs(KEY_DELAY);
+
+		/* If dev mode was disabled, loop forever */
+	} while (!vb2_dev_boot_allowed(ctx) || vb2_audio_looping());
+
+	/* Timeout, boot from the default option. */
+	if (rv == VBERROR_KEEP_LOOPING) {
+
+		enum vb2_dev_default_boot default_boot =
+			vb2_get_dev_boot_target(ctx);
+
+		/* Boot legacy does not return on success */
+		if (default_boot == VB2_DEV_DEFAULT_BOOT_LEGACY &&
+		    vb2_dev_boot_legacy_allowed(ctx) && vb2ex_commit_data(ctx))
+			VbExLegacy(VB_ALTFW_DEFAULT);
+
+		if (default_boot == VB2_DEV_DEFAULT_BOOT_USB &&
+		    vb2_dev_boot_usb_allowed(ctx) &&
+		    VbTryLoadKernel(ctx, VB_DISK_FLAG_REMOVABLE) == VB2_SUCCESS)
+			return VB2_SUCCESS;
+
+		return VbTryLoadKernel(ctx, VB_DISK_FLAG_FIXED);
+	}
+
+	return rv;
 }
 
 vb2_error_t vb2_broken_recovery_menu(struct vb2_context *ctx)
 {
-	/* TODO(roccochen): Init and wait for user to reset or shutdown. */
+	vb2_error_t rv;
+
+	/* TODO(roccochen): Init menus. */
 	vb2ex_display_ui(VB2_SCREEN_BLANK, 0);
 
-	while (1);
+	/* Loop and wait for the user to reset or shut down. */
+	VB2_DEBUG("waiting for manual recovery\n");
+	while (1) {
+		uint32_t key = VbExKeyboardRead();
+		rv = vb2_handle_menu_input(ctx, key, 0);
+		if (rv != VBERROR_KEEP_LOOPING)
+			break;
+	}
 
-	return VB2_SUCCESS;
+	return rv;
 }
 
 vb2_error_t vb2_manual_recovery_menu(struct vb2_context *ctx)
 {
-	/* TODO(roccochen): Init and wait for user. */
+	vb2_error_t rv;
+
+	/* TODO(roccochen): Init menus. */
 	vb2ex_display_ui(VB2_SCREEN_BLANK, 0);
 
-	while (1);
+	/* Loop and wait for a recovery image or keyboard inputs */
+	VB2_DEBUG("waiting for a recovery image or keyboard inputs\n");
+	while(1) {
+		/* TODO(roccochen): try load usb and check if usb good */
 
-	return VB2_SUCCESS;
+		/* Scan keyboard inputs. */
+		uint32_t key, key_flags;
+		key = VbExKeyboardReadWithFlags(&key_flags);
+
+		/*
+		 * TODO(roccochen): handle the following combo key sets
+		 *
+		 * Valid combo key sets:
+		 * Ctrl+D = enter to developer menu if keyboard is
+		 * trusted
+		 *
+		 * Valid combo press (for DETACHABLE):
+		 * VOL_UP_DOWN_COMBO_PRESS = enter to developer menu if
+		 * keyboard is trusted
+		 */
+		rv = vb2_handle_menu_input(ctx, key, key_flags);
+		if (rv != VBERROR_KEEP_LOOPING)
+			break;
+
+		VbExSleepMs(KEY_DELAY);
+	}
+
+	return rv;
 }
