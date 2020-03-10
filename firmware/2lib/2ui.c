df97273cc1277b3a3bf70504c611846ace8c5b68
Hsuan Ting Chen <roccochen@chromium.org>
2020-03-10 14:47:38 +0800

    vboot: Implement dev mode boot options for menu UI
    Implement parts of vb2_developer_menu functionalities:
    - Check the flags to determine if developer mode is allowed.
    - If allowed, try to boot from gbb-header-specified boot options.
    - Booting from internal disk if allowed.
    
    BRANCH=none
    BUG=b:146399181, chromium:1033815
    TEST=USE="menu_ui" emerge-nami vboot_reference depthcharge
    TEST=make runtests
    
    Change-Id: Ie82076f93b86ba5abe26a9e3e25076892684855d
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>

--- firmware/2lib/2ui.c
+++ firmware/2lib/2ui.c
@@ -7,19 +7,70 @@
 
 #include "2api.h"
 #include "2common.h"
+#include "2misc.h"
+#include "2nvstorage.h"
+#include "2return_codes.h"
+#include "2secdata.h"
 #include "2ui.h"
+#include "vboot_kernel.h"
+
+/*****************************************************************************/
+/* Menu actions */
+
+/* Boot from internal disk if allowed. */
+static vb2_error_t boot_from_internal_action(struct vb2_context *ctx)
+{
+	if (!vb2_dev_boot_allowed(ctx)) {
+		/* TODO(roccochen): flash screen then show error notify */
+		return VBERROR_KEEP_LOOPING;
+	}
+	VB2_DEBUG("trying fixed disk\n");
+
+	return VbTryLoadKernel(ctx, VB_DISK_FLAG_FIXED);
+}
+
+/* Boot legacy BIOS if allowed and available. */
+static vb2_error_t boot_legacy_action(struct vb2_context *ctx)
+{
+	/* TODO(roccochen) */
+	return VBERROR_KEEP_LOOPING;
+}
+
+/* Boot from USB or SD card if allowed and available. */
+static vb2_error_t boot_usb_action(struct vb2_context *ctx)
+{
+	/* TODO(roccochen) */
+	return VBERROR_KEEP_LOOPING;
+}
 
 /*****************************************************************************/
 /* Entry points */
 
 vb2_error_t vb2_developer_menu(struct vb2_context *ctx)
 {
+	vb2_error_t rv;
+	enum vb2_dev_default_boot default_boot;
+
 	/* TODO(roccochen): Init, wait for user, and boot. */
 	vb2ex_display_ui(VB2_SCREEN_BLANK, 0);
 
-	while (1);
+	/* If dev mode was disabled, loop forever. */
+	if (!vb2_dev_boot_allowed(ctx))
+		while (1);
 
-	return VB2_SUCCESS;
+	/* Boot from the default option. */
+	default_boot = vb2_get_dev_default_boot_target(ctx);
+
+	/* Boot legacy does not return on success */
+	if (default_boot == VB2_DEV_DEFAULT_BOOT_LEGACY)
+		boot_legacy_action(ctx);
+	if (default_boot == VB2_DEV_DEFAULT_BOOT_USB &&
+	    boot_usb_action(ctx) == VB2_SUCCESS)
+		rv = VB2_SUCCESS;
+	else
+		rv = boot_from_internal_action(ctx);
+
+	return rv;
 }
 
 vb2_error_t vb2_broken_recovery_menu(struct vb2_context *ctx)
