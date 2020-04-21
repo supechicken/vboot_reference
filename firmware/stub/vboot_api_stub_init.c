4924ab3eac7f931db36eb882864e95a806b0f044
Joel Kitching <kitching@google.com>
2020-04-21 12:50:03 +0800

    vboot: move timer and beep functions to vboot2 namespace
    Move these functions from vboot_api.h into 2api.h:
      VbExGetTimer --> vb2ex_mtime (usec -> msec)
      VbExSleepMs --> vb2ex_msleep
      VbExBeep --> vb2ex_beep
    
    Remove these constants:
      VB2_USEC_PER_MSEC
      VB2_MSEC_PER_SEC
      VB2_USEC_PER_SEC
    
    The error code VBERROR_NO_BACKGROUND_SOUND is dropped since
    it is not currently used.
    
    BUG=b:124141368, chromium:988410
    TEST=make clean && make runtests
    BRANCH=none
    
    Change-Id: I887112ffd5f68fb6a9c4d9ad624aa420cbd55b4b
    Signed-off-by: Joel Kitching <kitching@google.com>
    Cq-Depend: chromium:2158665

--- firmware/stub/vboot_api_stub_init.c
+++ firmware/stub/vboot_api_stub_init.c
@@ -13,11 +13,11 @@
 #include "2common.h"
 #include "vboot_api.h"
 
-uint64_t VbExGetTimer(void)
+uint32_t vb2ex_mtime(void)
 {
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
-	return (uint64_t)tv.tv_sec * VB_USEC_PER_SEC + (uint64_t)tv.tv_usec;
+	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
 
 vb2_error_t vb2ex_commit_data(struct vb2_context *ctx)
