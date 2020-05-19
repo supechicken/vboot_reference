135a85e3db8b8096abf72eb6b79640bf5245900a
Hsuan Ting Chen <roccochen@chromium.org>
2020-05-20 01:25:20 +0800

    vboot/ui: Add tests for to_dev transition flow
    Unit tests pair with CL:2168072.
    
    BUG=b:146399181, b:156448738
    TEST=make clean && make runtests
    TEST=make clean && DETACHABLE=1; make runtests
    TEST=make clean && PHYSICAL_PRESENCE_KEYBOARD=1; make runtests
    BRANCH=none
    
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>
    Change-Id: Ic6b109c514e1582d7eb29040135aeaa884b243be

--- tests/vb2_ui_action_tests.c
+++ tests/vb2_ui_action_tests.c
@@ -24,7 +24,9 @@
 #define MOCK_SCREEN_TARGET0 0xef20
 #define MOCK_SCREEN_TARGET1 0xef21
 #define MOCK_SCREEN_TARGET2 0xef22
-#define MOCK_SCREEN_TARGET3 0xef23
+#define MOCK_SCREEN_INIT 0xef30
+#define MOCK_SCREEN_ACTION 0xef31
+#define MOCK_SCREEN_ALL_ACTION 0xef32
 #define MOCK_SCREEN_ROOT 0xefff
 
 /* Mock data */
@@ -38,6 +40,7 @@ struct display_call {
 static uint8_t workbuf[VB2_KERNEL_WORKBUF_RECOMMENDED_SIZE]
 	__attribute__((aligned(VB2_WORKBUF_ALIGN)));
 static struct vb2_context *ctx;
+static struct vb2_shared_data *sd;
 static struct vb2_gbb_header gbb;
 
 static int mock_calls_until_shutdown;
@@ -59,6 +62,12 @@ static enum vb2_screen mock_get_screen_info_called;
 static vb2_error_t mock_vbtlk_retval;
 static uint32_t mock_vbtlk_expected_flag;
 
+static int mock_allow_recovery;
+
+static int mock_physical_presence_pressed;
+
+static int mock_enable_dev_mode;
+
 /* Mock actions */
 static uint32_t mock_action_called;
 static vb2_error_t mock_action_countdown(struct vb2_ui_context *ui)
@@ -71,6 +80,41 @@ static vb2_error_t mock_action_countdown(struct vb2_ui_context *ui)
 static vb2_error_t mock_action_change_screen(struct vb2_ui_context *ui)
 {
 	return vb2_ui_change_screen(ui, MOCK_SCREEN_BASE);
+}
+
+static vb2_error_t mock_action_base(struct vb2_ui_context *ui)
+{
+	mock_action_called++;
+	return VB2_SUCCESS;
+}
+
+static int mock_action_flags;
+static vb2_error_t mock_action_flag0(struct vb2_ui_context *ui)
+{
+	if ((1 << 0) & mock_action_flags)
+		return VB2_SUCCESS;
+	return VB2_REQUEST_UI_CONTINUE;
+}
+
+static vb2_error_t mock_action_flag1(struct vb2_ui_context *ui)
+{
+	if ((1 << 1) & mock_action_flags)
+		return VB2_SUCCESS;
+	return VB2_REQUEST_UI_CONTINUE;
+}
+
+static vb2_error_t mock_action_flag2(struct vb2_ui_context *ui)
+{
+	if ((1 << 2) & mock_action_flags)
+		return VB2_SUCCESS;
+	return VB2_REQUEST_UI_CONTINUE;
+}
+
+static vb2_error_t mock_action_flag3(struct vb2_ui_context *ui)
+{
+	if ((1 << 3) & mock_action_flags)
+		return VB2_SUCCESS;
+	return VB2_REQUEST_UI_CONTINUE;
 }
 
 /* Mock screens */
@@ -104,7 +148,7 @@ const struct vb2_menu_item mock_screen_menu_items[] = {
 	},
 	{
 		.text = "option 3",
-		.target = MOCK_SCREEN_TARGET3,
+		.action = mock_action_base,
 	},
 	{
 		.text = "option 4 (no target)",
@@ -134,11 +178,33 @@ const struct vb2_screen_info mock_screen_target2 = {
 	.num_items = ARRAY_SIZE(mock_empty_menu),
 	.items = mock_empty_menu,
 };
-const struct vb2_screen_info mock_screen_target3 = {
-	.id = MOCK_SCREEN_TARGET3,
-	.name = "mock_screen_target3",
+const struct vb2_screen_info mock_screen_init = {
+	.id = MOCK_SCREEN_INIT,
+	.name = "mock_screen_init",
+	.init = mock_action_base,
 	.num_items = ARRAY_SIZE(mock_empty_menu),
 	.items = mock_empty_menu,
+};
+const struct vb2_screen_info mock_screen_action = {
+	.id = MOCK_SCREEN_ACTION,
+	.name = "mock_screen_action",
+	.action = mock_action_countdown,
+	.num_items = ARRAY_SIZE(mock_empty_menu),
+	.items = mock_empty_menu,
+};
+const struct vb2_menu_item mock_screen_all_action_items[] = {
+	{
+		.text = "all_action_screen_option",
+		.action = mock_action_flag2,
+	},
+};
+const struct vb2_screen_info mock_screen_all_action = {
+	.id = MOCK_SCREEN_ALL_ACTION,
+	.name = "mock_screen_all_action",
+	.init = mock_action_flag0,
+	.action = mock_action_flag1,
+	.num_items = ARRAY_SIZE(mock_screen_all_action_items),
+	.items = mock_screen_all_action_items,
 };
 const struct vb2_screen_info mock_screen_root = {
 	.id = MOCK_SCREEN_ROOT,
@@ -247,6 +313,8 @@ static void reset_common_data(void)
 
 	vb2_nv_init(ctx);
 
+	sd = vb2_get_sd(ctx);
+
 	/* For check_shutdown_request */
 	mock_calls_until_shutdown = 10;
 
@@ -290,6 +358,7 @@ static void reset_common_data(void)
 
 	/* For mock actions */
 	mock_action_called = 0;
+	mock_action_flags = 0;
 
 	/* For chagen_screen and vb2_get_screen_info */
 	mock_get_screen_info_called = 0;
@@ -297,6 +366,15 @@ static void reset_common_data(void)
 	/* For VbTryLoadKernel */
 	mock_vbtlk_retval = VB2_ERROR_MOCK;
 	mock_vbtlk_expected_flag = MOCK_IGNORE;
+
+	/* For vb2_allow_recovery */
+	mock_allow_recovery = 0;
+
+	/* For vb2ex_physical_presence_pressed */
+	mock_physical_presence_pressed = 0;
+
+	/* For vb2_enable_developer_mode */
+	mock_enable_dev_mode = 0;
 }
 
 /* Mock functions */
@@ -333,8 +411,12 @@ const struct vb2_screen_info *vb2_get_screen_info(enum vb2_screen screen)
 		return &mock_screen_target1;
 	case MOCK_SCREEN_TARGET2:
 		return &mock_screen_target2;
-	case MOCK_SCREEN_TARGET3:
-		return &mock_screen_target3;
+	case MOCK_SCREEN_INIT:
+		return &mock_screen_init;
+	case MOCK_SCREEN_ACTION:
+		return &mock_screen_action;
+	case MOCK_SCREEN_ALL_ACTION:
+		return &mock_screen_all_action;
 	case MOCK_NO_SCREEN:
 		return NULL;
 	default:
@@ -395,6 +477,21 @@ vb2_error_t VbTryLoadKernel(struct vb2_context *c, uint32_t get_info_flags)
 		"  unexpected get_info_flags");
 
 	return mock_vbtlk_retval;
+}
+
+int vb2_allow_recovery(struct vb2_context *c)
+{
+	return mock_allow_recovery;
+}
+
+int vb2ex_physical_presence_pressed(void)
+{
+	return mock_physical_presence_pressed;
+}
+
+void vb2_enable_developer_mode(struct vb2_context *c)
+{
+	mock_enable_dev_mode = 1;
 }
 
 /* Tests */
@@ -528,8 +625,8 @@ static void menu_select_action_tests(void)
 		"menu_select_action with no item screen");
 	screen_state_eq(mock_state, MOCK_SCREEN_BASE, 0, MOCK_IGNORE);
 
-	/* Try to select target 0..3 */
-	for (i = 0; i <= 3; i++) {
+	/* Try to select target 0..2 */
+	for (i = 0; i <= 2; i++) {
 		sprintf(test_name, "select target %d", i);
 		target_id = MOCK_SCREEN_TARGET0 + i;
 		reset_common_data();
@@ -540,6 +637,15 @@ static void menu_select_action_tests(void)
 			VB2_REQUEST_UI_CONTINUE, test_name);
 		screen_state_eq(mock_state, target_id, 0, MOCK_IGNORE);
 	}
+
+	/* Try to select target with action (target 3) */
+	reset_common_data();
+	mock_state->screen = &mock_screen_menu;
+	mock_state->selected_item = 3;
+	mock_ui_context.key = VB_KEY_ENTER;
+	TEST_EQ(vb2_ui_menu_select_action(&mock_ui_context),
+		VB2_SUCCESS, "select target with action");
+	TEST_EQ(mock_action_called, 1, "  action called once");
 
 	/* Try to select no target item (target 4) */
 	reset_common_data();
@@ -576,6 +682,27 @@ static void vb2_ui_back_action_tests(void)
 	TEST_EQ(vb2_ui_back_action(&mock_ui_context), VB2_REQUEST_UI_CONTINUE,
 		"back to root screen");
 	screen_state_eq(mock_state, MOCK_SCREEN_ROOT, MOCK_IGNORE, MOCK_IGNORE);
+
+	VB2_DEBUG("...done.\n");
+}
+
+static void ctrl_d_action_tests(void)
+{
+	VB2_DEBUG("Testing ctrl_d_action...\n");
+
+	/* Allow recovery: to_dev */
+	reset_common_data();
+	mock_allow_recovery = 1;
+	TEST_EQ(ctrl_d_action(&mock_ui_context), VB2_REQUEST_UI_CONTINUE,
+		"allow recovery, to_dev");
+	screen_state_eq(mock_state, VB2_SCREEN_RECOVERY_TO_DEV,
+			MOCK_IGNORE, MOCK_IGNORE);
+
+	/* Recovery not allowed, do nothing */
+	reset_common_data();
+	TEST_EQ(ctrl_d_action(&mock_ui_context), VB2_REQUEST_UI_CONTINUE,
+		"recovery not allowed");
+	TEST_EQ(mock_get_screen_info_called, 0, "  no change_screen");
 
 	VB2_DEBUG("...done.\n");
 }
@@ -660,8 +787,180 @@ static void try_recovery_action_tests(void)
 	VB2_DEBUG("...done.\n");
 }
 
+static void recovery_to_dev_init_tests(void)
+{
+	VB2_DEBUG("Testing recovery_to_dev_init...\n");
+
+	/* Dev mode already enabled: back */
+	reset_common_data();
+	sd->flags |= VB2_SD_FLAG_DEV_MODE_ENABLED;
+	TEST_EQ(recovery_to_dev_init(&mock_ui_context), VB2_REQUEST_UI_CONTINUE,
+		"dev mode already enabled: back");
+	screen_state_eq(mock_state, MOCK_SCREEN_ROOT, MOCK_IGNORE, MOCK_IGNORE);
+
+	/* Press button stuck: back */
+	if (!PHYSICAL_PRESENCE_KEYBOARD) {
+		reset_common_data();
+		mock_physical_presence_pressed = 1;
+		TEST_EQ(recovery_to_dev_init(&mock_ui_context),
+			VB2_REQUEST_UI_CONTINUE,
+			"press button stuck: back");
+		screen_state_eq(mock_state, MOCK_SCREEN_ROOT, MOCK_IGNORE,
+				MOCK_IGNORE);
+	}
+
+	/* Disable confirm for other physical presence types */
+	if (!PHYSICAL_PRESENCE_KEYBOARD) {
+		reset_common_data();
+		TEST_EQ(recovery_to_dev_init(&mock_ui_context),
+			VB2_REQUEST_UI_CONTINUE,
+			"disable confirm for other physical presence type");
+		TEST_EQ(mock_get_screen_info_called, 0, "  no change_screen");
+		screen_state_eq(mock_state, MOCK_IGNORE, MOCK_IGNORE, 1);
+	}
+
+	/* Do nothing otherwise */
+	if (PHYSICAL_PRESENCE_KEYBOARD) {
+		reset_common_data();
+		TEST_EQ(recovery_to_dev_init(&mock_ui_context),
+			VB2_REQUEST_UI_CONTINUE, "do nothing otherwise");
+		TEST_EQ(mock_get_screen_info_called, 0, "  no change_screen");
+		screen_state_eq(mock_state, MOCK_IGNORE, MOCK_IGNORE, 0);
+	}
+
+	VB2_DEBUG("...done.\n");
+}
+
+static void vb2_ui_recovery_to_dev_action_tests(void)
+{
+	VB2_DEBUG("Testing vb2_ui_recovery_to_dev_action...\n");
+
+	/* Action need RECOVERY_TO_DEV screen */
+	reset_common_data();
+	mock_allow_recovery = 1;
+	mock_ui_context.key = VB_KEY_ENTER;
+	mock_ui_context.key_trusted = 1;
+	TEST_EQ(vb2_ui_recovery_to_dev_action(&mock_ui_context),
+		VB2_REQUEST_UI_CONTINUE,
+		"action need RECOVERY_TO_DEV screen");
+	TEST_EQ(mock_enable_dev_mode, 0, "  dev mode not enabled");
+
+	/* Space means cancel dev mode transition */
+	reset_common_data();
+	mock_allow_recovery = 1;
+	mock_screen_temp.id = VB2_SCREEN_RECOVERY_TO_DEV;
+	mock_ui_context.key = ' ';
+	TEST_EQ(vb2_ui_recovery_to_dev_action(&mock_ui_context),
+		VB2_REQUEST_UI_CONTINUE,
+		"space means cancel dev mode transition");
+	TEST_EQ(mock_enable_dev_mode, 0, "  dev mode not enabled");
+	screen_state_eq(mock_state, MOCK_SCREEN_ROOT, MOCK_IGNORE, MOCK_IGNORE);
+
+	/* Physical presence button */
+	if (!PHYSICAL_PRESENCE_KEYBOARD) {
+		reset_common_data();
+		mock_allow_recovery = 1;
+		mock_screen_temp.id = VB2_SCREEN_RECOVERY_TO_DEV;
+		mock_physical_presence_pressed = 1;
+		TEST_EQ(vb2_ui_recovery_to_dev_action(&mock_ui_context),
+			VB2_REQUEST_UI_CONTINUE,
+			"physical presence button pressed, await");
+	      	TEST_EQ(mock_enable_dev_mode, 0, "  dev mode not enabled");
+		mock_physical_presence_pressed = 0;
+		TEST_EQ(vb2_ui_recovery_to_dev_action(&mock_ui_context),
+			VB2_REQUEST_REBOOT_EC_TO_RO,
+			"physical presence button released");
+	      	TEST_EQ(mock_enable_dev_mode, 1, "  dev mode enabled");
+	}
+
+	/* Physical presence keyboard */
+	if (PHYSICAL_PRESENCE_KEYBOARD) {
+		/* VB_KEY_ENTER */
+		reset_common_data();
+		mock_allow_recovery = 1;
+		mock_screen_temp.id = VB2_SCREEN_RECOVERY_TO_DEV;
+		mock_ui_context.key = VB_KEY_ENTER;
+		mock_ui_context.key_trusted = 1;
+		TEST_EQ(vb2_ui_recovery_to_dev_action(&mock_ui_context),
+			VB2_REQUEST_REBOOT_EC_TO_RO,
+			"physical presence keyboard, VB_KEY_ENTER");
+		TEST_EQ(mock_enable_dev_mode, 1, "  dev mode enabled");
+
+		/* VB_BUTTON_POWER_SHORT_PRESS */
+		reset_common_data();
+		mock_allow_recovery = 1;
+		mock_screen_temp.id = VB2_SCREEN_RECOVERY_TO_DEV;
+		mock_ui_context.key = VB_BUTTON_POWER_SHORT_PRESS;
+		mock_ui_context.key_trusted = 1;
+		TEST_EQ(vb2_ui_recovery_to_dev_action(&mock_ui_context),
+			VB2_REQUEST_REBOOT_EC_TO_RO,
+			"physical presence keyboard,"
+			" VB_BUTTON_POWER_SHORT_PRESS");
+		TEST_EQ(mock_enable_dev_mode, 1, "  dev mode enabled");
+
+		/* Not a confirmation key */
+		reset_common_data();
+		mock_allow_recovery = 1;
+		mock_screen_temp.id = VB2_SCREEN_RECOVERY_TO_DEV;
+		mock_ui_context.key = 'A';
+		mock_ui_context.key_trusted = 1;
+		TEST_EQ(vb2_ui_recovery_to_dev_action(&mock_ui_context),
+			VB2_REQUEST_UI_CONTINUE,
+			"not a confirmation key");
+		TEST_EQ(mock_enable_dev_mode, 0, "  dev mode not enabled");
+
+		/* Not a trusted key */
+		reset_common_data();
+		mock_allow_recovery = 1;
+		mock_screen_temp.id = VB2_SCREEN_RECOVERY_TO_DEV;
+		mock_ui_context.key = VB_KEY_ENTER;
+		TEST_EQ(vb2_ui_recovery_to_dev_action(&mock_ui_context),
+			VB2_REQUEST_UI_CONTINUE,
+			"VB_KEY_ENTER not trusted");
+		TEST_EQ(mock_enable_dev_mode, 0, "  dev mode not enabled");
+		reset_common_data();
+		mock_allow_recovery = 1;
+		mock_screen_temp.id = VB2_SCREEN_RECOVERY_TO_DEV;
+		mock_ui_context.key = VB_BUTTON_POWER_SHORT_PRESS;
+		TEST_EQ(vb2_ui_recovery_to_dev_action(&mock_ui_context),
+			VB2_REQUEST_UI_CONTINUE,
+			"VB_BUTTON_POWER_SHORT_PRESS not trusted");
+		TEST_EQ(mock_enable_dev_mode, 0, "  dev mode not enabled");
+	}
+
+	/* Sanity check */
+	reset_common_data();
+	mock_screen_temp.id = VB2_SCREEN_RECOVERY_TO_DEV;
+	mock_ui_context.key = VB_KEY_ENTER;
+	mock_ui_context.key_trusted = 1;
+	TEST_EQ(vb2_ui_recovery_to_dev_action(&mock_ui_context),
+		VB2_REQUEST_UI_CONTINUE,
+		"sanity check: recovery not allowed");
+	TEST_EQ(mock_enable_dev_mode, 0, "  dev mode not enabled");
+	reset_common_data();
+	sd->flags |= VB2_SD_FLAG_DEV_MODE_ENABLED;
+	mock_allow_recovery = 1;
+	mock_screen_temp.id = VB2_SCREEN_RECOVERY_TO_DEV;
+	mock_ui_context.key = VB_KEY_ENTER;
+	mock_ui_context.key_trusted = 1;
+	TEST_EQ(vb2_ui_recovery_to_dev_action(&mock_ui_context),
+		VB2_REQUEST_UI_CONTINUE,
+		"sanity check: dev mode already enabled");
+	TEST_EQ(mock_enable_dev_mode, 0, "  no enable dev mode call");
+
+	VB2_DEBUG("...done.\n");
+}
+
 static void ui_loop_tests(void)
 {
+	int i;
+	const char *action_interfere_test_names[] = {
+		"action interfere test: root screen init",
+		"action interfere test: screen action",
+		"action interfere test: target option hooked",
+		"action interfere test: global action",
+	};
+
 	VB2_DEBUG("Testing ui_loop...\n");
 
 	/* Die if no root screen */
@@ -679,6 +978,19 @@ static void ui_loop_tests(void)
 		     MOCK_IGNORE, MOCK_IGNORE);
 	displayed_no_extra();
 
+	/* Root screen init */
+	reset_common_data();
+	TEST_EQ(ui_loop(ctx, MOCK_SCREEN_INIT, NULL),
+		VB2_SUCCESS, "root screen init");
+	TEST_EQ(mock_action_called, 1, "  action called");
+
+	/* Screen action */
+	reset_common_data();
+	mock_calls_until_shutdown = -1;
+	TEST_EQ(ui_loop(ctx, MOCK_SCREEN_ACTION, NULL),
+		VB2_SUCCESS, "screen action");
+	TEST_EQ(mock_action_called, 10, "  action called");
+
 	/* Global action */
 	reset_common_data();
 	mock_calls_until_shutdown = -1;
@@ -694,6 +1006,16 @@ static void ui_loop_tests(void)
 		     MOCK_IGNORE);
 	displayed_eq("change to mock_screen_base", MOCK_IGNORE, MOCK_IGNORE,
 		     MOCK_IGNORE, MOCK_IGNORE);
+
+	/* All actions does not interfere each other. */
+	for (i = 0; i <= 3; i++) {
+		reset_common_data();
+		add_mock_keypress(VB_KEY_ENTER);
+		mock_calls_until_shutdown = -1;
+		mock_action_flags |= (1 << i);
+		TEST_EQ(ui_loop(ctx, MOCK_SCREEN_ALL_ACTION, mock_action_flag3),
+			VB2_SUCCESS, action_interfere_test_names[i]);
+	}
 
 	/* KEY_UP, KEY_DOWN, and KEY_ENTER navigation */
 	reset_common_data();
@@ -769,9 +1091,14 @@ int main(void)
 	menu_down_action_tests();
 	menu_select_action_tests();
 	vb2_ui_back_action_tests();
+	ctrl_d_action_tests();
 
 	/* Global actions */
 	try_recovery_action_tests();
+
+	/* Recovery to dev mode */
+	recovery_to_dev_init_tests();
+	vb2_ui_recovery_to_dev_action_tests();
 
 	/* Core UI loop */
 	ui_loop_tests();
