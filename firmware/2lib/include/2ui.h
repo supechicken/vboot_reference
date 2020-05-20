1c9b35ff29c153378eac1920129507b16b8433a6
Joel Kitching <kitching@google.com>
2020-05-20 15:17:11 +0800

    vboot/ui: stop centralizing keyboard input actions
    BUG=b:146399181
    TEST=make clean && make runtests
    BRANCH=none
    
    Change-Id: Ia63537fb13be5f04ae81a6be7e0fac6eaf47cfb7
    Signed-off-by: Joel Kitching <kitching@google.com>

--- firmware/2lib/include/2ui.h
+++ firmware/2lib/include/2ui.h
@@ -72,11 +72,6 @@ struct vb2_ui_context {
 	vb2_error_t recovery_rv;
 };
 
-vb2_error_t vb2_ui_change_screen(struct vb2_ui_context *ui, enum vb2_screen id);
-vb2_error_t vb2_ui_menu_select_action(struct vb2_ui_context *ui);
-vb2_error_t vb2_ui_back_action(struct vb2_ui_context *ui);
-
-vb2_error_t vb2_ui_recovery_to_dev_action(struct vb2_ui_context *ui);
 vb2_error_t vb2_ui_developer_mode_boot_internal_action(
 	struct vb2_ui_context *ui);
 vb2_error_t vb2_ui_developer_mode_boot_external_action(
