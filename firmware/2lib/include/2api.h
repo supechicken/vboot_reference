176de6ef9d9f52d9bc2b5d66e1267020d3b67f7a
Yu-Ping Wu <yupingso@chromium.org>
2020-06-08 16:59:45 +0800

    vboot: publicize vb2_get_dev_boot_target()
    Publicize vb2_get_dev_boot_target() as vb2api_get_dev_boot_target(), and
    move enum vb2_dev_default_boot to 2api.h.
    
    BRANCH=none
    BUG=b:146399181, b:157871585
    TEST=make runtests
    TEST=emerge-puff depthcharge
    
    Change-Id: I8336e63cc273c32a41e6b1f2aad886c3940a6aef
    Signed-off-by: Yu-Ping Wu <yupingso@chromium.org>

--- firmware/2lib/include/2api.h
+++ firmware/2lib/include/2api.h
@@ -861,6 +861,26 @@ uint32_t vb2api_get_locale_id(struct vb2_context *ctx);
  */
 int vb2api_phone_recovery_enabled(struct vb2_context *ctx);
 
+/* Default boot target in developer mode. */
+enum vb2_dev_default_boot {
+	/* Default to boot from disk. */
+	VB2_DEV_DEFAULT_BOOT_DISK = 0,
+
+	/* Default to boot from USB. */
+	VB2_DEV_DEFAULT_BOOT_USB = 1,
+
+	/* Default to boot legacy OS. */
+	VB2_DEV_DEFAULT_BOOT_LEGACY = 2,
+};
+
+/**
+ * Get the default boot target in developer mode.
+ *
+ * @param ctx		Vboot context
+ * @return vb2_dev_default_boot.
+ */
+enum vb2_dev_default_boot vb2api_get_dev_boot_target(struct vb2_context *ctx);
+
 /*****************************************************************************/
 /* APIs provided by the caller to verified boot */
 
