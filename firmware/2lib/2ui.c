762b94fab3c87b5c4dd7249b28f70065e735ea1b
Meng-Huan Yu <menghuan@chromium.org>
2020-07-23 15:50:52 +0800

    [TEST-ONLY] Use diag screen instead of debug info screen
    BRANCH=none
    BUG=none
    TEST=none
    
    Signed-off-by: Meng-Huan Yu <menghuan@chromium.org>
    Change-Id: I20596fff1721879a2f35f71c141d11edc2ffba42

--- firmware/2lib/2ui.c
+++ firmware/2lib/2ui.c
@@ -411,7 +411,7 @@ vb2_error_t developer_action(struct vb2_ui_context *ui)
 	    (DETACHABLE && ui->key == VB_BUTTON_VOL_DOWN_LONG_PRESS))
 		return vb2_ui_developer_mode_boot_internal_action(ui);
 	if (ui->key == '\t')
-		return vb2_ui_screen_change(ui, VB2_SCREEN_DEBUG_INFO);
+		return vb2_ui_screen_change(ui, VB2_SCREEN_DIAGNOSTICS_STORAGE);
 
 	return VB2_REQUEST_UI_CONTINUE;
 }
@@ -428,7 +428,7 @@ vb2_error_t broken_recovery_action(struct vb2_ui_context *ui)
 {
 	/* Broken recovery keyboard shortcuts */
 	if (ui->key == '\t')
-		return vb2_ui_screen_change(ui, VB2_SCREEN_DEBUG_INFO);
+		return vb2_ui_screen_change(ui, VB2_SCREEN_DIAGNOSTICS_STORAGE);
 
 	return VB2_REQUEST_UI_CONTINUE;
 }
@@ -465,7 +465,7 @@ vb2_error_t manual_recovery_action(struct vb2_ui_context *ui)
 		return vb2_ui_screen_change(ui, VB2_SCREEN_RECOVERY_TO_DEV);
 
 	if (ui->key == '\t')
-		return vb2_ui_screen_change(ui, VB2_SCREEN_DEBUG_INFO);
+		return vb2_ui_screen_change(ui, VB2_SCREEN_DIAGNOSTICS_STORAGE);
 
 	return VB2_REQUEST_UI_CONTINUE;
 }
