a797d743d4722118a975c1a52c6aa2ecf3257e33
Daisuke Nojiri <dnojiri@chromium.org>
2020-08-12 15:46:30 -0700

    inclusive: change usage of sanity
    Google is working to change its source code to use more inclusive
    language. To that end, replace the term "sanity" with inclusive
    alternatives.
    
    BUG=b:163883397
    BRANCH=None
    TEST=grep -ir sanity
    TEST=make runtests
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I708a044d89050c442f14fb11a8ae5e98490d56af

--- firmware/lib/vboot_kernel.c
+++ firmware/lib/vboot_kernel.c
@@ -626,7 +626,7 @@ vb2_error_t LoadKernel(struct vb2_context *ctx, LoadKernelParams *params)
 		VB2_DEBUG("Good partition %d\n", params->partition_number);
 		shcall.check_result = VBSD_LKC_CHECK_GOOD_PARTITION;
 		/*
-		 * Sanity check - only store a new TPM version if we found one.
+		 * Validity check - only store a new TPM version if we found one.
 		 * If lowest_version is still at its initial value, we didn't
 		 * find one; for example, we're in developer mode and just
 		 * didn't look.
