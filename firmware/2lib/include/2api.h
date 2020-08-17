459d68c5f53c6237b6e88dde92c7918429e5f382
Shelley Chen <shchen@google.com>
2020-08-17 00:52:47 -0700

    firmware/2lib: Add error message for ctrl-U when usb boot is not enabled
    Currently, we only have a beep when the user presses ctrl-u.  Adding
    an error message to clarify what the error is.  Also adding error code
    for legacy boot, which has not landed in tot yet.  Will need to
    utilize error in 2ui.c after it lands in tot.  Puff branch will need a
    separate change to enable this error on legacy boot as it landed in
    puff branch first with a different CL.
    
    BUG=b:164944674
    BRANCH=puff, zork
    TEST=boot into developer mode
         make sure "boot from external disk" doesn't appear
         press ctrl+U and make sure dialog box with error appears
    
    Cq-Depend: chromium:2359765
    Signed-off-by: Shelley Chen <shchen@google.com>
    Change-Id: Id8fad79802cb61883758e8c7561163cc17fb0f87

--- firmware/2lib/include/2api.h
+++ firmware/2lib/include/2api.h
@@ -1323,6 +1323,10 @@ enum vb2_ui_error {
 	VB2_UI_ERROR_DEV_MODE_ALREADY_ENABLED,
 	/* Debug info screen initialization failed */
 	VB2_UI_ERROR_DEBUG_LOG,
+	/* External boot not enabled */
+	VB2_UI_ERROR_EXTERNAL_BOOT_NOT_ENABLED,
+	/* Legacy boot not enabled */
+	VB2_UI_ERROR_LEGACY_BOOT_NOT_ENABLED,
 	/* Untrusted confirmation */
 	VB2_UI_ERROR_UNTRUSTED_CONFIRMATION,
 };
