5743c85a88d4f391a2e961558daa326401faa6ae
Meng-Huan Yu <menghuan@chromium.org>
2020-08-18 18:05:19 +0800

    minidiag: Add diagnostic menu screens
    BRANCH=none
    BUG=b:156692539, b:156693348
    TEST=emerge-hatch vboot_reference
    TEST=unittest passed:
    ( export CC=x86_64-pc-linux-gnu-clang DEBUG=1 MENU_UI=0 DIAGNOSTIC_UI=0
        MINIMAL=1 TPM2_MODE= MOCK_TPM=; make clean &&
            make -j32 test_setup && make runtests; echo $? )
    ( export CC=x86_64-pc-linux-gnu-clang DEBUG=1 MENU_UI=1 DIAGNOSTIC_UI=0
        MINIMAL=1 TPM2_MODE= MOCK_TPM=; make clean &&
            make -j32 test_setup && make runtests; echo $? )
    ( export CC=x86_64-pc-linux-gnu-clang DEBUG=1 MENU_UI=0 DIAGNOSTIC_UI=1
        MINIMAL=1 TPM2_MODE= MOCK_TPM=; make clean &&
            make -j32 test_setup && make runtests; echo $? )
    ( export CC=x86_64-pc-linux-gnu-clang DEBUG=1 MENU_UI=1 DIAGNOSTIC_UI=1
        MINIMAL=1 TPM2_MODE= MOCK_TPM=; make clean &&
            make -j32 test_setup && make runtests; echo $? )
    
    Cq-Depend: chromium:2193314, chromium:2328704
    Signed-off-by: Meng-Huan Yu <menghuan@chromium.org>
    Change-Id: I4f3c64ce53b14437cb14d3c1109e14608d082141

--- firmware/lib/vboot_api_kernel.c
+++ firmware/lib/vboot_api_kernel.c
@@ -240,8 +240,7 @@ vb2_error_t VbSelectAndLoadKernel(struct vb2_context *ctx,
 		} else {
 			VB2_TRY(VbBootRecoveryLegacyClamshell(ctx));
 		}
-	} else if (DIAGNOSTIC_UI && !MENU_UI &&
-		   vb2api_diagnostic_ui_enabled(ctx) &&
+	} else if (DIAGNOSTIC_UI && vb2api_diagnostic_ui_enabled(ctx) &&
 		   vb2_nv_get(ctx, VB2_NV_DIAG_REQUEST)) {
 		vb2_nv_set(ctx, VB2_NV_DIAG_REQUEST, 0);
 
@@ -251,7 +250,10 @@ vb2_error_t VbSelectAndLoadKernel(struct vb2_context *ctx,
 		 * needed.  This mode is also 1-shot so it's placed
 		 * before developer mode.
 		 */
-		VB2_TRY(VbBootDiagnosticLegacyClamshell(ctx));
+		if (MENU_UI)
+			VB2_TRY(vb2_diagnostic_menu(ctx));
+		else
+			VB2_TRY(VbBootDiagnosticLegacyClamshell(ctx));
 		/*
 		 * The diagnostic menu should either boot a rom, or
 		 * return either of reboot or shutdown.
