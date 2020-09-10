cd63f879d64ca76e9df258eb3791e27df16e8545
Julius Werner <jwerner@chromium.org>
2020-09-09 18:45:38 -0700

    fuzzers: Initialize secdata
    CL:2353775 made the functions tested by vb2_keyblock_fuzzer and
    vb2_preamble_fuzzer look at secdata, which broke the fuzzer because they
    don't initialize secdata the way a normal boot would. This patch makes
    the fuzzers initialize both firmware and kernel secdata explicitly (and
    nvdata as well for good measure, although I think it's technically not
    needed).
    
    BRANCH=None
    BUG=chromium:1125143,chromium:1124172
    TEST=None
    
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Change-Id: Id9aaa4d44a20455133adc4c2bc524895629edfb9

--- tests/vb2_preamble_fuzzer.c
+++ tests/vb2_preamble_fuzzer.c
@@ -2,11 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <assert.h>
-
 #include "2api.h"
 #include "2common.h"
 #include "2misc.h"
+#include "2nvstorage.h"
 #include "2rsa.h"
 #include "2secdata.h"
 #include "vboot_test.h"
@@ -22,13 +21,6 @@ static size_t mock_preamble_size;
 void vb2api_fail(struct vb2_context *c, uint8_t reason, uint8_t subcode)
 {
 	return;
-}
-
-void vb2_secdata_firmware_set(struct vb2_context *c,
-			      enum vb2_secdata_firmware_param param,
-			      uint32_t value)
-{
-	/* prevent abort from uninitialized secdata */
 }
 
 vb2_error_t vb2ex_read_resource(struct vb2_context *c,
@@ -67,6 +59,11 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 		return 0;
 
 	if (vb2api_init(workbuf, sizeof(workbuf), &ctx))
+		abort();
+	vb2_nv_init(ctx);
+	vb2api_secdata_firmware_create(ctx);
+	vb2api_secdata_kernel_create(ctx);
+	if (vb2_secdata_firmware_init(ctx) || vb2_secdata_kernel_init(ctx))
 		abort();
 
 	struct vb2_workbuf wb;
