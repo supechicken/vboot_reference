b7f52ca31ba70266ff854be3f81b5e3d45f96280
Brian Norris <briannorris@chromium.org>
2020-10-27 15:57:29 -0700

    sign_official_build: dump more info in resign_android_image_if_exists
    We're getting silent errors in here somewhere.
    
    BRANCH=none
    BUG=chromium:1141907
    TEST=none
    
    Change-Id: I9af0a3ea1696920fe67c915660f82a68c1bddf34
    Signed-off-by: Brian Norris <briannorris@chromium.org>

--- scripts/image_signing/sign_official_build.sh
+++ scripts/image_signing/sign_official_build.sh
@@ -740,10 +740,17 @@ resign_android_image_if_exists() {
   fi
 
   info "Found ARC image version '${arc_version}', re-signing APKs."
+  set -x
   "${SCRIPT_DIR}/sign_android_image.sh" "${rootfs_dir}" "${KEY_DIR}/android"
 
-  sudo umount "${rootfs_dir}"
+  if ! sudo umount "${rootfs_dir}"; then
+    error "umount ${rootfs_dir} failed"
+    sudo lsof -n
+    set +x
+    return 1
+  fi
   info "Re-signed Android image"
+  set +x
 }
 
 # Sign UEFI binaries, if possible.
