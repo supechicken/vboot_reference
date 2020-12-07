01e454dc9ef5472146d31847cebd92fb3e1e5e70
Yi Chou <yich@google.com>
2020-12-07 16:36:24 +0800

    crossystem: support driver-level TPM2.0 simulator
    After implemented the driver-level TPM2.0 simulator on VM boards, the
    mount-encrypted would use the vTPM to encrypted the file system. So we
    don't need to remove the mount-encrypted key.
    And we would need to revmove the TPM simulator NVChip when we want to
    hard reset the TPM on VM.
    
    BUG=b:174807059
    BRANCH=none
    TEST=crossystem clear_tpm_owner_request=1
    
    Cq-Depend: chromium:2576865
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: Iba2c9b93ed9e558a9163542dfc1fbcb738c1d83d

--- host/lib/crossystem.c
+++ host/lib/crossystem.c
@@ -27,6 +27,10 @@
 /* Filename for the mount-encrypted key */
 #define MOUNT_ENCRYPTED_KEY_PATH "/mnt/stateful_partition/encrypted.key"
 
+/* Filename for the TPM simulator NV data */
+#define TPM_SIMULATOR_NVCHIP_PATH                                              \
+	"/mnt/stateful_partition/unencrypted/tpm2-simulator/NVChip"
+
 /* Fields that GetVdatString() can get */
 typedef enum VdatStringField {
 	VDAT_STRING_DEPRECATED_TIMERS = 0,  /* Timer values */
@@ -374,6 +378,9 @@ int VbGetSystemPropertyInt(const char *name)
 	} else if (!strcasecmp(name,"disable_dev_request")) {
 		value = vb2_get_nv_storage(VB2_NV_DISABLE_DEV_REQUEST);
 	} else if (!strcasecmp(name,"clear_tpm_owner_request")) {
+		if (TPM2_SIMULATOR & VTPM_PROXY)
+			/* Check TPM simulator NVChip status */
+			value = access(TPM_SIMULATOR_NVCHIP_PATH, F_OK) != 0;
 		if (TPM2_SIMULATOR)
 			/* Check mount-encrypted key status */
 			value = access(MOUNT_ENCRYPTED_KEY_PATH, F_OK) != 0;
@@ -556,16 +563,28 @@ int VbSetSystemPropertyInt(const char *name, int value)
 			 * on simulator */
 			if (value == 0)
 				return -1;
-			/* Check mount-encrypted key status */
-			if (!access(MOUNT_ENCRYPTED_KEY_PATH, F_OK)) {
-				/* Remove the mount_encrypted key, and it would
-				 * also clear the TPM2.0 simulator NV space on
-				 * it. */
-				return remove(MOUNT_ENCRYPTED_KEY_PATH);
+			if(VTPM_PROXY) {
+				/* Check TPM simulator NVChip status */
+				if (!access(TPM_SIMULATOR_NVCHIP_PATH, F_OK)) {
+					/* Remove the TPM2.0 simulator NV space */
+					return remove(TPM_SIMULATOR_NVCHIP_PATH);
+				} else {
+					/* Return success when the file is already
+					* removed */
+					return 0;
+				}
 			} else {
-				/* Return success when the file is already
-				 * removed */
-				return 0;
+				/* Check mount-encrypted key status */
+				if (!access(MOUNT_ENCRYPTED_KEY_PATH, F_OK)) {
+					/* Remove the mount_encrypted key, and it would
+					* also clear the TPM2.0 simulator NV space on
+					* it. */
+					return remove(MOUNT_ENCRYPTED_KEY_PATH);
+				} else {
+					/* Return success when the file is already
+					* removed */
+					return 0;
+				}
 			}
 		} else {
 			return vb2_set_nv_storage(
