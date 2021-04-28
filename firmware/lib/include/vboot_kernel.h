eeaeeaec58d1eec087b821087b9a7f3394d9f63c
Joel Kitching <kitching@google.com>
2021-04-28 16:50:21 +0800

    vboot: boot from miniOS recovery kernels on disk
    Add a new boot path within manual recovery mode, activated by the
    hotkey Ctrl+R.  In this boot path, a miniOS recovery kernel is
    located on internal disk, and an attempt is made to verify and
    boot this kernel.  Recovery proceeds from within the miniOS
    kernel by downloading a recovery image over the network.  No USB
    disk is used in the process.
    
    For more information, see go/nbr-firmware.
    
    BUG=b:188121855, b:186682292
    TEST=make clean && make runtests
    BRANCH=none
    
    Signed-off-by: Joel Kitching <kitching@google.com>
    Change-Id: Ic4d1fe5642a2bf71c51c78fd7830ad2b6e9eebeb

--- firmware/lib/include/vboot_kernel.h
+++ firmware/lib/include/vboot_kernel.h
@@ -28,4 +28,19 @@ struct vb2_context;
  */
 vb2_error_t VbTryLoadKernel(struct vb2_context *ctx, uint32_t disk_flags);
 
+/**
+ * Attempt loading a miniOS kernel from internal disk.
+ *
+ * Scans sectors at the start and end of the disk, and looks for miniOS kernels
+ * starting at the beginning of the sector.  Attempts loading any miniOS
+ * kernels found.
+ *
+ * If successful, sets lkp.disk_handle to the disk for the kernel and returns
+ * VB2_SUCCESS.
+ *
+ * @param ctx			Vboot context
+ * @return VB2_SUCCESS or the most specific VB2_ERROR_LK error.
+ */
+vb2_error_t VbTryLoadMiniOsKernel(struct vb2_context *ctx);
+
 #endif  /* VBOOT_REFERENCE_VBOOT_KERNEL_H_ */
