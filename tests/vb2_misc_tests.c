56e96b14baeeefdcb09d3abffc92bcbe5d261f08
edisonhello <edisonhello@google.com>
2021-07-28 15:03:38 +0800

    vboot/misc: Add validity check in vb2api_enable_developer_mode
    Check vb2api_allow_recovery before actual changes in
    vb2api_enable_developer_mode.
    
    Add new API error type VB2_ERROR_API_ENABLE_DEV_NOT_ALLOWED in
    vb2_error_t.
    
    BUG=b:172339016
    TEST=export CC=x86_64-pc-linux-gnu-clang DEBUG=1 DETACHABLE=0; \
         make -j32 test_setup && make -j32 runtests;
    TEST=export CC=x86_64-pc-linux-gnu-clang DEBUG=1 DETACHABLE=1; \
         make -j32 test_setup && make -j32 runtests;
    TEST=Hayato booted into developer mode
    BRANCH=none
    
    Change-Id: Ifebdd01ca87cf944c394907eca73a09fc3bf5457
    Signed-off-by: edisonhello <edisonhello@google.com>

--- tests/vb2_misc_tests.c
+++ tests/vb2_misc_tests.c
@@ -640,6 +640,14 @@ static void dev_switch_tests(void)
 static void enable_dev_tests(void)
 {
 	reset_common_data();
+	allow_recovery_retval = 0;
+	vb2api_enable_developer_mode(ctx);
+	TEST_EQ(vb2_secdata_firmware_get(ctx, VB2_SECDATA_FIRMWARE_FLAGS) &
+	         VB2_SECDATA_FIRMWARE_FLAG_DEV_MODE, 0,
+		 "dev mode flag not set");
+
+	reset_common_data();
+	allow_recovery_retval = 1;
 	vb2api_enable_developer_mode(ctx);
 	TEST_NEQ(vb2_secdata_firmware_get(ctx, VB2_SECDATA_FIRMWARE_FLAGS) &
 	         VB2_SECDATA_FIRMWARE_FLAG_DEV_MODE, 0,
@@ -649,6 +657,7 @@ static void enable_dev_tests(void)
 
 	/* secdata_firmware not initialized, aborts */
 	reset_common_data();
+	allow_recovery_retval = 1;
 	sd->status &= ~VB2_SD_STATUS_SECDATA_FIRMWARE_INIT;
 	TEST_ABORT(vb2api_enable_developer_mode(ctx),
 		   "secdata_firmware no init, enable dev mode aborted");
