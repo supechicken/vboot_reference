422191ea1cc8f32246fa7d8835c346ea2b3ab484
edisonhello <edisonhello@google.com>
2021-07-29 18:10:25 +0800

    vboot/ui: Remove broken recovery screen
    Remove broken recovery screen in vboot, and migrate to depthcharge.
    
    Remove broken recovery related tests in vboot.
    
    BUG=b:172339016
    TEST=Hayato booted into developer mode
    TEST=export CC=x86_64-pc-linux-gnu-clang DEBUG=1 DETACHABLE=0; \
         make -j test_setup && make -j runtests
    TEST=export CC=x86_64-pc-linux-gnu-clang DEBUG=1 DETACHABLE=1; \
         make -j test_setup && make -j runtests
    BRANCH=none
    
    Cq-Depend: chromium:3060295
    Change-Id: I7d830e992d9ae1703befba570dfbe3061319b9fe
    Signed-off-by: edisonhello <edisonhello@google.com>

--- firmware/2lib/2ui_screens.c
+++ firmware/2lib/2ui_screens.c
@@ -252,27 +252,12 @@ static const struct vb2_screen_info language_select_screen = {
 };
 
 /******************************************************************************/
-/* VB2_SCREEN_RECOVERY_BROKEN */
-
-static const struct vb2_menu_item recovery_broken_items[] = {
-	LANGUAGE_SELECT_ITEM,
-	ADVANCED_OPTIONS_ITEM,
-	POWER_OFF_ITEM,
-};
-
-static const struct vb2_screen_info recovery_broken_screen = {
-	.id = VB2_SCREEN_RECOVERY_BROKEN,
-	.name = "Recover broken device",
-	.menu = MENU_ITEMS(recovery_broken_items),
-};
-
-/******************************************************************************/
 /* VB2_SCREEN_ADVANCED_OPTIONS */
 
 #define ADVANCED_OPTIONS_ITEM_DEVELOPER_MODE 1
 #define ADVANCED_OPTIONS_ITEM_DEBUG_INFO 2
 
-vb2_error_t advanced_options_init(struct vb2_ui_context *ui)
+vb2_error_t vb2_advanced_options_init(struct vb2_ui_context *ui)
 {
 	ui->state->selected_item = ADVANCED_OPTIONS_ITEM_DEVELOPER_MODE;
 	if (vb2_get_sd(ui->ctx)->flags & VB2_SD_FLAG_DEV_MODE_ENABLED ||
@@ -306,7 +291,7 @@ static const struct vb2_menu_item advanced_options_items[] = {
 static const struct vb2_screen_info advanced_options_screen = {
 	.id = VB2_SCREEN_ADVANCED_OPTIONS,
 	.name = "Advanced options",
-	.init = advanced_options_init,
+	.init = vb2_advanced_options_init,
 	.menu = MENU_ITEMS(advanced_options_items),
 };
 
@@ -1372,7 +1357,6 @@ static const struct vb2_screen_info diagnostics_memory_full_screen = {
  */
 static const struct vb2_screen_info *screens[] = {
 	&language_select_screen,
-	&recovery_broken_screen,
 	&advanced_options_screen,
 	&debug_info_screen,
 	&firmware_log_screen,
