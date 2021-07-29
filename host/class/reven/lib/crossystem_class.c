65af7163ac15a00da20fc101c0b885c27bb623ef
Yong Hong <yhong@chromium.org>
2021-07-29 22:48:39 +0800

    host: return a static HWID on REVEN devices
    As b/195007267 describes, the normal call stack for `crossystem`
    library to get the HWID string doesn't work on REVEN devices because
    it's impossible to place the HWID into the firmware.
    
    In order to make `crossystem` capable to return a valid HWID string
    on those devices, this CL introduces yet another layer of customization
    for the host library called `class`.  This customization layer contains
    nothing for usual projects.  But for `REVEN`'s build, it overrides
    the getter function for `HWID` to return the pre-defined value.
    
    BUG=b:195007267
    TEST=manual test
    BRANCH=none
    
    Signed-off-by: Yong Hong <yhong@chromium.org>
    Cq-Depend: chromium:3060798
    Change-Id: Ie6238828a09f43390cc9d61ed33f76be7fe7454c

---
+++ host/class/reven/lib/crossystem_class.c
@@ -0,0 +1,34 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Provides REVEN-specific system properties.
+ */
+
+#include "crossystem_class.h"
+
+#include <string.h>
+
+#include "host_misc.h"
+
+int VbGetClassPropertyInt(const char *name)
+{
+	return -1;
+}
+
+const char *VbGetClassPropertyString(const char *name, char *dest, size_t size)
+{
+	if (!strcasecmp(name, "hwid") && size > 0)
+		return StrCopy(dest, "REVEN A6A-A9U", size);
+
+	return NULL;
+}
+
+int VbSetClassPropertyInt(const char *name, int value)
+{
+	return -1;
+}
+
+int VbSetClassPropertyString(const char *name, const char *value) {
+	return -1;
+}
