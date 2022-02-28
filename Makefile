coreboot/vboot

commit f0af257a9b333b27d6ed9540ffbbb5cf4e20ba12

vboot_ref: Reshuffle parallel flashrom impl
Bring sub-process and libflashrom wrapper implementations
under the common host/lib path to later be reconciled.
The WP implementation is left separated out due to the
lack of a libflashrom WP implementation which creates
circular dependencies for the moment.

BUG=b:207808292
BRANCH=none
TEST=none

Signed-off-by: Edward O'Callaghan <quasisec@google.com>
Change-Id: Ic759c4e9828778a0ef1443b163224aef2c7cba54
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/3461093
Reviewed-by: Nikolai Artemiev <nartemiev@google.com>
Auto-Submit: Edward O'Callaghan <quasisec@chromium.org>
Tested-by: Edward O'Callaghan <quasisec@chromium.org>
Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>

Makefile
@@ -436,6 +436,7 @@ ALL_OBJS += ${FWLIB_OBJS} ${TLCL_OBJS}
 COMMONLIB_SRCS = \
 	host/lib/fmap.c \
 	host/lib/flashrom.c \
+	host/lib/flashrom_drv.c \
 	host/lib/subprocess.c
 
 # Intermediate library for the vboot_reference utilities to link against.
@@ -664,7 +665,8 @@ USE_FLASHROM ?= 1
 ifneq ($(filter-out 0,${USE_FLASHROM}),)
 $(info building with libflashrom support)
 FLASHROM_LIBS := $(shell ${PKG_CONFIG} --libs flashrom)
-FUTIL_SRCS += futility/flashrom_drv.c \
+FUTIL_SRCS += host/lib/flashrom_drv.c \
+	futility/flashrom_wp_drv.c \
 	futility/updater_archive.c \
 	futility/updater_quirks.c \
 	futility/updater_utils.c \

futility/flashrom_wp_drv.c
@@ -1,0 +1,50 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * The utility functions for firmware updater.
+ */
+
+#include <libflashrom.h>
+
+#include "updater.h"
+
+#define FLASHROM_OUTPUT_WP_PATTERN "write protect is "
+
+/* System environment values. */
+static const char * const FLASHROM_OUTPUT_WP_ENABLED =
+			  FLASHROM_OUTPUT_WP_PATTERN "enabled",
+		  * const FLASHROM_OUTPUT_WP_DISABLED =
+			  FLASHROM_OUTPUT_WP_PATTERN "disabled";
+
+
+/* Helper function to return write protection status via given programmer. */
+enum wp_state flashrom_get_wp(const char *programmer)
+{
+	char *command, *result;
+	const char *postfix;
+	int r;
+
+	/* grep is needed because host_shell only returns 1 line. */
+	postfix = " 2>/dev/null | grep \"" FLASHROM_OUTPUT_WP_PATTERN "\"";
+
+
+	/* TODO(b/203715651): link with flashrom directly. */
+	ASPRINTF(&command, "flashrom --wp-status -p %s %s", programmer, postfix);
+
+	/* invokes flashrom(8) with non-zero result if error. */
+	result = host_shell(command);
+	strip_string(result, NULL);
+	free(command);
+	VB2_DEBUG("wp-status: %s\n", result);
+
+	if (strstr(result, FLASHROM_OUTPUT_WP_ENABLED))
+		r = WP_ENABLED;
+	else if (strstr(result, FLASHROM_OUTPUT_WP_DISABLED))
+		r = WP_DISABLED;
+	else
+		r = WP_ERROR;
+	free(result);
+
+	return r;
+}

futility/updater_utils.h
@@ -225,11 +225,4 @@ void init_system_properties(struct system_property *props, int num);
  */
 const char *get_firmware_rootkey_hash(const struct firmware_image *image);
 
-int flashrom_read_image(struct firmware_image *image, const char *region,
-			 int verbosity);
-int flashrom_write_image(const struct firmware_image *image,
-			const char *region,
-			const struct firmware_image *diff_image,
-			int do_verify, int verbosity);
-
 #endif  /* VBOOT_REFERENCE_FUTILITY_UPDATER_UTILS_H_ */

host/lib/flashrom_drv.c
@@ -11,15 +11,9 @@
 #include "crossystem.h"
 #include "host_misc.h"
 #include "util_misc.h"
-#include "updater.h"
-
-#define FLASHROM_OUTPUT_WP_PATTERN "write protect is "
-
-/* System environment values. */
-static const char * const FLASHROM_OUTPUT_WP_ENABLED =
-			  FLASHROM_OUTPUT_WP_PATTERN "enabled",
-		  * const FLASHROM_OUTPUT_WP_DISABLED =
-			  FLASHROM_OUTPUT_WP_PATTERN "disabled";
+//#include "updater.h"
+#include "../../futility/futility.h"
+#include "flashrom.h"
 
 // global to allow verbosity level to be injected into callback.
 static enum flashrom_log_level g_verbose_screen = FLASHROM_MSG_INFO;
@@ -191,37 +185,6 @@ err_cleanup:
 	flashrom_layout_release(layout);
 	flashrom_flash_release(flashctx);
 	free(tmp);
-
-	return r;
-}
-
-/* Helper function to return write protection status via given programmer. */
-enum wp_state flashrom_get_wp(const char *programmer)
-{
-	char *command, *result;
-	const char *postfix;
-	int r;
-
-	/* grep is needed because host_shell only returns 1 line. */
-	postfix = " 2>/dev/null | grep \"" FLASHROM_OUTPUT_WP_PATTERN "\"";
-
-
-	/* TODO(b/203715651): link with flashrom directly. */
-	ASPRINTF(&command, "flashrom --wp-status -p %s %s", programmer, postfix);
-
-	/* invokes flashrom(8) with non-zero result if error. */
-	result = host_shell(command);
-	strip_string(result, NULL);
-	free(command);
-	VB2_DEBUG("wp-status: %s\n", result);
-
-	if (strstr(result, FLASHROM_OUTPUT_WP_ENABLED))
-		r = WP_ENABLED;
-	else if (strstr(result, FLASHROM_OUTPUT_WP_DISABLED))
-		r = WP_DISABLED;
-	else
-		r = WP_ERROR;
-	free(result);
 
 	return r;
 }

host/lib/include/flashrom.h
@@ -40,6 +40,8 @@ struct firmware_image {
  * @return VB2_SUCCESS on success, or a relevant error.
  */
 vb2_error_t flashrom_read(struct firmware_image *image, const char *region);
+int flashrom_read_image(struct firmware_image *image, const char *region,
+			 int verbosity);
 
 /**
  * Write using flashrom from a buffer.
@@ -52,3 +54,7 @@ vb2_error_t flashrom_read(struct firmware_image *image, const char *region);
  * @return VB2_SUCCESS on success, or a relevant error.
  */
 vb2_error_t flashrom_write(struct firmware_image *image, const char *region);
+int flashrom_write_image(const struct firmware_image *image,
+			const char *region,
+			const struct firmware_image *diff_image,
+			int do_verify, int verbosity);

supechicken/vboot_reference

commit 76670e7a7cd6a2730c03cd7880a45b2de951d11a

vboot_reference/Makefile: Fix building with USE_FLASHROM:=0
Unfortunately the USE_FLASHROM build flag broke during a refactor
in commit f0af257a9b333b. This flag is needed for coreboot builds.
Resolve by putting relevant files under guards.

BUG=b:221234560,b:220833585
BRANCH=none
TEST=`USE_FLASHROM={0,1} make`

Signed-off-by: Edward O'Callaghan <quasisec@google.com>
Change-Id: Ic10a3909bf070897cef7f1556deef57eba6c57f3

Makefile
@@ -433,11 +433,18 @@ FWLIB_OBJS = ${FWLIB_SRCS:%.c=${BUILD}/%.o}
 TLCL_OBJS = ${TLCL_SRCS:%.c=${BUILD}/%.o}
 ALL_OBJS += ${FWLIB_OBJS} ${TLCL_OBJS}
 
+# Maintain behaviour of default on.
+USE_FLASHROM ?= 1
+
+ifneq ($(filter-out 0,${USE_FLASHROM}),)
+$(info building with libflashrom support)
+FLASHROM_LIBS := $(shell ${PKG_CONFIG} --libs flashrom)
 COMMONLIB_SRCS = \
-	host/lib/fmap.c \
 	host/lib/flashrom.c \
 	host/lib/flashrom_drv.c \
 	host/lib/subprocess.c
+CFLAGS += -DUSE_FLASHROM
+endif
 
 # Intermediate library for the vboot_reference utilities to link against.
 UTILLIB = ${BUILD}/libvboot_util.a
@@ -458,6 +465,7 @@ UTILLIB_SRCS = \
 	host/lib/crypto.c \
 	host/lib/file_keys.c \
 	$(COMMONLIB_SRCS) \
+	host/lib/fmap.c \
 	host/lib/host_common.c \
 	host/lib/host_key2.c \
 	host/lib/host_keyblock.c \
@@ -516,6 +524,7 @@ HOSTLIB_SRCS = \
 	host/lib/crypto.c \
 	host/lib/extract_vmlinuz.c \
 	$(COMMONLIB_SRCS) \
+	host/lib/fmap.c \
 	host/lib/host_misc.c \
 	host/lib21/host_misc.c \
 	${TLCL_SRCS}
@@ -659,19 +668,13 @@ FUTIL_SRCS = \
 	futility/vb1_helper.c \
 	futility/vb2_helper.c
 
-# Maintain behaviour of default on.
-USE_FLASHROM ?= 1
-
 ifneq ($(filter-out 0,${USE_FLASHROM}),)
-$(info building with libflashrom support)
-FLASHROM_LIBS := $(shell ${PKG_CONFIG} --libs flashrom)
 FUTIL_SRCS += host/lib/flashrom_drv.c \
 	futility/flashrom_wp_drv.c \
 	futility/updater_archive.c \
 	futility/updater_quirks.c \
 	futility/updater_utils.c \
 	futility/updater.c
-CFLAGS += -DUSE_FLASHROM
 endif
 
 # List of commands built in futility.