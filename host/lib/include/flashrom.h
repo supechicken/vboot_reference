8a1e1867a72d7ee3a4fe5c1e168238f74d22d7c5
Hung-Te Lin <hungte@chromium.org>
2022-03-07 16:34:58 +0800

    futility: flashrom_drv: merge parameters to a single flashrom_params
    There may be more and more optional flags and parameters that we will
    want to override (for example --noverify_all) from the futility programs
    when calling the flashrom driver. Having a huge parameter list is not
    easy for maintenance, so a new structure flashrom_params is introduced
    for adding new parameters easily.
    
    BUG=None
    TEST=make; build and run test
    BRANCH=None
    
    Change-Id: Ic8a8cf01a5e57d19cf9b073af4491a36c4ed26d6
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>

--- host/lib/include/flashrom.h
+++ host/lib/include/flashrom.h
@@ -6,6 +6,7 @@
  */
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "2return_codes.h"
 #include "fmap.h"
@@ -29,6 +30,18 @@ struct firmware_image {
 	FmapHeader *fmap_header;
 };
 
+/* Parameters when invoking flashrom. */
+struct flashrom_params {
+	struct firmware_image *image; /* The firmware image to read/write. */
+	const struct firmware_image *flash_contents; /* --flash-contents */
+	const char *const *regions; /* -i: only read/write <region> */
+	bool force; /* -f: force specific operations  */
+	bool noverify; /* -n: don't auto-verify */
+	bool noverify_all; /* -N: verify included regions only */
+	int verbose; /* -V: more verbose output */
+	/* do_not_diff and ignore_lock are currently not supported. */
+};
+
 /**
  * Read using flashrom into an allocated buffer.
  *
@@ -40,8 +53,7 @@ struct firmware_image {
  * @return VB2_SUCCESS on success, or a relevant error.
  */
 vb2_error_t flashrom_read(struct firmware_image *image, const char *region);
-int flashrom_read_image(struct firmware_image *image, const char *region,
-			 int verbosity);
+int flashrom_read_image(const struct flashrom_params * const params);
 
 /**
  * Write using flashrom from a buffer.
@@ -55,7 +67,4 @@ int flashrom_read_image(struct firmware_image *image, const char *region,
  * @return VB2_SUCCESS on success, or a relevant error.
  */
 vb2_error_t flashrom_write(struct firmware_image *image, const char *region);
-int flashrom_write_image(const struct firmware_image *image,
-			const char * const regions[],
-			const struct firmware_image *diff_image,
-			int do_verify, int verbosity);
+int flashrom_write_image(const struct flashrom_params * const params);
