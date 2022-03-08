9415450da8e0dd60acb662c755c4d3f326abe248
Robert Zieba <robertzieba@google.com>
2022-03-08 09:07:12 -0700

    scripts/image_signing: Add ensure_amd_psp_flags script
    Currently there is no script to ensure that AMD PSP flags are set
    correctly in a firmware image. This commit adds ensure_amd_psps_flags to
    handle that functionality. The script can check that certain flags are
    set as well as checking that certain flags are not set. The amdfwread
    tool is currently under review on coreboot.
    
    BRANCH=none
    BUG=b:202397678
    TEST=Ran script with ChromeOS image, verified that it responds correctly
    to PSP flag values
    
    Signed-off-by: Robert Zieba <robertzieba@google.com>
    Change-Id: Ie0864544b9b97704ee901d893b4d833c1ab068b9

---
+++ scripts/image_signing/ensure_amd_psp_flags.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# abort on error
+set -e
+
+# Load common constants and variables.
+. "$(dirname "$0")/common.sh"
+
+required_bits_guybrush=$((1 << 58)) # PSP_S0I3_RESUME_VERSTAGE bit
+forbidden_bits_guybrush="0x0"
+
+die_umount() {
+  sudo umount "$2"
+  die "$1"
+}
+
+main() {
+if [[ $# -ne 2 ]]; then
+    echo "Usage $0 <board> <image>"
+    exit 1
+  fi
+
+  local board="$1"
+  local image="$2"
+
+  echo "${board}"
+
+  local loopdev rootfs
+  if [[ -d "${image}" ]]; then
+    rootfs="${image}"
+  else
+    rootfs=$(make_temp_dir)
+    loopdev=$(loopback_partscan "${image}")
+    mount_loop_image_partition_ro "${loopdev}" 3 "${rootfs}"
+  fi
+
+  # Get the board for this image
+  local firmware_bundle shellball_dir
+  firmware_bundle="${rootfs}/usr/sbin/chromeos-firmwareupdate"
+  shellball_dir=$(make_temp_dir)
+
+  local required_bits forbidden_bits required_name forbidden_name
+  required_name=required_bits_${board}
+  forbidden_name=forbidden_bits_${board}
+  required_bits=${!required_name}
+  forbidden_bits=${!forbidden_name}
+
+  if [ -z "${forbidden_bits}" ] && [ -z "${required_bits}" ]; then
+    # Don't do any checking if we don't have at least one set of bits defined
+    sudo umount "${rootfs}"
+    exit
+  fi
+
+  # Substitute 0 if a set is missing
+  required_bits=${required_bits:-"0x0"}
+  forbidden_bits=${forbidden_bits:-"0x0"}
+
+  # Extract our firmware
+  if ! extract_firmware_bundle "${firmware_bundle}" "${shellball_dir}"; then
+        die_umount "${rootfs}" "Failed to extract firmware bundle"
+  fi
+
+  # Check the soft fuse bits in each FW image
+  local images
+  images=$(ls "${shellball_dir}"/images/bios-*)
+  echo ${images}
+  for image in ${images}
+  do
+    # Output is Soft-fuse:value
+    local soft_fuse forbidden_set
+    soft_fuse=$(amdfwread --soft-fuse "${image}" | \
+      sed -E -n 's/Soft-fuse:(0[xX][0-9a-fA-F]+)/\1/p')
+
+    if [ -z "${soft_fuse}" ]; then
+      die_umount "Failed to get soft-fuse bits from firmware" "${rootfs}"
+    fi
+
+    forbidden_set=$((soft_fuse & forbidden_bits))
+    if [ "${forbidden_set}" != 0 ]; then
+      die_umount "$(printf "%s: Forbidden AMD PSP soft fuse bits set: 0x%x" \
+        "${image}" "${forbidden_set}")" "${rootfs}"
+    fi
+
+    if [ $((soft_fuse & required_bits)) != $((required_bits)) ]; then
+      die_umount "$(printf "%s: Required AMD PSP soft fuse bits not set: 0x%x" \
+        "${image}" "$((~soft_fuse & required_bits))")" "${rootfs}"
+    fi
+  done
+}
+main "$@"
