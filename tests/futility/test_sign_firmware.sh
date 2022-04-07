9b5ffd3c51b30f8a987791a5d1aed563784cefbf
Jakub Czapiga <jacz@semihalf.com>
2022-04-07 14:44:44 +0200

    futility/file_type_bios: Rework ft_sign_bios()
    Previous implementation of ft_sign_bios() was signing whole untruncated
    FW_MAIN_x areas. This patch adds truncation and saving of CBFS area
    size. It also makes BIOS sign command able to sign image, when there is
    only "A" slot available, which is required by current coreboot build
    system.
    
    BUG=b:197114807
    TEST=sudo emerge vboot_reference
    TEST=build whole chromeos-bootimage after making it and coreboot use
         `futility sign --type bios ...`
    TEST=make runtests
    BRANCH=none
    
    Signed-off-by: Jakub Czapiga <jacz@semihalf.com>
    Change-Id: I7c84aa38776e8890a87f0e9b7ec7f32d86f82c13

--- tests/futility/test_sign_firmware.sh
+++ tests/futility/test_sign_firmware.sh
@@ -3,8 +3,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-me=${0##*/}
-TMP="$me.tmp"
+me="${0##*/}"
+TMP="${me}.tmp"
 
 # Work in scratch directory
 cd "$OUTDIR"
@@ -19,12 +19,27 @@ ${SCRIPT_DIR}/futility/data/bios_link_mp.bin
 ${SCRIPT_DIR}/futility/data/bios_peppy_mp.bin
 "
 
+# BIOS image containing CBFS RW/A and RW/B, and signed with developer keys.
+GOOD_CBFS="${SCRIPT_DIR}/futility/data/bios_voxel_dev.bin"
+
+# BIOS image with FW_MAIN_B and VBLOCK_B removed from FlashMap
+NO_B_SLOT="${SCRIPT_DIR}/futility/data/bios_voxel_dev.no_b_slot.bin"
+
+# BIOS image containing CBFS RW/A and RW/B, and signed with developer keys.
+INFILES="${INFILES}
+${GOOD_CBFS}
+"
+
 # We also want to test that we can sign an image without any valid firmware
 # preambles. That one won't be able to tell how much of the FW_MAIN region is
 # the valid firmware, so it'll have to sign the entire region.
 GOOD_VBLOCKS="${SCRIPT_DIR}/futility/data/bios_peppy_mp.bin"
 ONEMORE=bios_peppy_mp_no_vblock.bin
+CLEAN_B=bios_peppy_mp_clean_b_slot.bin
 cp "${GOOD_VBLOCKS}" "${ONEMORE}"
+cp "${GOOD_VBLOCKS}" "${CLEAN_B}"
+
+
 "${FUTILITY}" load_fmap "${ONEMORE}" VBLOCK_A:/dev/urandom VBLOCK_B:/dev/zero
 INFILES="${INFILES} ${ONEMORE}"
 
@@ -36,40 +51,13 @@ for infile in $INFILES; do
   base=${infile##*/}
 
   : $(( count++ ))
-  echo -n "$count " 1>&3
+  echo -n "${count} " 1>&3
 
   outfile="${TMP}.${base}.new"
   loemid="loem"
   loemdir="${TMP}.${base}_dir"
 
   mkdir -p "${loemdir}"
-
-  # resign_firmwarefd.sh works on BIOS image files. The args are:
-  #
-  #   infile
-  #   outfile
-  #   firmware_datakey
-  #   firmware_keyblock
-  #   dev_firmware_datakey   (these are only used if RW A & RW B differ)
-  #   dev_firmware_keyblock
-  #   kernel_subkey
-  #   firmware_version
-  #   preamble_flag
-  #   loem_output_dir        (optional: dir for copy of new vblocks)
-  #   loemid                 (optional: copy new vblocks using this name)
-  #
-  #OLD  ${BIN_DIR}/resign_firmwarefd.sh \
-  #OLD    ${infile} \
-  #OLD    ${outfile} \
-  #OLD    ${KEYDIR}/firmware_data_key.vbprivk \
-  #OLD    ${KEYDIR}/firmware.keyblock \
-  #OLD    ${KEYDIR}/dev_firmware_data_key.vbprivk \
-  #OLD    ${KEYDIR}/dev_firmware.keyblock \
-  #OLD    ${KEYDIR}/kernel_subkey.vbpubk \
-  #OLD    14 \
-  #OLD    8 \
-  #OLD    ${loemdir} \
-  #OLD    ${loemid}
 
   "${FUTILITY}" sign \
     -s "${KEYDIR}/firmware_data_key.vbprivk" \
@@ -137,9 +125,10 @@ cmp "${TMP}.onemore.body" "${TMP}.good.fw_main"
 
 
 # Sign the last one again but don't specify the version or the preamble flags.
-# The version should default to 1, but the preamble flags should be preserved.
+# The firmware version and preamble flags should be preserved.
+# NOTICE: Version preservation behavior changed from defaulting to 1.
 : $(( count++ ))
-echo -n "$count " 1>&3
+echo -n "${count} " 1>&3
 
 "${FUTILITY}" sign \
   -s "${KEYDIR}/firmware_data_key.vbprivk" \
@@ -148,13 +137,13 @@ echo -n "$count " 1>&3
   "${MORE_OUT}" "${MORE_OUT}.2"
 
 m=$("${FUTILITY}" verify --publickey "${KEYDIR}/root_key.vbpubk" \
-      "${MORE_OUT}.2" | grep -c -E 'Firmware version: +1$|Preamble flags: +8$')
+      "${MORE_OUT}.2" | grep -c -E 'Firmware version: +14$|Preamble flags: +8$')
 [ "$m" = "4" ]
 
 
 # If the original preamble is not present, the preamble flags should be zero.
 : $(( count++ ))
-echo -n "$count " 1>&3
+echo -n "${count} " 1>&3
 
 "${FUTILITY}" load_fmap "${MORE_OUT}" VBLOCK_A:/dev/urandom VBLOCK_B:/dev/zero
 "${FUTILITY}" sign \
@@ -165,9 +154,106 @@ echo -n "$count " 1>&3
 
 m=$("${FUTILITY}" verify --publickey "${KEYDIR}/root_key.vbpubk" \
       "${MORE_OUT}.3" | grep -c -E 'Firmware version: +1$|Preamble flags: +0$')
-[ "$m" = "4" ]
+[ "${m}" = "4" ]
+
+# Check signing when B slot is zero
+: $(( count++ ))
+echo -n "${count} " 1>&3
+
+"${FUTILITY}" load_fmap "${CLEAN_B}" VBLOCK_B:/dev/zero FW_MAIN_B:/dev/zero
+"${FUTILITY}" sign \
+  -s "${KEYDIR}/firmware_data_key.vbprivk" \
+  -b "${KEYDIR}/firmware.keyblock" \
+  -k "${KEYDIR}/kernel_subkey.vbpubk" \
+  "${CLEAN_B}" "${CLEAN_B}.1"
+
+"${FUTILITY}" verify --publickey "${KEYDIR}/root_key.vbpubk" "${CLEAN_B}.1" \
+  | awk '/Firmware body size:/ {print $4}' > "${TMP}.clean_b.body"
+"${FUTILITY}" dump_fmap -p "${CLEAN_B}.1" \
+  | awk '/FW_MAIN_/ {print $3}' > "${TMP}.clean_b.fw_main"
+
+# These should not be equal, as FW_MAIN_A size should be kept intact, when size
+# of FW_MAIN_B should be taken from FlashMap
+if cmp "${TMP}.clean_b.body" "${TMP}.clean_b.fw_main" ; then false; fi
+if cmp "${TMP}.clean_b.body" "${TMP}.good.body" ; then false; fi
+cmp <(head -n1 "${TMP}.clean_b.body") <(head -n1 "${TMP}.good.body")
+cmp <(tail -n1 "${TMP}.clean_b.body") <(tail -n1 "${TMP}.clean_b.fw_main")
+
+# Check signing when there is no B slot
+: $(( count++ ))
+echo -n "${count} " 1>&3
+
+NO_B_SLOT_SIGNED_IMG="${TMP}.${NO_B_SLOT##*/}"
+"${FUTILITY}" sign \
+  -s "${KEYDIR}/firmware_data_key.vbprivk" \
+  -b "${KEYDIR}/firmware.keyblock" \
+  -k "${KEYDIR}/kernel_subkey.vbpubk" \
+  -v 1 \
+  "${NO_B_SLOT}" "${NO_B_SLOT_SIGNED_IMG}"
+
+"${FUTILITY}" verify --publickey "${KEYDIR}/root_key.vbpubk" "${GOOD_CBFS}" \
+  | awk '/Firmware body size:/ {print $4}' > "${TMP}.good_cbfs.body"
+"${FUTILITY}" verify --publickey "${KEYDIR}/root_key.vbpubk" \
+    "${NO_B_SLOT_SIGNED_IMG}" \
+  | awk '/Firmware body size:/ {print $4}' > "${TMP}.no_b_slot.body"
+"${FUTILITY}" dump_fmap -p "${NO_B_SLOT_SIGNED_IMG}" \
+  | awk '/FW_MAIN_/ {print $3}' > "${TMP}.no_b_slot.fw_main"
+
+if cmp "${TMP}.no_b_slot.body" "${TMP}.no_b_slot.fw_main" ; then false; fi
+cmp "${TMP}.no_b_slot.body" <(tail -n1 "${TMP}.good_cbfs.body") #FIXHIR
+
+m=$("${FUTILITY}" verify --publickey "${KEYDIR}/root_key.vbpubk" \
+    "${NO_B_SLOT_SIGNED_IMG}" \
+  | grep -c -E 'Firmware version: +1$|Preamble flags: +0$')
+[ "${m}" = "2" ]
+
+# Check signing when cbfstool reports incorrect size
+# Signing should fail, as it should not be possible for CBFS contents to be
+# bigger than FlashMap size of the area
+: $(( count++ ))
+echo -n "${count} " 1>&3
+
+CBFSTOOL_STUB="$(realpath "${TMP}.cbfs_stub.sh")"
+echo -en 'echo "0xFFEEDD0"; exit 0;' > "${CBFSTOOL_STUB}"
+chmod +x "${CBFSTOOL_STUB}"
+
+if CBFSTOOL="${CBFSTOOL_STUB}" "${FUTILITY}" sign \
+  -s "${KEYDIR}/firmware_data_key.vbprivk" \
+  -b "${KEYDIR}/firmware.keyblock" \
+  -k "${KEYDIR}/kernel_subkey.vbpubk" \
+  -v 1 \
+  "${GOOD_CBFS}" "${TMP}.1.${GOOD_CBFS##*/}"
+then
+  false
+fi
+
+# Redefine cbfstool stub to return valid value for FW_MAIN_A and invalid for
+# FW_MAIN_B size
+cp "${GOOD_CBFS}" "${TMP}.good_cbfs.bin"
+FW_MAIN_A_SIZE="$(printf '0x%x' \
+  "$(cbfstool "${TMP}.good_cbfs.bin" truncate -r FW_MAIN_A)")"
+MARK_FILE="$(realpath "${TMP}.mark1")"
+
+cat << EOF > "${CBFSTOOL_STUB}"
+#!/usr/bin/env bash
+if ! [ -f "${MARK_FILE}" ]; then
+  echo "${FW_MAIN_A_SIZE}";
+  echo 1 > "${MARK_FILE}";
+else
+  echo 0xFFFFAA0;
+  rm "${MARK_FILE}";
+fi
+exit 0;
+EOF
+
+CBFSTOOL="${CBFSTOOL_STUB}" "${FUTILITY}" --debug sign \
+  -s "${KEYDIR}/firmware_data_key.vbprivk" \
+  -b "${KEYDIR}/firmware.keyblock" \
+  -k "${KEYDIR}/kernel_subkey.vbpubk" \
+  -v 1 \
+  "${GOOD_CBFS}" "${TMP}.2.${GOOD_CBFS##*/}"
 
 
 # cleanup
-rm -rf "${TMP}"* "${ONEMORE}"
+rm -rf "${TMP}"* "${ONEMORE}" "${CBFSTOOL_STUB}"
 exit 0
