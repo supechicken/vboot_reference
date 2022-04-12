4f67a33bd1a576b5d3a0b7987e2fbdeb9c42a7c3
Jakub Czapiga <jacz@semihalf.com>
2022-04-12 14:49:22 +0200

    futility: Remove --devsign and --devkeyblock
    This feature has not been needed since pre-2012 devices which have long
    since reached their end of life. We can safely remove it to simplify the
    code.
    Also remove ZGB image, as it is no longer needed.
    
    BUG=b:197114807
    TEST=sudo FEATURES=test emerge vboot_reference
    BRANCH=none
    
    Signed-off-by: Jakub Czapiga <jacz@semihalf.com>
    Change-Id: I889dc6300c5cb72bdfcb9c2b66d63e97d3f8c862

--- scripts/image_signing/resign_firmwarefd.sh
+++ scripts/image_signing/resign_firmwarefd.sh
@@ -20,6 +20,7 @@ SRC_FD=$1
 DST_FD=$2
 FIRMWARE_DATAKEY=$3
 FIRMWARE_KEYBLOCK=$4
+# Dev datakey and keyblock ignored. Left for compatibility.
 DEV_FIRMWARE_DATAKEY=$5
 DEV_FIRMWARE_KEYBLOCK=$6
 KERNEL_SUBKEY=$7
@@ -28,12 +29,6 @@ VERSION=$8
 PREAMBLE_FLAG=$9
 LOEM_OUTPUT_DIR=${10}
 LOEMID=${11}
-
-if [ ! -e $DEV_FIRMWARE_KEYBLOCK ] || [ ! -e $DEV_FIRMWARE_DATAKEY ] ; then
-  echo "No dev firmware keyblock/datakey found. Reusing normal keys."
-  DEV_FIRMWARE_KEYBLOCK="$FIRMWARE_KEYBLOCK"
-  DEV_FIRMWARE_DATAKEY="$FIRMWARE_DATAKEY"
-fi
 
 # pass optional args
 [ -n "$VERSION" ] && VERSION="--version $VERSION"
@@ -44,8 +39,6 @@ fi
 exec ${FUTILITY} sign \
   --signprivate $FIRMWARE_DATAKEY \
   --keyblock $FIRMWARE_KEYBLOCK \
-  --devsign $DEV_FIRMWARE_DATAKEY \
-  --devkeyblock $DEV_FIRMWARE_KEYBLOCK \
   --kernelkey $KERNEL_SUBKEY \
   $VERSION \
   $PREAMBLE_FLAG \
