f5e883e4281fda6df3406ebfe2a8c7e6023257a7
Daichi Hirono <hirono@google.com>
2022-04-28 16:19:05 +0900

    signer: Strace sign_android_image.sh
    to find the root cause of unwanted directory removals.
    
    BRANCH=None
    BUG=b:230688519
    TEST=Run ./sign_official_build.sh locally
    Signed-off-by: Daichi Hirono <hirono@chromium.org>
    Change-Id: Ia66671100d9900e10931494c8de7628596266607

---
+++ scripts/image_signing/diagnose_strace.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+#
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import re
+import typing
+import logging
+
+
+class Line(typing.NamedTuple):
+  pid: int
+  syscall: typing.Optional[str] # None for exit line
+  result: typing.Optional[int] # None for unfinished line
+  line: str # Original line
+
+
+def parse(line: str) -> typing.Optional[Line]:
+  """Parse lines from strace log."""
+
+  m = re.search(r'^(\d+)\s+(\w+).+ = (-?\d+)', line)
+  if m:
+    return Line(int(m[1]), m[2], int(m[3]), line)
+
+  m = re.search(r'^(\d+)\s+(\w+).+<unfinished ...>$', line)
+  if m:
+    return Line(int(m[1]), m[2], None, line)
+
+  m = re.search(r'^(\d+)\s+<... (\w+) resumed>.+ = (-?\d+)', line)
+  if m:
+    return Line(int(m[1]), m[2], m[3], line)
+
+  m = re.search(r'^(\d+)\s+\+\+\+', line)
+  if m:
+    return Line(int(m[1]), None, None, line)
+
+  logging.warning('Unexpected pattern %s', line.rstrip())
+  return None
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('log_file')
+  args = parser.parse_args()
+
+  with open(args.log_file) as f:
+    lines = list(filter(bool, map(parse, f.readlines())))
+
+  # Print syscalls which meet one of the following criteria.
+  # a. Contains 'dalvik-cache' (the directory name is chosen because it's
+  #    ofthen removed unintentionally when image corruption happens and it has
+  #    unique name)
+  # b. Forks of a process chain to the process invoking a).
+  # c. Execs for the PIDs invoking a) and b).
+  # d. Exit code for the PIDs invoking a) and b).
+
+  marked_pids = set()
+  marked_logs = set()
+  for l in reversed(lines):
+    if 'dalvik-cache' in l.line:
+      marked_logs.add(l)
+      if l.pid:
+        marked_pids.add(l.pid)
+    if l.result in marked_pids and l.syscall in ('clone', 'fork', 'vfork'):
+      marked_logs.add(l)
+      if l.pid:
+        marked_pids.add(l.pid)
+
+  for l in lines:
+    if l in marked_logs or l.pid in marked_pids and l.syscall in ('execve', 'execveat', None):
+      print(l.line, end='')
+
+if __name__ == '__main__':
+  main()
