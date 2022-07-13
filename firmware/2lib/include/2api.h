b96e2b15568356c08ee7372e32761fce5c146888
Subrata Banik <subratabanik@google.com>
2022-07-13 23:48:23 +0530

    firmware/2lib: Introduce `vb2api_get_fw_vboot_info` API
    This patch introduces a new API named `vb2api_get_fw_vboot_info` to get
    the FW slot information like tries, current boot slot, previous boot
    slot, previous boot status and boot mode.
    
    Additionally, added the required data structures to keep those vboot
    information.
    
    BUG=b:215615970
    TEST=Able to compile the Google/Kano board using the emerge command.
    
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Change-Id: Ib3f197c851dc4b445dbf64868c3f9157a4f6c9fe

--- firmware/2lib/include/2api.h
+++ firmware/2lib/include/2api.h
@@ -317,6 +317,16 @@ enum vb2_boot_mode {
 	VB2_BOOT_MODE_NORMAL = 5,
 };
 
+
+/* Firmware slot codes */
+enum vb2_fw_slot {
+	/* Slot A */
+	VB2_FW_SLOT_A = 0,
+
+	/* Slot B */
+	VB2_FW_SLOT_B = 1,
+};
+
 /* Firmware result codes for VB2_NV_FW_RESULT and VB2_NV_FW_PREV_RESULT */
 enum vb2_fw_result {
 	/* Unknown */
@@ -1564,5 +1574,90 @@ uint32_t vb2ex_mtime(void);
  * @param msec			Duration in milliseconds.
  */
 void vb2ex_msleep(uint32_t msec);
+
+union vb2_fw_vboot_info {
+	uint8_t raw[4];
+	struct {
+		uint8_t tries       : 4;
+		uint8_t slot        : 1;
+		uint8_t prev_slot   : 1;
+		uint8_t prev_result : 2;
+		uint8_t boot_mode;
+		/* The following 2 bytes only exist for recovery mode */
+		uint8_t recovery_reason;
+		uint8_t recovery_subcode;
+	};
+};
+
+/**
+ * Return `vb2_fw_vboot_info` and can be used
+ * to log information about the current boot in a compact format.
+ *
+ * Note: Only call this API at minimum after `vb2api_fw_phase2` function
+ * returns.
+ *
+ * @param ctx          Vboot context
+ * @return filled out vb2 info as per `union vb2_fw_vboot_info`.
+ */
+union vb2_fw_vboot_info vb2api_get_fw_boot_info(struct vb2_context *ctx);
+
+/**
+ * Convert Firmware Boot Mode into supported string
+ *
+ * @return char*   firmware boot mode string
+ */
+static inline const char *vb2api_boot_mode_string(uint8_t boot_mode)
+{
+	switch ((enum vb2_boot_mode)boot_mode) {
+	/* 0x00 */ case VB2_BOOT_MODE_UNDEFINED:
+		return "Undefined";
+	/* 0x01 */ case VB2_BOOT_MODE_MANUAL_RECOVERY:
+		return "Manual recovery";
+	/* 0x02 */ case VB2_BOOT_MODE_BROKEN_SCREEN:
+		return "Broken screen";
+	/* 0x03 */ case VB2_BOOT_MODE_DIAGNOSTICS:
+		return "Diagnostic";
+	/* 0x04 */ case VB2_BOOT_MODE_DEVELOPER:
+		return "Developer";
+	/* 0x05 */ case VB2_BOOT_MODE_NORMAL:
+		return "Secure";
+	}
+
+	return "Unknown";
+}
+
+/**
+ * Convert Firmware Slot result into supported string
+ *
+ * @return char*   firmware slot result string
+ */
+static inline const char *vb2api_result_string(uint8_t result)
+{
+	switch ((enum vb2_fw_result)result) {
+	/* 0x00 */ case VB2_FW_RESULT_UNKNOWN:
+		return "Unknown";
+	/* 0x01 */ case VB2_FW_RESULT_TRYING:
+		return "Trying";
+	/* 0x02 */ case VB2_FW_RESULT_SUCCESS:
+		return "Success";
+	/* 0x03 */ case VB2_FW_RESULT_FAILURE:
+		return "Failure";
+	}
+
+	return "Unknown";
+}
+
+/**
+ * Convert Firmware Slot into supported string
+ *
+ * @return char*   firmware slot name string
+ */
+static inline const char *vb2api_slot_string(uint8_t slot)
+{
+	if ((enum vb2_fw_slot)slot == VB2_FW_SLOT_A)
+	/* 0x00 */ return "A";
+	else
+	/* 0x01 */ return "B";
+}
 
 #endif  /* VBOOT_REFERENCE_2API_H_ */
