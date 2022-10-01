f1570b2a31cb0ba9ada70285f70954e545ecb999
Vadim Bendebury <vbendeb@chromium.org>
2022-10-01 10:21:36 -0700

    devkeys: rename arv root key
    Change the name of the generated AP RO verification root key file to
    match the name generated on the HSM. Generate a new root key and
    re-sign the platform key to make sure that git history shows proper
    provenance of the platform keyblock.
    
    The keyblock was generated as follows:
    
    $ futility vbutil_keyblock --pack tests/devkeys/arv_platform.keyblock \
             --datapubkey tests/devkeys/arv_platform.vbpubk \
    	 --signprivate tests/devkeys/root_key_arv_root.vbprivk
    
    BUG=b:247652363
    TEST=used the new keys to sign and verify a Nissa AP firmware image
    
    Change-Id: Ibe73472a65eb78c118ff08a642c1e111914874b5

--- scripts/keygeneration/create_new_keys.sh
+++ scripts/keygeneration/create_new_keys.sh
@@ -158,7 +158,7 @@ main() {
   make_pair recovery_kernel_data_key ${recovery_kernel_algoid}
   make_pair minios_kernel_data_key   ${minios_kernel_algoid}
   make_pair installer_kernel_data_key ${installer_kernel_algoid}
-  make_pair arv_root ${ARV_ROOT_ALGOID}
+  make_pair root_key_arv_root ${ARV_ROOT_ALGOID}
   make_pair arv_platform ${ARV_PLATFORM_ALGOID}
 
   # Create the firmware keyblock for use only in Normal mode. This is redundant,
