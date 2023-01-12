5d57f66a8c0e74ff2d1b86aebc0b441960738cdf
Evan Benn <evanbenn@chromium.org>
2023-01-12 17:22:57 +1100

    2lib: Add vb2_get_gbb_flag_description
    Add a function to convert a gbb flag to the name and description of that
    flag. Use this function in cmd_gbb to format a help page and implement
    --explicit.
    
    BUG=b:260531154
    BRANCH=None
    TEST=futility gbb --get /dev/bios -e
    TEST=futility gbb --help
    
    Change-Id: I884b6e0e7322128409f8d62d76824d8e6e6ca330
    Signed-off-by: Evan Benn <evanbenn@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/4161092
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>

--- firmware/2lib/include/2gbb_flags.h
+++ firmware/2lib/include/2gbb_flags.h
@@ -10,6 +10,8 @@
 #ifndef VBOOT_REFERENCE_2GBB_FLAGS_H_
 #define VBOOT_REFERENCE_2GBB_FLAGS_H_
 
+#include "2return_codes.h"
+
 enum vb2_gbb_flag {
 	/*
 	 * Reduce the dev screen delay to 2 sec from 30 sec to speed up
@@ -95,5 +97,9 @@ enum vb2_gbb_flag {
 	/* Enforce CSE SYNC, even if current CSE is same as CBFS CSE */
 	VB2_GBB_FLAG_FORCE_CSE_SYNC = 1 << 17,
 };
+
+vb2_error_t vb2_get_gbb_flag_description(enum vb2_gbb_flag flag,
+					 const char **name,
+					 const char **description);
 
 #endif  /* VBOOT_REFERENCE_2GBB_FLAGS_H_ */
