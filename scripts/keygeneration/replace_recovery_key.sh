fbbbb77803525de53d49a5ee6e55479670ed2cfc
Julius Werner <jwerner@chromium.org>
2023-01-25 18:46:39 -0800

    scripts/keygeneration: Add replace_recovery_key.sh
    This patch adds a new keygeneration script that can be used to replace
    the recovery key from an existing key set, but preserved the old key as
    a secondary recovery_key_2. All dependent kernel data keys are
    regenerated, but we will create both keyblocks signed by the new and the
    old recovery key. This is useful in cases where we want newly produced
    devices to use a different recovery key that will no longer boot older
    images, but still give older boards already shipped with the old
    recovery key a chance to boot new recovery images built after that point
    (if they have been dual-signed).
    
    BRANCH=none
    BUG=b:266371047
    TEST=Created new keyset, ran the script, manually reviewed newly created
    keys.
    
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Change-Id: Id240c26815cc6ee883315a65e788c68e1a0549e4

---
+++ scripts/keygeneration/replace_recovery_key.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Script to replace the recovery key with a newly generated one, and retain the
+# old recovery key (and corresponding keyblocks) as recovery_key_2. This is
+# necessary when we want to prevent units fabricated in the future from booting
+# current recovery or factory shim images, but still want future recovery and
+# factory shim images to be able to run on both new units and those that had
+# already been shipped with the old recovery key.
+
+# Load common constants and variables.
+. "$(dirname "$0")/common.sh"
+
+# Abort on errors.
+set -e
+
+if [ $# -ne 1 ]; then
+  cat <<EOF
+Usage: $0 <keyset directory>
+
+Creates a new recovery_key (incl. dependent kernel data keys)
+and renames the old one to recovery_key_2.
+EOF
+  exit 1
+fi
+
+KEY_DIR=$1
+
+# Recovery key versions are not meaningful, so always use "1".
+VERSION="1"
+
+main() {
+  cd "${KEY_DIR}"
+
+  if [ -e "recovery_key_2.vbpubk" ] || [ -e "recovery_key_2.vbprivk" ]; then
+    echo "recovery_key_2 already exists!"
+    return 1
+  fi
+
+  echo
+  echo "Moving old recovery key to recovery_key_2."
+
+  for ext in "vbpubk" "vbprivk"; do
+    mv "recovery_key.${ext}" "recovery_key_2.${ext}"
+  done
+
+  echo
+  echo "Backing up old kernel data keys (no longer needed) as XXX.old_2.YYY."
+
+  for k in "recovery_kernel" "installer_kernel" "minios_kernel"; do
+    for ext in "vbpubk" "vbprivk"; do
+      mv "${k}_data_key.${ext}" "${k}_data_key.old_2.${ext}"
+    done
+    mv "${k}.keyblock" "${k}.old_2.keyblock"
+  done
+
+  echo
+  echo "Creating new recovery key."
+
+  make_pair recovery_key "${RECOVERY_KEY_ALGOID}" "${VERSION}"
+
+  echo
+  echo "Creating new recovery, minios and installer kernel data keys."
+
+  make_pair recovery_kernel_data_key "${RECOVERY_KERNEL_ALGOID}" "${VERSION}"
+  make_pair minios_kernel_data_key "${MINIOS_KERNEL_ALGOID}" "${VERSION}"
+  make_pair installer_kernel_data_key "${INSTALLER_KERNEL_ALGOID}" "${VERSION}"
+
+  echo
+  echo "Creating new keyblocks signed with new recovery key."
+
+  make_keyblock recovery_kernel "${RECOVERY_KERNEL_KEYBLOCK_MODE}" recovery_kernel_data_key recovery_key
+  make_keyblock minios_kernel "${MINIOS_KERNEL_KEYBLOCK_MODE}" minios_kernel_data_key recovery_key
+  make_keyblock installer_kernel "${INSTALLER_KERNEL_KEYBLOCK_MODE}" installer_kernel_data_key recovery_key
+
+  echo
+  echo "Creating secondary XXX_2.keyblocks signing new kernel data keys with old recovery key."
+
+  make_keyblock recovery_kernel_2 "${RECOVERY_KERNEL_KEYBLOCK_MODE}" recovery_kernel_data_key recovery_key_2
+  make_keyblock minios_kernel_2 "${MINIOS_KERNEL_KEYBLOCK_MODE}" minios_kernel_data_key recovery_key_2
+  make_keyblock installer_kernel_2 "${INSTALLER_KERNEL_KEYBLOCK_MODE}" installer_kernel_data_key recovery_key_2
+
+  echo
+  echo "All done. ($(basename "$0"))"
+}
+
+main "$@"
