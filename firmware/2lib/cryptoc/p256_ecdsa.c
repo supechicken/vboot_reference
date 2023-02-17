fefe8b210a348450452725eef44a096c1bed35db
Yi Chou <yich@google.com>
2023-02-17 14:37:02 +0800

    2lib: Import P256 ECDSA support from cryptoc
    According to the current widevine L1 design, we will need the P256
    ECDSA signing support in the coreboot & depthcharge to calculate the
    Boot Certificate Chain (BCC).
    
    Import the necessary crypto related code from third_party/cryptoc.
    And remove the unused code.
    
    BUG=b:248612641
    TEST=CQ
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I42bade69eddcdf60c64f721bd082d81b59221ae5

---
+++ firmware/2lib/cryptoc/p256_ecdsa.c
@@ -0,0 +1,82 @@
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "2sha.h"
+#include "cryptoc/p256_ecdsa.h"
+#include "cryptoc/p256.h"
+
+// Compute k based on given {key, message} pair, 0 < k < n.
+static void determine_k(const p256_int *key, const p256_int *message,
+			uint8_t *tweak, p256_int *k)
+{
+	p256_int p;
+
+	do {
+		// SHA256 prng pick until p <= SECP256r1_n - 2,
+		// in a (re)pick and test fashion for a clean distribution.
+
+		struct vb2_sha256_context ctx;
+		uint8_t tmp[P256_NBYTES];
+
+		vb2_sha256_init(&ctx, VB2_HASH_SHA256);
+
+		p256_to_bin(key, tmp);
+		vb2_sha256_update(&ctx, tmp, P256_NBYTES);
+
+		vb2_sha256_update(&ctx, tweak, 1);
+
+		p256_to_bin(message, tmp);
+		vb2_sha256_update(&ctx, tmp, P256_NBYTES);
+
+		vb2_sha256_finalize(&ctx, tmp, VB2_HASH_SHA256);
+		p256_from_bin(tmp, &p);
+	} while (p256_cmp(&p, &SECP256r1_nMin2) > 0);
+
+	p256_add_d(&p, 1, k); // 0 < k < SECP256r1_n
+	p256_clear(&p);
+}
+
+void p256_ecdsa_sign(const p256_int *key, const p256_int *message, p256_int *r,
+		     p256_int *s)
+{
+	uint8_t tweak = 'A';
+	p256_digit top;
+
+	for (;;) {
+		p256_int k, kinv;
+
+		determine_k(key, message, &tweak, &k);
+		p256_base_point_mul(&k, r, s);
+		p256_mod(&SECP256r1_n, r, r);
+
+		// Make sure r != 0
+		if (p256_is_zero(r))
+			continue;
+
+		p256_modmul(&SECP256r1_n, r, 0, key, s);
+		top = p256_add(s, message, s);
+		p256_modinv(&SECP256r1_n, &k, &kinv);
+		p256_modmul(&SECP256r1_n, &kinv, top, s, s);
+
+		// (Attempt to) clear stack state.
+		p256_clear(&k);
+		p256_clear(&kinv);
+
+		// Make sure s != 0
+		if (p256_is_zero(s))
+			continue;
+
+		break;
+	}
+}
