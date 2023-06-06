coreboot/vboot

commit 1ee88b4cb3781af8795bb64b94ac90289f0c70e4

futility/: Inline write_firmware() as write_system_firmware()
Avoid confusing indirections.

BUG=b:282585789
BRANCH=none
TEST=`cros_run_unit_tests --host --packages vboot_reference`.

Change-Id: I5fdc01affcce2399955b594abf98c7550815b56b
Signed-off-by: Edward O'Callaghan <quasisec@google.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/4532319
Tested-by: Edward O'Callaghan <quasisec@chromium.org>
Reviewed-by: Nikolai Artemiev <nartemiev@google.com>
Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>

futility/updater.c
@@ -292,16 +292,6 @@ static int set_try_cookies(struct updater_config *cfg, const char *target,
 	return 0;
 }
 
-int write_firmware(struct updater_config *cfg,
-		   const struct firmware_image *image, const char *section_name)
-{
-	const char *sections[2] = {0};
-
-	sections[0] = section_name;
-	return write_system_firmware(cfg, image,
-				     section_name ? sections : NULL);
-}
-
 /*
  * Returns True if we should start the update process for given image.
  */
@@ -348,7 +338,9 @@ static int write_ec_firmware(struct updater_config *cfg,
 		return 0;
 	}
 
-	return write_firmware(cfg, image, section_name);
+	/* TODO(quasisec): Uses cros_ec to program the EC. */
+	const char *sections[2] = { section_name, NULL };
+	return write_system_firmware(cfg, image, sections);
 }
 
 /*
@@ -925,7 +917,8 @@ static enum updater_error_codes update_try_rw_firmware(
 		STATUS("TRY-RW UPDATE: Updating %s to try on reboot.\n",
 		       target);
 
-		if (write_firmware(cfg, image_to, target))
+		const char *sections[2] = { target, NULL };
+		if (write_system_firmware(cfg, image_to, sections))
 			return UPDATE_ERR_WRITE_FIRMWARE;
 
 		/*
@@ -955,7 +948,8 @@ static enum updater_error_codes update_try_rw_firmware(
 	if (legacy_needs_update(cfg)) {
 		has_update = 1;
 		STATUS("LEGACY UPDATE: Updating %s.\n", FMAP_RW_LEGACY);
-		write_firmware(cfg, image_to, FMAP_RW_LEGACY);
+		const char *sections[2] = { FMAP_RW_LEGACY, NULL };
+		write_system_firmware(cfg, image_to, sections);
 	}
 
 	return UPDATE_ERR_DONE;
@@ -1032,7 +1026,8 @@ static enum updater_error_codes update_legacy_firmware(
 {
 	STATUS("LEGACY UPDATE: Updating firmware %s.\n", FMAP_RW_LEGACY);
 
-	if (write_firmware(cfg, image_to, FMAP_RW_LEGACY))
+	const char *sections[2] = { FMAP_RW_LEGACY, NULL };
+	if (write_system_firmware(cfg, image_to, sections))
 		return UPDATE_ERR_WRITE_FIRMWARE;
 
 	return UPDATE_ERR_DONE;
@@ -1090,7 +1085,7 @@ static enum updater_error_codes update_whole_firmware(
 		return UPDATE_ERR_TPM_ROLLBACK;
 
 	/* FMAP may be different so we should just update all. */
-	if (write_firmware(cfg, image_to, NULL) || update_ec_firmware(cfg))
+	if (write_system_firmware(cfg, image_to, NULL) || update_ec_firmware(cfg))
 		return UPDATE_ERR_WRITE_FIRMWARE;
 
 	return UPDATE_ERR_DONE;

futility/cmd_gbb_utility.c
@@ -413,7 +413,10 @@ static int write_to_flash(struct updater_config *cfg, uint8_t *outbuf,
 	}
 	cfg->image.data = outbuf;
 	cfg->image.size = filesize;
-	int ret = write_firmware(cfg, &cfg->image, FMAP_RO_GBB);
+
+	const char *sections[2] = { FMAP_RO_GBB, NULL };
+	int ret = write_system_firmware(cfg, &cfg->image, sections);
+
 	cfg->image.data = NULL;
 	cfg->image.size = 0;
 	return ret;

futility/updater.h
@@ -258,15 +258,6 @@ int quirk_override_signature_id(struct updater_config *cfg,
 				struct model_config *model,
 				const char **signature_id);
 
-/*
- * Writes a single section from the given firmware image to the system.
- * Writes the whole firmware image if the section_name is NULL.
- * Returns 0 if success, non-zero if error.
- */
-int write_firmware(struct updater_config *cfg,
-		   const struct firmware_image *image,
-		   const char *section_name);
-
 /* Functions from updater_archive.c */
 
 /*

supechicken/vboot_reference

commit 839f5064f2f0ae2086ce97802509b65193e6ee24

futility/updater: Fix EC firmware whole image update
This patch fixes a bug introduced by commit 1ee88b4cb3 ("futility/:
Inline write_firmware() as write_system_firmware()").

To update the whole image, write_system_firmware() expects a NULL
pointer in place of the list of sections.

BUG=b:285922461
BRANCH=none
TEST=FAFT firmware_FWupdate

Change-Id: I1e7d19e18e9e0c7e42dde217f3123f0918580fda
Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>

futility/updater.c
@@ -845,7 +845,8 @@ static int update_ec_firmware(struct updater_config *cfg)
 	}
 
 	/* TODO(quasisec): Uses cros_ec to program the EC. */
-	return write_system_firmware(cfg, ec_image, sections);
+	return write_system_firmware(cfg, ec_image,
+				     sections[0] ? sections : NULL);
 }
 
 const char * const updater_error_messages[] = {