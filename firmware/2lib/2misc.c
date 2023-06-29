bd47d30ac35b0faf4bb57b5e1f13763991ebc7fb
Michał Żygowski <miczyg94@gmail.com>
2023-06-29 17:54:01 +0200

    firmware/2lib: Switch to RO immediately if only one slot present
    Introduce a new context flag indicating a single slot flash layout.
    Use the flag to avoid additional reset due to next slot selection.
    
    TEST=Boot MSI PRO Z690-A with single slot layout. Make the slot A
    invalid and observe only one reset is needed to enter recovery mode
    instead of two resets.
    
    Change-Id: I7c27aa9a2da43bf9e2ae2c8f8048ede85518dee5
    Signed-off-by: Michał Żygowski <miczyg94@gmail.com>

--- firmware/2lib/2misc.c
+++ firmware/2lib/2misc.c
@@ -114,20 +114,22 @@ static void fail_impl(struct vb2_context *ctx,
 		/* Use up remaining tries */
 		vb2_nv_set(ctx, VB2_NV_TRY_COUNT, 0);
 
-		/*
-		 * Try the other slot next time.  We'll alternate
-		 * between slots, which may help if one or both slots is
-		 * flaky.
-		 */
-		vb2_nv_set(ctx, VB2_NV_TRY_NEXT, 1 - fw_slot);
+		if (!(ctx->flags & VB2_CONTEXT_ONLY_ONE_SLOT)) {
+			/*
+			 * Try the other slot next time.  We'll alternate
+			 * between slots, which may help if one or both slots
+			 * is flaky.
+			 */
+			vb2_nv_set(ctx, VB2_NV_TRY_NEXT, 1 - fw_slot);
 
-		/*
-		 * If we didn't try the other slot last boot, or we tried it
-		 * and it didn't fail, try it next boot.
-		 */
-		if (last_fw_slot != 1 - fw_slot ||
-		    last_fw_result != VB2_FW_RESULT_FAILURE)
-			return;
+			/*
+			 * If we didn't try the other slot last boot, or we
+			 * tried it and it didn't fail, try it next boot.
+			 */
+			if (last_fw_slot != 1 - fw_slot ||
+			    last_fw_result != VB2_FW_RESULT_FAILURE)
+				return;
+		}
 	}
 
 	/*
@@ -388,6 +390,12 @@ vb2_error_t vb2_select_fw_slot(struct vb2_context *ctx)
 	if (sd->last_fw_result == VB2_FW_RESULT_TRYING &&
 	    sd->last_fw_slot == sd->fw_slot &&
 	    tries == 0) {
+		/*
+		 * If there is only RW A slot available, we have no other slot
+		 * to fall back to.
+		 */
+		if ((ctx->flags & VB2_CONTEXT_ONLY_ONE_SLOT) && sd->fw_slot == 0)
+			return VB2_ERROR_API_NEXT_SLOT_UNAVAILABLE;
 		/*
 		 * We used up our last try on the previous boot, so fall back
 		 * to the other slot this boot.
