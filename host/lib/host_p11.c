f56210aa4e800af68d77a862dbb785db2cc59de8
Ching-Kang Yen <chingkang@google.com>
2023-08-10 14:13:28 +0800

    vboot: support signing with pkcs11 private key
    The subcommands of futility using the vb2_private_key struct now could
    read the pkcs11 key and use it for signing process.
    
    BRANCH=none
    BUG=b:294369070
    TEST=cros_run_unit_tests --host --packages vboot_reference
    TEST=manual run `futility vbutil_kernel` with pkcs11 key
    
    Signed-off-by: Ching-Kang Yen <chingkang@google.com>
    Change-Id: I971880fe4ff0c94d4ff259f2bd7b3c92cf45cc6f

---
+++ host/lib/host_p11.c
@@ -0,0 +1,219 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <dlfcn.h>
+
+#include <nss/pkcs11.h>
+
+#include "2common.h"
+#include "host_p11.h"
+
+static CK_FUNCTION_LIST_PTR p11 = NULL;
+
+static void *pkcs11_load(const char *mspec, CK_FUNCTION_LIST_PTR_PTR funcs)
+{
+	void *mod;
+	CK_RV rv, (*c_get_function_list)(CK_FUNCTION_LIST_PTR_PTR);
+
+	if (mspec == NULL)
+		return NULL;
+
+	mod = dlopen(mspec, RTLD_LAZY);
+	if (mod == NULL) {
+		fprintf(stderr, "dlopen failed: %s\n", dlerror());
+		return NULL;
+	}
+
+	/* Get the list of function pointers */
+	c_get_function_list =
+		(CK_RV(*)(CK_FUNCTION_LIST_PTR_PTR))dlsym(mod, "C_GetFunctionList");
+	if (!c_get_function_list)
+		goto err;
+	rv = c_get_function_list(funcs);
+	if (rv == CKR_OK)
+		return mod;
+	fprintf(stderr, "C_GetFunctionList failed 0x%lx", rv);
+err:
+	dlclose(mod);
+	return NULL;
+}
+
+static bool pkcs11_find(CK_SESSION_HANDLE session, CK_ATTRIBUTE attributes[],
+			CK_ULONG num_attributes, CK_OBJECT_HANDLE *object)
+{
+	CK_RV result = p11->C_FindObjectsInit(session, attributes, num_attributes);
+	if (result != CKR_OK)
+		return false;
+
+	CK_ULONG object_count = 1;
+	result = p11->C_FindObjects(session, object, 1, &object_count);
+	if (result != CKR_OK || object_count == 0)
+		return false;
+
+	result = p11->C_FindObjectsFinal(session);
+	if (result != CKR_OK)
+		return false;
+
+	return true;
+}
+
+static bool valid_pkcs11_mechanism(CK_MECHANISM_TYPE p11_mechanism)
+{
+	switch (p11_mechanism) {
+	case CKM_SHA1_RSA_PKCS:
+	case CKM_SHA256_RSA_PKCS:
+	case CKM_SHA512_RSA_PKCS:
+	case CKM_SHA224_RSA_PKCS:
+	case CKM_SHA384_RSA_PKCS:
+		return true;
+	}
+	return false;
+}
+
+enum vb2_signature_algorithm sig_size_to_sig_alg(uint32_t sig_size)
+{
+	switch (sig_size) {
+	case 1024 / 8:
+		return VB2_SIG_RSA1024;
+	case 2048 / 8:
+		return VB2_SIG_RSA2048;
+	case 4096 / 8:
+		return VB2_SIG_RSA4096;
+	case 8192 / 8:
+		return VB2_SIG_RSA8192;
+	}
+	return VB2_SIG_INVALID;
+}
+
+bool pkcs11_init(const char *pkcs11_lib)
+{
+	static void *pkcs11_mod = NULL;
+	if (pkcs11_mod != NULL) {
+		fprintf(stderr, "Pkcs11 module is already loaded\n");
+		return true;
+	}
+
+	if (pkcs11_lib == NULL) {
+		fprintf(stderr, "Missing the path of pkcs11 library\n");
+		return false;
+	}
+
+	pkcs11_mod = pkcs11_load(pkcs11_lib, &p11);
+	if (pkcs11_mod == NULL) {
+		fprintf(stderr, "Failed to load pkcs11\n");
+		return false;
+	}
+	CK_RV result = p11->C_Initialize(NULL);
+	if (result != CKR_OK) {
+		fprintf(stderr, "Failed to C_Initialize\n");
+		return false;
+	}
+	return true;
+}
+
+bool pkcs11_get_key(int slot_id, char *label, struct pkcs11_key *p11_key)
+{
+	if (!p11) {
+		fprintf(stderr, "pkcs11 is not loaded\n");
+		return false;
+	}
+
+	CK_RV result = p11->C_OpenSession(slot_id, CKF_SERIAL_SESSION | CKF_RW_SESSION, NULL,
+					  NULL, &p11_key->session);
+	if (result != CKR_OK) {
+		fprintf(stderr, "Failed to open session\n");
+		return false;
+	}
+
+	/* Find the private key */
+	CK_OBJECT_CLASS class_value = CKO_PRIVATE_KEY;
+	CK_ATTRIBUTE attributes[] = {
+		{CKA_CLASS, &class_value, sizeof(class_value)},
+		{CKA_LABEL, label, strlen(label)},
+	};
+	if (!pkcs11_find(p11_key->session, attributes, 2, &p11_key->handle)) {
+		fprintf(stderr, "Failed to pkcs11 find\n");
+		return false;
+	}
+
+	/* Get the signature size */
+	CK_ATTRIBUTE modulus_attr = {CKA_MODULUS, NULL, 0};
+	if (p11->C_GetAttributeValue(p11_key->session, p11_key->handle, &modulus_attr, 1) !=
+	    CKR_OK) {
+		fprintf(stderr, "Failed to get modulus attribute length\n");
+		return false;
+	}
+	p11_key->signature_size = modulus_attr.ulValueLen;
+
+	/* Find the suitable mechanism to sign */
+	CK_ATTRIBUTE mechanism_attr = {CKA_ALLOWED_MECHANISMS, NULL, 0};
+	if (p11->C_GetAttributeValue(p11_key->session, p11_key->handle, &mechanism_attr, 1) !=
+	    CKR_OK) {
+		fprintf(stderr, "Failed to get mechanisum attribute length\n");
+		return false;
+	}
+	mechanism_attr.pValue = malloc(mechanism_attr.ulValueLen);
+	if (p11->C_GetAttributeValue(p11_key->session, p11_key->handle, &mechanism_attr, 1) !=
+	    CKR_OK) {
+		fprintf(stderr, "Failed to get mechanisum attribute value\n");
+		free(mechanism_attr.pValue);
+		return false;
+	}
+	CK_MECHANISM_TYPE *mechanisms = mechanism_attr.pValue;
+	uint32_t mechanism_count = mechanism_attr.ulValueLen / sizeof(CK_MECHANISM_TYPE);
+	for (int i = 0; i < mechanism_count; ++i) {
+		if (valid_pkcs11_mechanism(mechanisms[i])) {
+			p11_key->mechanism.mechanism = mechanisms[i];
+			break;
+		}
+	}
+	p11_key->mechanism.pParameter = NULL;
+	p11_key->mechanism.ulParameterLen = 0;
+	free(mechanism_attr.pValue);
+	return true;
+}
+
+bool pkcs11_sign(struct pkcs11_key *p11_key, const uint8_t *data, int data_size, uint8_t *sig,
+		 CK_ULONG sig_size)
+{
+	if (!p11) {
+		fprintf(stderr, "pkcs11 is not loaded\n");
+		return false;
+	}
+	CK_RV result = p11->C_SignInit(p11_key->session, &p11_key->mechanism, p11_key->handle);
+	if (result != CKR_OK) {
+		fprintf(stderr, "Failed to sign init\n");
+		return NULL;
+	}
+	result = p11->C_Sign(p11_key->session, (unsigned char *)data, data_size, NULL,
+			     &sig_size);
+	if (result != CKR_OK) {
+		fprintf(stderr, "Failed to get signature size\n");
+		return NULL;
+	}
+	result =
+		p11->C_Sign(p11_key->session, (unsigned char *)data, data_size, sig, &sig_size);
+	if (result != CKR_OK) {
+		fprintf(stderr, "Failed to sign\n");
+		return NULL;
+	}
+	return sig;
+}
+
+void pkcs11_free_key(struct pkcs11_key *p11_key)
+{
+	if (!p11) {
+		fprintf(stderr, "pkcs11 is not loaded\n");
+		return;
+	}
+	CK_RV result = p11->C_CloseSession(p11_key->session);
+	if (result != CKR_OK)
+		fprintf(stderr, "Failed to close session\n");
+	free(p11_key);
+}
