add8b023d3debff4ef4a7c1faffc079c5b2fcb5a
Ching-Kang Yen <chingkang@google.com>
2023-08-10 14:13:28 +0800

    vboot: support signing with pkcs11 private key
    The subcommands of futility using the vb2_private_key struct now could
    read the pkcs11 key and use it for signing process.
    
    BRANCH=none
    BUG=b:294369070
    TEST=cros_run_unit_tests --host --packages vboot_reference
    TEST=manual run `futility vbutil_kernel` with pkcs11 key
    
    Signed-off-by: Ching-Kang Yen <chingkang@google.com>
    Change-Id: I971880fe4ff0c94d4ff259f2bd7b3c92cf45cc6f

---
+++ host/lib/include/host_p11.h
@@ -0,0 +1,84 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef VBOOT_REFERENCE_HOST_P11_H_
+#define VBOOT_REFERENCE_HOST_P11_H_
+
+#include <nss/pkcs11.h>
+
+#include "2id.h"
+#include "2return_codes.h"
+#include "2struct.h"
+
+/* Pkcs11 key for the signing */
+struct pkcs11_key {
+	CK_OBJECT_HANDLE handle;
+	CK_SESSION_HANDLE session;
+	CK_MECHANISM mechanism;
+};
+
+/**
+ * Find the corresponding vb2_signature_algorithm with |sig_zie|.
+ *
+ * @param sig_size	Size of signature
+ *
+ * @return VB2_SUCCESS, or non-zero if error.
+ */
+enum vb2_signature_algorithm sig_size_to_sig_alg(uint32_t sig_size);
+
+/**
+ * Initialize the pkcs11 library. Note that there is only one pkcs11 module can be loaded
+ * at a time.
+ *
+ * @param pkcs11_lib	Path of the Pkcs11 library to be initialized
+ *
+ * @return VB2_SUCCESS, or non-zero if error.
+ */
+vb2_error_t pkcs11_init(const char *pkcs11_lib);
+
+/**
+ * Get the pkcs11 key by the slot id and label.
+ *
+ * @param slot_id	Slot id of the pkcs11 key
+ * @param label		Label of the pkcs11 key
+ * @param p11_key	Output pkcs11 key
+ *
+ * @return VB2_SUCCESS, or non-zero if error.
+ */
+vb2_error_t pkcs11_get_key(int slot_id, char *label, struct pkcs11_key *p11_key);
+
+/**
+ * Get the signature algorithm of the pkcs11 key.
+ *
+ * @param p11_key	Pkcs11 Key
+ * @param p11_key	Output signature algorithm of the input pkcs11 key
+ *
+ * @return VB2_SUCCESS, or non-zero if error.
+ */
+vb2_error_t pkcs11_get_sig_alg(struct pkcs11_key *p11_key,
+			       enum vb2_signature_algorithm *sig_alg);
+
+/**
+ * Calculate a signature for the data using pkcs11 key.
+ *
+ * @param p11_key	Private key to use to sign data
+ * @param data		Pointer to data to sign
+ * @param data_size	Size of data in bytes
+ * @param sig		Pointer to the output signature
+ * @param sig_size	Size of sig in bytes
+ *
+ * @return VB2_SUCCESS, or non-zero if error.
+ */
+vb2_error_t pkcs11_sign(struct pkcs11_key *p11_key, const uint8_t *data, int data_size,
+			uint8_t *sig, CK_ULONG sig_size);
+
+/**
+ * Free a pkcs11 key.
+ *
+ * @param key		Pkcs11 key to free.
+ */
+void pkcs11_free_key(struct pkcs11_key *p11_key);
+
+#endif /* VBOOT_REFERENCE_HOST_P11_H_ */
