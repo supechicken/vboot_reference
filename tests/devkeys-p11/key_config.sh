4fac67f0a6544a1a51c7652b414035787730a28c
Ching-Kang Yen <chingkang@google.com>
2023-08-10 14:13:28 +0800

    [DO NO MERGE] test futility pkcs11
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: Ifdea61c8a388636e458c1aa1e8606e57a28a8bd0
    Signed-off-by: Ching-Kang Yen <chingkang@google.com>

---
+++ tests/devkeys-p11/key_config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "sourcing key_config.sh"
+#P11_LIB="$HOME/Documents/pkcs11/libkmsp11-1.2-linux-amd64/libkmsp11.so"
+P11_LIB="$HOME/Documents/pkcs11/libkmsp11.so"
+KEYCFG_KERNEL_KEYBLOCK="${KEY_DIR}/kms_rsa_key2.keyblock"
+KEYCFG_KERNEL_VBPRIVK="pkcs11:${P11_LIB}:0:rsa_key2"
+KEYCFG_MINIOS_KERNEL_KEYBLOCK="${KEY_DIR}/kms_rsa_key2.keyblock"
+KEYCFG_MINIOS_KERNEL_VBPRIVK="pkcs11:${P11_LIB}:0:rsa_key2"
+KEYCFG_RECOVERY_KERNEL_KEYBLOCK="${KEY_DIR}/kms_rsa_key2.keyblock"
+KEYCFG_RECOVERY_KERNEL_VBPRIVK="pkcs11:${P11_LIB}:0:rsa_key2"
+KEYCFG_INSTALLER_KERNEL_KEYBLOCK="${KEY_DIR}/kms_rsa_key2.keyblock"
+KEYCFG_INSTALLER_KERNEL_VBPRIVK="pkcs11:${P11_LIB}:0:rsa_key2"
+KEYCFG_ARV_PLATFORM_KEYBLOCK="${KEY_DIR}/kms_rsa_key2.keyblock"
+KEYCFG_ARV_PLATFORM_VBPRIVK="pkcs11:${P11_LIB}:0:rsa_key2"
+KEYCFG_FIRMWARE_VBPRIVK="pkcs11:${P11_LIB}:0:rsa_key2"
+KEYCFG_ACCESSORY_RWSIG_VBRPIK2="pkcs11:${P11_LIB}:0:rsa_key2"
+
+KEYCFG_KEY_EC_EFS_VBRPIK2="pkcs11:${P11_LIB}:0:rsa_key2"
+KEYCFG_KEY_ACCESSIRY_RWSIG_VBRPIK2="pkcs11:${P11_LIB}:0:rsa_key2"
+
+for i in {1..4};
+do
+  KEYCFG_KEY_FIRMARE_VBPRIVK_LOEM[${i}]="pkcs11:${P11_LIB}:0:rsa_key2"
+done
+
