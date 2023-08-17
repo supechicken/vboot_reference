2cf74473bcf9c4aefaad3a3065e91aff9a9b6637
Hung-Te Lin <hungte@chromium.org>
2023-08-17 08:57:24 +0800

    futility: updater: fix build warning 'incompatible function pointer'
    The builder function should always return an integer.
    
    Note: the warning will be caught if we do 'CC=clang make' in chroot,
    but the default compiler (gcc) won't catch it. Although the portage
    build system will select clang, it won't output the warning messages by
    default.
    
    BUG=None
    TEST=make
    BRANCH=None
    
    Change-Id: I576ed5d6dd8daca304f859012bf9642d55915bdd

--- futility/updater_manifest.c
+++ futility/updater_manifest.c
@@ -832,14 +832,14 @@ static bool manifest_must_enforce_setvars(struct manifest *manifest)
 	return false;
 }
 
-static void manifest_from_setvars_sh(struct manifest *manifest) {
+static int manifest_from_setvars_sh(struct manifest *manifest) {
 	VB2_DEBUG("Try to build the manifest from *%s\n", PATH_ENDSWITH_SETVARS);
-	archive_walk(manifest->archive, manifest, manifest_scan_entries);
+	return archive_walk(manifest->archive, manifest, manifest_scan_entries);
 }
 
-static void manifest_from_build_artifacts(struct manifest *manifest) {
+static int manifest_from_build_artifacts(struct manifest *manifest) {
 	VB2_DEBUG("Try to build the manifest from a */firmware folder\n");
-	archive_walk(manifest->archive, manifest, manifest_scan_raw_entries);
+	return archive_walk(manifest->archive, manifest, manifest_scan_raw_entries);
 }
 
 /*
@@ -851,7 +851,7 @@ struct manifest *new_manifest_from_archive(struct u_archive *archive)
 	int i;
 	struct manifest manifest = {0}, *new_manifest;
 	bool try_builders = true;
-	void (*manifest_builders[])(struct manifest *) = {
+	int (*manifest_builders[])(struct manifest *) = {
 		manifest_from_signer_config,
 		manifest_from_setvars_sh,
 		manifest_from_build_artifacts,
