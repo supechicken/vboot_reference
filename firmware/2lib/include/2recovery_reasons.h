c0e8d1539d4d253b234398c841804d58b168e358
Jon Murphy <jpmurphy@google.com>
2023-08-18 18:16:52 -0600

    recovery_reasons: Add Failed TPM Comm
    Add failed TPM comm recovery reason to allow auto-recovery in failure
    cases.
    
    BUG=b:296439237
    TEST=Force error and observe correct error codes/messages
    
    Change-Id: I3751fcd2533ce3a11a6717370dfb53b27da47630
    Signed-off-by: Jon Murphy <jpmurphy@google.com>

--- firmware/2lib/include/2recovery_reasons.h
+++ firmware/2lib/include/2recovery_reasons.h
@@ -217,6 +217,18 @@ enum vb2_nv_recovery {
 	/* Rebooting did not correct TPM_E_FAIL or TPM_E_FAILEDSELFTEST  */
 	VB2_RECOVERY_TPM_E_FAIL = 0x49,
 
+	/* TPM Address not specified */
+	VB2_RECOVERY_TPM_BAD_ADDR = 0x4a,
+
+	/* TPM Failed to communicate with AP */
+	VB2_RECOVERY_TPM_COMM_FAILED = 0x4b,
+
+	/* TPM Failed to reset */
+	VB2_RECOVERY_TPM_FAILED_RST = 0x4c,
+
+	/* TPM Failed to set locality */
+	VB2_RECOVERY_TPM_FAILED_LOC = 0x4d,
+
 	/* TPM setup error in read-only firmware */
 	VB2_RECOVERY_RO_TPM_S_ERROR = 0x50,
 
@@ -282,7 +294,6 @@ enum vb2_nv_recovery {
 
 	/* Unspecified/unknown error in rewritable firmware */
 	VB2_RECOVERY_RW_UNSPECIFIED = 0x7f,
-
 
 
 	/**********************************************************************/
