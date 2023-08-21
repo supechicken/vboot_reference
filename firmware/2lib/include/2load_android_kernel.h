abf8b270eed98021445c00abf401749e8c99c57d
Konrad Adamczyk <konrada@google.com-copybot-pick>
2023-08-21 10:05:15 +0000

    2lib: Load and verify android partitions Android Bringup: See http://go/android-fw-sync
    Make use of the libavb library for loading and verification of android
    images. Verify partitions necessary for booting - "boot", "init_boot"
    and "vendor_boot".
    To compile with AVB support set, 'USE_AVB' when building vboot.
    
    BUG=b:309426555
    TEST=Build FW and boot android
    TEST=Use description from b/309426555#comment3
    BRANCH=main
    
    (cherry picked from commit 076d4d38f9f62ecbc88180f7a075706710d7588a)
    
    Change-Id: Ic6b4f432690e7c13e841cce01dfcb764b707fe66
    Original-Signed-off-by: Konrad Adamczyk <konrada@google.com>
    Original-Signed-off-by: Jan Dabros <dabros@google.com>
    Original-Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6030619
    Original-Reviewed-by: Julius Werner <jwerner@chromium.org>
    Original-Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    GitOrigin-RevId: 076d4d38f9f62ecbc88180f7a075706710d7588a
    Cr-Build-Id: 8717491780244891409
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8717491780244891409
    Copybot-Job-Name: android-main-vboot_reference-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6460663
    Tested-by: Grzegorz Bernacki <bernacki@google.com>
    Commit-Queue: Grzegorz Bernacki <bernacki@google.com>
    Reviewed-by: Jon Murphy <jpmurphy@google.com>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    (cherry picked from commit 813844b8d74248da805133e2dee29c34566b21e6)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6562191
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Tested-by: Yidi Lin <yidilin@google.com>
    Commit-Queue: Yidi Lin <yidilin@google.com>

--- firmware/2lib/include/2load_android_kernel.h
+++ firmware/2lib/include/2load_android_kernel.h
@@ -1,4 +1,4 @@
-/* Copyright 2024 The ChromiumOS Authors
+/* Copyright 2025 The ChromiumOS Authors
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  *
@@ -9,12 +9,13 @@
 #define VBOOT_REFERENCE_2LOAD_ANDROID_KERNEL_H_
 
 #include "2api.h"
+#include "cgptlib.h"
 #include "gpt_misc.h"
 #include "vboot_api.h"
 
 /**
  * Load and verify Android kernel partitions (boot, init_boot, vendor_boot,
- * pvmfw) from the stream.
+ * pvmfw) from the stream (legacy).
  *
  * @param ctx			Vboot context
  * @param params		Load-kernel parameters
@@ -28,5 +29,22 @@ vb2_error_t vb2_load_android_kernel(
 	struct vb2_context *ctx, struct vb2_kernel_params *params,
 	VbExStream_t stream, GptData *gpt, vb2ex_disk_handle_t disk_handle,
 	int need_keyblock_valid);
+
+/**
+ * Loads and verifies Android partitions (boot, init_boot, vendor_boot, pvmfw).
+ *
+ * @param ctx			Vboot context
+ * @param gpt			Partition table from the disk
+ * @param entry			GPT entry with VBMETA partition
+ * @param params		Load-kernel parameters
+ * @param disk_handle		Handle to the disk containing kernel
+ * @return VB2_SUCCESS, or non-zero error code.
+ */
+vb2_error_t vb2_load_android(
+	struct vb2_context *ctx,
+	GptData *gpt,
+	GptEntry *entry,
+	struct vb2_kernel_params *params,
+	vb2ex_disk_handle_t disk_handle);
 
 #endif  /* VBOOT_REFERENCE_2LOAD_ANDROID_KERNEL_H_ */
