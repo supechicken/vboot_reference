bf34f65d69e3c6a07d3c706aefb9a0a049a20681
Konrad Adamczyk <konrada@google.com>
2023-08-21 10:05:15 +0000

    2lib: Load and verify android partitions
    Make use of the libavb library for loading and verification of android
    images. Verify partitions necessary for booting - "boot", "init_boot"
    and "vendor_boot".
    
    Android requires bootloader to provide verifiedbootstate property which
    informs about UNLOCKED -"orange", LOCKED(with dev keys) - "yellow" or
    LOCKED(with MP keys) - "green" state.
    
    Currently support "orange" and "green" states only, need to add logic
    to differentiate between MP and user keys - b/335901799.
    
    BUG=b:309426555
    TEST=Build FW and boot android
    TEST=Use description from b/309426555#comment3
    BRANCH=main
    
    Change-Id: Ic6b4f432690e7c13e841cce01dfcb764b707fe66
    Signed-off-by: Konrad Adamczyk <konrada@google.com>
    Signed-off-by: Jan Dabros <dabros@google.com>
    Signed-off-by: Grzegorz Bernacki <bernacki@google.com>

--- firmware/2lib/2load_android_kernel.c
+++ firmware/2lib/2load_android_kernel.c
@@ -15,6 +15,39 @@
 #include "gpt_misc.h"
 #include "vboot_api.h"
 
+#define GPT_ENT_NAME_ANDROID_A_SUFFIX "_a"
+#define GPT_ENT_NAME_ANDROID_B_SUFFIX "_b"
+
+#define VERIFIED_BOOT_PROPERTY_NAME "androidboot.verifiedbootstate"
+#define SLOT_SUFFIX_BOOT_PROPERTY_NAME "androidboot.slot_suffix"
+
+static int vb2_map_libavb_errors(AvbSlotVerifyResult avb_error)
+{
+	/* Map AVB error into VB2 */
+	switch (avb_error) {
+	case AVB_SLOT_VERIFY_RESULT_OK:
+		return VB2_SUCCESS;
+	case AVB_SLOT_VERIFY_RESULT_ERROR_OOM:
+		return VB2_ERROR_AVB_OOM;
+	case AVB_SLOT_VERIFY_RESULT_ERROR_IO:
+		return VB2_ERROR_AVB_ERROR_IO;
+	case AVB_SLOT_VERIFY_RESULT_ERROR_VERIFICATION:
+		return VB2_ERROR_AVB_ERROR_VERIFICATION;
+	case AVB_SLOT_VERIFY_RESULT_ERROR_ROLLBACK_INDEX:
+		return VB2_ERROR_AVB_ERROR_ROLLBACK_INDEX;
+	case AVB_SLOT_VERIFY_RESULT_ERROR_PUBLIC_KEY_REJECTED:
+		return VB2_ERROR_AVB_ERROR_PUBLIC_KEY_REJECTED;
+	case AVB_SLOT_VERIFY_RESULT_ERROR_INVALID_METADATA:
+		return VB2_ERROR_AVB_ERROR_INVALID_METADATA;
+	case AVB_SLOT_VERIFY_RESULT_ERROR_UNSUPPORTED_VERSION:
+		return VB2_ERROR_AVB_ERROR_UNSUPPORTED_VERSION;
+	case AVB_SLOT_VERIFY_RESULT_ERROR_INVALID_ARGUMENT:
+		return VB2_ERROR_AVB_ERROR_INVALID_ARGUMENT;
+	default:
+		return VB2_ERROR_AVB_ERROR_VERIFICATION;
+	}
+}
+
 static vb2_error_t vb2_load_android_partition(GptData *gpt, enum GptPartition part,
 					      const char *suffix, uint8_t *buffer,
 					      size_t buffer_size, vb2ex_disk_handle_t dh,
@@ -125,4 +158,99 @@ static int vboot_preload_partition(struct vb2_kernel_params *params, GptData *gp
 	buf += bytes_used;
 
 	return 0;
+}
+
+vb2_error_t vb2_load_android(struct vb2_context *ctx, GptData *gpt, GptEntry *entry,
+			     struct vb2_kernel_params *params, vb2ex_disk_handle_t disk_handle)
+{
+	AvbSlotVerifyData *verify_data = NULL;
+	AvbOps *avb_ops;
+	AvbSlotVerifyFlags avb_flags;
+	AvbSlotVerifyResult result;
+	vb2_error_t ret;
+	const char *boot_partitions[] = {
+		GptPartitionNames[GPT_ANDROID_BOOT],
+		GptPartitionNames[GPT_ANDROID_INIT_BOOT],
+		GptPartitionNames[GPT_ANDROID_VENDOR_BOOT],
+		NULL,
+	};
+	const char *slot_suffix = NULL;
+	bool need_verification = vb2_need_kernel_verification(ctx);
+
+	/* Update flags to mark loaded GKI image */
+	params->flags &= ~VB2_KERNEL_TYPE_MASK;
+	params->flags |= VB2_KERNEL_TYPE_BOOTIMG;
+
+	const char *vb = GptPartitionNames[GPT_ANDROID_VBMETA];
+	if (GptEntryHasName(entry, vb, GPT_ENT_NAME_ANDROID_A_SUFFIX))
+		slot_suffix = GPT_ENT_NAME_ANDROID_A_SUFFIX;
+	else if (GptEntryHasName(entry, vb, GPT_ENT_NAME_ANDROID_B_SUFFIX))
+		slot_suffix = GPT_ENT_NAME_ANDROID_B_SUFFIX;
+
+	ret = vboot_preload_partition(params, gpt, slot_suffix, disk_handle);
+	if (ret != VB2_SUCCESS) {
+		VB2_DEBUG("Cannot preload android partitions\n");
+		return VB2_ERROR_LK_NO_KERNEL_FOUND;
+	}
+
+	avb_ops = vboot_avb_ops_new(ctx, params, gpt, disk_handle, slot_suffix);
+
+	avb_flags = AVB_SLOT_VERIFY_FLAGS_NONE;
+	if (!need_verification)
+		avb_flags |= AVB_SLOT_VERIFY_FLAGS_ALLOW_VERIFICATION_ERROR;
+
+	result = avb_slot_verify(avb_ops, boot_partitions, slot_suffix, avb_flags,
+				 AVB_HASHTREE_ERROR_MODE_RESTART_AND_INVALIDATE,
+				 &verify_data);
+
+	/* Ignore verification errors in developer mode */
+	if (!need_verification) {
+		switch (result) {
+		case AVB_SLOT_VERIFY_RESULT_OK:
+		case AVB_SLOT_VERIFY_RESULT_ERROR_VERIFICATION:
+		case AVB_SLOT_VERIFY_RESULT_ERROR_ROLLBACK_INDEX:
+		case AVB_SLOT_VERIFY_RESULT_ERROR_PUBLIC_KEY_REJECTED:
+			result = AVB_SLOT_VERIFY_RESULT_OK;
+			break;
+		default:
+			result = AVB_SLOT_VERIFY_RESULT_ERROR_VERIFICATION;
+			break;
+		}
+	}
+
+	/* Map AVB return code into VB2 code */
+	ret = vb2_map_libavb_errors(result);
+
+	/*
+	 * Return from this function early so that caller can try fallback to
+	 * other partition in case of error.
+	 */
+	if (ret != VB2_SUCCESS) {
+		if (verify_data != NULL)
+			avb_slot_verify_data_free(verify_data);
+		return ret;
+	}
+
+	/* TODO(b/335901799): Add support for marking verifiedbootstate yellow */
+	/* Possible values for this property are "yellow", "orange" and "green"
+	 * so allocate 6 bytes plus 1 byte for NULL terminator.
+	 */
+	int chars = snprintf(params->vboot_cmdline_buffer, params->vboot_cmdline_size,
+			     "%s %s=%s %s=%s", verify_data->cmdline,
+			     VERIFIED_BOOT_PROPERTY_NAME,
+			     need_verification ? "green" : "orange",
+			     SLOT_SUFFIX_BOOT_PROPERTY_NAME, slot_suffix);
+	if (chars < 0 || chars >= params->vboot_cmdline_size) {
+		VB2_DEBUG("ERROR: Command line doesn't fit provided buffer: %s\n",
+			  verify_data->cmdline);
+		return VB2_ERROR_ANDROID_CMDLINE_BUF_TOO_SMALL;
+	}
+
+	/* No need for slot data, partitions should be already at correct
+	 * locations in memory since we are using "get_preloaded_partitions"
+	 * callbacks.
+	 */
+	avb_slot_verify_data_free(verify_data);
+
+	return ret;
 }
