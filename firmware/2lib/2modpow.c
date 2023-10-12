38353c5ba40649b00fe4f2d5fd0f823d8988624f
Muhammad Monir Hossain <muhammad.monir.hossain@intel.com>
2023-10-12 12:10:40 -0700

    [DONOTMERGE] firmware/2lib: Vectorizable Montgomery multiplication
    This commit implement the Algorithm 2 as described in Montgomery
    Multiplication Using Vector Instructions from August 20, 2013
    (cf. https://eprint.iacr.org/2013/519.pdf). This variation of the
    Montgomery multiplication algorithm decouples some arithmetic steps
    which can then be performed in parallel.
    
    This new algorithm uses the modulus positive inverse (1 / N[0] mod
    2^32) which can be easily computed from the modulus negative inverse
    provided by the public key data structure `n0inv` field.
    
    This basic implementation is an example implementation. It can easily
    be replaced with an hardware architecture optimized version. This
    version offers comparable performances in our testings. Here are some
    measurements of the modpow execution time on two different boards with
    both the original and the new algorithm.
    
    On a Raptor Lake brya0 board:
    
    | modpow() function call during boot | original | Algorithm 2 |
    |------------------------------------+----------+-------------|
    | coreboot/verstage 1                |    5.642 |       5.818 |
    | coreboot/verstage 2                |    1.560 |       1.426 |
    | coreboot/verstage 3                |    1.548 |       1.414 |
    | depthcharge 1                      |    0.693 |       0.708 |
    | depthcharge 2                      |    0.172 |       0.180 |
    | depthcharge 3                      |    0.223 |       0.217 |
    |------------------------------------+----------+-------------|
    | Total (ms)                         |    9.838 |       9.763 |
    
    On a Meteor Lake rex0 board:
    
    | modpow() function call during boot | original | Algorithm 2 |
    |------------------------------------+----------+-------------|
    | coreboot/verstage 1                |    6.531 |       6.610 |
    | coreboot/verstage 2                |    1.854 |       1.810 |
    | coreboot/verstage 3                |    1.841 |       1.808 |
    | depthcharge 1                      |    0.547 |       0.592 |
    | depthcharge 2                      |    0.152 |       0.155 |
    | depthcharge 3                      |    0.164 |       0.165 |
    |------------------------------------+----------+-------------|
    | Total (ms)                         |   10.925 |      11.140 |
    
    Change-Id: I8739f94c0f6423f6e848bcae10b56e8be8e352cd
    Co-authored-by: Jeremy Compostella <jeremy.compostella@intel.com>

--- firmware/2lib/2modpow.c
+++ firmware/2lib/2modpow.c
@@ -4,8 +4,14 @@
  */
 
 /*
- * Implementation of Montgomery multiplication using negative inverse modulus.
- * n0inv = -1 / N[0] mod 2^32
+ * Montgomery multiplication implementation using positive inverse modulus,
+ * p0inv = 1 / N[0] mod 2^32. This algorithm is described in Montgomery
+ * Multiplication Using Vector Instructions from August 20, 2013
+ * (cf. https://eprint.iacr.org/2013/519.pdf).
+ *
+ * This algorithm provides good performances (comparable to the original
+ * algorithm) and is designed to be able to take advantage of vector
+ * instructions.
  */
 
 #include "2return_codes.h"
@@ -13,64 +19,36 @@
 #include "2rsa_private.h"
 
 /**
- * a[] -= mod
+ * Montgomery c[] = d[] - e[] if d[] > e[], c[] = d[] - e[] + mod[] otherwise.
+ *
+ * d[] and e[] are used as temporary buffers and therefore the content will be
+ * lost.
  */
-static void subM(const struct vb2_public_key *key, uint32_t *a)
+static void subMod(const struct vb2_public_key *key,
+		   uint32_t *d, uint32_t *e, uint32_t *c)
 {
-	int64_t A = 0;
-	uint32_t i;
-	for (i = 0; i < key->arrsize; ++i) {
-		A += (uint64_t)a[i] - key->n[i];
-		a[i] = (uint32_t)A;
-		A >>= 32;
-	}
-}
+	uint32_t i, borrow = 0, carry = 0;
+	uint64_t sum;
 
-/**
- * Montgomery c[] += a * b[] / R % mod
- */
-static void montMulAdd(const struct vb2_public_key *key,
-		       uint32_t *c,
-		       const uint32_t a,
-		       const uint32_t *b)
-{
-	uint64_t A = (uint64_t)a * b[0] + c[0];
-	uint32_t d0 = (uint32_t)A * key->n0inv;
-	uint64_t B = (uint64_t)d0 * key->n[0] + (uint32_t)A;
-	uint32_t i;
-
-	for (i = 1; i < key->arrsize; ++i) {
-		A = (A >> 32) + (uint64_t)a * b[i] + c[i];
-		B = (B >> 32) + (uint64_t)d0 * key->n[i] + (uint32_t)A;
-		c[i - 1] = (uint32_t)B;
+	for (i = 0; i < key->arrsize; i++) {
+		uint32_t tmp = d[i] - e[i] - borrow;
+		borrow = d[i] ^ ((d[i] ^ e[i]) | (d[i] ^ tmp));
+		borrow >>= 31;
+		/* Use d[] as temporary storage. */
+		d[i] = tmp;
 	}
 
-	A = (A >> 32) + (B >> 32);
-
-	c[i - 1] = (uint32_t)A;
-
-	if (A >> 32) {
-		subM(key, c);
-	}
-}
-
-/**
- * Montgomery c[] += 0 * b[] / R % mod
- */
-static void montMulAdd0(const struct vb2_public_key *key,
-			uint32_t *c,
-			const uint32_t *b)
-{
-	uint32_t d0 = c[0] * key->n0inv;
-	uint64_t B = (uint64_t)d0 * key->n[0] + c[0];
-	uint32_t i;
-
-	for (i = 1; i < key->arrsize; ++i) {
-		B = (B >> 32) + (uint64_t)d0 * key->n[i] + c[i];
-		c[i - 1] = (uint32_t)B;
+	/* To keep the code running in constant-time for side-channel
+	 * resistance, d[] − e[] + mod[] is systematically computed even if we
+	 * do not need it. */
+	for (i = 0; i < key->arrsize; i++) {
+		sum = (uint64_t)d[i] + key->n[i] + carry;
+		carry = sum >> 32;
+		/* Use e[] as temporary storage. */
+		e[i] = (uint32_t)sum;
 	}
 
-	c[i - 1] = B >> 32;
+	memcpy(c, borrow ? e : d, key->arrsize * sizeof(*c));
 }
 
 /**
@@ -79,30 +57,61 @@ static void montMulAdd0(const struct vb2_public_key *key,
 static void montMul(const struct vb2_public_key *key,
 		    uint32_t *c,
 		    const uint32_t *a,
-		    const uint32_t *b)
+		    const uint32_t *b,
+		    const uint32_t mu,
+		    uint32_t *d,
+		    uint32_t *e)
 {
-	uint32_t i;
-	for (i = 0; i < key->arrsize; ++i) {
-		c[i] = 0;
+	const uint32_t mub0 = mu * b[0];
+	uint32_t i, j, q, muc0, t0, t1;
+	uint64_t p0, p1;
+
+	for (i = 0; i < key->arrsize; i++) {
+		d[i] = 0;
+		e[i] = 0;
 	}
-	for (i = 0; i < key->arrsize; ++i) {
-		montMulAdd(key, c, a[i], b);
+
+	for (j = 0; j < key->arrsize; j++) {
+		c[0] = d[0] - e[0];
+		muc0 = mu * c[0];
+		q = muc0 + mub0 * a[j];
+
+		p0 = (uint64_t)a[j] * b[0] + (uint64_t)d[0];
+		t0 = (p0 >> 32);
+
+		p1 = (uint64_t)q * key->n[0] + (uint64_t)e[0];
+		t1 = (p1 >> 32);
+
+		for (i = 1; i < key->arrsize; i++) {
+			p0 = (uint64_t)a[j] * b[i] + (uint64_t)t0 + (uint64_t)d[i];
+			t0 = p0 >> 32;
+			d[i - 1] = (uint32_t)p0;
+
+			p1 = (uint64_t)q * key->n[i] + (uint64_t)t1 + (uint64_t)e[i];
+			t1 = p1 >> 32;
+			e[i - 1] = (uint32_t)p1;
+		}
+
+		d[key->arrsize - 1] = t0;
+		e[key->arrsize - 1] = t1;
 	}
+
+	subMod(key, d, e, c);
 }
 
-/* Montgomery c[] = a[] * 1 / R % key. */
-static void montMul1(const struct vb2_public_key *key,
-		     uint32_t *c,
-		     const uint32_t *a)
+static uint32_t swap_uint32(uint32_t val)
 {
-	int i;
+	val = ((val << 8) & 0xff00ff00) | ((val >> 8) & 0xff00ff);
+	return (val << 16) | (val >> 16);
+}
 
-	for (i = 0; i < key->arrsize; ++i)
-		c[i] = 0;
+static void swap_bignumber_endianess(const uint32_t *in, uint32_t *out,
+				     size_t size)
+{
+	size_t i;
 
-	montMulAdd(key, c, 1, a);
-	for (i = 1; i < key->arrsize; ++i)
-		montMulAdd0(key, c, a);
+	for (i = 0; i < size; i++)
+		out[i] = swap_uint32(in[size - 1 - i]);
 }
 
 /**
@@ -111,54 +120,45 @@ static void montMul1(const struct vb2_public_key *key,
  * @param key		Key to use in signing
  * @param inout		Input and output big-endian byte array
  * @param workbuf32	Work buffer; caller must verify this is
- *			(3 * key->arrsize) elements long.
+ *			(5 * key->arrsize) elements long.
  * @param exp		RSA public exponent: either 65537 (F4) or 3
  */
 void modpow(const struct vb2_public_key *key, uint8_t *inout,
 	    uint32_t *workbuf32, int exp)
 {
+	const uint32_t mu = (uint32_t)(1ULL << 32) - key->n0inv;
 	uint32_t *a = workbuf32;
 	uint32_t *aR = a + key->arrsize;
 	uint32_t *aaR = aR + key->arrsize;
 	uint32_t *aaa = aaR;  /* Re-use location. */
-	int i;
+	uint32_t *d = aaa + key->arrsize;
+	uint32_t *e = d + key->arrsize;
+	uint32_t i;
 
 	/* Convert from big endian byte array to little endian word array. */
-	for (i = 0; i < (int)key->arrsize; ++i) {
-		uint32_t tmp =
-			((uint32_t)inout[((key->arrsize - 1 - i) * 4) + 0]
-			 << 24) |
-			(inout[((key->arrsize - 1 - i) * 4) + 1] << 16) |
-			(inout[((key->arrsize - 1 - i) * 4) + 2] << 8) |
-			(inout[((key->arrsize - 1 - i) * 4) + 3] << 0);
-		a[i] = tmp;
-	}
+	swap_bignumber_endianess((uint32_t *)inout, a, key->arrsize);
 
-	montMul(key, aR, a, key->rr);  /* aR = a * RR / R mod M   */
+	montMul(key, aR, a, key->rr, mu, d, e);  /* aR = a * RR / R mod M   */
 	if (exp == 3) {
-		montMul(key, aaR, aR, aR); /* aaR = aR * aR / R mod M */
-		montMul(key, a, aaR, aR); /* a = aaR * aR / R mod M */
-		montMul1(key, aaa, a); /* aaa = a * 1 / R mod M */
+		montMul(key, aaR, aR, aR, mu, d, e); /* aaR = aR * aR / R mod M */
+		montMul(key, a, aaR, aR, mu, d, e); /* a = aaR * aR / R mod M */
+
+		/* To multiply with 1, prepare aR with first element 1 and
+		   others as 0. */
+		aR[0] = 1;
+		for (i = 1; i < key->arrsize; i++)
+			aR[i] = 0;
+
+		montMul(key, aaa, a, aR, mu, d, e); /* aaa = a * aR / R mod M = a * 1 / R mod M */
 	} else {
 		/* Exponent 65537 */
 		for (i = 0; i < 16; i+=2) {
-			montMul(key, aaR, aR, aR);  /* aaR = aR * aR / R mod M */
-			montMul(key, aR, aaR, aaR);  /* aR = aaR * aaR / R mod M */
+			montMul(key, aaR, aR, aR, mu, d, e);  /* aaR = aR * aR / R mod M */
+			montMul(key, aR, aaR, aaR, mu, d, e);  /* aR = aaR * aaR / R mod M */
 		}
-		montMul(key, aaa, aR, a);  /* aaa = aR * a / R mod M */
-	}
-
-	/* Make sure aaa < mod; aaa is at most 1x mod too large. */
-	if (vb2_mont_ge(key, aaa)) {
-		subM(key, aaa);
+		montMul(key, aaa, aR, a, mu, d, e);  /* aaa = aR * a / R mod M */
 	}
 
 	/* Convert to bigendian byte array */
-	for (i = (int)key->arrsize - 1; i >= 0; --i) {
-		uint32_t tmp = aaa[i];
-		*inout++ = (uint8_t)(tmp >> 24);
-		*inout++ = (uint8_t)(tmp >> 16);
-		*inout++ = (uint8_t)(tmp >>  8);
-		*inout++ = (uint8_t)(tmp >>  0);
-	}
+	swap_bignumber_endianess(aaa, (uint32_t *)inout, key->arrsize);
 }
