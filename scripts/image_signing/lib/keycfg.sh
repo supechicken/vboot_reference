6a5bd81f044e9144b7a3d1d7772cd92fc51f6689
Ching-Kang Yen <chingkang@google.com>
2023-10-20 17:36:43 +0800

    scripts: add lib/keycfg.sh
    BRANCH=None
    BUG=b:294370817
    TEST=CQ
    
    Signed-off-by: Ching-Kang Yen <chingkang@google.com>
    Change-Id: Ie50dbfb5f17944c703eb2d96a6d3cb61d4f9d53f

---
+++ scripts/image_signing/lib/keycfg.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Setup the default key configuration by using the local key in `key_dir`.
+setup_default_keycfg() {
+  local key_dir=$1
+  KEYCFG_KERNEL_KEYBLOCK="${key_dir}/kernel.keyblock"
+  KEYCFG_KERNEL_VBPRIVK="${key_dir}/kernel_data_key.vbprivk"
+  KEYCFG_MINIOS_KERNEL_KEYBLOCK="${key_dir}/minios_kernel.keyblock"
+  KEYCFG_MINIOS_KERNEL_V1_KEYBLOCK="${key_dir}/minios_kernel.v1.keyblock"
+  KEYCFG_MINIOS_KERNEL_VBPRIVK="${key_dir}/minios_kernel_data_key.vbprivk"
+  KEYCFG_RECOVERY_KERNEL_KEYBLOCK="${key_dir}/recovery_kernel.keyblock"
+  KEYCFG_RECOVERY_KERNEL_V1_KEYBLOCK="${key_dir}/recovery_kernel.v1.keyblock"
+  KEYCFG_RECOVERY_KERNEL_VBPRIVK="${key_dir}/recovery_kernel_data_key.vbprivk"
+  KEYCFG_INSTALLER_KERNEL_KEYBLOCK="${key_dir}/installer_kernel.keyblock"
+  KEYCFG_INSTALLER_KERNEL_V1_KEYBLOCK="${key_dir}/installer_kernel.v1.keyblock"
+  KEYCFG_INSTALLER_KERNEL_VBPRIVK="${key_dir}/installer_kernel_data_key.vbprivk"
+  KEYCFG_ARV_PLATFORM_KEYBLOCK="${key_dir}/arv_platform.keyblock"
+  KEYCFG_ARV_PLATFORM_VBPRIVK="${key_dir}/arv_platform.vbprivk"
+}
+
+setup_keycfg() {
+  local key_dir=$1
+  setup_default_keycfg "${key_dir}"
+  if [ -f "${key_dir}/key_config.sh" ]; then
+    . "${key_dir}/key_config.sh"
+  fi
+}
