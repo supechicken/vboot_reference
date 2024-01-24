6a0faaea3db84e04a176b7651beac30b3539919b
Jakub Czapiga <czapiga@google.com>
2024-01-24 15:27:28 +0100

    scripts/image_signing/make_dev_ssh.sh: Improve parameter removal
    Improve sed regexp to match only whole parameter names and to capture
    potential values without need to pass them to the function.
    
    BRANCH=none
    BUG=b:322156997
    TEST=Test make_dev_ssd.sh with --{enable,disable}_console
    TEST=Manually test function to check corner cases
    
    Change-Id: I4bf2a3ce31bf5c96d4ef97165229e62c3bbdbf27
    Signed-off-by: Jakub Czapiga <czapiga@google.com>

--- scripts/image_signing/make_dev_ssd.sh
+++ scripts/image_signing/make_dev_ssd.sh
@@ -138,8 +138,9 @@ remove_parameter() {
   local cmdline="$1"
   local param="$2"
 
-  cmdline=$(echo "${cmdline}" | sed '
-    s/'"${param} "'//g')
+  cmdline=$(echo "${cmdline}" | \
+    sed -E 's/(^|\s)\s*'"${param}"'=?(\s|$)\s*/\1/g' | \
+    sed -E 's/(^|\s)\s*'"${param}"'=(("?)(\\"|[^"])*\3)(\s|$)\s*/\1/g')
 
   echo "${cmdline}"
 }
@@ -294,7 +295,7 @@ resign_ssd_kernel() {
 
     if [ "${FLAGS_enable_console}" = "${FLAGS_TRUE}" ]; then
       debug_msg "Enabling serial console"
-      kernel_config="$(remove_parameter "${kernel_config}" "console=")"
+      kernel_config="$(remove_parameter "${kernel_config}" "console")"
       debug_msg "New kernel config: ${kernel_config}"
     elif [ "${FLAGS_disable_console}" = "${FLAGS_TRUE}" ]; then
       debug_msg "Disabling serial console"
@@ -308,7 +309,7 @@ resign_ssd_kernel() {
       debug_msg "New kernel config: ${kernel_config}"
     elif [ "${FLAGS_disable_kdump}" = "${FLAGS_TRUE}" ]; then
       debug_msg "Disabling kdump"
-      kernel_config="$(remove_parameter "${kernel_config}" "crashkernel=256M")"
+      kernel_config="$(remove_parameter "${kernel_config}" "crashkernel")"
       debug_msg "New kernel config: ${kernel_config}"
     fi
 
