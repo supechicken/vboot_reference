ff996719ba3e26fb2714fb298f5d9bbd8bc652f8
Jan Dabros <dabros@google.com>
2024-01-28 23:52:12 +0000

    avb: Add avb_ops for IO operations
    Implement callbacks for reading partitions size and content. For read
    operation with offset value below 0, it means the offset from the end of
    the partitions, thus needs to do correct operations in the driver.
    
    BUG=b:309426555
    TEST=With consecutive CLs applied build FW and boot android
    BRANCH=main
    
    Change-Id: Ibe25476de7773fcdf30069f4b042afdc9e8cca89
    Signed-off-by: Jan Dabros <dabros@google.com>
    Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6030616
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    (cherry picked from commit 6dc845b9b4c9e3fe10dde3fed6fbd382a68ff77e)

---
+++ firmware/2lib/include/2avb.h
@@ -0,0 +1,71 @@
+<<<<<<< HEAD   (425ede 2lib: Add gbb flag to enforce CSE sync)
+||||||| BASE
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef VBOOT_REFERENCE_2AVB_H_
+#define VBOOT_REFERENCE_2AVB_H_
+
+#include "2common.h"
+#include "gpt_misc.h"
+#include "vboot_api.h"
+
+#include <libavb.h>
+
+/*
+ * Initialize platform callbacks used within libavb.
+ *
+ * @param  vb2_ctx     Vboot context
+ * @return pointer to AvbOps structure which should be used for invocation of
+ *         libavb methods.
+ */
+AvbOps *vboot_avb_ops_new(struct vb2_context *vb2_ctx);
+
+/*
+ * Free structure associated with AvbOps structure.
+ *
+ * @param  ops    pointer AvbOps structure
+ */
+void vboot_avb_ops_free(AvbOps *ops);
+
+#endif // VBOOT_REFERENCE_2AVB_H_
+=======
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef VBOOT_REFERENCE_2AVB_H_
+#define VBOOT_REFERENCE_2AVB_H_
+
+#include "2api.h"
+#include "2common.h"
+#include "gpt_misc.h"
+#include "vboot_api.h"
+
+#include <libavb.h>
+
+/*
+ * Initialize platform callbacks used within libavb.
+ *
+ * @param  vb2_ctx     Vboot context
+ * @param  gpt         Pointer to gpt struct correlated with boot disk
+ * @param  disk_handle Handle to boot disk
+ * @return pointer to AvbOps structure which should be used for invocation of
+ *         libavb methods.
+ */
+AvbOps *vboot_avb_ops_new(struct vb2_context *vb2_ctx,
+			  GptData *gpt,
+			  vb2ex_disk_handle_t disk_handle);
+
+/*
+ * Free structure associated with AvbOps structure.
+ *
+ * @param  ops    pointer AvbOps structure
+ */
+void vboot_avb_ops_free(AvbOps *ops);
+
+#endif // VBOOT_REFERENCE_2AVB_H_
+>>>>>>> CHANGE (6dc845 avb: Add avb_ops for IO operations)
