e2c71f51c1a7e20cec71ebd89e770feaf9efaff3
Jan Dabros <dabros@google.com>
2024-01-28 23:56:56 +0000

    avb: Add callback for public key verification
    In order to establish secure link between platform and software it is
    necessary to embed public part of the signing key within the persistent
    storage (in our case flash memory). During boot & verification process,
    this key is compared by AVB library to the one used to sign vbmeta
    partition.
    
    BUG=b:309426555
    TEST=With consecutive CLs applied build FW and boot android
    BRANCH=main
    
    Change-Id: Ia2dd88bdbfc745f509922369a7c7b3f808c2e48e
    Signed-off-by: Jan Dabros <dabros@google.com>
    Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6030617
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    (cherry picked from commit 45176543c260047aa7e0623533e7feacb898bdf0)

---
+++ firmware/avb/vboot_avb_ops.c
@@ -0,0 +1,544 @@
+<<<<<<< HEAD   (425ede 2lib: Add gbb flag to enforce CSE sync)
+||||||| BASE
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Implementation of callbacks needed by libavb library
+ */
+
+#include <libavb.h>
+
+#include "2avb.h"
+#include "2common.h"
+#include "2misc.h"
+#include "2nvstorage.h"
+#include "2secdata.h"
+#include "cgptlib.h"
+#include "cgptlib_internal.h"
+#include "gpt_misc.h"
+
+struct vboot_avb_ctx {
+	GptData *gpt;
+	vb2ex_disk_handle_t disk_handle;
+	struct vb2_context *vb2_ctx;
+};
+
+static inline struct vboot_avb_ctx *user_data(AvbOps *ops)
+{
+	return ops->user_data;
+}
+
+static AvbIOResult read_from_partition(AvbOps *ops,
+				       const char *partition_name,
+				       int64_t offset_from_partition,
+				       size_t num_bytes,
+				       void *buf,
+				       size_t *out_num_read)
+{
+	struct vboot_avb_ctx *avbctx;
+	VbExStream_t stream;
+	uint64_t part_bytes, part_start_sector;
+	uint64_t start_sector, sectors_to_read;
+	uint32_t sector_bytes;
+	GptData *gpt;
+	GptEntry *e;
+
+	avbctx = user_data(ops);
+	gpt = avbctx->gpt;
+	*out_num_read = 0;
+
+	e = GptFindEntryByName(gpt, partition_name, NULL);
+	if (e == NULL) {
+		VB2_DEBUG("Unable to find %s partition\n", partition_name);
+		return AVB_IO_RESULT_ERROR_NO_SUCH_PARTITION;
+	}
+
+	part_bytes = GptGetEntrySizeBytes(gpt, e);
+	part_start_sector = e->starting_lba;
+	sector_bytes = gpt->sector_bytes;
+
+	if (part_start_sector * sector_bytes > (part_start_sector * sector_bytes) + part_bytes)
+		return AVB_IO_RESULT_ERROR_IO;
+
+	if (offset_from_partition < 0)
+		offset_from_partition += part_bytes;
+
+	if (offset_from_partition < 0 || offset_from_partition > part_bytes) {
+		VB2_DEBUG("Incorrect offset from partition %" PRId64
+			  "for partition %s with size %" PRIu64 "\n",
+			  offset_from_partition, partition_name, part_bytes);
+		return AVB_IO_RESULT_ERROR_RANGE_OUTSIDE_PARTITION;
+	}
+
+	if (num_bytes > part_bytes - offset_from_partition) {
+		VB2_DEBUG("Trying to read %zu bytes from %s@%" PRIu64
+			  ", but only %" PRIu64 " bytes long\n",
+			  num_bytes, partition_name, offset_from_partition,
+			  part_bytes - offset_from_partition);
+		num_bytes = part_bytes - offset_from_partition;
+	}
+
+	/* TODO(b/331881159): remove this check when misaligned read implemented */
+	if ((offset_from_partition % sector_bytes) || (num_bytes % sector_bytes)) {
+		VB2_DEBUG("Misaligned read from %s, offset %" PRId64 " num_bytes %zu\n",
+			  partition_name, offset_from_partition, num_bytes);
+		return AVB_IO_RESULT_ERROR_IO;
+	}
+
+	start_sector = part_start_sector +  offset_from_partition / sector_bytes;
+	sectors_to_read = num_bytes / sector_bytes;
+
+	if (VbExStreamOpen(avbctx->disk_handle, start_sector, sectors_to_read,
+			   &stream)) {
+		VB2_DEBUG("Unable to open disk handle\n");
+		return AVB_IO_RESULT_ERROR_IO;
+	}
+
+	if (VbExStreamRead(stream, sectors_to_read * sector_bytes, buf)) {
+		VB2_DEBUG("Unable to read ramdisk partition\n");
+		return AVB_IO_RESULT_ERROR_IO;
+	}
+
+	*out_num_read = num_bytes;
+
+	VbExStreamClose(stream);
+
+	return AVB_IO_RESULT_OK;
+}
+
+static AvbIOResult read_rollback_index(AvbOps *ops,
+				       size_t rollback_index_slot,
+				       uint64_t *out_rollback_index)
+{
+	/*
+	 * TODO(b/324230492): Implement rollback protection
+	 * For now we always return 0 as the stored rollback index.
+	 */
+	VB2_DEBUG("TODO: not implemented yet\n");
+	if (out_rollback_index != NULL)
+		*out_rollback_index = 0;
+
+	return AVB_IO_RESULT_OK;
+}
+
+static AvbIOResult read_is_device_unlocked(AvbOps *ops, bool *out_is_unlocked)
+{
+	struct vboot_avb_ctx *avbctx = user_data(ops);
+
+	if (vb2_need_kernel_verification(avbctx->vb2_ctx))
+		*out_is_unlocked = false;
+	else if (avbctx->vb2_ctx->boot_mode == VB2_BOOT_MODE_DEVELOPER &&
+	    vb2_secdata_fwmp_get_flag(avbctx->vb2_ctx, VB2_SECDATA_FWMP_DEV_USE_KEY_HASH))
+		*out_is_unlocked = false;
+	else
+		*out_is_unlocked = true;
+
+	VB2_DEBUG("%s\n", *out_is_unlocked ? "true" : "false");
+
+	return AVB_IO_RESULT_OK;
+}
+
+static AvbIOResult get_unique_guid_for_partition(AvbOps *ops,
+						 const char *partition,
+						 char *guid_buf,
+						 size_t guid_buf_size)
+{
+	struct vboot_avb_ctx *avbctx;
+	GptData *gpt;
+	GptEntry *e;
+
+	VB2_ASSERT(ops && ops->user_data);
+
+	avbctx = user_data(ops);
+	gpt = avbctx->gpt;
+	VB2_ASSERT(gpt);
+
+	e = GptFindEntryByName(gpt, partition, NULL);
+	if (e == NULL)
+		return AVB_IO_RESULT_ERROR_NO_SUCH_PARTITION;
+
+	GptGuidToStr(&e->unique, guid_buf, guid_buf_size, GPT_GUID_UPPERCASE);
+	return AVB_IO_RESULT_OK;
+}
+
+static AvbIOResult get_size_of_partition(AvbOps *ops,
+					 const char *partition_name,
+					 uint64_t *out_size)
+{
+	struct vboot_avb_ctx *avbctx = user_data(ops);
+	GptEntry *e;
+
+	e = GptFindEntryByName(avbctx->gpt, partition_name, NULL);
+	if (e == NULL) {
+		VB2_DEBUG("Unable to find %s partition\n", partition_name);
+		return AVB_IO_RESULT_ERROR_NO_SUCH_PARTITION;
+	}
+
+	*out_size = GptGetEntrySizeBytes(avbctx->gpt, e);
+
+	return AVB_IO_RESULT_OK;
+}
+
+/*
+ * Initialize platform callbacks used within libavb.
+ *
+ * @param  vb2_ctx     Vboot context
+ * @param  gpt         Pointer to gpt struct correlated with boot disk
+ * @param  disk_handle Handle to boot disk
+ * @return pointer to AvbOps structure which should be used for invocation of
+ *         libavb methods.
+ */
+AvbOps *vboot_avb_ops_new(struct vb2_context *vb2_ctx,
+			  GptData *gpt,
+			  vb2ex_disk_handle_t disk_handle)
+{
+	struct vboot_avb_ctx *avbctx;
+	AvbOps *avb_ops;
+
+	avb_ops = malloc(sizeof(*avb_ops));
+	if (avb_ops == NULL)
+		return NULL;
+	memset(avb_ops, 0, sizeof(*avb_ops));
+
+	avbctx = malloc(sizeof(*avbctx));
+	if (avbctx == NULL) {
+		free(avb_ops);
+		return NULL;
+	}
+	memset(avbctx, 0, sizeof(*avbctx));
+
+	avbctx->disk_handle = disk_handle;
+	avbctx->gpt = gpt;
+	avbctx->vb2_ctx = vb2_ctx;
+	avb_ops->user_data = avbctx;
+
+	avb_ops->read_from_partition = read_from_partition;
+	avb_ops->read_rollback_index = read_rollback_index;
+	avb_ops->read_is_device_unlocked = read_is_device_unlocked;
+	avb_ops->get_unique_guid_for_partition = get_unique_guid_for_partition;
+	avb_ops->get_size_of_partition = get_size_of_partition;
+
+	return avb_ops;
+}
+
+void vboot_avb_ops_free(AvbOps *ops)
+{
+	if (ops == NULL)
+		return;
+
+	free(ops->user_data);
+	free(ops);
+}
+=======
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Implementation of callbacks needed by libavb library
+ */
+
+#include <libavb.h>
+
+#include "2avb.h"
+#include "2common.h"
+#include "2misc.h"
+#include "2nvstorage.h"
+#include "2secdata.h"
+#include "cgptlib.h"
+#include "cgptlib_internal.h"
+#include "gpt_misc.h"
+
+struct vboot_avb_ctx {
+	GptData *gpt;
+	vb2ex_disk_handle_t disk_handle;
+	struct vb2_context *vb2_ctx;
+};
+
+static inline struct vboot_avb_ctx *user_data(AvbOps *ops)
+{
+	return ops->user_data;
+}
+
+static AvbIOResult read_from_partition(AvbOps *ops,
+				       const char *partition_name,
+				       int64_t offset_from_partition,
+				       size_t num_bytes,
+				       void *buf,
+				       size_t *out_num_read)
+{
+	struct vboot_avb_ctx *avbctx;
+	VbExStream_t stream;
+	uint64_t part_bytes, part_start_sector;
+	uint64_t start_sector, sectors_to_read;
+	uint32_t sector_bytes;
+	GptData *gpt;
+	GptEntry *e;
+
+	avbctx = user_data(ops);
+	gpt = avbctx->gpt;
+	*out_num_read = 0;
+
+	e = GptFindEntryByName(gpt, partition_name, NULL);
+	if (e == NULL) {
+		VB2_DEBUG("Unable to find %s partition\n", partition_name);
+		return AVB_IO_RESULT_ERROR_NO_SUCH_PARTITION;
+	}
+
+	part_bytes = GptGetEntrySizeBytes(gpt, e);
+	part_start_sector = e->starting_lba;
+	sector_bytes = gpt->sector_bytes;
+
+	if (part_start_sector * sector_bytes > (part_start_sector * sector_bytes) + part_bytes)
+		return AVB_IO_RESULT_ERROR_IO;
+
+	if (offset_from_partition < 0)
+		offset_from_partition += part_bytes;
+
+	if (offset_from_partition < 0 || offset_from_partition > part_bytes) {
+		VB2_DEBUG("Incorrect offset from partition %" PRId64
+			  "for partition %s with size %" PRIu64 "\n",
+			  offset_from_partition, partition_name, part_bytes);
+		return AVB_IO_RESULT_ERROR_RANGE_OUTSIDE_PARTITION;
+	}
+
+	if (num_bytes > part_bytes - offset_from_partition) {
+		VB2_DEBUG("Trying to read %zu bytes from %s@%" PRIu64
+			  ", but only %" PRIu64 " bytes long\n",
+			  num_bytes, partition_name, offset_from_partition,
+			  part_bytes - offset_from_partition);
+		num_bytes = part_bytes - offset_from_partition;
+	}
+
+	/* TODO(b/331881159): remove this check when misaligned read implemented */
+	if ((offset_from_partition % sector_bytes) || (num_bytes % sector_bytes)) {
+		VB2_DEBUG("Misaligned read from %s, offset %" PRId64 " num_bytes %zu\n",
+			  partition_name, offset_from_partition, num_bytes);
+		return AVB_IO_RESULT_ERROR_IO;
+	}
+
+	start_sector = part_start_sector +  offset_from_partition / sector_bytes;
+	sectors_to_read = num_bytes / sector_bytes;
+
+	if (VbExStreamOpen(avbctx->disk_handle, start_sector, sectors_to_read,
+			   &stream)) {
+		VB2_DEBUG("Unable to open disk handle\n");
+		return AVB_IO_RESULT_ERROR_IO;
+	}
+
+	if (VbExStreamRead(stream, sectors_to_read * sector_bytes, buf)) {
+		VB2_DEBUG("Unable to read ramdisk partition\n");
+		return AVB_IO_RESULT_ERROR_IO;
+	}
+
+	*out_num_read = num_bytes;
+
+	VbExStreamClose(stream);
+
+	return AVB_IO_RESULT_OK;
+}
+
+static AvbIOResult read_rollback_index(AvbOps *ops,
+				       size_t rollback_index_slot,
+				       uint64_t *out_rollback_index)
+{
+	/*
+	 * TODO(b/324230492): Implement rollback protection
+	 * For now we always return 0 as the stored rollback index.
+	 */
+	VB2_DEBUG("TODO: not implemented yet\n");
+	if (out_rollback_index != NULL)
+		*out_rollback_index = 0;
+
+	return AVB_IO_RESULT_OK;
+}
+
+static AvbIOResult read_is_device_unlocked(AvbOps *ops, bool *out_is_unlocked)
+{
+	struct vboot_avb_ctx *avbctx = user_data(ops);
+
+	if (vb2_need_kernel_verification(avbctx->vb2_ctx))
+		*out_is_unlocked = false;
+	else if (avbctx->vb2_ctx->boot_mode == VB2_BOOT_MODE_DEVELOPER &&
+	    vb2_secdata_fwmp_get_flag(avbctx->vb2_ctx, VB2_SECDATA_FWMP_DEV_USE_KEY_HASH))
+		*out_is_unlocked = false;
+	else
+		*out_is_unlocked = true;
+
+	VB2_DEBUG("%s\n", *out_is_unlocked ? "true" : "false");
+
+	return AVB_IO_RESULT_OK;
+}
+
+static AvbIOResult get_unique_guid_for_partition(AvbOps *ops,
+						 const char *partition,
+						 char *guid_buf,
+						 size_t guid_buf_size)
+{
+	struct vboot_avb_ctx *avbctx;
+	GptData *gpt;
+	GptEntry *e;
+
+	VB2_ASSERT(ops && ops->user_data);
+
+	avbctx = user_data(ops);
+	gpt = avbctx->gpt;
+	VB2_ASSERT(gpt);
+
+	e = GptFindEntryByName(gpt, partition, NULL);
+	if (e == NULL)
+		return AVB_IO_RESULT_ERROR_NO_SUCH_PARTITION;
+
+	GptGuidToStr(&e->unique, guid_buf, guid_buf_size, GPT_GUID_UPPERCASE);
+	return AVB_IO_RESULT_OK;
+}
+
+static AvbIOResult get_size_of_partition(AvbOps *ops,
+					 const char *partition_name,
+					 uint64_t *out_size)
+{
+	struct vboot_avb_ctx *avbctx = user_data(ops);
+	GptEntry *e;
+
+	e = GptFindEntryByName(avbctx->gpt, partition_name, NULL);
+	if (e == NULL) {
+		VB2_DEBUG("Unable to find %s partition\n", partition_name);
+		return AVB_IO_RESULT_ERROR_NO_SUCH_PARTITION;
+	}
+
+	*out_size = GptGetEntrySizeBytes(avbctx->gpt, e);
+
+	return AVB_IO_RESULT_OK;
+}
+
+static AvbIOResult validate_vbmeta_public_key(AvbOps *ops,
+					      const uint8_t *public_key_data,
+					      size_t public_key_length,
+					      const uint8_t *public_key_metadata,
+					      size_t public_key_metadata_length,
+					      bool *out_key_is_trusted)
+{
+	struct vboot_avb_ctx *avbctx = user_data(ops);
+	struct vb2_shared_data *sd = vb2_get_sd(avbctx->vb2_ctx);
+	struct vb2_public_key kernel_key;
+	AvbRSAPublicKeyHeader h;
+	uint8_t *key_data;
+	uint32_t key_size;
+	uint32_t arrsize;
+	const uint32_t *avb_n, *avb_rr;
+	vb2_error_t rv;
+
+	*out_key_is_trusted = false;
+	key_data = vb2_member_of(sd, sd->kernel_key_offset);
+	key_size = sd->kernel_key_size;
+	rv = vb2_unpack_key_buffer(&kernel_key, key_data, key_size);
+	if (rv != VB2_SUCCESS) {
+		VB2_DEBUG("Problem with unpacking key buffer: %#x\n", rv);
+		goto out;
+	}
+
+	/*
+	 * Convert key format stored in the vbmeta image - it has different
+	 * endianness and size units compared to the kernel key stored in
+	 * flash
+	 */
+	if (public_key_length < sizeof(AvbRSAPublicKeyHeader)) {
+		VB2_DEBUG("Public key length too small: %zu\n", public_key_length);
+		goto out;
+	}
+
+	if (!avb_rsa_public_key_header_validate_and_byteswap(
+		(const AvbRSAPublicKeyHeader *)public_key_data, &h)) {
+		VB2_DEBUG("Invalid vbmeta public key\n");
+		goto out;
+	}
+
+	if (public_key_length < sizeof(AvbRSAPublicKeyHeader) + h.key_num_bits / 8 * 2) {
+		VB2_DEBUG("Invalid vbmeta public key length: %zu, key_num_bits: %d\n",
+			  public_key_length, h.key_num_bits);
+		goto out;
+	}
+
+	arrsize = kernel_key.arrsize;
+	if (arrsize != (h.key_num_bits / 32)) {
+		VB2_DEBUG("Mismatch in key length! arrsize: %u key_num_bits: %u\n",
+			  arrsize, h.key_num_bits);
+		goto out;
+	}
+
+	if (kernel_key.n0inv != h.n0inv) {
+		VB2_DEBUG("Mismatch in n0inv value: %x! Expected: %x\n",
+			  h.n0inv, kernel_key.n0inv);
+		goto out;
+	}
+
+	avb_n = (uint32_t *)(public_key_data + sizeof(AvbRSAPublicKeyHeader));
+	avb_rr = avb_n + arrsize;
+	for (int i = 0; i < arrsize; i++) {
+		if (kernel_key.n[i] != be32toh(avb_n[arrsize - 1 - i])) {
+			VB2_DEBUG("Mismatch in n key component!\n");
+			goto out;
+		}
+		if (kernel_key.rr[i] != be32toh(avb_rr[arrsize - 1 - i])) {
+			VB2_DEBUG("Mismatch in rr key component!\n");
+			goto out;
+		}
+	}
+
+	*out_key_is_trusted = true;
+out:
+	return AVB_IO_RESULT_OK;
+}
+
+/*
+ * Initialize platform callbacks used within libavb.
+ *
+ * @param  vb2_ctx     Vboot context
+ * @param  gpt         Pointer to gpt struct correlated with boot disk
+ * @param  disk_handle Handle to boot disk
+ * @return pointer to AvbOps structure which should be used for invocation of
+ *         libavb methods.
+ */
+AvbOps *vboot_avb_ops_new(struct vb2_context *vb2_ctx,
+			  GptData *gpt,
+			  vb2ex_disk_handle_t disk_handle)
+{
+	struct vboot_avb_ctx *avbctx;
+	AvbOps *avb_ops;
+
+	avb_ops = malloc(sizeof(*avb_ops));
+	if (avb_ops == NULL)
+		return NULL;
+	memset(avb_ops, 0, sizeof(*avb_ops));
+
+	avbctx = malloc(sizeof(*avbctx));
+	if (avbctx == NULL) {
+		free(avb_ops);
+		return NULL;
+	}
+	memset(avbctx, 0, sizeof(*avbctx));
+
+	avbctx->disk_handle = disk_handle;
+	avbctx->gpt = gpt;
+	avbctx->vb2_ctx = vb2_ctx;
+	avb_ops->user_data = avbctx;
+
+	avb_ops->read_from_partition = read_from_partition;
+	avb_ops->read_rollback_index = read_rollback_index;
+	avb_ops->read_is_device_unlocked = read_is_device_unlocked;
+	avb_ops->get_unique_guid_for_partition = get_unique_guid_for_partition;
+	avb_ops->get_size_of_partition = get_size_of_partition;
+	avb_ops->validate_vbmeta_public_key = validate_vbmeta_public_key;
+
+	return avb_ops;
+}
+
+void vboot_avb_ops_free(AvbOps *ops)
+{
+	if (ops == NULL)
+		return;
+
+	free(ops->user_data);
+	free(ops);
+}
+>>>>>>> CHANGE (451765 avb: Add callback for public key verification)
