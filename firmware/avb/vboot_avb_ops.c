8c228ee9408cb0caa55511006897a1803d43c6b7
Jan Dabros <dabros@google.com-copybot-pick>
2024-01-28 23:52:12 +0000

    avb: Add avb_ops for IO operations Android Bringup: See http://go/android-fw-sync
    Implement callbacks for reading partitions size and content. For read
    operation with offset value below 0, it means the offset from the end of
    the partitions, thus needs to do correct operations in the driver.
    
    BUG=b:309426555
    TEST=With consecutive CLs applied build FW and boot android
    BRANCH=main
    
    (cherry picked from commit 6dc845b9b4c9e3fe10dde3fed6fbd382a68ff77e)
    
    Change-Id: Ibe25476de7773fcdf30069f4b042afdc9e8cca89
    Original-Signed-off-by: Jan Dabros <dabros@google.com>
    Original-Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6030616
    Original-Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    GitOrigin-RevId: 6dc845b9b4c9e3fe10dde3fed6fbd382a68ff77e
    Cr-Build-Id: 8717491780244891409
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8717491780244891409
    Copybot-Job-Name: android-main-vboot_reference-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6460038
    Reviewed-by: Jon Murphy <jpmurphy@google.com>
    Tested-by: Grzegorz Bernacki <bernacki@google.com>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Commit-Queue: Grzegorz Bernacki <bernacki@google.com>
    (cherry picked from commit fb4b13d0bea79c4035cdaf8c1f7098bc61ca97e8)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6562189
    Tested-by: Yidi Lin <yidilin@google.com>
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Commit-Queue: Yidi Lin <yidilin@google.com>

--- firmware/avb/vboot_avb_ops.c
+++ firmware/avb/vboot_avb_ops.c
@@ -29,86 +29,6 @@ static inline struct vboot_avb_ctx *user_data(AvbOps *ops)
 	return ops->user_data;
 }
 
-static AvbIOResult vboot_avb_read_from_partition(AvbOps *ops,
-				       const char *partition_name,
-				       int64_t offset_from_partition,
-				       size_t num_bytes,
-				       void *buf,
-				       size_t *out_num_read)
-{
-	struct vboot_avb_ctx *ctx = (struct vboot_avb_ctx *)ops->user_data;
-	VbExStream_t stream;
-	uint64_t part_start, part_size;
-	uint64_t start_sector, sectors_to_read, pre_misalign;
-	uint8_t *tmp_buf;
-
-	if (GptFindOffsetByName(ctx->gpt, partition_name, &part_start, &part_size) !=
-	    GPT_SUCCESS) {
-		VB2_DEBUG("Unable to find %s partition\n", partition_name);
-		return AVB_IO_RESULT_ERROR_NO_SUCH_PARTITION;
-	}
-
-	if (offset_from_partition >= 0) {
-		start_sector = (offset_from_partition / ctx->gpt->sector_bytes) + part_start;
-		pre_misalign = offset_from_partition % ctx->gpt->sector_bytes;
-	} else {
-		if (-offset_from_partition > part_size * ctx->gpt->sector_bytes)
-			return AVB_IO_RESULT_ERROR_RANGE_OUTSIDE_PARTITION;
-
-		start_sector = part_start +
-			       (part_size * ctx->gpt->sector_bytes + offset_from_partition) /
-				ctx->gpt->sector_bytes;
-		pre_misalign = ctx->gpt->sector_bytes -
-			       (-offset_from_partition % ctx->gpt->sector_bytes);
-		if (pre_misalign == ctx->gpt->sector_bytes)
-			pre_misalign = 0;
-	}
-
-	sectors_to_read = (pre_misalign + num_bytes) / ctx->gpt->sector_bytes;
-	if ((pre_misalign + num_bytes) % ctx->gpt->sector_bytes)
-		sectors_to_read += 1;
-
-	if (sectors_to_read > part_size) {
-		VB2_DEBUG("Read request bigger than available data\n");
-		return AVB_IO_RESULT_ERROR_RANGE_OUTSIDE_PARTITION;
-	}
-
-	if (VbExStreamOpen(ctx->disk_handle, start_sector, sectors_to_read,
-			   &stream)) {
-		VB2_DEBUG("Unable to open disk handle.\n");
-		return AVB_IO_RESULT_ERROR_IO;
-	}
-
-	if (pre_misalign != 0 || (num_bytes % ctx->gpt->sector_bytes)) {
-		tmp_buf = malloc(sectors_to_read * ctx->gpt->sector_bytes);
-		if (tmp_buf == NULL) {
-			VB2_DEBUG("Cannot allocate buffer for unaligned read\n");
-			return AVB_IO_RESULT_ERROR_OOM;
-		}
-	} else {
-		tmp_buf = buf;
-	}
-
-	if (VbExStreamRead(stream, sectors_to_read * ctx->gpt->sector_bytes, tmp_buf)) {
-		VB2_DEBUG("Unable to read ramdisk partition\n");
-		return AVB_IO_RESULT_ERROR_IO;
-	}
-
-	/*
-	 * TODO(b/331881159): "Add support for non-sector size reads in
-	 * depthcharge block driver
-	 */
-	if (pre_misalign != 0 || (num_bytes % ctx->gpt->sector_bytes)) {
-		memcpy(buf, tmp_buf + pre_misalign, num_bytes);
-		free(tmp_buf);
-	}
-	*out_num_read = num_bytes;
-
-	VbExStreamClose(stream);
-
-	return AVB_IO_RESULT_OK;
-}
-
 static AvbIOResult vboot_avb_write_to_partition(AvbOps *ops,
 						const char *partition_name,
 						int64_t offset,
@@ -151,20 +71,80 @@ static AvbIOResult vboot_avb_write_to_partition(AvbOps *ops,
 	return AVB_IO_RESULT_OK;
 }
 
-static AvbIOResult vboot_avb_get_size_of_partition(AvbOps *ops,
-					 const char *partition_name,
-					 uint64_t *out_size)
+static AvbIOResult read_from_partition(AvbOps *ops,
+				       const char *partition_name,
+				       int64_t offset_from_partition,
+				       size_t num_bytes,
+				       void *buf,
+				       size_t *out_num_read)
 {
-	struct vboot_avb_ctx *ctx = (struct vboot_avb_ctx *)ops->user_data;
-	uint64_t part_start, part_size;
+	struct vboot_avb_ctx *avbctx;
+	VbExStream_t stream;
+	uint64_t part_bytes, part_start_sector;
+	uint64_t start_sector, sectors_to_read;
+	uint32_t sector_bytes;
+	GptData *gpt;
+	GptEntry *e;
 
-	if (GptFindOffsetByName(ctx->gpt, partition_name, &part_start, &part_size) !=
-	    GPT_SUCCESS) {
+	avbctx = user_data(ops);
+	gpt = avbctx->gpt;
+	*out_num_read = 0;
+
+	e = GptFindEntryByName(gpt, partition_name, NULL);
+	if (e == NULL) {
 		VB2_DEBUG("Unable to find %s partition\n", partition_name);
 		return AVB_IO_RESULT_ERROR_NO_SUCH_PARTITION;
 	}
 
-	*out_size = ctx->gpt->sector_bytes * part_size;
+	part_bytes = GptGetEntrySizeBytes(gpt, e);
+	part_start_sector = e->starting_lba;
+	sector_bytes = gpt->sector_bytes;
+
+	if (part_start_sector * sector_bytes > (part_start_sector * sector_bytes) + part_bytes)
+		return AVB_IO_RESULT_ERROR_IO;
+
+	if (offset_from_partition < 0)
+		offset_from_partition += part_bytes;
+
+	if (offset_from_partition < 0 || offset_from_partition > part_bytes) {
+		VB2_DEBUG("Incorrect offset from partition %" PRId64
+			  "for partition %s with size %" PRIu64 "\n",
+			  offset_from_partition, partition_name, part_bytes);
+		return AVB_IO_RESULT_ERROR_RANGE_OUTSIDE_PARTITION;
+	}
+
+	if (num_bytes > part_bytes - offset_from_partition) {
+		VB2_DEBUG("Trying to read %zu bytes from %s@%" PRIu64
+			  ", but only %" PRIu64 " bytes long\n",
+			  num_bytes, partition_name, offset_from_partition,
+			  part_bytes - offset_from_partition);
+		num_bytes = part_bytes - offset_from_partition;
+	}
+
+	/* TODO(b/331881159): remove this check when misaligned read implemented */
+	if ((offset_from_partition % sector_bytes) || (num_bytes % sector_bytes)) {
+		VB2_DEBUG("Misaligned read from %s, offset %" PRId64 " num_bytes %zu\n",
+			  partition_name, offset_from_partition, num_bytes);
+		return AVB_IO_RESULT_ERROR_IO;
+	}
+
+	start_sector = part_start_sector +  offset_from_partition / sector_bytes;
+	sectors_to_read = num_bytes / sector_bytes;
+
+	if (VbExStreamOpen(avbctx->disk_handle, start_sector, sectors_to_read,
+			   &stream)) {
+		VB2_DEBUG("Unable to open disk handle\n");
+		return AVB_IO_RESULT_ERROR_IO;
+	}
+
+	if (VbExStreamRead(stream, sectors_to_read * sector_bytes, buf)) {
+		VB2_DEBUG("Unable to read ramdisk partition\n");
+		return AVB_IO_RESULT_ERROR_IO;
+	}
+
+	*out_num_read = num_bytes;
+
+	VbExStreamClose(stream);
 
 	return AVB_IO_RESULT_OK;
 }
@@ -646,6 +626,24 @@ out:
 
 }
 
+static AvbIOResult get_size_of_partition(AvbOps *ops,
+					 const char *partition_name,
+					 uint64_t *out_size)
+{
+	struct vboot_avb_ctx *avbctx = user_data(ops);
+	GptEntry *e;
+
+	e = GptFindEntryByName(avbctx->gpt, partition_name, NULL);
+	if (e == NULL) {
+		VB2_DEBUG("Unable to find %s partition\n", partition_name);
+		return AVB_IO_RESULT_ERROR_NO_SUCH_PARTITION;
+	}
+
+	*out_size = GptGetEntrySizeBytes(avbctx->gpt, e);
+
+	return AVB_IO_RESULT_OK;
+}
+
 /*
  * Initialize platform callbacks used within libavb.
  *
@@ -662,7 +660,6 @@ AvbOps *vboot_avb_ops_new(struct vb2_context *vb2_ctx,
 			  VbExStream_t stream,
 			  GptData *gpt,
 			  vb2ex_disk_handle_t disk_handle)
-)
 {
 	struct vboot_avb_ctx *avbctx;
 	AvbOps *avb_ops;
@@ -687,12 +684,12 @@ AvbOps *vboot_avb_ops_new(struct vb2_context *vb2_ctx,
 
 	avb_ops->user_data = avbctx;
 
-	avb_ops->read_from_partition = vboot_avb_read_from_partition;
+	avb_ops->read_from_partition = read_from_partition;
 	avb_ops->write_to_partition = vboot_avb_write_to_partition;
-	avb_ops->get_size_of_partition = vboot_avb_get_size_of_partition;
 	avb_ops->read_rollback_index = read_rollback_index;
 	avb_ops->read_is_device_unlocked = read_is_device_unlocked;
 	avb_ops->get_unique_guid_for_partition = get_unique_guid_for_partition;
+	avb_ops->get_size_of_partition = get_size_of_partition;
 	avb_ops->validate_vbmeta_public_key = validate_vbmeta_public_key;
 	avb_ops->get_preloaded_partition = vboot_avb_get_preloaded_partition;
 
