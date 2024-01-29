48cd212308e478ffa180e0bbf1e2a474060cee53
Jan Dabros <dabros@google.com>
2024-01-29 00:00:32 +0000

    avb: Get preloaded partitions
    In order to speed up boot process and limit number of memcpy it is
    beneficial to make use of get_preloaded* callback. Instead of IO
    operations, one just need to provide pointer to the memory block which
    is already there.
    
    BUG=b:309426555
    TEST=With consecutive CLs applied build FW and boot android
    BRANCH=main
    
    Change-Id: I056d2976dffdfd90ea804144bf06144dde411536
    Signed-off-by: Konrad Adamczyk <konrada@google.com>
    Signed-off-by: Jan Dabros <dabros@google.com>
    Signed-off-by: Bartłomiej Grzesik <bgrzesik@google.com>
    Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6030618
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    (cherry picked from commit f1df391e6d0565e30a49b88fc51d096926fc4753)

---
+++ firmware/2lib/include/2avb.h
@@ -0,0 +1,80 @@
+<<<<<<< HEAD   (425ede 2lib: Add gbb flag to enforce CSE sync)
+||||||| BASE
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef VBOOT_REFERENCE_2AVB_H_
+#define VBOOT_REFERENCE_2AVB_H_
+
+#include "2api.h"
+#include "2common.h"
+#include "gpt_misc.h"
+#include "vboot_api.h"
+
+#include <libavb.h>
+
+/*
+ * Initialize platform callbacks used within libavb.
+ *
+ * @param  vb2_ctx     Vboot context
+ * @param  gpt         Pointer to gpt struct correlated with boot disk
+ * @param  disk_handle Handle to boot disk
+ * @return pointer to AvbOps structure which should be used for invocation of
+ *         libavb methods.
+ */
+AvbOps *vboot_avb_ops_new(struct vb2_context *vb2_ctx,
+			  GptData *gpt,
+			  vb2ex_disk_handle_t disk_handle);
+
+/*
+ * Free structure associated with AvbOps structure.
+ *
+ * @param  ops    pointer AvbOps structure
+ */
+void vboot_avb_ops_free(AvbOps *ops);
+
+#endif // VBOOT_REFERENCE_2AVB_H_
+=======
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef VBOOT_REFERENCE_2AVB_H_
+#define VBOOT_REFERENCE_2AVB_H_
+
+#include "2api.h"
+#include "2common.h"
+#include "gpt_misc.h"
+#include "vboot_api.h"
+
+#include <libavb.h>
+
+/*
+ * Initialize platform callbacks used within libavb.
+ *
+ * @param  vb2_ctx     Vboot context
+ * @param  params      Vboot kernel parameters
+ * @param  gpt         Pointer to gpt struct correlated with boot disk
+ * @param  disk_handle Handle to boot disk
+ * @param  slot_suffix Suffix of active partition
+ * @return pointer to AvbOps structure which should be used for invocation of
+ *         libavb methods.
+ */
+AvbOps *vboot_avb_ops_new(struct vb2_context *vb2_ctx,
+			  struct vb2_kernel_params *params,
+			  GptData *gpt,
+			  vb2ex_disk_handle_t disk_handle,
+			  const char *slot_suffix);
+
+/*
+ * Free structure associated with AvbOps structure.
+ *
+ * @param  ops    pointer AvbOps structure
+ */
+void vboot_avb_ops_free(AvbOps *ops);
+
+#endif // VBOOT_REFERENCE_2AVB_H_
+>>>>>>> CHANGE (f1df39 avb: Get preloaded partitions)
