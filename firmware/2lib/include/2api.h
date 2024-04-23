26338157cbcdc6ecab9c5fddc39df1da48f3101d
Julius Werner <jwerner@chromium.org>
2024-04-23 15:20:21 -0700

    DEDEDE-ONLY: HACK: Record extra debugging info for vboot corruption
    This patch adds a function that will check for secdata_kernel
    corruption, and record several hints about the system state in the
    recovery subcode field when it finds some. This is intended to help us
    track down the source of secdata_kernel corruption that was observed on
    Metaknight and Cret.
    
    BRANCH=dedede
    BUG=b:331989856
    TEST=Built for Dedede, booted and manually triggered on a Trogdor
    
    Change-Id: Ibbeef35a4e3c098b9afceb2530ed33415b3c05a2
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/5499389
    Reviewed-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    (cherry picked from commit 0825a81e4c067f31001a0a0e597e7c00e87d834d)

--- firmware/2lib/include/2api.h
+++ firmware/2lib/include/2api.h
@@ -1410,4 +1410,8 @@ union vb2_fw_boot_info vb2api_get_fw_boot_info(struct vb2_context *ctx);
  */
 void vb2api_clear_recovery(struct vb2_context *ctx);
 
+/* Returns 0 if no compromise found, otherwise a value that contains all
+   relevant information for logging. */
+uint16_t vb2hack_is_secdata_compromised(struct vb2_context *ctx, int index);
+
 #endif  /* VBOOT_REFERENCE_2API_H_ */
