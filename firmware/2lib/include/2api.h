86b81586019d697d5fac7951ef9bd7844b18742e
Bartłomiej Grzesik <bgrzesik@google.com>
2024-08-27 13:31:34 +0000

    2api: Expand vb2_kernel_params for pvmfw loading
    This change expands vb2_kernel_params struct with two new fields that
    are going to used by following patches in both vboot and depthcharge.
    The fields include the offset of the pvmfw in the kernel_buffer and its
    size.
    
    BUG=b:410735713
    TEST=Build
    BRANCH=main
    
    Change-Id: Ia9cc02a66b4f8d774dfd6846c995fcfc984a69ec
    Signed-off-by: Bartłomiej Grzesik <bgrzesik@google.com>

--- firmware/2lib/include/2api.h
+++ firmware/2lib/include/2api.h
@@ -629,6 +629,17 @@ struct vb2_kernel_params {
 	char *vboot_cmdline_buffer;
 	/* Size of the region with kernel cmdline parameters. */
 	uint32_t vboot_cmdline_size;
+
+	/* Destination buffer for pvmfw. Shall be ignored if pvmfw_buffer_size is 0 */
+	void *pvmfw_buffer;
+	/*
+	 * Size of pvmfw buffer in bytes. If non-zero then implementation shall
+	 * try to load pvmfw to the pvmfw buffer. If successful the pvmfw_out_size
+	 * shall be set to the correct non-zero value.
+	 */
+	uint32_t pvmfw_buffer_size;
+	/* Size of pvmfw partition in bytes in pvmfw buffer. */
+	uint32_t pvmfw_out_size;
 };
 
 /*****************************************************************************/
