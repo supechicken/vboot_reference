4fdf8b5fe2008fb93b8a46da659f77f96e40f144
Benjamin Shai <bshai@google.com>
2024-09-03 20:39:53 +0000

    signing: miniOS signing in docker.
    For some reason, lsblk reports null partition types in docker, even in
    privileged
    (https://forums.docker.com/t/why-does-lsblk-report-partition-as-null-from-within-a-docker-container/139393).
    Since we are already hard-coding the miniOS loop devices, stop checking
    for the partition type explicitly.
    
    This commit also removed the check for the private key on disk because
    the key lives in Cloud KMS, and adds a newline to an info statement that
    was getting concatenated to the previous log message.
    
    BUG=b:363466852
    BRANCH=None
    TEST=manual
    
    Change-Id: I044c626dfac5ae7115b9f6b7e65e8303595544fb
    Signed-off-by: Benjamin Shai <bshai@google.com>

--- scripts/image_signing/sign_official_build.sh
+++ scripts/image_signing/sign_official_build.sh
@@ -1050,11 +1050,6 @@ resign_minios_kernels() {
 
   local loop_minios
   for loop_minios in "${loopdev}p"*; do
-    local part_type_guid
-    part_type_guid=$(sudo lsblk -rnb -o PARTTYPE "${loop_minios}")
-    if [[ "${part_type_guid}" != "${MINIOS_KERNEL_GUID}" ]]; then
-      continue
-    fi
 
     local keyblock
     if [[ "${loop_minios}" == "${loopdev}p9" ]]; then
@@ -1062,8 +1057,10 @@ resign_minios_kernels() {
     elif [[ "${loop_minios}" == "${loopdev}p10" ]]; then
       keyblock="${minios_b_keyblock}"
     else
-      error "Unexpected miniOS partition ${loop_minios}"
-      return 1
+      # miniOS partitions are only present in loop devices p9 and p10, so skip
+      # all other devices. The next check below will actually verify that the
+      # loop device contains a miniOS partition.
+      continue
     fi
 
     # Skip miniOS partitions which are empty. This happens when miniOS
@@ -1074,15 +1071,14 @@ resign_minios_kernels() {
       continue
     fi
 
-    # Delay checking that keyblock and private key exist until we are certain
-    # of a valid miniOS partition.  Images that don't support miniOS might not
-    # provide these.  (This check is repeated twice, but that's okay.)
+    # Delay checking that keyblock exists until we are certain of a valid miniOS
+    # partition. Images that don't support miniOS might not provide these.
+    # (This check is repeated twice, but that's okay.)
+    # Update (9/3/24): we no longer check if the private key exists on disk
+    # because it may live in Cloud KMS instead, opting instead to let futility
+    # below fail if the key is missing.
     if [[ ! -e "${keyblock}" ]]; then
       error "Resign miniOS: keyblock doesn't exist: ${keyblock}"
-      return 1
-    fi
-    if [[ ! -e "${priv_key}" ]]; then
-      error "Resign miniOS: private key doesn't exist: ${priv_key}"
       return 1
     fi
 
@@ -1093,7 +1089,7 @@ resign_minios_kernels() {
         --signprivate "${priv_key}" \
         --version "${minios_kernel_version}" \
         --oldblob "${loop_minios}"; then
-      info "Resign miniOS ${loop_minios}: done"
+      info "\nResign miniOS ${loop_minios}: done"
     else
       error "Resign miniOS ${loop_minios}: failed"
       return 1
