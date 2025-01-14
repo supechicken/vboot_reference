88e9581302bb0b79a9f0205aac6b111844ecb89b
Jian-Jia Su <jjsu@chromium.org>
2025-01-14 17:00:20 +0800

    swap_ec_rw: Support swap from AP image
    For the upcoming firmware release for Android, we want to update the
    AP_RW without changing the EC_RW version inside AP_RW. Currently the
    swap_ec_rw script only supports getting the EC_RW from an EC image.
    Therefore, we would need the EC image (on BCS) with the same version as
    the old AP_RW to perform the swap, which is a tedious task.
    
    To simplify the process, support extracting EC_RW from one AP image and
    swapping into another AP image.
    
    BUG=b:388690994
    TEST=None
    BRANCH=none
    
    Change-Id: If982e80f9f831a902d0c1cf67d7816613cb5d6fd
    Signed-off-by: Jian-Jia Su <jjsu@chromium.org>

--- tests/swap_ec_rw_tests.sh
+++ tests/swap_ec_rw_tests.sh
@@ -29,17 +29,30 @@ echo "Testing swap_ec_rw..."
 cp -f "${AP_IMAGE}" "${TMP}"
 if "${SWAP}" -i "${TMP}"; then false; fi
 
-# Good case: no ecrw.version
+# Good case: swap from EC source (--ec), no ecrw.version
 cp -f "${AP_IMAGE}" "${TMP}"
 "${SWAP}" -i "${TMP}" -e "${EC_IMAGE}"
 cmp "${TMP}" "${DATA}/bios.expect.bin"
 
-# Good case: swap ecrw.version
+# Good case: swap from EC source (--ec), with ecrw.version
 cp -f "${AP_IMAGE}" "${TMP}"
 cbfstool "${TMP}" extract -r "FW_MAIN_A" -n ecrw.version -f "${TMPD}/v.old"
 "${SWAP}" -i "${TMP}" -e "${EC_IMAGE}"
 cbfstool "${TMP}" extract -r "FW_MAIN_A" -n ecrw.version -f "${TMPD}/v.new"
 cmp -s "${TMPD}/v.old" "${TMPD}/v.new" && error "ecrw.version was not modified"
+
+# Good case: swap from AP source (--ap_for_ec)
+cp -f "${AP_IMAGE}" "${TMP}"
+"${SWAP}" -i "${TMP}" -a "${AP_IMAGE}"
+cp -f "${TMP}" "${TMP}.copy"
+
+cbfstool "${TMP}" extract -r "FW_MAIN_A" -n ecrw.version -f "${TMPD}/v.old"
+"${SWAP}" -i "${TMP}" -e "${EC_IMAGE}"
+
+cbfstool "${TMP}" extract -r "FW_MAIN_A" -n ecrw.version -f "${TMPD}/v.new"
+cmp -s "${TMPD}/v.old" "${TMPD}/v.new" && error "ecrw.version was not modified"
+"${SWAP}" -i "${TMP}" -a "${AP_IMAGE}"
+cmp "${TMP}" "${TMP}.copy"
 
 # Cleanup
 rm -rf "${TMPD}"
