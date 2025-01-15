d185cbc02bb07a981d3ee83b1c847e602514221c
Grzegorz Bernacki <bernacki@google.com-copybot-pick>
2025-01-15 12:37:18 +0000

    vboot: modify GptNextKernelEntry function Android Bringup: See http://go/android-fw-sync
    The GptNextKernelEntry function returns the start & size of the boot
    partition. These data are required for booting ChromeOS, but to boot
    Android we need a partition suffix. This change modifies
    GptNextKernelEntry, so it returns a pointer to GptEntry now. It will
    allow to determine which OS is going to be booted by entry type GUID
    and simplify fetching data required for a given OS.
    
    BUG=b:384095358
    TEST='make runtest' and verify build ChromeOS on redrix
    BRANCH=main
    
    (cherry picked from commit 621899d6e89f39b7e1ecb8102fe8288b41ddc06b)
    
    Change-Id: I6c3e4555db9f429cd7bcd96e29c8ab661330de55
    Original-Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6169943
    Original-Reviewed-by: Julius Werner <jwerner@chromium.org>
    GitOrigin-RevId: 621899d6e89f39b7e1ecb8102fe8288b41ddc06b
    Cr-Build-Id: 8725509613852573553
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8725509613852573553
    Copybot-Job-Name: android-main-vboot_reference-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6181147
    Tested-by: Jonathon Murphy <jpmurphy@google.com>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Commit-Queue: Jonathon Murphy <jpmurphy@google.com>
    (cherry picked from commit 5a32d0f2ff30953f44cb20626b2eedcc4c83c769)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6244807
    Reviewed-by: Konrad Adamczyk <konrada@google.com>
    Commit-Queue: Tomasz Michalec <tmichalec@google.com>
    Tested-by: Tomasz Michalec <tmichalec@google.com>

--- firmware/lib/cgptlib/include/cgptlib.h
+++ firmware/lib/cgptlib/include/cgptlib.h
@@ -20,17 +20,15 @@ enum GptPartition {
 extern const char *GptPartitionNames[];
 
 /**
- * Provides the location of the next kernel partition, in order of decreasing
+ * Provides the location of the next bootable partition, in order of decreasing
  * priority.
  *
- * On return the start_sector parameter contains the LBA sector for the start
- * of the kernel partition, and the size parameter contains the size of the
- * kernel partition in LBA sectors.  gpt.current_kernel contains the partition
- * index of the current chromeos kernel partition.
+ * On return gpt.current_kernel contains the partition index of the current
+ * bootable partition.
  *
- * Returns GPT_SUCCESS if successful, else
- *   GPT_ERROR_NO_VALID_KERNEL, no avaliable kernel, enters recovery mode */
-int GptNextKernelEntry(GptData *gpt, uint64_t *start_sector, uint64_t *size);
+ * Returns gpt entry of partition to boot if successful, else NULL
+ */
+GptEntry *GptNextKernelEntry(GptData *gpt);
 
 /**
  * Checks is entry name field is equal to name+suffix.
