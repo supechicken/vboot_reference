37f8e3b6e812890e8cd9645737f9fc5ad75a8724
Grzegorz Bernacki <bernacki@google.com-copybot-pick>
2025-02-12 13:20:42 +0000

    2lib: Rearrange buffers with android partitions Android Bringup: See http://go/android-fw-sync
    Android requires ramdisks being prepared by FW. Ramdisks are stored
    in init_boot and vendor_boot partitions and must be loaded into
    memory in specific order.
    Specifics are described here:
    https://source.android.com/docs/core/architecture/partitions/vendor-boot-partitions#bootloader-support
    
    BUG=b:309426555
    TEST=Build FW and boot android
    TEST=Use description from b/309426555#comment3
    BRANCH=main
    
    (cherry picked from commit a682da7d92db70d0b7e0ad539e978452e05943bb)
    
    Original-Signed-off-by: Konrad Adamczyk <konrada@google.com>
    Original-Signed-off-by: Jan Dabros <dabros@google.com>
    Original-Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
    Change-Id: Iec36df36417c20e32842d4a378c718012014579c
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6281345
    Original-Tested-by: Grzegorz Bernacki <bernacki@google.com>
    Original-Reviewed-by: Julius Werner <jwerner@chromium.org>
    Original-Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Original-Commit-Queue: Grzegorz Bernacki <bernacki@google.com>
    GitOrigin-RevId: a682da7d92db70d0b7e0ad539e978452e05943bb
    Cr-Build-Id: 8717491780244891409
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8717491780244891409
    Copybot-Job-Name: android-main-vboot_reference-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6460664
    Reviewed-by: Jon Murphy <jpmurphy@google.com>
    Tested-by: Grzegorz Bernacki <bernacki@google.com>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Commit-Queue: Grzegorz Bernacki <bernacki@google.com>
    (cherry picked from commit 867f122e5c538b65c69a86b1ad463548bdc1ea60)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6562192
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Tested-by: Yidi Lin <yidilin@google.com>
    Commit-Queue: Yidi Lin <yidilin@google.com>

--- firmware/2lib/include/2api.h
+++ firmware/2lib/include/2api.h
@@ -638,6 +638,17 @@ struct vb2_kernel_params {
 	uint8_t partition_guid[16];
 	/* Flags with kernel type. */
 	uint32_t flags;
+	/* Ramdisk address */
+	uint8_t *ramdisk;
+	/* Size of the ramdisk */
+	size_t ramdisk_size;
+	/* Bootconfig address */
+	void *bootconfig;
+	/* Size of the bootconfig */
+	size_t bootconfig_size;
+	/* Pointer to Android vendor command line buffer */
+	char *vendor_cmdline_buffer;
+	// Legacy Android boot
 	/* Android vendor_boot partition offset (in bytes) in kernel_buffer. */
 	uint32_t vendor_boot_offset;
 	/* Android init_boot partition offset (in bytes) in kernel_buffer. */
