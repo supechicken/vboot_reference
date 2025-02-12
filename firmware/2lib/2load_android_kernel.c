019345fa705727e661d4f387abcb66bf6f16e3d4
Grzegorz Bernacki <bernacki@google.com>
2025-02-12 13:20:42 +0000

    2lib: Rearrange buffers with android partitions
    Android requires ramdisks being prepared by FW. Ramdisks are stored
    in init_boot and vendor_boot partition and must by loaded into
    memory in specific order.
    Specifics are described here:
    https://source.android.com/docs/core/architecture/partitions/vendor-boot-partitions#bootloader-support
    
    BUG=b:309426555
    TEST=Build FW and boot android
    TEST=Use description from b/309426555#comment3
    BRANCH=main
    
    Signed-off-by: Konrad Adamczyk <konrada@google.com>
    Signed-off-by: Jan Dabros <dabros@google.com>
    Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
    
    Change-Id: Iec36df36417c20e32842d4a378c718012014579c

--- firmware/2lib/2load_android_kernel.c
+++ firmware/2lib/2load_android_kernel.c
@@ -52,6 +52,97 @@ static int vb2_map_libavb_errors(AvbSlotVerifyResult avb_error)
 	}
 }
 
+/*
+ * Copy bootconfig into separate buffer, it can be overwritten when ramdisks
+ * are concatenated. Bootconfig buffer will be processed by depthcharge.
+ */
+static int save_bootconfig(struct vendor_boot_img_hdr_v4 *vendor_hdr,
+			    struct vb2_kernel_params *params)
+{
+	uint8_t *bootconfig;
+	uint32_t page_size = vendor_hdr->page_size;
+
+	params->bootconfig_size = vendor_hdr->bootconfig_size;
+	if (!params->bootconfig_size)
+		return 0;
+
+	bootconfig = (uint8_t *)vendor_hdr +
+		     ALIGN_UP(sizeof(struct vendor_boot_img_hdr_v4), page_size) +
+		     ALIGN_UP(vendor_hdr->vendor_ramdisk_size, page_size) +
+		     ALIGN_UP(vendor_hdr->dtb_size, page_size) +
+		     ALIGN_UP(vendor_hdr->vendor_ramdisk_table_size, page_size);
+
+	params->bootconfig = malloc(params->bootconfig_size);
+	if (!params->bootconfig) {
+		VB2_DEBUG("Cannot malloc %ld bytes for bootconfig", params->bootconfig_size);
+		return -1;
+	}
+
+	memcpy(params->bootconfig, bootconfig, params->bootconfig_size);
+	return 0;
+}
+
+
+/*
+ * This function validates the partitions magic numbers and move them into place requested
+ * from linux.
+ */
+static int android_rearrange_partitions(struct vb2_kernel_params *params)
+{
+	struct vendor_boot_img_hdr_v4 *vendor_hdr;
+	struct boot_img_hdr_v4 *init_hdr;
+	uint8_t *vendor_ramdisk_end = 0;
+	int ret;
+
+	vendor_hdr = (struct vendor_boot_img_hdr_v4 *)parts[GPT_ANDROID_VENDOR_BOOT].buffer;
+	if (memcmp(vendor_hdr->magic, VENDOR_BOOT_MAGIC, VENDOR_BOOT_MAGIC_SIZE)) {
+		VB2_DEBUG("VENDOR_BOOT_MAGIC mismatch!\n");
+		return VB2_ERROR_LK_NO_KERNEL_FOUND;
+	}
+
+	/* Save bootconfig for depthcharge, it can be overwritten when ramdisk are moved */
+	ret = save_bootconfig(vendor_hdr, params);
+	if (ret)
+		return VB2_ERROR_LK_NO_KERNEL_FOUND;
+
+
+	/* Validate init_boot partition */
+	init_hdr = (struct boot_img_hdr_v4 *)parts[GPT_ANDROID_INIT_BOOT].buffer;
+	if (memcmp(init_hdr->magic, BOOT_MAGIC, BOOT_MAGIC_SIZE)) {
+		VB2_DEBUG("BOOT_MAGIC mismatch!\n");
+		return VB2_ERROR_LK_NO_KERNEL_FOUND;
+	}
+	if (init_hdr->kernel_size != 0) {
+		printf("GKI: Kernel size on init_boot partition has to be zero\n");
+		return -1;
+	}
+
+	/* On init_boot there's no kernel, so ramdisk follows the header */
+	uint8_t *init_boot_ramdisk = (uint8_t *)init_hdr + ANDROID_GKI_BOOT_HDR_SIZE;
+	size_t init_boot_ramdisk_size = init_hdr->ramdisk_size;
+
+	/*
+	 * Move init_boot ramdisk to directly follow the vendor_boot ramdisk.
+	 * This is a requirement from Android system. The cpio/gzip/lz4
+	 * compression formats support this type of concatenation. After
+	 * the kernel decompresses, it extracts contatenated file into
+	 * an initramfs, which results in a file structure that's a generic
+	 * ramdisk (from init_boot) overlaid on the vendor ramdisk (from
+	 * vendor_boot) file structure.
+	 */
+	vendor_ramdisk_end = (uint8_t *)vendor_hdr +
+		ALIGN_UP(sizeof(struct vendor_boot_img_hdr_v4), vendor_hdr->page_size) +
+		vendor_hdr->vendor_ramdisk_size;
+	VB2_ASSERT(vendor_ramdisk_end < init_boot_ramdisk);
+	memmove(vendor_ramdisk_end, init_boot_ramdisk, init_boot_ramdisk_size);
+	params->ramdisk_size += init_boot_ramdisk_size;
+
+	/* Save vendor cmdline for booting */
+	params->vendor_cmdline_buffer = (char *)vendor_hdr->cmdline;
+
+	return 0;
+}
+
 vb2_error_t vb2_load_android(struct vb2_context *ctx, GptData *gpt, GptEntry *entry,
 			     struct vb2_kernel_params *params, vb2ex_disk_handle_t disk_handle)
 {
@@ -122,6 +213,12 @@ vb2_error_t vb2_load_android(struct vb2_context *ctx, GptData *gpt, GptEntry *en
 		return ret;
 	}
 
+	/*
+	 * Before booting we need to rearrange buffers with partition data, which includes:
+	 * - save bootconfig in separate buffer, so depthcharge can modify it
+	 * - concatenate ramdisks from vendor_boot & init_boot partitions
+	 */
+	ret = android_rearrange_partitions(params);
 	/* No need for slot data, partitions should be already at correct
 	 * locations in memory since we are using "get_preloaded_partitions"
 	 * callbacks.
