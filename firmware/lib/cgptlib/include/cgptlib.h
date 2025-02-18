33faa3c817f5df6e9c3e7dbaf852c1d7bd4341dc
Kornel Dulęba <korneld@google.com-copybot-pick>
2025-02-18 16:20:37 +0000

    cgptlib: Move GptNextKernelEntry declaration Android Bringup: See http://go/android-fw-sync
    Previously it resided in cgptlib.h. Move it to gpt_misc.h, so that it
    can be used in depthcharge. Specifically it'll be used by fastboot
    logic to figure out the which slot is active. ("a"/"b")
    Since cgptlib.h already includes gpt_misc.h, this change won't affect
    existing code.
    
    BRANCH=none
    BUG=b:397322409
    TEST=emerge-brya vboot_reference libpayload depthcharge
    
    (cherry picked from commit 04d98aac457df44473dd13ed8163d14b7972c70a)
    
    Original-Signed-off-by: Kornel Dulęba <korneld@google.com>
    Change-Id: I2f2af87cfc5e7e92532c6a13515f638bbae5d451
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6297974
    Original-Reviewed-by: Tomasz Michalec <tmichalec@google.com>
    GitOrigin-RevId: 04d98aac457df44473dd13ed8163d14b7972c70a
    Cr-Build-Id: 8721953682624439537
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8721953682624439537
    Copybot-Job-Name: android-main-fw-corsola-vboot_reference-copybot-downstream

--- firmware/lib/cgptlib/include/cgptlib.h
+++ firmware/lib/cgptlib/include/cgptlib.h
@@ -10,19 +10,6 @@
 #include "gpt_misc.h"
 
 /**
- * Provides the location of the next kernel partition, in order of decreasing
- * priority.
- *
- * On return the start_sector parameter contains the LBA sector for the start
- * of the kernel partition, and the size parameter contains the size of the
- * kernel partition in LBA sectors.  gpt.current_kernel contains the partition
- * index of the current chromeos kernel partition.
- *
- * Returns GPT_SUCCESS if successful, else
- *   GPT_ERROR_NO_VALID_KERNEL, no avaliable kernel, enters recovery mode */
-int GptNextKernelEntry(GptData *gpt, uint64_t *start_sector, uint64_t *size);
-
-/**
  * Find init_boot partition for selected slot.
  * Must be called after GptNextKernelEntry.
  *
