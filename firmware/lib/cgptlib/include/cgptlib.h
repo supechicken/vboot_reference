1dc123cfe8a982e12f04075f6245eeed69ba4c75
Grzegorz Bernacki <bernacki@google.com-copybot-pick>
2025-02-20 11:56:56 +0000

    cgptlib,2lib: Allow to boot from vbmeta Android Bringup: See http://go/android-fw-sync
    BUG=b:384095358
    TEST=boot Android from boot and vbmeta partition
    BRANCH=firmware-android-15949.B
    
    (cherry picked from commit 194c9a1ce02c5761cadbfa60fc8ef84e9ee0c3dd)
    
    Cq-Depend: chromium:6285393
    Change-Id: I94bd75bac3eb1959c5bc9f75b2a043cb1574d5b5
    Original-Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6285330
    Original-Reviewed-by: Konrad Adamczyk <konrada@google.com>
    Original-Reviewed-by: Patryk Busse <pbusse@google.com>
    GitOrigin-RevId: 194c9a1ce02c5761cadbfa60fc8ef84e9ee0c3dd
    Cr-Build-Id: 8720436183838893489
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8720436183838893489
    Copybot-Job-Name: android-main-fw-corsola-vboot_reference-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6355033
    Tested-by: Yu-Ping Wu <yupingso@chromium.org>
    Commit-Queue: Yu-Ping Wu <yupingso@chromium.org>
    Commit-Queue: Yidi Lin <yidilin@google.com>
    Reviewed-by: Yidi Lin <yidilin@google.com>

--- firmware/lib/cgptlib/include/cgptlib.h
+++ firmware/lib/cgptlib/include/cgptlib.h
@@ -10,6 +10,17 @@
 #include "gpt_misc.h"
 
 /**
+ * Find boot partition for selected slot.
+ * Must be called after GptNextKernelEntry.
+ *
+ * On return the start_sector parameter contains the LBA sector for the start
+ * of the init_boot partition, and the size parameter contains the size of the
+ * init_boot partition in LBA sectors.
+ * Returns GPT_SUCCESS if successful.
+ */
+int GptFindBoot(GptData *gpt, uint64_t *start_sector, uint64_t *size);
+
+/**
  * Find init_boot partition for selected slot.
  * Must be called after GptNextKernelEntry.
  *
