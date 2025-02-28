d50f7a15abf3cd56f1ff31679bdd8d1d0ea7dbe4
Grzegorz Bernacki <bernacki@google.com>
2025-02-28 13:08:28 +0000

    cgpt: Reformat code using clang-format
    BUG=None
    TEST=Build and run firmware on redrix
    BRANCH=main
    
    Change-Id: Ic1034ca2c7f5592cb73439301618417a0160b406
    Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6329626
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    (cherry picked from commit 2df1fe56f58806fb05a690d27d8908c24b9813b5)

--- cgpt/cgpt_repair.c
+++ cgpt/cgpt_repair.c
@@ -9,30 +9,31 @@
 #include "cgptlib_internal.h"
 #include "vboot_host.h"
 
-int CgptRepair(CgptRepairParams *params) {
-  struct drive drive;
+int CgptRepair(CgptRepairParams *params)
+{
+	struct drive drive;
 
-  if (params == NULL)
-    return CGPT_FAILED;
+	if (params == NULL)
+		return CGPT_FAILED;
 
-  if (CGPT_OK != DriveOpen(params->drive_name, &drive, O_RDWR,
-                           params->drive_size))
-    return CGPT_FAILED;
+	if (CGPT_OK !=
+	    DriveOpen(params->drive_name, &drive, O_RDWR, params->drive_size))
+		return CGPT_FAILED;
 
-  int gpt_retval = GptValidityCheck(&drive.gpt);
-  if (params->verbose)
-    printf("GptValidityCheck() returned %d: %s\n",
-           gpt_retval, GptError(gpt_retval));
+	int gpt_retval = GptValidityCheck(&drive.gpt);
+	if (params->verbose)
+		printf("GptValidityCheck() returned %d: %s\n", gpt_retval,
+		       GptError(gpt_retval));
 
-  GptRepair(&drive.gpt);
-  if (drive.gpt.modified & GPT_MODIFIED_HEADER1)
-    printf("Primary Header is updated.\n");
-  if (drive.gpt.modified & GPT_MODIFIED_ENTRIES1)
-    printf("Primary Entries is updated.\n");
-  if (drive.gpt.modified & GPT_MODIFIED_ENTRIES2)
-    printf("Secondary Entries is updated.\n");
-  if (drive.gpt.modified & GPT_MODIFIED_HEADER2)
-    printf("Secondary Header is updated.\n");
+	GptRepair(&drive.gpt);
+	if (drive.gpt.modified & GPT_MODIFIED_HEADER1)
+		printf("Primary Header is updated.\n");
+	if (drive.gpt.modified & GPT_MODIFIED_ENTRIES1)
+		printf("Primary Entries is updated.\n");
+	if (drive.gpt.modified & GPT_MODIFIED_ENTRIES2)
+		printf("Secondary Entries is updated.\n");
+	if (drive.gpt.modified & GPT_MODIFIED_HEADER2)
+		printf("Secondary Header is updated.\n");
 
-  return DriveClose(&drive, 1);
+	return DriveClose(&drive, 1);
 }
