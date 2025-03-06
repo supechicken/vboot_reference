7de3b020f14163ec6d13016806939842659eba51
Tomasz Michalec <tmichalec@google.com>
2025-03-06 14:01:51 +0100

    cgptlib: Move GptPartition and GptPartitionNames declarations
    Previously it resided in cgptlib.h. Move it to gpt_misc.h, so that it
    can be used in depthcharge. Specifically it'll be used by fastboot logic
    to get the name of misc partition. Since cgptlib.h already includes
    gpt_misc.h, this change won't affect existing code.
    
    BRANCH=main
    BUG=None
    TEST=emerge-brya vboot_reference libpayload depthcharge
    
    Change-Id: I22e82fb72cd200977b30dc36f667b31d04e3a155
    Signed-off-by: Tomasz Michalec <tmichalec@google.com>

--- firmware/lib/cgptlib/include/cgptlib.h
+++ firmware/lib/cgptlib/include/cgptlib.h
@@ -9,20 +9,6 @@
 #include "2sysincludes.h"
 #include "gpt_misc.h"
 
-enum GptPartition {
-	GPT_ANDROID_BOOT = 0,
-	GPT_ANDROID_VENDOR_BOOT,
-	GPT_ANDROID_INIT_BOOT,
-	GPT_ANDROID_PRELOADED_NUM,
-
-	/* Partitions below this point do not get preloaded */
-	GPT_ANDROID_PVMFW = GPT_ANDROID_PRELOADED_NUM,
-	GPT_ANDROID_MISC,
-	GPT_ANDROID_VBMETA,
-};
-
-extern const char *GptPartitionNames[];
-
 /**
  * Checks if entry name field is equal to name+suffix.
  *
