4535f7f63b4504baaa32574928e82b2165d79529
Ihor Lukianov <ihorlukianov@google.com>
2025-07-10 13:30:04 +0000

    futility/update: locate fmap using helper image and read only needed regions
    Change the way update works by only reading regions necessary for the
    update to finish successfully.
    When another image is provided, use it to attempt locating the local FMAP.
    
    BRANCH=main
    BUG=b:431709118
    TEST=runfutiltests
    
    Change-Id: I982e28bf6661909fbbcc85b4733d25273b0b18df
    Signed-off-by: Ihor Lukianov <ihorlukianov@google.com>

--- futility/updater_utils.h
+++ futility/updater_utils.h
@@ -73,12 +73,29 @@ int load_firmware_image(struct firmware_image *image, const char *file_name,
 struct updater_config;
 
 /*
+* Loads entire firmware except RO_SECTION.
+* Returns 0 if success, other value for error.
+*/
+int load_system_firmware_without_ro(struct updater_config *cfg, struct firmware_image *image);
+
+/*
  * Loads the active system firmware image (usually from SPI flash chip).
  * Returns 0 if success. Returns IMAGE_PARSE_FAILURE for non-vboot images.
  * Returns other values for error.
  */
 int load_system_firmware(struct updater_config *cfg,
 			 struct firmware_image *image);
+/*
+ * Loads regions of the active system firmware image.
+ *
+ * If `helper_image` is provided, uses it to locate local FMAP.
+ *
+ * Returns 0 if success. Returns IMAGE_PARSE_FAILURE for non-vboot images.
+ * Returns other values for error.
+ */
+int load_system_firmware_regions(struct updater_config *cfg, struct firmware_image *image,
+			 struct firmware_image *helper_image, const char *const regions[],
+			 size_t regions_len);
 
 /* Frees the allocated resource from a firmware image object. */
 void free_firmware_image(struct firmware_image *image);
