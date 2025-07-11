cf56166158bab5ad7f366cd40476b5d26550a9c5
Tomasz Michalec <tmichalec@google.com>
2025-07-11 15:25:41 +0200

    cgptlib: Make IsBootableEntry available for depthcharge
    Make IsBootableEntry, IsAndroid and IsChromeOS functions available for
    depthcharge, so fastboot code can distinguish different kinds of
    bootable partitions.
    
    BRANCH=main
    BUG=None
    TEST=Build AP FW
    
    Change-Id: I6561e121b934c52e811254185e7b91c392f8ff96
    Signed-off-by: Tomasz Michalec <tmichalec@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6728037
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    (cherry picked from commit 9b362962860caef101b6d11debd2dd6366a2d274)

--- firmware/lib/cgptlib/include/cgptlib_internal.h
+++ firmware/lib/cgptlib/include/cgptlib_internal.h
@@ -162,6 +162,7 @@ void GptRepair(GptData *gpt);
 void GptModified(GptData *gpt);
 
 /**
+<<<<<<< HEAD   (2677abff42366f5b6b1e381c51dec81403064b67 2lib: Get Android boot mode)
  * Return 1 if the entry is a Android VBMETA partition, else 0.
  */
 int IsAndroid(const GptEntry *e);
@@ -178,6 +179,25 @@ int IsChromeOS(const GptEntry *e);
 int IsBootableEntry(const GptEntry *e);
 
 /**
+||||||| BASE   (ac6cc721c1ef4b62f834c56760e44fe2aeb76b65 2lib: Get Android boot mode)
+ * Return true if the entry is a Android VBMETA partition, else false.
+ */
+bool IsAndroid(const GptEntry *e);
+
+/**
+ * Return true if the entry is a ChromeOS kernel partition, else false.
+ */
+bool IsChromeOS(const GptEntry *e);
+
+/**
+ * Return true if the entry is a ChromeOS or Android partition,
+ * else false.
+ */
+bool IsBootableEntry(const GptEntry *e);
+
+/**
+=======
+>>>>>>> CHANGE (9b362962860caef101b6d11debd2dd6366a2d274 cgptlib: Make IsBootableEntry available for depthcharge)
  * Copy the current kernel partition's UniquePartitionGuid to the dest.
  */
 void GetCurrentKernelUniqueGuid(GptData *gpt, void *dest);
