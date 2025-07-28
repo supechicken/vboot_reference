e211181eed90f1e8ad2fa7ca2d7a8f53eeeb00e8
Jakub Czapiga <czapiga@google.com>
2025-07-28 13:37:56 +0000

    host: crossystem: gpio: Use correct GPIO controller offset
    Some x86 devices have more than one /dev/gpiochipX. Because of that
    crossystem was reading the first available pin value instead of the
    correct one. To solve this issue this CL makes use of the GPIO
    controller name stored in GPIO.3 API pin attribute, extracts the
    controller offset and uses it to select the matching /dev/gpiochipX
    device. If the controller name does not contain the offset value, then
    the code defaults to the first device i.e. /dev/gpiochip0.
    The cases when there are more than one controller, but the attribute
    does not contain it, are not covered by this implementation.
    The ARM code behavior remains unchanged, and still looks for the named
    pin.
    
    BRANCH=main
    BUG=b:434590349
    TEST=`crossystem wpsw_cur` output reflects actual HW WP state
    on x86 and ARM64 devices.
    
    Change-Id: I987e75389481ed6ca01003453eec78c1cfce79dc
    Signed-off-by: Jakub Czapiga <czapiga@google.com>

--- host/lib/gpio_uapi.c
+++ host/lib/gpio_uapi.c
@@ -219,12 +219,11 @@ static int gpiochip_scan_filter(const struct dirent *d)
 }
 
 /*
- * Read the pin value. If `name` is passed (non NULL), then look for the named pin.
- * Otherwise use `idx` as the pin index.
+ * Read value of a named pin.
  *
  * Returns -1 on failure, 1 for active pin state and 0 for inactive pin state.
  */
-static int gpio_read_value_by_name_or_index(const char *name, int idx, bool active_low)
+int gpio_read_value_by_name(const char *name, bool active_low)
 {
 	struct dirent **list;
 	int i, entries_num, ret;
@@ -251,16 +250,8 @@ static int gpio_read_value_by_name_or_index(const char *name, int idx, bool acti
 		}
 		fd = ret;
 
-		if (name) {
-			ret = gpio_uapi_read_value_by_name(fd, name, active_low);
-		} else if (idx != -1) {
-			ret = gpio_uapi_read_value_by_idx(fd, idx, active_low);
-		} else {
-			fprintf(stderr, "%s: No correct GPIO pin name or index provided\n",
-				__func__);
-			ret = -1;
-			break;
-		}
+		ret = gpio_uapi_read_value_by_name(fd, name, active_low);
+
 		close(fd);
 		if (ret >= 0)
 			break;
@@ -273,12 +264,22 @@ static int gpio_read_value_by_name_or_index(const char *name, int idx, bool acti
 	return ret;
 }
 
-int gpio_read_value_by_name(const char *name, bool active_low)
+int gpio_read_value_by_idx(int controller_num, int idx, bool active_low)
 {
-	return gpio_read_value_by_name_or_index(name, -1, active_low);
-}
+	int fd, ret;
+	char path[5 + NAME_MAX + 1];
 
-int gpio_read_value_by_idx(int idx, bool active_low)
-{
-	return gpio_read_value_by_name_or_index(NULL, idx, active_low);
+	snprintf(path, sizeof(path), "/dev/gpiochip%d", controller_num);
+
+	fd = open(path, O_RDWR);
+	if (fd < 0) {
+		fprintf(stderr, "Unable to open %s\n", path);
+		perror("open");
+		return -1;
+	}
+
+	ret = gpio_uapi_read_value_by_idx(fd, idx, active_low);
+
+	close(fd);
+	return ret;
 }
