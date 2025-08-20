89488d9de602802050ee58809a2e5c72069b9a07
Karthikeyan Ramasubramanian <kramasub@google.com-copybot-pick>
2025-08-20 16:15:06 -0600

    2api: Add support to load DTB/DTBO images from GPT partitions Android Bringup: See http://go/android-fw-sync
    This change:
    * Checks if DTB/DTBO partitions exist and add them to the list of
      preloaded partitions
    * Adds new vb2_kernel_params parameters to return loaded DTB/DTBO
      partition address & size
    * Introduces a private function to check the preloaded DTB/DTBO
      partitions and return the loaded address and size information to the
      payload
    
    While at it, check whether the PVMFW partition exists and add it to the
    list of preloaded partitions.
    
    BUG=b:394980221, b:443764205
    BRANCH=None
    TEST=USE=avb emerge-rauru coreboot libpayload vboot_reference
    depthcharge chromeos-bootimage. Boot to ChromeOS from internal disk and
    Android from external disk.
    ChromeOS Boot logs:
    -------------------
    Modified kernel command line: cros_secure console= loglevel=7 \
    console_msg_format=syslog init=/sbin/init cros_secure drm.trace=0x106 \
    root=/dev/dm-0 rootwait ro dm_verity.erre
    Loading FIT.
    <snip>
    Choosing best match conf-3 for compat google,hylia-sku1.
    LZ4 decompressing fdt-3 to 0xf0000000
    <snip>
    Exiting depthcharge with code 4 at timestamp: 12530127
    LZ4 decompressing kernel-1 to 0xf3206ff8
    LZ4 decompressing kernel-1 to 0x84c00000
    jumping to kernel
    Android Boot logs:
    ------------------
    vb2api_load_kernel: Found Android kernel entry at 17510402 size 8192
    read_rollback_index: TODO: not implemented yet
    reserve_buffers: Reserved buffer for 'boot' 0xd0000000[4000000]
    <snip>
    reserve_buffers: Reserved buffer for 'dtb' 0xd8000000[200000]
    reserve_buffers: Reserved buffer for 'dtbo' 0xd8200000[800000]
    <snip>
    get_preloaded_partition: Load dtbo_a into 0xd8200000 bytes:800000
    <snip>
    prepare_dtb: Continuing without a DTB image
    <snip>
    Modified kernel command line: cros_secure console=null 8250.nr_uarts=4 \
    log_buf_len=4M cgroup_disable=pressure root=/dev/ram0 \
    firmware_class.path=/vendor/firmware reserve_mee
    Loading FIT.
    Invalid device tree magic 0x4d5a40fa!
    Failed to unflatten FIT image!
    get_base_dt_entry: Num of DTB entries: 7
    get_base_dt_entry: Entry 4 is compatible with the board
    <snip>
    Exiting depthcharge with code 4 at timestamp: 20253824
    Relocating kernel to 0xf3206ff8
    Relocating kernel to 0x84c00000
    jumping to kernel
    
    (cherry picked from commit b97b60477b607d7fbcc6cde96ddfc549680b154b)
    
    Change-Id: Ief47e0eb380a13343213c3605737763d648f36a8
    Original-Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6931737
    Original-Reviewed-by: Grzegorz Bernacki <bernacki@google.com>
    Original-Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    GitOrigin-RevId: b97b60477b607d7fbcc6cde96ddfc549680b154b
    Cr-Build-Id: 8703336007521902641
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8703336007521902641
    Copybot-Job-Name: android-main-vboot_reference-copybot-downstream

--- firmware/2lib/include/2load_android_kernel.h
+++ firmware/2lib/include/2load_android_kernel.h
@@ -15,24 +15,7 @@
 #include "vboot_api.h"
 
 /**
- * Load and verify Android kernel partitions (boot, init_boot, vendor_boot,
- * pvmfw) from the stream (legacy).
- *
- * @param ctx			Vboot context
- * @param params		Load-kernel parameters
- * @param stream		Stream to load kernel from
- * @param gpt			Partition table from the disk
- * @param disk_handle		Handle to the disk containing kernel
- * @param need_keyblock_valid	Controls if successful verification is required
- * @return VB2_SUCCESS, or non-zero error code.
- */
-vb2_error_t vb2_load_android_kernel(
-	struct vb2_context *ctx, struct vb2_kernel_params *params,
-	VbExStream_t stream, GptData *gpt, vb2ex_disk_handle_t disk_handle,
-	int need_keyblock_valid);
-
-/**
- * Loads and verifies Android partitions (boot, init_boot, vendor_boot, pvmfw).
+ * Loads and verifies Android partitions (boot, init_boot, vendor_boot, pvmfw, dtb, dtbo).
  *
  * @param ctx			Vboot context
  * @param gpt			Partition table from the disk
