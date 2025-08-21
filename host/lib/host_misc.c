b1c769c9ebcef0705365eaa6bbbd5f7b75b9d7b1
Ihor Lukianov <ihorlukianov@google.com>
2025-08-21 09:45:37 +0000

    host: Add error messages to NV read/write
    Add error messages to relevant NV read/write functions.
    
    BRANCH=main
    BUG=None
    TEST=compile
    TEST=runfutiltests
    
    Change-Id: I3ace282596726c28283a41f2ccb799196b2f05fa
    Signed-off-by: Ihor Lukianov <ihorlukianov@google.com>

--- host/lib/host_misc.c
+++ host/lib/host_misc.c
@@ -7,6 +7,7 @@
 
 /* TODO: change all 'return 0', 'return 1' into meaningful return codes */
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -66,8 +67,11 @@ char* ReadFileFirstLine(char* dest, int size, const char* filename)
 	FILE* f;
 
 	f = fopen(filename, "rt");
-	if (!f)
+	if (!f) {
+		fprintf(stderr, "ERROR: %s: Failed to open %s: %s\n", __func__, filename,
+			strerror(errno));
 		return NULL;
+	}
 
 	got = fgets(dest, size, f);
 	fclose(f);
@@ -88,8 +92,12 @@ int ReadFileInt(const char* filename, unsigned* value)
 
 	/* Convert to integer.  Allow characters after the int ("123 blah"). */
 	*value = (unsigned)strtoul(buf, &e, 0);
-	if (e == buf)
+	if (e == buf) {
+		fprintf(stderr,
+			"ERROR: %s: File does not contain a correct integer value: %s\n",
+			__func__, filename);
 		return -1;  /* No characters consumed, so conversion failed */
+	}
 
 	return 0;
 }
