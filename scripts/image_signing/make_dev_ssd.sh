8f1ebb4404c9e6c9638fe4cd320818e89e13b3e6
Yu-Ping Wu <yupingso@chromium.org>
2025-09-19 14:02:25 +0800

    make_dev_ssd: Add --minios_key
    Add --minios_key to support re-signing MiniOS kernels (partitions 9 and
    10) with MiniOS keys. Note that both --recovery_key and --minios_key use
    the same public key recovery_key.vbpubk.
    
    BUG=none
    TEST=cq
    BRANCH=none
    
    Change-Id: Iefeed1307811aadb0d08402e890c4853ebfcb705
    Signed-off-by: Yu-Ping Wu <yupingso@chromium.org>

--- scripts/image_signing/make_dev_ssd.sh
+++ scripts/image_signing/make_dev_ssd.sh
@@ -57,6 +57,8 @@ DEFINE_string partitions "" \
   "List of partitions to examine (default: $DEFAULT_PARTITIONS)" ""
 DEFINE_boolean recovery_key "$FLAGS_FALSE" \
   "Use recovery key to sign image (to boot from USB)" ""
+DEFINE_boolean minios_key "$FLAGS_FALSE" \
+  "Use minios key to sign image (for MiniOS partitions)" ""
 DEFINE_boolean force "$FLAGS_FALSE" \
   "Skip validity checks and make the change" "f"
 DEFINE_boolean default_rw_root "${FLAGS_TRUE}" \
@@ -543,7 +545,11 @@ main() {
   local num_signed=0
   local num_given=$(echo "$FLAGS_partitions" | wc -w)
   # Check parameters
-  if [ "$FLAGS_recovery_key" = "$FLAGS_TRUE" ]; then
+  if [ "$FLAGS_minios_key" = "$FLAGS_TRUE" ]; then
+    KERNEL_KEYBLOCK="$FLAGS_keys/minios_kernel.keyblock"
+    KERNEL_DATAKEY="$FLAGS_keys/minios_kernel_data_key.vbprivk"
+    KERNEL_PUBKEY="$FLAGS_keys/recovery_key.vbpubk"
+  elif [ "$FLAGS_recovery_key" = "$FLAGS_TRUE" ]; then
     KERNEL_KEYBLOCK="$FLAGS_keys/recovery_kernel.keyblock"
     KERNEL_DATAKEY="$FLAGS_keys/recovery_kernel_data_key.vbprivk"
     KERNEL_PUBKEY="$FLAGS_keys/recovery_key.vbpubk"
